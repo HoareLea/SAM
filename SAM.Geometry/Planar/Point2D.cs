ca6b6404ee12df79c3bc15dad0932967b8ac88de
Michal Dengusiak <dengusiak@gmail.com>
2019-11-27 22:14:30 +0100

    initial implemntation WIP

---
+++ SAM.Geometry/Planar/Point2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry.Planar
+{
+    public class Point2D
+    {
+        private double[] coordinates = new double[2] { 0, 0 };
+    }
+}
