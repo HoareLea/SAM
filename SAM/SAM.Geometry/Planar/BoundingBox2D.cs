88b92679eb6485acd68ce0e9bb06ecd4377de3f0
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2019-12-04 13:55:39 +0000

    BoundingBox2D added

---
+++ SAM/SAM.Geometry/Planar/BoundingBox2D.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry.Planar
+{
+    public class BoundingBox2D
+    {
+        private Point2D min;
+        private Point2D max;
+
+        public BoundingBox2D(Point2D point2D_1, Point2D point2D_2)
+        { 
+
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Utils/BoundingBox2DUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry
+{
+    public static class BoundingBox2DUtils
+    {
+
+    }
+}
