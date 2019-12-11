c72b081a2a8b71b546f80c166fc71d70be1cba63
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2019-12-11 18:32:02 +0000

    Extrusion definition added

---
+++ SAM/SAM.Geometry/Spatial/Extrusion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry.Spatial
+{
+    public class Extrusion
+    {
+        private Face face;
+        private Vector3D vector;
+
+        public Extrusion(Face face, double height)
+        {
+            this.face = face;
+            vector = new Vector3D(0, 0, height);
+        }
+
+        public Extrusion(Face face, Vector3D vector)
+        {
+            this.face = face;
+            this.vector = vector;
+        }
+    }
+}
