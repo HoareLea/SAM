96e00392dd44aaaead66ea6429417cea8baaa2c3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-07 09:04:33 +0000

    SAM Dynamo Project Reorganisation

---
+++ Dynamo/SAMAnalyticalDynamo/Create/Construction.cs
@@ -0,0 +1,28 @@
+﻿using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMAnalyticalDynamo
+{
+    /// <summary>
+    /// SAM Analytical Construction
+    /// </summary>
+    public static class Construction
+    {
+        /// <summary>
+        /// Creates SAM Analytical Construction by SAM Point3Ds
+        /// </summary>
+        /// <param name="name">Construction Name</param>
+        /// <returns name="construction">SAM Analytical Construction</returns>
+        /// <search>
+        /// SAM Analytical Construction, ByName
+        /// </search>
+        public static SAM.Analytical.Construction ByName(string name)
+        {
+            return new SAM.Analytical.Construction(name);
+        }
+    }
+}
---
+++ Dynamo/SAMAnalyticalDynamo/Create/Panel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SAM.Geometry.Spatial;
+
+namespace SAMAnalyticalDynamo
+{
+    /// <summary>
+    /// SAM Analytical Panel
+    /// </summary>
+    public static class Panel
+    {
+        /// <summary>
+        /// Creates SAM Analytical Panel by SAM Point3Ds
+        /// </summary>
+        /// <param name="construction">SAM Analytical Construction</param>
+        /// <param name="point3Ds">SAM Point3Ds</param>
+        /// <returns name="panel">SAM Analytical Panel</returns>
+        /// <search>
+        /// SAM Analytical Panel, ByPoint3Ds
+        /// </search>
+        public static SAM.Analytical.Panel ByPoint3Ds(SAM.Analytical.Construction construction,  IEnumerable<Point3D> point3Ds)
+        {
+            List<Segment3D> segment3Ds = Point3D.GetSegments(point3Ds, true);
+
+            return new SAM.Analytical.Panel(Guid.NewGuid(), construction, segment3Ds.ConvertAll(x => new SAM.Analytical.Edge(x)));
+        }
+
+        //public static SAM.Analytical.Panel BySAMGeometry(SAM.Analytical.Construction construction, IGeometry3D geometry3D)
+        //{
+
+        //}
+    }
+}
