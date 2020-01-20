9787084ea859be4e352e846c1aa8c5902cdecdae
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-20 14:54:39 +0000

    Panels removed from Space

--- Grasshopper/SAM.Analytical.Grasshopper/ToGrasshopper/IGH_Goos.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/ToGrasshopper/IGH_Goos.cs
@@ -15,10 +15,6 @@ namespace SAM.Analytical.Grasshopper
             if (space == null)
                 return null;
 
-            List<Panel> panelList = space.Panels;
-            if (panelList != null && panelList.Count > 0)
-                return panelList.ConvertAll(x => SAM.Geometry.Grasshopper.Convert.ToGrasshopper(x.ToSurface()) as IGH_Goo);
-
             IGH_Goo iGH_Goo = Geometry.Grasshopper.Convert.ToGrasshopper(space.Location);
             if (iGH_Goo == null)
                 return null;
--- SAM/SAM.Analytical/Classes/Space.cs
+++ SAM/SAM.Analytical/Classes/Space.cs
@@ -10,43 +10,29 @@ namespace SAM.Analytical
     public class Space : SAMObject
     {
         private Geometry.Spatial.Point3D location;
-        private List<Panel> panels;
 
 
         public Space(Space space)
             : base(space)
         {
             this.location = space.Location;
-            this.panels = space.panels;
         }
 
-        public Space(Guid guid, Space space, IEnumerable<Panel> panels)
+        public Space(Guid guid, Space space)
         : base(guid, space)
         {
             this.location = space.Location;
-
-            if (panels != null)
-                this.panels = new List<Panel>(panels);
-        }
-
-        public Space(Guid guid, string name, Geometry.Spatial.Point3D location, IEnumerable<Panel> panels)
-            : base(guid, name)
-        {
-            this.location = location;
-            if (panels != null)
-                this.panels = new List<Panel>(panels);
-        }
-
-        public Space(string name, IEnumerable<Panel> panels)
-            : base(name)
-        {
-            this.panels = new List<Panel>(panels);
         }
 
         public Space(Guid guid, string name, Geometry.Spatial.Point3D location)
             : base(guid, name)
         {
             this.location = location;
+        }
+
+        public Space(string name)
+            : base(name)
+        {
         }
 
         public Space(string name, Geometry.Spatial.Point3D location)
@@ -60,17 +46,6 @@ namespace SAM.Analytical
             get
             {
                 return location;
-            }
-        }
-
-        public List<Panel> Panels
-        {
-            get
-            {
-                if (panels == null)
-                    return null;
-
-                return new List<Panel>(panels);
             }
         }
     }
