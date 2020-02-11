763f67a27551888336dc1931cf86328aa1d3f789
Michal Dengusiak <tassimulation@gmail.com>
2020-02-11 16:39:42 +0000

    TO DO

--- Grasshopper/SAM.Geometry.Grasshopper/Convert/ToRhino/Curve.cs
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToRhino/Curve.cs
@@ -12,6 +12,7 @@ namespace SAM.Geometry.Grasshopper
     {
         public static Rhino.Geometry.Curve ToRhino(this Spatial.ICurve3D curve3D)
         {
+            //TODO: Add handling of Polyline3D!!
             if (curve3D is Spatial.Segment3D)
                 return Convert.ToRhino_LineCurve((Spatial.Segment3D)curve3D);
 
