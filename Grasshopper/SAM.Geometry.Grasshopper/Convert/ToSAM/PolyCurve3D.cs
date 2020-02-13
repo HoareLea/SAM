5240094e2b6ae536805e094747a102626c2d646d
Michal Dengusiak <tassimulation@gmail.com>
2020-02-13 14:05:20 +0000

    Update PolyCurve3D.cs

--- Grasshopper/SAM.Geometry.Grasshopper/Convert/ToSAM/PolyCurve3D.cs
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToSAM/PolyCurve3D.cs
@@ -8,7 +8,16 @@ namespace SAM.Geometry.Grasshopper
         public static Spatial.Polycurve3D ToSAM(this Rhino.Geometry.PolyCurve polyCurve)
         {
             List<Rhino.Geometry.Curve> curves = polyCurve.Explode().ToList();
-            List<Spatial.ICurve3D> curve3Ds = Spatial.Query.Explode(curves.ConvertAll(x => x.ToSAM() as Spatial.ICurve3D));
+
+            List<Spatial.ICurve3D> curve3Ds = new List<Spatial.ICurve3D>();
+            foreach (Rhino.Geometry.Curve curve in curves)
+            {
+                if (!curve.IsLinear())
+                    curve3Ds.AddRange(Spatial.Query.Explode(curves.ConvertAll(x => x.ToSAM() as Spatial.ICurve3D)));  
+                else
+                    curve3Ds.Add(new Spatial.Segment3D(curve.PointAtStart.ToSAM(), curve.PointAtEnd.ToSAM()));
+            }
+
             return new Spatial.Polycurve3D(curve3Ds);
         }
     }
