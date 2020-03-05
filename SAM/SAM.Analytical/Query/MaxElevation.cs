964416bad5100c9e2a386b2c437af44bbaabbb7e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-05 16:34:33 +0000

    MidDirection Renamed to MidVector2D, MaxElevation and MinElevation Added for Panel

---
+++ SAM/SAM.Analytical/Query/MaxElevation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double MaxElevation(this Panel panel)
+        {
+            Geometry.Spatial.BoundingBox3D boundingBox3D = panel?.PlanarBoundary3D?.GetBoundingBox();
+            if (boundingBox3D == null)
+                return double.NaN;
+
+            return boundingBox3D.Max.Z;
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Planar/Query/MidVector2D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Query
+    {
+        public static Vector2D MidVector2D(this Segment2D segment2D_1, Segment2D segment2D_2)
+        {
+            if (segment2D_1 == null || segment2D_2 == null)
+                return null;
+
+            Point2D point2D_Closets_1 = null;
+            Point2D point2D_Closets_2 = null;
+
+            Point2D point2D_Intersection = segment2D_1.Intersection(segment2D_2, out point2D_Closets_1, out point2D_Closets_2);
+            if (point2D_Intersection == null)
+                return null;
+
+            if (point2D_Closets_1 != null && point2D_Closets_2 != null)
+                return MidVector2D(new Vector2D(point2D_Closets_1, point2D_Intersection), new Vector2D(point2D_Closets_2, point2D_Intersection));
+
+            Segment2D segment2D_1_Temp = new Segment2D(segment2D_1);
+            Segment2D segment2D_2_Temp = new Segment2D(segment2D_2);
+            Modify.OrientByEnds(segment2D_1_Temp, segment2D_2_Temp, true, true);
+
+            return MidVector2D(new Vector2D(segment2D_1_Temp.Start, point2D_Intersection), new Vector2D(segment2D_2_Temp.End, point2D_Intersection));
+        }
+
+        public static Vector2D MidVector2D(this Vector2D vector2D_1, Vector2D vector2D_2)
+        {
+            if (vector2D_1 == null || vector2D_2 == null)
+                return null;
+
+            return (vector2D_1.Unit + vector2D_2.Unit).Unit;
+        }
+
+    }
+}
