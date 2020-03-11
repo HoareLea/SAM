738b2867b8da5ea80e9d37c9cdae5b0a6c36d9c7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-11 16:51:54 +0000

    Median Added for SAM.Math

---
+++ SAM/SAM.Math/Query/Median.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Math
+{
+    public static partial class Query
+    {
+        public static double Median(this IEnumerable<double> values)
+        {
+            if (values == null)
+                return double.NaN;
+
+            int count = values.Count();
+            if (count == 0)
+                return double.NaN;
+
+            if (count == 1)
+                return values.First();
+
+            int half = count / 2;
+            if (count % 2 == 0)
+                return (values.ElementAt(half) + values.ElementAt(half - 1)) / 2;
+            else
+                return values.ElementAt(half);
+        }
+    }
+}
