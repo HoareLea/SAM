26ee8dc6521ef14e5128e43956eb676238d7f664
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-20 14:13:56 +0000

    Code Cleaning

--- SAM/SAM.Geometry/Spatial/Create/Plane.cs
+++ SAM/SAM.Geometry/Spatial/Create/Plane.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace SAM.Geometry.Spatial
 {
@@ -63,57 +62,5 @@ namespace SAM.Geometry.Spatial
 
             return new Plane(point3Ds.ElementAt(0), normal.Unit);
         }
-
-        //public static Plane Plane(this IEnumerable<Point3D> point3Ds, bool useMode, bool closed, double tolerance = Core.Tolerance.MicroDistance)
-        //{
-        //    if (point3Ds == null)
-        //        return null;
-
-        //    if (!useMode)
-        //        return Plane(point3Ds, tolerance);
-
-        //    List<Point3D> point3Ds_Temp = new List<Point3D>(point3Ds);
-        //    Modify.SimplifyByAngle(point3Ds_Temp, closed);
-
-        //    List<Vector3D> vector3Ds = Query.Normals(point3Ds_Temp);
-        //    if (vector3Ds == null || vector3Ds.Count == 0)
-        //        return null;
-
-        //    int count = vector3Ds.Count;
-        //    if (count == 0)
-        //        return null;
-
-        //    Dictionary<int, Tuple<Vector3D, Point3D>> dictionary = new Dictionary<int, Tuple<Vector3D, Point3D>>();
-        //    List<int> hashSets = new List<int>();
-        //    for (int i = 0; i < count; i++)
-        //    {
-        //        Vector3D vector3D = vector3Ds[i];
-        //        if (!Query.IsValid(vector3D))
-        //            continue;
-
-        //        int hashSet = vector3D.GetHashCode();
-
-        //        hashSets.Add(hashSet);
-
-        //        if (!dictionary.ContainsKey(hashSet))
-        //            dictionary[hashSet] = new Tuple<Vector3D, Point3D>(vector3D, point3Ds_Temp[i]);
-        //    }
-
-        //    if (dictionary == null || dictionary.Count() == 0)
-        //        return null;
-
-        //    Tuple<Vector3D, Point3D> tuple = null;
-        //    if (dictionary.Count != 1)
-        //    {
-        //        int hashSet_Modal = Math.Query.Modal(hashSets);
-        //        tuple = dictionary[hashSet_Modal];
-        //    }
-        //    else
-        //    {
-        //        tuple = dictionary.First().Value;
-        //    }
-
-        //    return new Plane(tuple.Item2, tuple.Item1);
-        //}
     }
 }
