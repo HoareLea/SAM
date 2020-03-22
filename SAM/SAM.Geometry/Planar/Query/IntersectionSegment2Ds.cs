3556862e8cb1c323d09e69afb4d5316e5a29f04e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-22 09:20:02 +0000

    IntersectionSegment2Ds Implemenetd for Multiple Polygon2Ds

--- SAM/SAM.Geometry/Planar/Query/IntersectionSegment2Ds.cs
+++ SAM/SAM.Geometry/Planar/Query/IntersectionSegment2Ds.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Geometry.Planar
 {
@@ -10,15 +11,28 @@ namespace SAM.Geometry.Planar
             if (polygon2D_1 == null || polygon2D_2 == null)
                 return null;
 
-            Polygon2D[] polygon2Ds = new Polygon2D[] { polygon2D_1, polygon2D_2 };
-            List<Polygon2D> polygon2Ds_Temp = new PointGraph2D(polygon2Ds, true).GetPolygon2Ds();
-            if (polygon2Ds_Temp == null || polygon2Ds_Temp.Count == 0)
+            return IntersectionSegment2Ds(polygon2D_1, new Polygon2D[] { polygon2D_2 }, tolerance);
+        }
+
+        public static List<Segment2D> IntersectionSegment2Ds(this Polygon2D polygon2D, IEnumerable<Polygon2D> polygon2Ds, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (polygon2D == null || polygon2Ds == null)
+                return null;
+
+            if (polygon2Ds.Count() == 0)
+                return new List<Segment2D>();
+
+            List<Polygon2D> polygon2Ds_Temp_1 = new List<Polygon2D>() { polygon2D };
+            polygon2Ds_Temp_1.AddRange(polygon2Ds);
+
+            List<Polygon2D> polygon2Ds_Temp_2 = new PointGraph2D(polygon2Ds_Temp_1, true).GetPolygon2Ds();
+            if (polygon2Ds_Temp_2 == null || polygon2Ds_Temp_2.Count == 0)
                 return null;
 
             List<Segment2D> segment2Ds_2 = new List<Segment2D>();
-            foreach (Polygon2D polygon2D in polygon2Ds_Temp)
+            foreach (Polygon2D polygon2D_Temp in polygon2Ds_Temp_2)
             {
-                List<Segment2D> segment2Ds_Temp = polygon2D.GetSegments();
+                List<Segment2D> segment2Ds_Temp = polygon2D_Temp.GetSegments();
                 if (segment2Ds_Temp == null || segment2Ds_Temp.Count == 0)
                     continue;
 
@@ -28,17 +42,19 @@ namespace SAM.Geometry.Planar
             List<Segment2D> result = new List<Segment2D>();
             foreach (Segment2D segment2D_2 in segment2Ds_2)
             {
-                if (polygon2D_2 == null)
+                if (polygon2Ds == null)
                     continue;
 
                 Point2D point2D = segment2D_2.Mid();
+                if (!polygon2D.On(point2D, tolerance))
+                    continue;
 
-                bool on = true;
-                foreach (Polygon2D polygon2D in polygon2Ds)
+                bool on = false;
+                foreach (Polygon2D polygon2D_Temp in polygon2Ds)
                 {
-                    if (!Query.On(polygon2D, point2D, tolerance))
+                    if (Query.On(polygon2D_Temp, point2D, tolerance))
                     {
-                        on = false;
+                        on = true;
                         break;
                     }
                 }
