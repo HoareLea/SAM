65838b6a380af9107713a121845dacd5a547d4d2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-03-22 00:10:00 +0000

    Opposite Method Added for Corner

--- SAM/SAM.Geometry/Query/Opposite.cs
+++ SAM/SAM.Geometry/Query/Opposite.cs
@@ -1,6 +1,6 @@
 ﻿namespace SAM.Geometry
 {
-    public  static partial class Query
+    public static partial class Query
     {
         public static Alignment Opposite(Alignment alignment)
         {
@@ -26,6 +26,54 @@
                 default:
                     return Orientation.Undefined;
             }
+        }
+
+        public static Corner Opposite(Corner corner, Alignment alignment = Alignment.Undefined)
+        {
+            switch (alignment)
+            {
+                case Alignment.Horizontal:
+                    switch (corner)
+                    {
+                        case Corner.BottomLeft:
+                            return Corner.TopLeft;
+                        case Corner.BottomRight:
+                            return Corner.TopRight;
+                        case Corner.TopLeft:
+                            return Corner.BottomLeft;
+                        case Corner.TopRight:
+                            return Corner.BottomRight;
+                    }
+                    break;
+                case Alignment.Undefined:
+                    switch (corner)
+                    {
+                        case Corner.BottomLeft:
+                            return Corner.TopRight;
+                        case Corner.BottomRight:
+                            return Corner.TopLeft;
+                        case Corner.TopLeft:
+                            return Corner.BottomRight;
+                        case Corner.TopRight:
+                            return Corner.BottomLeft;
+                    }
+                    break;
+                case Alignment.Vertical:
+                    switch (corner)
+                    {
+                        case Corner.BottomLeft:
+                            return Corner.BottomRight;
+                        case Corner.BottomRight:
+                            return Corner.BottomLeft;
+                        case Corner.TopLeft:
+                            return Corner.TopRight;
+                        case Corner.TopRight:
+                            return Corner.TopLeft;
+                    }
+                    break;
+            }
+
+            return Corner.Undefined;
         }
     }
 }
