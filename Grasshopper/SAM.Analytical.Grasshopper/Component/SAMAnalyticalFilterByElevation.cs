def4a67c8fbc47300320ec08f0d56123c211e68a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-02 11:53:21 +0100

    FilterByElevation Compoenent Added for Grasshopper

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalFilterByElevation.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Geometry.Grasshopper;
+using SAM.Geometry.Spatial;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalFilterByElevation : GH_Component
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("25f4e28b-071f-4d82-aa0f-ed48055ed607");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalFilterByElevation()
+          : base("SAMAnalytical.FilterByElevation", "SAMAnalytical.FilterByElevation",
+              "Filter Analytical Objects By Elevation",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            int index;
+
+            index = inputParamManager.AddParameter(new GooPanelParam(), "_panels", "_panels", "SAM Analytical Panels", GH_ParamAccess.list);
+            inputParamManager[index].DataMapping = GH_DataMapping.Flatten;
+
+            index = inputParamManager.AddNumberParameter("_elevation", "_elevation", "Elevation", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooPanelParam(), "Panels", "Panels", "SAM Analytical Panels", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            List<Panel> panels = new List<Panel>();
+            if (!dataAccess.GetDataList(0, panels))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double elevation = double.NaN;
+            if (!dataAccess.GetData(1, ref elevation))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Panel> result = new List<Panel>();
+            foreach(Panel panel in panels)
+            {
+                double max = panel.MaxElevation();
+                double min = panel.MinElevation();
+
+                if(Math.Abs(min - elevation) < Core.Tolerance.Distance || (min - Core.Tolerance.Distance  < elevation &&  max - Core.Tolerance.Distance > elevation))
+                    result.Add(panel);
+            }
+
+            if (result.Count == 1)
+                dataAccess.SetData(0, new GooPanel(result[0]));
+            else
+                dataAccess.SetDataList(0, result.ConvertAll(x => new GooPanel(x)));
+        }
+    }
+}
