9f8d251abf8ef80ca11dd086389d8d548b89d1e1
Michal Dengusiak <tassimulation@gmail.com>
2020-04-10 12:29:49 +0100

    Update Brep.cs

--- Grasshopper/SAM.Analytical.Grasshopper/Convert/ToRhino/Brep.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Convert/ToRhino/Brep.cs
@@ -4,12 +4,12 @@
     {
         public static Rhino.Geometry.Brep ToRhino(this PlanarBoundary3D planarBoundary3D)
         {
-            return Geometry.Grasshopper.Convert.ToRhino_Brep(planarBoundary3D.GetFace3D());
+            return Geometry.Grasshopper.Convert.ToRhino_Brep(planarBoundary3D?.GetFace3D());
         }
 
         public static Rhino.Geometry.Brep ToRhino(this Panel panel)
         {
-            return Geometry.Grasshopper.Convert.ToRhino_Brep(panel.PlanarBoundary3D.GetFace3D());
+            return Geometry.Grasshopper.Convert.ToRhino_Brep(panel?.PlanarBoundary3D?.GetFace3D());
         }
     }
 }
