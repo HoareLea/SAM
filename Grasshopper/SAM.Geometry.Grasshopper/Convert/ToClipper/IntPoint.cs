a07f51aac4b19d2a565a3a4c2d4021a48e894753
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-15 11:28:57 +0100

    Clipper Package Added to SAM.Geometry Project

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToClipper/IntPoint.cs
@@ -0,0 +1,18 @@
+﻿using ClipperLib;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public static partial class Convert
+    {
+        public static IntPoint ToClipper(this Planar.Point2D point2D, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (point2D == null)
+                return default;
+            
+            if(tolerance == 0)
+                return new IntPoint(point2D.X, point2D.Y);
+
+            return new IntPoint(point2D.X / tolerance, point2D.Y / tolerance);
+        }
+    }
+}
---
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToClipper/IntPoints.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClipperLib;
+using SAM.Geometry.Planar;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public static partial class Convert
+    {
+        public static List<IntPoint> ToClipper(this ISegmentable2D segmentable2D, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (segmentable2D == null)
+                return null;
+
+            return ToClipper(segmentable2D.GetPoints(), tolerance);
+
+        }
+
+        public static List<IntPoint> ToClipper(this IEnumerable<Point2D> point2Ds, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (point2Ds == null)
+                return null;
+
+            if (point2Ds.Count() == 0)
+                return new List<IntPoint>();
+
+            List<IntPoint> result = new List<IntPoint>();
+            foreach (Point2D point2D in point2Ds)
+                result.Add(point2D.ToClipper(tolerance));
+
+            return result;
+        }
+    }
+}
