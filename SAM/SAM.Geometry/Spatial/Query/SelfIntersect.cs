8895904a014edf58ab890369a42ce0436bfc79e4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-16 08:16:01 +0100

    SelfIntersection Methods Added for Polygon3D

---
+++ SAM/SAM.Geometry/Spatial/Query/SelfIntersect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static bool SelfIntersect(this Polygon3D polygon3D, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (polygon3D == null)
+                return false;
+
+            Plane plane = polygon3D.GetPlane();
+            if (plane == null)
+                return false;
+
+            return Planar.Query.SelfIntersect(plane.Convert(polygon3D), tolerance);
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Spatial/Query/SelfIntersectionPolygon3Ds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static List<Polygon3D> SelfIntersectionPolygon2Ds(this Polygon3D polygon3D, double tolerance = Core.Tolerance.MicroDistance)
+        {
+            if (polygon3D == null)
+                return null;
+
+            Plane plane = polygon3D.GetPlane();
+            if (plane == null)
+                return null;
+
+            Planar.Polygon2D polygon2D = plane.Convert(polygon3D);
+
+            List<Planar.Polygon2D> polygon2Ds = Planar.Query.SelfIntersectionPolygon2Ds(polygon2D, tolerance);
+            if (polygon2Ds == null)
+                return null;
+
+            return polygon2Ds.ConvertAll(x => plane.Convert(x));
+        }
+    }
+}
