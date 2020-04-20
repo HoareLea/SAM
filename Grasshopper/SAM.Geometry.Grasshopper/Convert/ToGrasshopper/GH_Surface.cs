bb70dbc222fa6e6f561ad8f8317edf2bd5ca9bcb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-04-20 15:00:51 +0100

    Simplify Query Implemented

--- Grasshopper/SAM.Geometry.Grasshopper/Convert/ToGrasshopper/GH_Surface.cs
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToGrasshopper/GH_Surface.cs
@@ -5,23 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Grasshopper.Kernel.Types;
+using SAM.Geometry.Spatial;
 
 namespace SAM.Geometry.Grasshopper
 {
     public static partial class Convert
     {
-        public static GH_Surface ToGrasshopper(this Spatial.Surface surface)
+        public static GH_Surface ToGrasshopper(this Surface surface)
         {
-            Spatial.IClosed3D closed3D = surface.GetExternalEdge();
+            IClosed3D closed3D = surface.GetExternalEdge();
 
-            if(closed3D is Spatial.ICurvable3D)
+            if(closed3D is ICurvable3D)
             {
-                List<Spatial.ICurve3D> curve3Ds = ((Spatial.ICurvable3D)(closed3D)).GetCurves();
-
                 Rhino.Geometry.Brep[] breps = null;
 
-                if (closed3D is Spatial.IClosedPlanar3D)
+                List<ICurve3D> curve3Ds = null;
+
+                if (closed3D is IClosedPlanar3D)
                 {
+                    if(closed3D is Polygon3D)
+                    {
+                        Polygon3D polygon3D = (Polygon3D)closed3D;
+
+                        Plane plane = polygon3D.GetPlane();
+                        Planar.Polygon2D polygon2D = plane.Convert(polygon3D);
+                        List<Planar.Polygon2D> polygon2Ds = Planar.Query.Simplify(polygon2D);
+                        if(polygon2Ds == null)
+                        {
+                            curve3Ds = polygon3D.GetCurves();
+                        }
+                        else
+                        {
+                            curve3Ds = new List<ICurve3D>();
+                            polygon2Ds.ConvertAll(x => plane.Convert(x)).ForEach(x => curve3Ds.AddRange(x.GetCurves()));
+                        }
+                    }
+                    else
+                    {
+                        curve3Ds = ((ICurvable3D)(closed3D)).GetCurves();
+                    }
+
                     breps = Rhino.Geometry.Brep.CreatePlanarBreps(curve3Ds.ToRhino_PolylineCurve(true), Core.Tolerance.Distance);
                 }
                 else
---
+++ SAM/SAM.Geometry/Planar/Query/Simplify.cs
@@ -0,0 +1,25 @@
+﻿using ClipperLib;
+using SAM.Core;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Query
+    {
+        public static List<Polygon2D> Simplify(this Polygon2D polygon2D, double tolerance = Tolerance.MicroDistance)
+        {
+            if (polygon2D == null)
+                return null;
+
+            List<IntPoint> intPoints = ((ISegmentable2D)polygon2D).ToClipper(tolerance);
+            if (intPoints == null || intPoints.Count == 0)
+                return null;
+
+            List<List<IntPoint>> intPointsList = Clipper.SimplifyPolygon(intPoints);
+            if (intPointsList == null || intPointsList.Count == 0)
+                return null;
+
+            return intPointsList.ConvertAll(x => new Polygon2D(x.ToSAM(tolerance)));
+        }
+    }
+}
