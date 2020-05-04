9b624786ceb5d2bf6861e0ce2db1735a2c5d5e49
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-05-04 09:49:49 +0100

    Determinants and ObtuseAngles Queries Added

---
+++ SAM/SAM.Geometry/Planar/Query/Determinants.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Query
+    {
+        public static List<double> Determinants<T>(this T segmentable2D) where T: IClosed2D, ISegmentable2D
+        {
+            List<Point2D> point2Ds = segmentable2D.GetPoints();
+            if (point2Ds == null || point2Ds.Count == 0)
+                return null;
+
+            int index = point2Ds.Count - 1;
+
+            point2Ds.Add(point2Ds[0]);
+            point2Ds.Insert(0, point2Ds[index]);
+            
+
+            List<double> result = new List<double>();
+            for (int i = 1; i < point2Ds.Count - 1; i++)
+                result.Add(Determinant(point2Ds[i - 1], point2Ds[i], point2Ds[i + 1]));
+
+            return result;
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Planar/Query/ObtuseAngles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Query
+    {
+        public static List<bool> ObtuseAngles<T>(this T segmentable2D) where T: IClosed2D, ISegmentable2D
+        {
+            List<double> determinats = Determinants(segmentable2D);
+            if (determinats == null)
+                return null;
+
+            List<bool> result = new List<bool>();
+
+            if (determinats.Count == 0)
+                return result;
+
+            List<Point2D> point2Ds = segmentable2D.GetPoints();
+            if (point2Ds == null)
+                return null;
+
+            if (point2Ds.Count != determinats.Count)
+                return null;
+
+            Orientation orientation = Orientation(point2Ds, true);
+
+            switch(orientation)
+            {
+                case Geometry.Orientation.Clockwise:
+                    return determinats.ConvertAll(x => x < 0);
+                case Geometry.Orientation.CounterClockwise:
+                    return determinats.ConvertAll(x => x > 0);
+            }
+
+            return null;
+        }
+    }
+}
