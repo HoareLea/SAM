09e08be14fafa55d7fb173ea96defa22d49bab14
Michal Dengusiak <tassimulation@gmail.com>
2020-05-19 14:16:54 +0100

    add colors to Panels

--- Grasshopper/SAM.Analytical.Grasshopper/Query/DisplayMaterial.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Query/DisplayMaterial.cs
@@ -8,11 +8,56 @@
 
             switch (panelType)
             {
-                case PanelType.Wall:
-                    color = System.Drawing.Color.Red;
+                case PanelType.Ceiling:
+                    color = System.Drawing.ColorTranslator.FromHtml("##FF8080");
+                    break;
+                case PanelType.CurtainWall:
+                    color = System.Drawing.Color.BlueViolet;
                     break;
                 case PanelType.Floor:
-                    color = System.Drawing.Color.Red;
+                    color = System.Drawing.ColorTranslator.FromHtml("##804000");
+                    break;
+                case PanelType.FloorExposed:
+                    color = System.Drawing.ColorTranslator.FromHtml("#40B4FF");
+                    break;
+                case PanelType.FloorInternal:
+                    color = System.Drawing.ColorTranslator.FromHtml("#80FFFF");
+                    break;
+                case PanelType.FloorRaised:
+                    color = System.Drawing.ColorTranslator.FromHtml("#80FFFF");
+                    break;
+                case PanelType.Roof:
+                    color = System.Drawing.ColorTranslator.FromHtml("#800000");
+                    break;
+                case PanelType.Shade:
+                    color = System.Drawing.ColorTranslator.FromHtml("#FFCE9D");
+                    break;
+                case PanelType.SlabOnGrade:
+                    color = System.Drawing.ColorTranslator.FromHtml("#804000");
+                    break;
+                case PanelType.SolarPanel:
+                    color = System.Drawing.ColorTranslator.FromHtml("#FFB400");
+                    break;
+                case PanelType.Undefined:
+                    color = System.Drawing.ColorTranslator.FromHtml("#FFB400");
+                    break;
+                case PanelType.UndergroundCeiling:
+                    color = System.Drawing.ColorTranslator.FromHtml("#408080");
+                    break;
+                case PanelType.UndergroundSlab:
+                    color = System.Drawing.ColorTranslator.FromHtml("#804000");
+                    break;
+                case PanelType.UndergroundWall:
+                    color = System.Drawing.ColorTranslator.FromHtml("#A55200");
+                    break;
+                case PanelType.Wall:
+                    color = System.Drawing.ColorTranslator.FromHtml("#FFB400");
+                    break;
+                case PanelType.WallExternal:
+                    color = System.Drawing.ColorTranslator.FromHtml("#FFB400");
+                    break;
+                case PanelType.WallInternal:
+                    color = System.Drawing.ColorTranslator.FromHtml("#008000");
                     break;
             }
 
@@ -29,10 +74,10 @@
             switch (apertureType)
             {
                 case ApertureType.Door:
-                    color = System.Drawing.Color.Red;
+                    color = System.Drawing.Color.Brown;
                     break;
                 case ApertureType.Window:
-                    color = System.Drawing.Color.Red;
+                    color = System.Drawing.Color.Blue;
                     break;
             }
 
