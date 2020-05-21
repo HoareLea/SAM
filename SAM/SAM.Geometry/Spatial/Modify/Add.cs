ed8d7178fb4a30d1af1c155a24d533ff6d59e051
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-05-21 10:20:54 +0100

    Add Method Point3D to Point3D List Implemented

---
+++ SAM/SAM.Geometry/Spatial/Modify/Add.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Geometry.Spatial
+{
+    public static partial class Modify
+    {
+        public static bool Add(this List<Point3D> point3Ds, Point3D point3D, double tolerance = Core.Tolerance.Distance)
+        {
+            if (point3Ds == null || point3D == null)
+                return false;
+
+            Point3D point3D_Temp = point3Ds.Find(x => x.Distance(point3D) <= tolerance);
+            if(point3D_Temp == null)
+            {
+                point3Ds.Add(point3D);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
