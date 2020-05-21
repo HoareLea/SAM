ca6599af2d73d76ad5d9a6e98b47f11d68de2c0e
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-05-21 12:14:36 +0100

    Create Face3D form Polygon3Ds Method Added

--- SAM/SAM.Geometry/Spatial/Create/Face3Ds.cs
+++ SAM/SAM.Geometry/Spatial/Create/Face3Ds.cs
@@ -20,5 +20,40 @@ namespace SAM.Geometry.Spatial
 
             return face2Ds.ConvertAll(x => new Face3D(plane, x));
         }
+
+        public static List<Face3D> Face3Ds(this IEnumerable<Polygon3D> polygon3Ds, bool orientInternalEdges = true, double tolerance = Core.Tolerance.Distance)
+        {
+            if (polygon3Ds == null)
+                return null;
+
+            List<Face3D> result = new List<Face3D>();
+            
+            if (polygon3Ds.Count() == 0)
+                return result;
+
+            if (polygon3Ds.Count() == 1)
+            {
+                result.Add(new Face3D(polygon3Ds.First()));
+                return result;
+            }
+
+            List<Polygon3D> polygon3Ds_Temp = new List<Polygon3D>(polygon3Ds);
+            polygon3Ds_Temp.Sort((x, y) => y.GetArea().CompareTo(x.GetArea()));
+            while(polygon3Ds_Temp.Count > 0)
+            {
+                Polygon3D polygon3D = polygon3Ds_Temp.First();
+                Plane plane = polygon3D.GetPlane();
+
+                List<Polygon3D> polygon3Ds_Plane = polygon3Ds_Temp.FindAll(x => x.GetPlane().Coplanar(plane, tolerance));
+                polygon3Ds_Temp.RemoveAll(x => polygon3Ds_Plane.Contains(x));
+
+                IEnumerable<Planar.IClosed2D> closed2Ds = polygon3Ds_Plane.ConvertAll(x => plane.Convert(plane.Project(x)));
+                List<Face3D> face3Ds = Face3Ds(closed2Ds, plane, orientInternalEdges);
+                if (face3Ds != null && face3Ds.Count > 0)
+                    result.AddRange(face3Ds);
+            }
+
+            return result;
+        }
     }
 }
