2c30b7c5ee82208e7003c77efd80c696d08aa9f2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-05-26 08:41:53 +0100

    GooAdjacencyCluster Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Classes/GooAdjacencyCluster.cs
@@ -0,0 +1,129 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class GooAdjacencyCluster : GooSAMObject<AdjacencyCluster>, IGH_PreviewData
+    {
+        public GooAdjacencyCluster()
+            : base()
+        {
+        }
+
+        public GooAdjacencyCluster(AdjacencyCluster adjacencyCluster)
+            : base(adjacencyCluster)
+        {
+        }
+
+        public BoundingBox ClippingBox
+        {
+            get
+            {
+                if (Value == null)
+                    return BoundingBox.Empty;
+
+                List<Geometry.Spatial.BoundingBox3D> boundingBox3Ds = new List<Geometry.Spatial.BoundingBox3D>();
+
+                IEnumerable<Panel> panels = Value.GetPanels();
+                if (panels != null)
+                {
+                    foreach (Panel panel in panels)
+                        boundingBox3Ds.Add(panel.GetBoundingBox());
+                }
+
+                IEnumerable<Space> spaces = Value.GetSpaces();
+                if (spaces != null)
+                {
+                    foreach (Space space in spaces)
+                        boundingBox3Ds.Add(space.Location.GetBoundingBox(1));
+                }
+
+                if (boundingBox3Ds == null)
+                    return BoundingBox.Empty;
+
+                boundingBox3Ds.RemoveAll(x => x == null);
+
+                if (boundingBox3Ds.Count == 0)
+                    return BoundingBox.Empty;
+
+                return Geometry.Grasshopper.Convert.ToRhino(new Geometry.Spatial.BoundingBox3D(boundingBox3Ds));
+            }
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new GooAdjacencyCluster(Value);
+        }
+
+        public void DrawViewportWires(GH_PreviewWireArgs args)
+        {
+            foreach (Panel panel in Value.GetPanels())
+            {
+                GooPlanarBoundary3D gooPlanarBoundary3D = new GooPlanarBoundary3D(panel.PlanarBoundary3D);
+                gooPlanarBoundary3D.DrawViewportWires(args);
+            }
+
+            foreach (Space space in Value.GetSpaces())
+            {
+                GooSpace gooSpace = new GooSpace(space);
+                gooSpace.DrawViewportWires(args);
+            }
+        }
+
+        public void DrawViewportMeshes(GH_PreviewMeshArgs args)
+        {
+            foreach (Panel panel in Value.GetPanels())
+            {
+                GooPlanarBoundary3D gooPlanarBoundary3D = new GooPlanarBoundary3D(panel.PlanarBoundary3D);
+                gooPlanarBoundary3D.DrawViewportMeshes(args);
+            }
+
+            foreach (Space space in Value.GetSpaces())
+            {
+                GooSpace gooSpace = new GooSpace(space);
+                gooSpace.DrawViewportMeshes(args);
+            }
+        }
+    }
+
+    //Params Components -> SAM used for internalizing data
+    public class GooAdjacencyClusterParam : GH_PersistentParam<GooAdjacencyCluster>, IGH_PreviewObject
+    {
+        public override Guid ComponentGuid => new Guid("408ca3f4-0598-4f18-8b25-1f9646c53ef0");
+
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        //Here we control name, nickname, description, category, sub-category as deafult we use typeofclass name
+        public GooAdjacencyClusterParam()
+            : base(typeof(AdjacencyCluster).Name, typeof(AdjacencyCluster).Name, typeof(AdjacencyCluster).FullName.Replace(".", " "), "Params", "SAM")
+        {
+        }
+
+        protected override GH_GetterResult Prompt_Plural(ref List<GooAdjacencyCluster> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override GH_GetterResult Prompt_Singular(ref GooAdjacencyCluster value)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IGH_PreviewObject
+
+        bool IGH_PreviewObject.Hidden { get; set; }
+        bool IGH_PreviewObject.IsPreviewCapable => !VolatileData.IsEmpty;
+        BoundingBox IGH_PreviewObject.ClippingBox => Preview_ComputeClippingBox();
+
+        void IGH_PreviewObject.DrawViewportMeshes(IGH_PreviewArgs args) => Preview_DrawMeshes(args);
+
+        void IGH_PreviewObject.DrawViewportWires(IGH_PreviewArgs args) => Preview_DrawWires(args);
+
+        #endregion IGH_PreviewObject
+    }
+}
--- SAM/SAM.Analytical/Classes/AdjacencyCluster.cs
+++ SAM/SAM.Analytical/Classes/AdjacencyCluster.cs
@@ -6,6 +6,12 @@ namespace SAM.Analytical
 {
     public class AdjacencyCluster : Core.RelationCluster
     {        
+        public AdjacencyCluster()
+            : base()
+        {
+
+        }
+        
         public AdjacencyCluster(AdjacencyCluster adjacencyCluster)
             :base(adjacencyCluster)
         {
@@ -36,6 +42,77 @@ namespace SAM.Analytical
 
             List<Space> spaces = GetRelatedObjects<Space>(panel);
             return spaces != null && spaces.Count > 1;
+        }
+
+        public bool External(Panel panel)
+        {
+            if (panel == null)
+                return false;
+
+            if (!Contains(typeof(Panel)))
+                return false;
+
+            List<Space> spaces = GetRelatedObjects<Space>(panel);
+            return spaces != null && spaces.Count == 1;
+        }
+
+        public bool Shade(Panel panel)
+        {
+            if (panel == null)
+                return false;
+
+            if (!Contains(typeof(Panel)))
+                return false;
+
+            List<Space> spaces = GetRelatedObjects<Space>(panel);
+            return spaces == null || spaces.Count == 0;
+        }
+
+        public List<Panel> GetInternalPanels()
+        {
+            List<Panel> panels = GetObjects<Panel>();
+            if (panels == null || panels.Count == 0)
+                return null;
+
+            return panels.FindAll(x => Internal(x));
+        }
+
+        public List<Panel> GetExternalPanels()
+        {
+            List<Panel> panels = GetObjects<Panel>();
+            if (panels == null || panels.Count == 0)
+                return null;
+
+            return panels.FindAll(x => External(x));
+        }
+
+        public List<Panel> GetShadingPanels()
+        {
+            List<Panel> panels = GetObjects<Panel>();
+            if (panels == null || panels.Count == 0)
+                return null;
+
+            return panels.FindAll(x => Shade(x));
+        }
+
+        public List<Panel> GetPanels()
+        {
+            return GetObjects<Panel>();
+        }
+
+        public List<Panel> GetPanels(Space space)
+        {
+            return GetRelatedObjects<Panel>(space);
+        }
+
+        public List<Space> GetSpaces()
+        {
+            return GetObjects<Space>();
+        }
+
+        public List<Space> GetSpaces(Panel panel)
+        {
+            return GetRelatedObjects<Space>(panel);
         }
     }
 }
