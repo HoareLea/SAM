7685474da534ff0dad247d56e12b83934a4c4361
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-06-17 09:10:43 +0100

    ExternalVector3D Component Added for Grashopper

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalExternalVector3D.cs
@@ -0,0 +1,83 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using SAM.Geometry;
+using SAM.Geometry.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalExternalVector3D : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("fc163ed0-4757-44c3-964a-d2f6985e60f0");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalExternalVector3D()
+          : base("SAMAnalytical.ExternalVector3D", "SAMAnalytical.ExternalVector3D",
+              "Gets External Vector3D for Panel",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooAdjacencyClusterParam(), "_adjacencyCluster", "_adjacencyCluster", "SAM Analytical AdjacencyCluster", GH_ParamAccess.item);
+            inputParamManager.AddParameter(new GooPanelParam(), "_panel", "_panel", "SAM Analytical Panel", GH_ParamAccess.item);
+            inputParamManager.AddParameter(new GooSpaceParam(), "_space", "_space", "SAM Analytical Space", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddGenericParameter("Vector3D", "Vector3D", "External Vector3D", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            AdjacencyCluster adjacencyCluster = null;
+            if (!dataAccess.GetData(0, ref adjacencyCluster) || adjacencyCluster == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Panel panel = null;
+            if (!dataAccess.GetData(1, ref panel) || panel == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Space space = null;
+            if (!dataAccess.GetData(2, ref space) || space == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            dataAccess.SetData(0, Analytical.Query.ExternalVector3D(adjacencyCluster, space, panel));
+        }
+    }
+}
