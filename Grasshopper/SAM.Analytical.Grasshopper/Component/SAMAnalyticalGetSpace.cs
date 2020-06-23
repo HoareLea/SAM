20f338449a20731ee770adc94b408b233521b7a4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-06-23 16:34:02 +0100

    Get Space by Point3D Method Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalGetSpace.cs
@@ -0,0 +1,110 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalGetSpace : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("60861d78-829e-4ee0-b8bc-49be767d1de2");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalGetSpace()
+          : base("SAMAnalytical.GetSpace", "SAMAnalytical.GetSpace",
+              "Get Space from SAM Analytical Model",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooAdjacencyClusterParam(), "_adjacencyCluster", "_adjacencyCluster", "SAM Analytical AdjacencyCluster", GH_ParamAccess.item);
+            inputParamManager.AddGenericParameter("_point", "_point", "Geometry Point", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooSpaceParam(), "Spaces", "Spaces", "SAM Geometry Spaces", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            AdjacencyCluster adjacencyCluster = null;
+            if(!dataAccess.GetData(0, ref adjacencyCluster) || adjacencyCluster == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<GH_ObjectWrapper> objectWrappers = null;
+            if (!dataAccess.GetData(1, ref objectWrappers))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Geometry.Spatial.Point3D> point3Ds = new List<Geometry.Spatial.Point3D>();
+            foreach (GH_ObjectWrapper objectWrapper in objectWrappers)
+            {
+                Geometry.Spatial.Point3D point3D = null;
+
+                if (objectWrapper.Value is GH_Point)
+                {
+                    point3D = ((GH_Point)objectWrapper.Value).ToSAM();
+                }
+                else if (objectWrapper.Value is Geometry.Spatial.Point3D)
+                {
+                    point3D = ((Geometry.Spatial.Point3D)objectWrapper.Value);
+                }
+
+                if (point3D != null)
+                    point3Ds.Add(point3D);
+            }
+
+            Dictionary<Space, Geometry.Spatial.Shell> dictionary = adjacencyCluster.ShellDictionary();
+
+            List<Space> spaces = new List<Space>();
+            foreach(Geometry.Spatial.Point3D point3D in point3Ds)
+            {
+                Space space = null;
+                foreach(KeyValuePair<Space, Geometry.Spatial.Shell> keyValuePair in dictionary)
+                {
+                    if(keyValuePair.Value.InRange(point3D))
+                    {
+                        space = keyValuePair.Key;
+                        break;
+                    }
+                }
+                spaces.Add(space);
+            }
+
+            dataAccess.SetDataList(0, spaces.ConvertAll(x => new GooSpace(x)));
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Query/ShellDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static Dictionary<Space, Geometry.Spatial.Shell> ShellDictionary(this AdjacencyCluster adjacencyCluster)
+        {
+            if (adjacencyCluster == null)
+                return null;
+
+            List<Space> spaces = adjacencyCluster.GetSpaces();
+            if (spaces == null)
+                return null;
+
+
+            Dictionary<Space, Geometry.Spatial.Shell> result = new Dictionary<Space, Geometry.Spatial.Shell>();
+            Dictionary<System.Guid, Geometry.Spatial.Face3D> dictionary_Shell = new Dictionary<System.Guid, Geometry.Spatial.Face3D>();
+            foreach (Space space in spaces)
+            {
+                List<Panel> panels = adjacencyCluster.GetRelatedObjects<Panel>(space);
+                if (panels == null)
+                    continue;
+
+                List<Geometry.Spatial.Face3D> face3Ds = new List<Geometry.Spatial.Face3D>();
+                foreach(Panel panel in panels)
+                {
+                    Geometry.Spatial.Face3D face3D = null;
+                    if(!dictionary_Shell.TryGetValue(panel.Guid, out face3D))
+                    {
+                        face3D = panel.GetFace3D();
+                        dictionary_Shell[panel.Guid] = face3D;
+                    }
+
+                    if (face3D != null)
+                        face3Ds.Add(face3D);
+                }
+
+                result[space] = new Geometry.Spatial.Shell(face3Ds);
+            }
+
+            return result;
+        }
+    }
+}
