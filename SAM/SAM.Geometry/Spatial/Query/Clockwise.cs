99356ee36016e773503959291a4f3fe4b1063c43
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-06-29 15:58:29 +0100

    Clockwise Method for Point3Ds Added

--- SAM/SAM.Geometry/Spatial/Query/Clockwise.cs
+++ SAM/SAM.Geometry/Spatial/Query/Clockwise.cs
@@ -19,17 +19,33 @@ namespace SAM.Geometry.Spatial
             if (point3Ds == null || point3Ds.Count < 3)
                 return false;
 
+            return Clockwise(point3Ds, normal, tolerance);
+
+        }
+
+        public static bool Clockwise(this IEnumerable<Point3D> point3Ds, Vector3D normal = null, double tolerance_Angle = Core.Tolerance.Angle, double tolerance_Distance = Core.Tolerance.Distance)
+        {
+            if (point3Ds == null || point3Ds.Count() < 3)
+                return false;
+
+            Vector3D vector3D_Normal = normal;
+            if (vector3D_Normal == null)
+                vector3D_Normal = point3Ds.Normal(tolerance_Distance);
+
+            if (vector3D_Normal == null)
+                return false;
+
             Vector3D direction = (point3Ds.First() - point3Ds.Last()).Unit;
 
             double angle_Total = 0;
 
-            for (int i = 1; i < point3Ds.Count; i++)
+            for (int i = 1; i < point3Ds.Count(); i++)
             {
-                Vector3D dirtection_Temp = (point3Ds[i] - point3Ds[i - 1]).Unit;
-                double angle = direction.SignedAngle(dirtection_Temp, normal);
+                Vector3D dirtection_Temp = (point3Ds.ElementAt(i) - point3Ds.ElementAt(i - 1)).Unit;
+                double angle = direction.SignedAngle(dirtection_Temp, vector3D_Normal);
                 direction = dirtection_Temp;
 
-                if (System.Math.PI - System.Math.Abs(angle) <= tolerance)
+                if (System.Math.PI - System.Math.Abs(angle) <= tolerance_Angle)
                     direction *= -1;
                 else
                     angle_Total += angle;
