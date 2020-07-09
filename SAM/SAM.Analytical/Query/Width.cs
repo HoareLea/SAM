633eff908f5e14a62b0b76c86ea1a4fbd1d9bf22
Michal Dengusiak <tassimulation@gmail.com>
2020-07-09 09:29:13 +0100

    WIP Rooglight placement - TBC

--- SAM/SAM.Analytical/Query/Width.cs
+++ SAM/SAM.Analytical/Query/Width.cs
@@ -1,4 +1,6 @@
 ﻿
+using SAM.Geometry.Planar;
+
 namespace SAM.Analytical
 {
     public static partial class Query
@@ -7,11 +9,20 @@ namespace SAM.Analytical
         {
             //TODO: Find better way to determine Width
 
-            Geometry.Planar.IClosed2D closed2D = planarBoundary3D?.Edge2DLoop?.GetClosed2D();
+            IClosed2D closed2D = planarBoundary3D?.Edge2DLoop?.GetClosed2D();
             if (closed2D == null)
                 return double.NaN;
 
-            return closed2D.GetBoundingBox().Width;
+            ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
+
+            if (segmentable2D == null)
+                throw new System.NotImplementedException();
+
+            Rectangle2D rectangle2D = Geometry.Planar.Create.Rectangle2D(segmentable2D.GetPoints());
+            if (rectangle2D == null)
+                return double.NaN;
+
+            return rectangle2D.Width;
         }
     }
 }
