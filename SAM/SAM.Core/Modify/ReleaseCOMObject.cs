8bcdc97e857ee990530335d4e8d664317e3a8624
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-07-17 16:39:36 +0100

    ReleaseCOMObject Method Added

---
+++ SAM/SAM.Core/Modify/ReleaseCOMObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SAM.Core
+{
+    public static partial class Modify
+    {
+        public static void ReleaseCOMObject(this object cOMObject)
+        {
+            if (cOMObject == null)
+                return;
+
+            int referenceCount = 0;
+            do
+            {
+                referenceCount = Marshal.FinalReleaseComObject(cOMObject);
+            }
+            while (referenceCount > 0);
+
+            cOMObject = null;
+        }
+    }
+}
