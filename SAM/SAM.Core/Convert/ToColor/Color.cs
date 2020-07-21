127a6ba4c3d1152cc234d580672b7e673836e722
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-07-21 15:35:43 +0100

    Color Conversion Added

---
+++ SAM/SAM.Core/Convert/ToColor/Color.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace SAM.Core
+{
+    public static partial class Convert
+    {
+        public static Color ToColor(this uint @uint)
+        {
+            byte a = (byte)(@uint >> 24);
+            byte b = (byte)(@uint >> 16);
+            byte g = (byte)(@uint >> 8);
+            byte r = (byte)(@uint >> 0);
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        public static Color ToColor(this uint @uint, byte alpha)
+        {
+            byte b = (byte)(@uint >> 16);
+            byte g = (byte)(@uint >> 8);
+            byte r = (byte)(@uint >> 0);
+            return Color.FromArgb(alpha, r, g, b);
+        }
+    }
+}
---
+++ SAM/SAM.Core/Convert/ToUint/Uint.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace SAM.Core
+{
+    public static partial class Convert
+    {
+        public static uint ToUint(this Color color)
+        {
+            return (uint)((color.R << 16) | (color.G << 8) | (color.B << 0));
+        }
+
+        public static uint ToUint(this Color color, bool includeAlpha)
+        {
+            if (includeAlpha)
+                return (uint)((color.R << 24) | (color.R << 16) | (color.G << 8) | (color.B << 0));
+            else
+                return ToUint(color);
+        }
+    }
+}
