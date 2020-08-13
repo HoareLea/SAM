d0a60b6f60b2dd27aa4f08e02b5774805f95e407
Michal Dengusiak <tassimulation@gmail.com>
2020-08-13 11:16:04 +0100

    Query Method Added for Panels Extraction from AdjacencyCluster

---
+++ SAM/SAM.Analytical/Query/Panels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static List<Panel> Panels(this AdjacencyCluster adjacencyCluster, Construction construction)
+        {
+            if (adjacencyCluster == null || construction == null)
+                return null;
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if (panels == null)
+                return null;
+
+            Guid guid = construction.Guid;
+
+            List<Panel> result = new List<Panel>();
+            foreach (Panel panel in panels)
+                if (panel.SAMTypeGuid.Equals(guid))
+                    result.Add(panel);
+
+            return result;
+        }
+    }
+}
