4e50094dff4da0c9ff9a22483aa4ca105510c0fb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-08-19 09:55:31 +0100

    PlanarBoundary3D Width and Height Calculations Updated

--- SAM/SAM.Analytical/Query/Height.cs
+++ SAM/SAM.Analytical/Query/Height.cs
@@ -13,17 +13,41 @@ namespace SAM.Analytical
             if (closed2D == null)
                 return double.NaN;
 
-            IClosedPlanar3D closedPlanar3D = Plane.WorldXY.Convert(closed2D);
-            if (closedPlanar3D == null)
+            ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
+            if (segmentable2D == null)
+                throw new System.NotImplementedException();
+
+            Rectangle2D rectangle2D = Geometry.Planar.Create.Rectangle2D(segmentable2D.GetPoints());
+            if (rectangle2D == null)
                 return double.NaN;
 
-            BoundingBox3D boundingBox3D = closedPlanar3D.GetBoundingBox();
-            if(boundingBox3D == null)
-                return double.NaN;
+            Plane plane = Plane.WorldXY;
 
-            return boundingBox3D.Max.Y - boundingBox3D.Min.Y;
+            Vector3D vector3D_WidthDirection = plane.Convert(rectangle2D.WidthDirection);
+            Vector3D vector3D_HeightDirection = plane.Convert(rectangle2D.HeightDirection);
+
+            Vector3D axisY = plane.AxisY;
+
+            double result = rectangle2D.Height;
+            if (axisY.SmallestAngle(vector3D_WidthDirection) < axisY.SmallestAngle(vector3D_HeightDirection))
+                result = rectangle2D.Width;
+
+            return result;
 
 
+            //Method 2
+            //IClosedPlanar3D closedPlanar3D = Plane.WorldXY.Convert(closed2D);
+            //if (closedPlanar3D == null)
+            //    return double.NaN;
+
+            //BoundingBox3D boundingBox3D = closedPlanar3D.GetBoundingBox();
+            //if(boundingBox3D == null)
+            //    return double.NaN;
+
+            //return boundingBox3D.Max.Y - boundingBox3D.Min.Y;
+
+
+            //Method 1
             //ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
 
             //if (segmentable2D == null)
--- SAM/SAM.Analytical/Query/Width.cs
+++ SAM/SAM.Analytical/Query/Width.cs
@@ -14,16 +14,40 @@ namespace SAM.Analytical
             if (closed2D == null)
                 return double.NaN;
 
-            IClosedPlanar3D closedPlanar3D = Plane.WorldXY.Convert(closed2D);
-            if (closedPlanar3D == null)
+            ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
+            if (segmentable2D == null)
+                throw new System.NotImplementedException();
+
+            Rectangle2D rectangle2D = Geometry.Planar.Create.Rectangle2D(segmentable2D.GetPoints());
+            if (rectangle2D == null)
                 return double.NaN;
 
-            BoundingBox3D boundingBox3D = closedPlanar3D.GetBoundingBox();
-            if (boundingBox3D == null)
-                return double.NaN;
+            Plane plane = Plane.WorldXY;
 
-            return boundingBox3D.Max.X - boundingBox3D.Min.X;
+            Vector3D vector3D_WidthDirection = plane.Convert(rectangle2D.WidthDirection);
+            Vector3D vector3D_HeightDirection = plane.Convert(rectangle2D.HeightDirection);
 
+            Vector3D axisX = plane.AxisX;
+
+            double result = rectangle2D.Width;
+            if (axisX.SmallestAngle(vector3D_HeightDirection) < axisX.SmallestAngle(vector3D_WidthDirection))
+                result = rectangle2D.Height;
+
+            return result;
+
+            //Method 2
+            //IClosedPlanar3D closedPlanar3D = Plane.WorldXY.Convert(closed2D);
+            //if (closedPlanar3D == null)
+            //    return double.NaN;
+
+            //BoundingBox3D boundingBox3D = closedPlanar3D.GetBoundingBox();
+            //if (boundingBox3D == null)
+            //    return double.NaN;
+
+            //return boundingBox3D.Max.X - boundingBox3D.Min.X;
+
+
+            //Method 1
             //ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
 
             //if (segmentable2D == null)
