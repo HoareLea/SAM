f2c3f5b326b7957db1b84456ec466d26b3bce9ec
Michal Dengusiak <tassimulation@gmail.com>
2020-08-21 16:43:19 +0100

    fixes to rooflgiht orientation

---
+++ SAM/SAM.Analytical/Create/Rectangle2D.cs
@@ -0,0 +1,23 @@
+﻿using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+
+namespace SAM.Analytical
+{
+    public static partial class Create
+    {
+        public static Rectangle2D Rectangle2D(this PlanarBoundary3D planarBoundary3D)
+        {
+            //TODO: Find better way to determine Height
+
+            IClosed2D closed2D = planarBoundary3D?.Edge2DLoop?.GetClosed2D();
+            if (closed2D == null)
+                return null;
+
+            ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
+            if (segmentable2D == null)
+                throw new System.NotImplementedException();
+
+           return Geometry.Planar.Create.Rectangle2D(segmentable2D.GetPoints());
+        }
+    }
+}
--- SAM/SAM.Analytical/Query/Width.cs
+++ SAM/SAM.Analytical/Query/Width.cs
@@ -9,15 +9,7 @@ namespace SAM.Analytical
         {
             //TODO: Find better way to determine Width
 
-            IClosed2D closed2D = planarBoundary3D?.Edge2DLoop?.GetClosed2D();
-            if (closed2D == null)
-                return double.NaN;
-
-            ISegmentable2D segmentable2D = closed2D as ISegmentable2D;
-            if (segmentable2D == null)
-                throw new System.NotImplementedException();
-
-            Rectangle2D rectangle2D = Geometry.Planar.Create.Rectangle2D(segmentable2D.GetPoints());
+            Rectangle2D rectangle2D = Create.Rectangle2D(planarBoundary3D);
             if (rectangle2D == null)
                 return double.NaN;
 
