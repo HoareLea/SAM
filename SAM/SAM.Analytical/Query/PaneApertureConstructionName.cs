455934619eba2188e55db8e5e2fe4c1501ed1f9f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-23 16:50:35 +0100

    Pane and Frame ApertureConstruction Name Methods Implemented

---
+++ SAM/SAM.Analytical/Query/PaneApertureConstructionName.cs
@@ -0,0 +1,18 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static string PaneApertureConstructionName(this ApertureConstruction apertureConstruction)
+        {
+            return PaneApertureConstructionName(apertureConstruction?.Name);
+        }
+
+        public static string PaneApertureConstructionName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return name;
+
+            return string.Format("{0} -pane", name);
+        }
+    }
+}
