c2a0c65dc35eac8a02adf4db0ec640db5407823c
Michal Dengusiak <tassimulation@gmail.com>
2020-09-24 09:01:25 +0100

    WIP fix for Material Ladybug

--- SAM/SAM.Analytical/Query/Tilt.cs
+++ SAM/SAM.Analytical/Query/Tilt.cs
@@ -2,6 +2,11 @@
 {
     public static partial class Query
     {
+        /// <summary>
+        /// Tilit of the Panel mesured in degrees
+        /// </summary>
+        /// <param name="panel">SAM Analytical Panel</param>
+        /// <returns>Tilt in degrees</returns>
         public static double Tilt(this Panel panel)
         {
             if (panel == null)
@@ -10,6 +15,11 @@
             return Geometry.Spatial.Query.Tilt(panel.GetFace3D());
         }
 
+        /// <summary>
+        /// Tilit of the Aperture mesured in degrees
+        /// </summary>
+        /// <param name="aperture">SAM Analytical Aperture</param>
+        /// <returns>Tilt in degrees</returns>
         public static double Tilt(this Aperture aperture)
         {
             if (aperture == null)
--- SAM/SAM.Geometry/Spatial/Query/Tilt.cs
+++ SAM/SAM.Geometry/Spatial/Query/Tilt.cs
@@ -4,6 +4,11 @@ namespace SAM.Geometry.Spatial
 {
     public static partial class Query
     {
+        /// <summary>
+        /// Angle between normal taken from given geometry and XY Plane mesured in degrees 
+        /// </summary>
+        /// <param name="closedPlanar3D">Closed Planar 3D Geometry</param>
+        /// <returns>Tilt in degrees</returns>
         public static double Tilt(this IClosedPlanar3D closedPlanar3D)
         {
             if (closedPlanar3D == null)
@@ -16,6 +21,11 @@ namespace SAM.Geometry.Spatial
             return Tilt(normal);
         }
 
+        /// <summary>
+        /// Angle between normal and XY Plane mesured in degrees 
+        /// </summary>
+        /// <param name="normal">Normal Vector 3D</param>
+        /// <returns>Tilt in degrees</returns>
         public static double Tilt(this Vector3D normal)
         {
             if (normal == null)
