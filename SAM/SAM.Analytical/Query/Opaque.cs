388b3321afdaa4fc34244e683adda092ab950f4c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-25 11:15:53 +0100

    Opaque Query for ConstructionLayers Added

---
+++ SAM/SAM.Analytical/Query/Opaque.cs
@@ -0,0 +1,30 @@
+﻿using SAM.Core;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static MaterialType MaterialType(this IEnumerable<ConstructionLayer> constructionLayers, MaterialLibrary materialLibrary)
+        {
+            if (constructionLayers == null || materialLibrary == null)
+                return Core.MaterialType.Undefined;
+
+            bool gas = true;
+            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            {
+                IMaterial material = constructionLayer.Material(materialLibrary);
+                if (material is OpaqueMaterial)
+                    return Core.MaterialType.Opaque;
+
+                if (material is TransparentMaterial)
+                    gas = false;
+            }
+
+            if (gas)
+                return Core.MaterialType.Gas;
+
+            return Core.MaterialType.Transparent;
+        }
+    }
+}
