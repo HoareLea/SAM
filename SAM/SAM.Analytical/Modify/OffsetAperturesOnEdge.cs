921130b37e75926e57cb8765d3589b4c162d84bf
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-29 08:34:32 +0100

    OffsetAperturesOnEdge Implementation for AdjacencyCluster

---
+++ SAM/SAM.Analytical/Modify/OffsetAperturesOnEdge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static void OffsetAperturesOnEdge(this AdjacencyCluster adjacencyCluster, double distance, double tolerance = Core.Tolerance.Distance)
+        {
+            if (adjacencyCluster == null)
+                return;
+
+            List<Aperture> result = new List<Aperture>();
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if (panels == null || panels.Count == 0)
+                return;
+
+            foreach(Panel panel in panels)
+            {
+                panel.OffsetAperturesOnEdge(distance, tolerance);
+                adjacencyCluster.AddObject(panel);
+            }
+        }
+    }
+}
