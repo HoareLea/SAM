0a8b5ccfbf598cabae95c6dac4ceb7e0bd7142ce
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-30 11:50:46 +0100

    ApertureConstructionLibrary File Updated

---
+++ SAM/SAM.Analytical/Create/ApertureConstruction.cs
@@ -0,0 +1,23 @@
+﻿using SAM.Core;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Create
+    {
+        public static ApertureConstruction ApertureConstruction(Guid guid, string name, ApertureType apertureType, IEnumerable<ConstructionLayer> paneConstructionLayers, IEnumerable<ConstructionLayer> frameConstructionLayers, PanelType panelType)
+        {
+            return ApertureConstruction(guid, name, apertureType, paneConstructionLayers, frameConstructionLayers, panelType, ActiveSetting.Setting);
+        }
+
+        public static ApertureConstruction ApertureConstruction(Guid guid, string name, ApertureType apertureType, IEnumerable<ConstructionLayer> paneConstructionLayers, IEnumerable<ConstructionLayer> frameConstructionLayers, PanelType panelType, Setting setting)
+        {
+            ApertureConstruction apertureConstruction = new ApertureConstruction(guid, name, apertureType, paneConstructionLayers, frameConstructionLayers);
+
+            apertureConstruction.SetPanelType(panelType, setting);
+
+            return apertureConstruction;
+        }
+    }
+}
