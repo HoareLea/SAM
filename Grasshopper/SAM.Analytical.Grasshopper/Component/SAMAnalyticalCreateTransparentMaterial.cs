de1f9d779756101ac685917689d4b6de6756cf41
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-30 15:23:05 +0100

    Input Fix for CreateTransparentMaterial Component (WIP)

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalCreateTransparentMaterial.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalCreateTransparentMaterial.cs
@@ -32,10 +32,12 @@ namespace SAM.Analytical.Grasshopper
         /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
         {
+            string defaultString = null;
+            
             inputParamManager.AddTextParameter("_name", "_name", "Name", GH_ParamAccess.item);
-            inputParamManager.AddTextParameter("_group_", "_group_", "Group", GH_ParamAccess.item, default(string));
-            inputParamManager.AddTextParameter("_displayName_", "_displayName_", "Display Name", GH_ParamAccess.item, default(string));
-            inputParamManager.AddTextParameter("_description_", "_description_", "Description", GH_ParamAccess.item, default(string));
+            inputParamManager.AddTextParameter("_group_", "_group_", "Group", GH_ParamAccess.item, defaultString);
+            inputParamManager.AddTextParameter("_displayName_", "_displayName_", "Display Name", GH_ParamAccess.item, defaultString);
+            inputParamManager.AddTextParameter("_description_", "_description_", "Description", GH_ParamAccess.item, defaultString);
             inputParamManager.AddNumberParameter("_thermalConductivity_", "_thermalConductivity_", "Thermal Conductivity [W/mK]", GH_ParamAccess.item, double.NaN);
             inputParamManager.AddNumberParameter("_defaultThickness_", "_defaultThickness_", "Default Thickness [m]", GH_ParamAccess.item, double.NaN);
             inputParamManager.AddNumberParameter("_vapourDiffusionFactor_", "_vapourDiffusionFactor_", "Vapour Diffusion Factor [-]", GH_ParamAccess.item, double.NaN);
