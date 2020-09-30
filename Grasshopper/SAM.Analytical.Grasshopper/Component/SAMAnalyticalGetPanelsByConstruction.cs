aab30d7302ddcd89704a01bcce329abfd8c619f8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-09-30 16:34:21 +0100

    GetPanelsByConstruction Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalGetPanelsByConstruction.cs
@@ -0,0 +1,116 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalGetPanelsByConstruction : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("f30c5943-55be-4ed5-bacc-9c9d265b5225");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalGetPanelsByConstruction()
+          : base("SAMAnalytical.GetPanelsByConstruction", "SAMAnalytical.GetPanelsByConstruction",
+              "Gets Panels By Construction",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            int index;
+
+            index = inputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "_analyticalObject", "_analyticalObject", "SAM Analytical Object such as AdjacencyCluster, Panel or AnalyticalModel", GH_ParamAccess.list);
+            inputParamManager[index].DataMapping = GH_DataMapping.Flatten;
+
+            inputParamManager.AddParameter(new GooConstructionParam(), "_construction", "_construction", "SAM Analytical Construction", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooPanelParam(), "Panels", "Panels", "Panels", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            List<SAMObject> sAMObjects = new List<SAMObject>();
+            if (!dataAccess.GetDataList(0, sAMObjects) || sAMObjects == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Construction construction = null;
+            if (!dataAccess.GetData(1, ref construction) || construction == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+                List<Panel> redundantPanels = new List<Panel>();
+
+            List<Panel> panels = sAMObjects.ConvertAll(x => x as Panel);
+            panels.RemoveAll(x => x == null);
+
+            List<AdjacencyCluster> adjacencyClusters = sAMObjects.ConvertAll(x => x as AdjacencyCluster);
+            adjacencyClusters.RemoveAll(x => x == null);
+
+            List<AnalyticalModel> analyticalModels = sAMObjects.ConvertAll(x => x as AnalyticalModel);
+            analyticalModels.RemoveAll(x => x == null);
+
+            panels = panels?.FindAll(x => x.SAMTypeGuid.Equals(construction.Guid));
+
+            if(analyticalModels != null)
+            {
+                foreach(AnalyticalModel analyticalModel in analyticalModels)
+                {
+                    List<Panel> panels_Temp = analyticalModel?.AdjacencyCluster?.GetPanels(construction);
+                    if (panels_Temp == null)
+                        continue;
+
+                    panels.AddRange(panels_Temp);
+                }
+            }
+
+            if(adjacencyClusters != null)
+            {
+                foreach (AdjacencyCluster adjacencyCluster in adjacencyClusters)
+                {
+                    List<Panel> panels_Temp = adjacencyCluster?.GetPanels(construction);
+                    if (panels_Temp == null)
+                        continue;
+
+                    panels.AddRange(panels_Temp);
+                }
+            }
+
+            dataAccess.SetDataList(0, panels.ConvertAll(x => new GooPanel(x)));
+        }
+    }
+}
