73fdc9f193be51bfd7746cf60bc67ef38ed3cbbe
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-02 08:21:46 +0100

    Create Log Method for ApertureConstruction Added

--- SAM/SAM.Analytical/Create/Log.cs
+++ SAM/SAM.Analytical/Create/Log.cs
@@ -1,6 +1,7 @@
 ﻿using SAM.Core;
 using SAM.Geometry.Spatial;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Analytical
 {
@@ -33,22 +34,29 @@ namespace SAM.Analytical
                 if(shell == null || !shell.IsClosed())
                 {
                     result.Add("Space {0} (Guid: {1}) is not enclosed.", LogRecordType.Warning, space.Name, space.Guid);
-                    return result;
+                    continue;
                 }
 
                 List<Space> spaces_InRange = spaces.FindAll(x => shell.InRange(x.Location));
                 if(spaces_InRange == null || spaces_InRange.Count > 1)
                 {
                     result.Add("There are more than one space enclosed in single shell: {0}", LogRecordType.Warning, string.Join(", ",spaces_InRange.ConvertAll(x => x.Name)));
-                    return result;
+                   continue;
                 }
                 
                 List<Panel> panels_Space = adjacencyCluster.GetPanels(space);
                 if(panels_Space == null || panels_Space.Count == 0)
                 {
                     result.Add("Space {0} (Guid: {1}) is not enclosed.", LogRecordType.Warning, space.Name, space.Guid);
-                    return result;
+                    continue;
                 }
+
+                if(panels_Space.Count < 4)
+                    result.Add("Space {0} (Guid: {1}) has less than 4 panels.", LogRecordType.Warning, space.Name, space.Guid);
+
+                Panel panel_Floor = panels_Space.Find(x => Query.PanelGroup(x.PanelType) == PanelGroup.Floor);
+                if(panel_Floor == null)
+                    result.Add("Space {0} (Guid: {1}) has no floor panels.", LogRecordType.Warning, space.Name, space.Guid);
 
                 foreach(Panel panel in panels_Space)
                 {
@@ -77,9 +85,17 @@ namespace SAM.Analytical
                         result.AddRange(log_Construction);
                 }
             }
-                
 
-
+            List<ApertureConstruction> apertureConstructions = adjacencyCluster.GetApertureConstructions();
+            if (apertureConstructions != null && apertureConstructions.Count > 0)
+            {
+                foreach (ApertureConstruction apertureConstruction in apertureConstructions)
+                {
+                    Log log_Construction = apertureConstruction.Log();
+                    if (log_Construction != null)
+                        result.AddRange(log_Construction);
+                }
+            }
 
             return result;
         }
@@ -129,8 +145,18 @@ namespace SAM.Analytical
                             result.AddRange(log_Construction);
                     }
                 }
-            }
 
+                List<ApertureConstruction> apertureConstructions = adjacencyCluster.ApertureConstructions();
+                if (apertureConstructions != null && materialLibrary != null)
+                {
+                    foreach (ApertureConstruction apertureConstruction in apertureConstructions)
+                    {
+                        Log log_Construction = apertureConstruction.Log(materialLibrary);
+                        if (log_Construction != null)
+                            result.AddRange(log_Construction);
+                    }
+                }
+            }
 
             return result;
         }
@@ -152,8 +178,6 @@ namespace SAM.Analytical
             
             foreach(IMaterial material in materials)
             {
-               
-
                 Log log_Material = material.Log();
                 if (log_Material != null)
                     result.AddRange(log_Material);
@@ -169,22 +193,12 @@ namespace SAM.Analytical
 
             Log result = new Log();
 
-            List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
-            if(constructionLayers == null || constructionLayers.Count == 0)
+            List<ConstructionLayer> constructionLayers = construction?.ConstructionLayers;
+            if (constructionLayers != null && constructionLayers.Count > 0)
             {
-                result.Add(string.Format("{0} Construction (Guid: {1}) has no construction layers", construction.Name, construction.Guid), LogRecordType.Warning);
-                return result;
-            }
-
-            for(int i=0; i < constructionLayers.Count; i++)
-            {
-                ConstructionLayer constructionLayer = constructionLayers[i];
-
-                if (string.IsNullOrWhiteSpace(constructionLayer.Name))
-                    result.Add(string.Format("{0} Construction (Guid: {1}) has layer with no name (Construction Layer Index: {2})", construction.Name, construction.Guid, i), LogRecordType.Error);
-
-                if (constructionLayer.Thickness <= 0)
-                    result.Add(string.Format("{0} Construction (Guid: {1}) has layer with thickness equal or less than 0 (Construction Layer Index: {2})", construction.Name, construction.Guid, i), LogRecordType.Error);
+                Log log_ConstructionLayers = constructionLayers.Log(construction.Name, construction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
             }
 
             return result;
@@ -198,14 +212,11 @@ namespace SAM.Analytical
             Log result = new Log();
 
             List<ConstructionLayer> constructionLayers = construction?.ConstructionLayers;
-            if (constructionLayers == null || constructionLayers.Count == 0)
-                return result;
-
-            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            if (constructionLayers != null && constructionLayers.Count > 0)
             {
-                IMaterial material = constructionLayer.Material(materialLibrary);
-                if (material == null)
-                    result.Add(string.Format("Material Library does not contain Material {0} for {1} Construction (Guid: {2})", constructionLayer.Name, construction.Name, construction.Guid), LogRecordType.Error);
+                Log log_ConstructionLayers = constructionLayers.Log(materialLibrary, construction.Name, construction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
             }
 
             return result;
@@ -217,6 +228,52 @@ namespace SAM.Analytical
                 return null;
 
             Log result = new Log();
+
+            List<ConstructionLayer> constructionLayers = null;
+
+            constructionLayers = apertureConstruction?.PaneConstructionLayers;
+            if (constructionLayers != null && constructionLayers.Count > 0)
+            {
+                Log log_ConstructionLayers = constructionLayers.Log(apertureConstruction.Name, apertureConstruction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
+            }
+
+            constructionLayers = apertureConstruction?.FrameConstructionLayers;
+            if (constructionLayers != null && constructionLayers.Count > 0)
+            {
+                Log log_ConstructionLayers = constructionLayers.Log(apertureConstruction.Name, apertureConstruction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
+            }
+
+            return result;
+        }
+
+        public static Log Log(this ApertureConstruction apertureConstruction, MaterialLibrary materialLibrary)
+        {
+            if (apertureConstruction == null || materialLibrary == null)
+                return null;
+
+            Log result = new Log();
+
+            List<ConstructionLayer> constructionLayers = null;
+
+            constructionLayers = apertureConstruction?.PaneConstructionLayers;
+            if (constructionLayers != null && constructionLayers.Count > 0)
+            {
+                Log log_ConstructionLayers = constructionLayers.Log(materialLibrary, apertureConstruction.Name, apertureConstruction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
+            }
+
+            constructionLayers = apertureConstruction?.FrameConstructionLayers;
+            if (constructionLayers != null && constructionLayers.Count > 0)
+            {
+                Log log_ConstructionLayers = constructionLayers.Log(materialLibrary, apertureConstruction.Name, apertureConstruction.Guid);
+                if (log_ConstructionLayers != null)
+                    result.AddRange(log_ConstructionLayers);
+            }
 
             return result;
         }
@@ -230,17 +287,70 @@ namespace SAM.Analytical
 
             Log result = new Log();
 
+            string name = material.Name;
+            if(string.IsNullOrWhiteSpace(name))
+                result.Add(string.Format("Material (Guid: {0}) has no name assigned", material.Guid), LogRecordType.Warning);
+
             if (material is GasMaterial)
             {
-
+                GasMaterial gasMaterial = (GasMaterial)material;
             }
             else if (material is TransparentMaterial)
             {
-
+                TransparentMaterial transparentMaterial = (TransparentMaterial)material;
             }
             else if (material is OpaqueMaterial)
             {
+                OpaqueMaterial opaqueMaterial = (OpaqueMaterial)material;
+            }
 
+            return result;
+        }
+
+
+        private static Log Log(this IEnumerable<ConstructionLayer> constructionLayers, MaterialLibrary materialLibrary, string name, System.Guid guid)
+        {
+            if (constructionLayers == null)
+                return null;
+
+            string name_Temp = name;
+            if (string.IsNullOrEmpty(name))
+                name_Temp = "???";
+
+            Log result = new Log();
+
+            foreach (ConstructionLayer constructionLayer in constructionLayers)
+            {
+                IMaterial material = constructionLayer.Material(materialLibrary);
+                if (material == null)
+                    result.Add(string.Format("Material Library does not contain Material {0} for {1} (Guid: {2})", constructionLayer.Name, name_Temp, guid), LogRecordType.Error);
+            }
+
+            return result;
+        }
+
+        private static Log Log(this IEnumerable<ConstructionLayer> constructionLayers, string name, System.Guid guid)
+        {
+            string name_Temp = name;
+            if (string.IsNullOrEmpty(name))
+                name_Temp = "???";
+
+            Log result = new Log();
+            if (constructionLayers == null || constructionLayers.Count() == 0)
+            {
+                result.Add(string.Format("{0} (Guid: {1}) has no construction layers", name_Temp, guid), LogRecordType.Warning);
+                return result;
+            }
+
+            for (int i = 0; i < constructionLayers.Count(); i++)
+            {
+                ConstructionLayer constructionLayer = constructionLayers.ElementAt(i);
+
+                if (string.IsNullOrWhiteSpace(constructionLayer.Name))
+                    result.Add(string.Format("{0} (Guid: {1}) has layer with no name (Construction Layer Index: {2})", name_Temp, guid, i), LogRecordType.Error);
+
+                if (constructionLayer.Thickness <= 0)
+                    result.Add(string.Format("{0} (Guid: {1}) has layer with thickness equal or less than 0 (Construction Layer Index: {2})", name_Temp, guid, i), LogRecordType.Error);
             }
 
             return result;
