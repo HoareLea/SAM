0d98625cf411f8fea1e54dc22fee1a280caed147
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-06 10:32:43 +0100

    PanelType Description Implemented

--- SAM/SAM.Analytical/Enums/PanelType.cs
+++ SAM/SAM.Analytical/Enums/PanelType.cs
@@ -1,24 +1,27 @@
-﻿namespace SAM.Analytical
+﻿using System.ComponentModel;
+
+namespace SAM.Analytical
 {
+    [Description("Analytical Panel Type.")]
     public enum PanelType
     {
-        Undefined,
-        Ceiling,
-        CurtainWall,
-        Floor,
-        FloorExposed,
-        FloorInternal,
-        FloorRaised,
-        Roof,
-        Shade,
-        SlabOnGrade,
-        SolarPanel,
-        UndergroundCeiling,
-        UndergroundSlab,
-        UndergroundWall,
-        Wall,
-        WallExternal,
-        WallInternal,
-        Air
+        [Description("Undefined")] Undefined,
+        [Description("Ceiling")] Ceiling,
+        [Description("Curtain Wall")] CurtainWall,
+        [Description("Floor")] Floor,
+        [Description("Exposed Floor")] FloorExposed,
+        [Description("Internal Floor")] FloorInternal,
+        [Description("Raised Floor")] FloorRaised,
+        [Description("Roof")] Roof,
+        [Description("Shade")] Shade,
+        [Description("Slab on Grade")] SlabOnGrade,
+        [Description("Solar/PV panel")] SolarPanel,
+        [Description("Underground Ceiling")] UndergroundCeiling,
+        [Description("Underground Slab")] UndergroundSlab,
+        [Description("Underground Wall")] UndergroundWall,
+        [Description("Wall")] Wall,
+        [Description("External Wall")] WallExternal,
+        [Description("Internal Wall")] WallInternal,
+        [Description("Air")] Air
     }
 }
--- SAM/SAM.Analytical/Query/Text.cs
+++ SAM/SAM.Analytical/Query/Text.cs
@@ -1,50 +1,10 @@
-﻿using System;
-
-namespace SAM.Analytical
+﻿namespace SAM.Analytical
 {
     public static partial class Query
     {
         public static string Text(this PanelType panelType)
         {
-            switch(panelType)
-            {
-                case Analytical.PanelType.Ceiling:
-                    return "Ceiling";
-                case Analytical.PanelType.CurtainWall:
-                    return "Curtain Wall";
-                case Analytical.PanelType.Floor:
-                    return "Floor";
-                case Analytical.PanelType.FloorExposed:
-                    return "Exposed Floor";
-                case Analytical.PanelType.FloorInternal:
-                    return "Internal Floor";
-                case Analytical.PanelType.FloorRaised:
-                    return "Raised Floor";
-                case Analytical.PanelType.Roof:
-                    return "Roof";
-                case Analytical.PanelType.Shade:
-                    return "Shade";
-                case Analytical.PanelType.SlabOnGrade:
-                    return "Slab on Grade";
-                case Analytical.PanelType.SolarPanel:
-                    return "Solar/PV panel";
-                case Analytical.PanelType.Air:
-                    return "No Type";
-                case Analytical.PanelType.UndergroundCeiling:
-                    return "Underground Ceiling";
-                case Analytical.PanelType.UndergroundSlab:
-                    return "Underground Slab";
-                case Analytical.PanelType.UndergroundWall:
-                    return "Underground Wall";
-                case Analytical.PanelType.Wall:
-                    return "Wall";
-                case Analytical.PanelType.WallExternal:
-                    return "External Wall";
-                case Analytical.PanelType.WallInternal:
-                    return "Internal Wall";
-            }
-
-            return null;
+            return Core.Query.Description(panelType);
         }
     }
 }
