0545af04343ebda043873e8e5eccdfbc8abc805c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-09 16:46:16 +0100

    SpaceParameter Enum Added

---
+++ SAM/SAM.Analytical/Enums/Parameter/SpaceParameter.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using SAM.Core.Attributes;
+
+namespace SAM.Analytical
+{
+    [ParameterTypes(typeof(Space)), Description("Space Parameter")]
+    public enum SpaceParameter
+    {
+        [ParameterProperties("Design Heating Load", "Design Heating Load"), DoubleParameterType(0)] DesignHeatingLoad,
+        [ParameterProperties("Specified Exhaust Airflow", "Extract Airflow"), DoubleParameterType(0)] ExtractAirflow,
+        [ParameterProperties("Specified Supply Airflow", "Supply Airflow"), DoubleParameterType(0)] SupplyAirflow,
+    }
+}
