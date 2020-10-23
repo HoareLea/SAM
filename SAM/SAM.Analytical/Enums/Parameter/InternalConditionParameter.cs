ba4999b759b7841bf11d3c230e6abd980ede1473
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-23 16:11:48 +0100

    Naming Updated for InternalConditionParameter

--- SAM/SAM.Analytical/Enums/Parameter/InternalConditionParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/InternalConditionParameter.cs
@@ -6,13 +6,13 @@ namespace SAM.Analytical
     [AssociatedTypes(typeof(InternalCondition)), Description("Internal Condition Parameter")]
     public enum InternalConditionParameter
     {
-        [ParameterProperties("SAM_NoPeople", "Number Of People"), DoubleParameterValue(0)] NumberOfPeople,
-        [ParameterProperties("SAM_OccupacyProfile", "Occupancy Profile Name"), ParameterValue(Core.ParameterType.String)] OccupancyProfileName,
-        [ParameterProperties("SAM_OccupantSensGain", "Occupant Sensible Gain"), ParameterValue(Core.ParameterType.Double)] OccupantSensibleGain,
-        [ParameterProperties("SAM_OccupantLatGain", "Occupant Latent Gain"), ParameterValue(Core.ParameterType.Double)] OccupantLatentGain,
-        [ParameterProperties("SAM_SmallPowerSensProfile", "Equipment Sensible Profile Name"), ParameterValue(Core.ParameterType.String)] EquipmentSensibleProfileName,
-        [ParameterProperties("SAM_SmallPowerLatProfile", "Equipment Latent Profile Name"), ParameterValue(Core.ParameterType.String)] EquipmentLatentProfileName,
-        [ParameterProperties("SAM_GenLightingGain", "Lighting Gain"), ParameterValue(Core.ParameterType.Double)] LightingGain,
-        [ParameterProperties("SAM_DesignLuxLevel", "Design Lux Level"), DoubleParameterValue(0)] DesignLuxLevel,
+        [ParameterProperties("Number Of People", "Number Of People"), DoubleParameterValue(0)] NumberOfPeople,
+        [ParameterProperties("Occupancy Profile Name", "Occupancy Profile Name"), ParameterValue(Core.ParameterType.String)] OccupancyProfileName,
+        [ParameterProperties("Occupant Sensible Gain", "Occupant Sensible Gain"), ParameterValue(Core.ParameterType.Double)] OccupantSensibleGain,
+        [ParameterProperties("Occupant Latent Gain", "Occupant Latent Gain"), ParameterValue(Core.ParameterType.Double)] OccupantLatentGain,
+        [ParameterProperties("Equipment Sensible Profile Name", "Equipment Sensible Profile Name"), ParameterValue(Core.ParameterType.String)] EquipmentSensibleProfileName,
+        [ParameterProperties("Equipment Latent Profile Name", "Equipment Latent Profile Name"), ParameterValue(Core.ParameterType.String)] EquipmentLatentProfileName,
+        [ParameterProperties("Lighting Gain", "Lighting Gain"), ParameterValue(Core.ParameterType.Double)] LightingGain,
+        [ParameterProperties("Design Lux Level", "Design Lux Level"), DoubleParameterValue(0)] DesignLuxLevel,
     }
 }
