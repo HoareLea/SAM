d1afdce28bc10d83f40d275ed90c7d981f458b90
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-28 16:35:29 +0000

    Unnecessary Parameter Removed from ApertureConstructionParameter

--- SAM/SAM.Analytical/Enums/Parameter/ApertureConstructionParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/ApertureConstructionParameter.cs
@@ -6,7 +6,6 @@ namespace SAM.Analytical
     [AssociatedTypes(typeof(ApertureConstruction)), Description("ApertureConstruction Parameter")]
     public enum ApertureConstructionParameter
     {
-        [ParameterProperties("Type", "Type"), ParameterValue(Core.ParameterType.String)] Type,
         [ParameterProperties("Dafault Panel Type", "Dafault Panel Type"), ParameterValue(Core.ParameterType.String)] DefaultPanelType,
         [ParameterProperties("Description", "Description"), ParameterValue(Core.ParameterType.String)] Description,
         [ParameterProperties("Transparent", "Transparent"), ParameterValue(Core.ParameterType.Boolean)] Transparent,
