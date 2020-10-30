13857a7d9bcfc4b36abddc0947fa3248ef053af7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-10-30 10:48:51 +0000

    DefaultThickness Parameter Added for Construction

--- SAM/SAM.Analytical/Enums/Parameter/ConstructionParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/ConstructionParameter.cs
@@ -13,5 +13,6 @@ namespace SAM.Analytical
         [ParameterProperties("Is Ground", "Is Ground"), ParameterValue(Core.ParameterType.Boolean)] IsGround,
         [ParameterProperties("Is Internal Shadow", "Is Internal Shadow"), ParameterValue(Core.ParameterType.Boolean)] IsInternalShadow,
         [ParameterProperties("Transparent", "Transparent"), ParameterValue(Core.ParameterType.Boolean)] Transparent,
+        [ParameterProperties("Default Thickness", "Default Thickness"), DoubleParameterValue(0)] DefaultThickness,
     }
 }
