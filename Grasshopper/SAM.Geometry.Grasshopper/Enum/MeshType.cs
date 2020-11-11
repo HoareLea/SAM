a399b33b9fee7ad15d57c85b41d207eb17e1f7a2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-11 14:33:58 +0000

    Reduce Mesh Method Added to SAM.Geometry.Grasshopper

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Enum/MeshType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Geometry.Grasshopper
+{
+    [Description("Mesh Type")]
+    public enum MeshType
+    {
+        [Description("Undefined")] Undefined,
+        [Description("Triangle")] Triangle,
+        [Description("Quad")] Quad
+    }
+}
---
+++ Grasshopper/SAM.Geometry.Grasshopper/Query/Reduce.cs
@@ -0,0 +1,31 @@
+﻿using Rhino.Geometry;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public static partial class Query
+    {
+        public static bool Reduce(this Mesh mesh, MeshType meshType, bool allowDistortion, int desiredPolygonCount, int accuracy, bool normalizeSize)
+        {
+            if (mesh == null)
+                return false;
+
+            if (!mesh.Reduce(desiredPolygonCount, allowDistortion, accuracy, normalizeSize))
+                return false;
+
+            switch (meshType)
+            {
+                case MeshType.Quad:
+                    mesh.Faces.ConvertTrianglesToQuads(System.Math.PI / 90.0, 0.0);
+                    break;
+                case MeshType.Triangle:
+                    mesh.Faces.ConvertQuadsToTriangles();
+                    break;
+            }
+
+            mesh.Normals.ComputeNormals();
+            mesh.FaceNormals.ComputeFaceNormals();
+            mesh.Compact();
+            return true;
+        }
+    }
+}
