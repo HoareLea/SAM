46c8edcb57856bef5d99973f6b8b5c924ff16163
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-11 16:31:17 +0000

    MeshClose Component Added

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Component/GeometryMeshClose.cs
@@ -0,0 +1,88 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper.Properties;
+using System;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public class GeometryMeshClose : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("e9ea89ab-6df3-4050-9a3f-134d2445d57c");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Geometry;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public GeometryMeshClose()
+          : base("Geometry.MeshClose", "Geometry.MeshClose",
+              "Close Rhino Mesh",
+              "SAM", "Geometry")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddMeshParameter("_mesh", "_mesh", "Rhino Mesh", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddMeshParameter("Mesh", "Mesh", "Mesh", GH_ParamAccess.item);
+            outputParamManager.AddBooleanParameter("Closed", "Closed", "Closed", GH_ParamAccess.item);
+            //outputParamManager.AddParameter( string, "Normal", "Normal", "Normal", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            Mesh mesh = null;
+            if (!dataAccess.GetData(0, ref mesh))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            bool allowDistortion = true;
+            dataAccess.GetData(1, ref allowDistortion);
+
+            int deisredPolygonCount = 0;
+            dataAccess.GetData(2, ref deisredPolygonCount);
+
+            int accuracy = 0;
+            dataAccess.GetData(3, ref accuracy);
+
+            bool normalizedSize = true;
+            dataAccess.GetData(1, ref normalizedSize);
+
+            Mesh mesh_Result = null;
+            bool result = Query.TryClose(mesh, out mesh_Result);
+
+            dataAccess.SetData(0, mesh_Result);
+            dataAccess.SetData(1, result);
+        }
+    }
+}
