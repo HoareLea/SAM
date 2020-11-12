a0530118d8dfae5018834fec40b6afed1a126aed
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-12 14:18:09 +0000

    PanelsClose Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalPanelsClose.cs
@@ -0,0 +1,131 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper;
+using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalPanelsClose : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("be3ca54c-0795-4b75-b277-9c863481e3e6");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalPanelsClose()
+          : base("SAMAnalytical.PanelsClose", "SAMAnalytical.PanelsClose",
+              "Close Panels",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooPanelParam(), "_panels", "_panels", "SAM Analytical Panels", GH_ParamAccess.list);
+            inputParamManager.AddGenericParameter("_elevation", "_elevation", "Elevation or Plane", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooPanelParam(), "Panels", "Panels", "Panels", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            List<Panel> panels = new List<Panel>();
+            if (!dataAccess.GetDataList(0, panels) || panels == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            GH_ObjectWrapper objectWrapper = null;
+            if (!dataAccess.GetData(1, ref objectWrapper) || objectWrapper == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            object @object = objectWrapper.Value;
+
+            if (@object is IGH_Goo)
+            {
+                try
+                {
+                    @object = (@object as dynamic).Value;
+                }
+                catch (Exception exception)
+                {
+                    @object = null;
+                }
+            }
+
+            if (@object == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            Plane plane = null;
+
+            if (Core.Query.IsNumeric(@object))
+            {
+                plane = Geometry.Spatial.Create.Plane((double)@object);
+            }
+            else if (@object is Plane)
+            {
+                plane = (Plane)@object;
+            }
+            else if (@object is GH_Plane)
+            {
+                plane = ((GH_Plane)@object).ToSAM();
+            }
+            else if (@object is string)
+            {
+                double value;
+                if (double.TryParse((string)@object, out value))
+                    plane = Geometry.Spatial.Create.Plane(value);
+            }
+
+            if (plane != null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            panels = panels.Close(plane);
+
+            dataAccess.SetDataList(0, panels?.ConvertAll(x => new GooPanel(x)));
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Spatial/Query/Close.cs
@@ -0,0 +1,86 @@
+﻿using SAM.Geometry.Planar;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static List<Polygon3D> Close(this IEnumerable<IClosedPlanar3D> closedPlanar3Ds, Plane plane, double tolerance = Core.Tolerance.Distance)
+        {
+            if (closedPlanar3Ds == null || plane == null)
+                return null;
+
+            List<Segment2D> segment2Ds = new List<Segment2D>();
+            for(int i=0; i < closedPlanar3Ds.Count(); i++)
+            {
+                IClosedPlanar3D closedPlanar3D = closedPlanar3Ds.ElementAt(i);
+                if (closedPlanar3D == null)
+                    continue;
+
+                ISegmentable3D segmentable3D = closedPlanar3D as ISegmentable3D;
+                if (segmentable3D == null)
+                    continue;
+
+                Plane plane_Temp = closedPlanar3D.GetPlane();
+                if (plane_Temp == null)
+                    continue;
+
+                if (closedPlanar3D is Face3D)
+                {
+                    closedPlanar3D = ((Face3D)closedPlanar3D).GetExternalEdge3D();
+                    if (closedPlanar3D == null)
+                        continue;
+                }
+
+                PlanarIntersectionResult planarIntersectionResult = PlanarIntersectionResult.Create(plane, plane_Temp);
+                if (planarIntersectionResult == null || !planarIntersectionResult.Intersecting)
+                    continue;
+
+                Line3D line3D = planarIntersectionResult.GetGeometry3D<Line3D>();
+                if (line3D == null)
+                    continue;
+
+                List<Point3D> point3Ds = segmentable3D.GetPoints();
+                if (point3Ds == null || point3Ds.Count == 0)
+                    continue;
+
+                point3Ds = point3Ds.ConvertAll(x => line3D.Project(x));
+
+                Point3D point3D_1 = null;
+                Point3D point3D_2 = null;
+                ExtremePoints(point3Ds, out point3D_1, out point3D_2);
+                if (point3D_1 == null || point3D_2 == null || point3D_1.AlmostEquals(point3D_2, tolerance))
+                    continue;
+
+                Segment3D segment3D = new Segment3D(point3D_1, point3D_2);
+
+                segment2Ds.Add(plane.Convert(segment3D));
+            }
+
+            if (segment2Ds == null || segment2Ds.Count == 0)
+                return null;
+
+            segment2Ds.Snap(true, tolerance);
+
+            segment2Ds = segment2Ds.Split(tolerance);
+
+            List<Polygon2D> polygon2Ds = Planar.Create.Polygon2Ds(segment2Ds, tolerance);
+            if (polygon2Ds == null || polygon2Ds.Count == 0)
+                return null;
+
+            List<Polygon3D> result = new List<Polygon3D>();
+
+            foreach(Polygon2D polygon2D in polygon2Ds)
+            {
+                Polygon2D polygon2D_Temp = Planar.Query.Snap(polygon2Ds[0], segment2Ds, tolerance);
+                if (polygon2D_Temp != null)
+                    polygon2D_Temp = polygon2Ds[0];
+
+                result.Add(plane.Convert(polygon2D_Temp));
+            }
+
+            return result;
+        }
+    }
+}
