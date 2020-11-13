cdf4bd63a2d48a4fd5e3b6c8ae6a06b4cff54212
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-13 12:21:50 +0000

    Between Planes Query Added

---
+++ SAM/SAM.Geometry/Spatial/Query/Between.cs
@@ -0,0 +1,13 @@
+﻿namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static bool Between(this Plane plane_1, Plane plane_2, Point3D point3D, double tolerance = Core.Tolerance.Distance)
+        {
+            if (plane_1 == null || plane_2 == null || point3D == null)
+                return false;
+
+            return (plane_1.Above(point3D, tolerance) && plane_2.Below(point3D, tolerance)) || (plane_2.Above(point3D, tolerance) && plane_1.Below(point3D, tolerance));
+        }
+    }
+}
