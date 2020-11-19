06e4e6ddd345ae248787e7746686cd47924bf979
Michal Dengusiak <tassimulation@gmail.com>
2020-11-19 08:47:10 +0000

    Update SetValue.cs

--- SAM/SAM.Core/Modify/SetValue.cs
+++ SAM/SAM.Core/Modify/SetValue.cs
@@ -14,47 +14,47 @@ namespace SAM.Core
 
         public static bool SetValue(this SAMObject sAMObject, string name, Guid value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, double value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, int value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, bool value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, IJSAMObject value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, JObject value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, DateTime value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, System.Drawing.Color value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
         public static bool SetValue(this SAMObject sAMObject, string name, SAMColor value)
         {
-            return SetValue(sAMObject, sAMObject?.GetType().Assembly, name, value as object);
+            return SetValue(sAMObject, null, name, value as object);
         }
 
 
