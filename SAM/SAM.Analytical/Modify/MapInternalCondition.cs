4ff4743a8e3f45483c93cc0289a9ca974ca3ffac
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-19 07:27:13 +0000

    MapInternalCondition Method Added

---
+++ SAM/SAM.Analytical/Modify/MapInternalCondition.cs
@@ -0,0 +1,34 @@
+﻿using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static InternalCondition MapInternalCondition(this Space space, InternalConditionLibrary internalConditionLibrary, TextMapCluster textMapCluster)
+        {
+            if (space == null || internalConditionLibrary == null || textMapCluster == null)
+                return null;
+
+            string name = space.Name;
+            if (string.IsNullOrWhiteSpace(name))
+                return null;
+
+            HashSet<string> names_InternalCondition = textMapCluster.GetSortedKeys(name);
+            if (names_InternalCondition == null || names_InternalCondition.Count == 0)
+                return null;
+
+            List<InternalCondition> internalConditions = internalConditionLibrary.GetInternalConditions(names_InternalCondition.First());
+            if (internalConditions == null || internalConditions.Count == 0)
+                return null;
+
+            InternalCondition internalCondition = internalConditions[0];
+            if (internalCondition == null)
+                return null;
+
+            space.InternalCondition = internalCondition;
+            return internalCondition;
+        }
+    }
+}
