d21c250624922c25029a3517230bf311bc533381
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-11-21 08:25:49 +0000

    Flip Panel Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalFlipPanel.cs
@@ -0,0 +1,76 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalFlipPanel : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("8899c11a-c18f-4c88-a2a4-2c7f1d4ed181");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalFlipPanel()
+          : base("SAMAnalytical.FlipPanel", "SAMAnalytical.FlipPanel",
+              "Flip SAM Analytical Panel",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            int index;
+
+            index = inputParamManager.AddParameter(new GooPanelParam(), "panel_", "panel_", "SAM Analytical Panel", GH_ParamAccess.item);
+            inputParamManager[index].Optional = true;
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooPanelParam(), "Panel", "Panel", "SAM Analytical Panel", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            Panel panel = null;
+            if (!dataAccess.GetData(0, ref panel))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            panel = new Panel(panel);
+            panel.FlipNormal(true);
+
+            dataAccess.SetData(0, new GooPanel(panel));
+        }
+    }
+}
