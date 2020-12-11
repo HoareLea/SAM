bfe697245726fc309f300ae6af6d929a0dbc55cb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-12-11 16:24:17 +0000

    SystemTypeLibrary Grashopper Components Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalGetDefaultSystemTypeLibrary.cs
@@ -0,0 +1,62 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using System;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalGetDefaultSystemTypeLibrary : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("2ed4b34f-5fad-4b15-b39a-7b83251484be");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalGetDefaultSystemTypeLibrary()
+          : base("SAMAnalytical.GetDefaultSystemTypeLibrary", "SAMAnalytical.GetDefaultSystemTypeLibrary",
+              "Get Default SAM SystemTypeibrary",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooSystemTypeLibraryParam(), "SystemTypeLibrary", "SystemTypeLibrary", "SAM Analytical SystemTypeLibrary", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            dataAccess.SetData(0, new GooSystemTypeLibrary(ActiveSetting.Setting.GetValue<Core.SystemTypeLibrary>(AnalyticalSettingParameter.DefaultSystemTypeLibrary)));
+        }
+    }
+}
---
+++ Grasshopper/SAM.Core.Grasshopper/Classes/GooSystemTypeLibrary.cs
@@ -0,0 +1,48 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper
+{
+    public class GooSystemTypeLibrary : GooSAMObject<SystemTypeLibrary>
+    {
+        public GooSystemTypeLibrary()
+            : base()
+        {
+        }
+
+        public GooSystemTypeLibrary(SystemTypeLibrary systemTypeLibrary)
+            : base(systemTypeLibrary)
+        {
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new GooSystemTypeLibrary(Value);
+        }
+    }
+
+    public class GooSystemTypeLibraryParam : GH_PersistentParam<GooSystemTypeLibrary>
+    {
+        public override Guid ComponentGuid => new Guid("4835eafb-5626-4d53-8e45-d2cc43c01b6c");
+
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public GooSystemTypeLibraryParam()
+            : base(typeof(SystemTypeLibrary).Name, typeof(SystemTypeLibrary).Name, typeof(SystemTypeLibrary).FullName.Replace(".", " "), "Params", "SAM")
+        {
+        }
+
+        protected override GH_GetterResult Prompt_Plural(ref List<GooSystemTypeLibrary> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override GH_GetterResult Prompt_Singular(ref GooSystemTypeLibrary value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
