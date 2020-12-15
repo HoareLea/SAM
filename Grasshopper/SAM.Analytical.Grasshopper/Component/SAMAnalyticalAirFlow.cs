623b26f97b80b43114c1b523374b75f991853738
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-12-15 12:16:36 +0000

    Calculate SupplyAirFlow and ExhaustAirFlow Method Added for Space

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalAirFlow.cs
@@ -0,0 +1,73 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalAirFlow : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("b73301da-72d3-4ab6-a625-17a1f59d128c");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalAirFlow()
+          : base("SAMAnalytical.AirFlow", "SAMAnalytical.AirFlow",
+              "Calculates Exhaust and Supply Air Flow",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooSpaceParam(), "_space", "_space", "SAM Analytical Space", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddNumberParameter("Supply", "Supply", "Supply Air Flow [m3/s]", GH_ParamAccess.item);
+            outputParamManager.AddNumberParameter("Exhaust", "Exhasut", "Exhaust Air Flow [m3/s]", GH_ParamAccess.item);
+            outputParamManager.AddNumberParameter("Supply_LpS", "Supply_LpS", "Supply Air Flow [l/s]", GH_ParamAccess.item);
+            outputParamManager.AddNumberParameter("Exhaust_LpS", "Exhasut_LpS", "Exhaust Air Flow [l/s]", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            Space space= null;
+            if (!dataAccess.GetData(0, ref space) || space == null)
+            {
+
+            }
+
+            double exhaustAirFlow = Analytical.Query.ExhaustAirFlow(space);
+            double supplyAirFlow = Analytical.Query.SupplyAirFlow(space);
+
+            dataAccess.SetData(0, supplyAirFlow);
+            dataAccess.SetData(1, exhaustAirFlow);
+            dataAccess.SetData(2, supplyAirFlow * 1000);
+            dataAccess.SetData(3, exhaustAirFlow * 1000);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Query/ExhaustAirFlow.cs
@@ -0,0 +1,70 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double ExhaustAirFlow(this Space space)
+        {
+            if (space == null)
+                return double.NaN;
+
+            InternalCondition internalCondition = space.InternalCondition;
+
+            if (internalCondition == null)
+                return double.NaN;
+
+            double area;
+            if (!space.TryGetValue(SpaceParameter.Area, out area))
+                area = double.NaN;
+
+            double occupancy;
+            if (!space.TryGetValue(SpaceParameter.Occupancy, out occupancy))
+                occupancy = double.NaN;
+
+            if (double.IsNaN(occupancy))
+            {
+                if (!double.IsNaN(area))
+                {
+                    double occupancyPerArea = double.NaN;
+                    if (internalCondition.TryGetValue(InternalConditionParameter.OccupancyPerArea, out occupancyPerArea))
+                        occupancy = occupancyPerArea * area;
+                }
+            }
+
+            double airFlow_1 = double.NaN;
+            if (!double.IsNaN(occupancy))
+            {
+                double airFlowPerPerson = double.NaN;
+                if (internalCondition.TryGetValue(InternalConditionParameter.ExhaustAirFlowPerPerson, out airFlowPerPerson))
+                    airFlow_1 = airFlowPerPerson * occupancy;
+            }
+
+            double airFlow_2 = double.NaN;
+            if (!double.IsNaN(area))
+            {
+                double airFlowPerArea = double.NaN;
+                if (internalCondition.TryGetValue(InternalConditionParameter.ExhaustAirFlowPerArea, out airFlowPerArea))
+                    airFlow_2 = airFlowPerArea * area;
+            }
+
+            double airFlow_3 = double.NaN;
+            if (!internalCondition.TryGetValue(InternalConditionParameter.ExhaustAirFlow, out airFlow_3))
+                airFlow_3 = double.NaN;
+
+            if (double.IsNaN(airFlow_1) && double.IsNaN(airFlow_2) && double.IsNaN(airFlow_3))
+                return double.NaN;
+
+            double result = 0;
+
+            if (!double.IsNaN(airFlow_1))
+                result += airFlow_1;
+
+            if (!double.IsNaN(airFlow_2))
+                result += airFlow_2;
+
+            if (!double.IsNaN(airFlow_3))
+                result += airFlow_3;
+
+            return result;
+        }
+    }
+}
