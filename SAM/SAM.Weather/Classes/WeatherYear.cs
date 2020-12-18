05e88d4c06996b069b7dcff1e4d2cdf15d6cdf30
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-12-18 16:06:44 +0000

    WeatherYear Updates

--- SAM/SAM.Weather/Classes/WeatherYear.cs
+++ SAM/SAM.Weather/Classes/WeatherYear.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Weather
 {
@@ -29,6 +31,55 @@ namespace SAM.Weather
         public WeatherYear(JObject jObject)
             : base(jObject)
         {
+        }
+
+        public WeatherDay this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= 365)
+                    return null;
+
+                if (weatherDays == null)
+                    return null;
+
+                return weatherDays[i];
+            }
+            set
+            {
+                if (i < 0 || i >= 365)
+                    return;
+
+                weatherDays[i] = value;
+            }
+        }
+
+        public List<double> GetValues(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return null;
+
+            List<double> result = new List<double>();
+            for(int i=0; i < weatherDays.Length; i++)
+            {
+                WeatherDay weatherDay = weatherDays[i];
+                if(weatherDay == null)
+                {
+                    result.AddRange(Enumerable.Repeat(double.NaN, 24));
+                    continue;
+                }
+
+                double[] values = weatherDay[name];
+                if (values == null || values.Length != 24)
+                    result.AddRange(Enumerable.Repeat(double.NaN, 24));
+            }
+
+            return result;
+        }
+
+        public List<double> GetValues(WeatherDataType weatherDataType)
+        {
+            return GetValues(weatherDataType.ToString());
         }
 
         public override bool FromJObject(JObject jObject)
