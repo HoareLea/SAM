11e0c6fbcf5594f62c221e4fff24a6c31927eca1
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-12-22 08:26:57 +0000

    Delete File Method Added

---
+++ SAM/SAM.Core/Modify/Delete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SAM.Core
+{
+    public static partial class Modify
+    {
+        public static bool Delete(string path, int maxDays_CreationTime)
+        {
+            if (string.IsNullOrWhiteSpace(path) || !System.IO.File.Exists(path))
+                return false;
+
+            DateTime dateTime_Write = System.IO.File.GetCreationTime(path);
+            DateTime dateTime_Now = DateTime.Now;
+
+            int days = (dateTime_Now - dateTime_Write).Days;
+            if (days < maxDays_CreationTime)
+                return false;
+            try
+            {
+                System.IO.File.Delete(path);
+            }
+            catch(Exception exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
