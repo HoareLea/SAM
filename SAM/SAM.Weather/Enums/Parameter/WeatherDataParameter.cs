f3228b33db258832d2abbf155a5a01f71cbde7e2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-12-22 16:31:21 +0000

    WeatherDataParameter Added

---
+++ SAM/SAM.Weather/Enums/Parameter/WeatherDataParameter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using SAM.Core.Attributes;
+
+namespace SAM.Weather
+{
+    [AssociatedTypes(typeof(WeatherData)), Description("WeatherData Parameter")]
+    public enum WeatherDataParameter
+    {
+        [ParameterProperties("Ground Temperatures", "Ground Temperatures"), SAMObjectParameterValue(typeof(Core.SAMCollection<GroundTemperature>))] GroundTemperatures,
+    }
+}
