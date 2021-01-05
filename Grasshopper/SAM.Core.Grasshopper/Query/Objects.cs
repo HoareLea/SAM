f023b3a4daf2a989f310b63df717a69a54048c8c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-05 14:55:50 +0000

    Query Objects from GH_Structure for Grasshopper Updated

--- Grasshopper/SAM.Core.Grasshopper/Query/Objects.cs
+++ Grasshopper/SAM.Core.Grasshopper/Query/Objects.cs
@@ -1,15 +1,64 @@
-﻿using Grasshopper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Core.Grasshopper
 {
     public static partial class Query
     {
-        public static object[,] Objects<T>(DataTree<T> dataTree) where T: global::Grasshopper.Kernel.Types.IGH_Goo
+        public static object[,] Objects<T>(global::Grasshopper.Kernel.Data.GH_Structure<T> structure) where T: global::Grasshopper.Kernel.Types.IGH_Goo
         {
-            if (dataTree == null)
+            if (structure == null)
                 return null;
 
-            throw new System.NotImplementedException();
+            IEnumerable<List<T>> branches = structure.Branches;
+            if (branches == null)
+                return null;
+
+            int max = 0;
+            foreach(List<T> list in branches)
+            {
+                if (list == null)
+                    continue;
+
+                if (list.Count > max)
+                    max = list.Count;
+            }
+
+            object[,] result = new object[branches.Count(), max];
+
+            if (max == 0)
+                return result;
+
+            int index = 0;
+            foreach(List<T> list in branches)
+            {
+                if (list == null)
+                    continue;
+
+                for(int i=0; i < list.Count; i++)
+                {
+                    T t = list[i];
+                    if (t == null)
+                        continue;
+
+                    object value = null;
+                    try
+                    {
+                        value = (t as dynamic).Value;
+                    }
+                    catch(Exception exception)
+                    {
+                        continue;
+                    }
+
+                    result[index, i] = value;
+                }
+                
+                index++;
+            }
+
+            return result;
         }
     }
 }
