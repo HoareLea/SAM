503a102b31f6af41bc0b78a89501799516e33e33
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-08 11:28:57 +0000

    IntegerRange Changed to Range

---
+++ SAM/SAM.Core/Classes/Base/Range.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json.Linq;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core
+{
+    public class Range<T> : IJSAMObject
+    {
+        private T min;
+        private T max;
+
+        public Range(T value_1, T value_2)
+        {
+            min = System.Math.Min(value_1 as dynamic, value_2 as dynamic);
+            max = System.Math.Max(value_1 as dynamic, value_2 as dynamic);
+        }
+
+        public Range(T value)
+        {
+            min = value;
+            max = value;
+        }
+
+        public Range(IEnumerable<T> values)
+        {
+            min = default;
+            max = default;
+
+            if(values != null)
+            {
+                List<T> list = values.ToList();
+                max = list.Max();
+                min = list.Min();
+            }
+        }
+
+        public Range(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public T Max
+        {
+            get
+            {
+                return max;
+            }
+        }
+
+        public T Min
+        {
+            get
+            {
+                return min;
+            }
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+                return false;
+
+            max = jObject.Value<T>("Max");
+            min = jObject.Value<T>("Min");
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Query.FullTypeName(this));
+            jObject.Add("Max", max as dynamic);
+            jObject.Add("Min", min as dynamic);
+
+            return jObject;
+        }
+
+        public bool In(T value)
+        {
+            return (value as dynamic) <= (max as dynamic) && (value as dynamic) >= (min as dynamic);
+        }
+
+        public bool Out(T value)
+        {
+            return !In(value);
+        }
+    }
+}
--- SAM/SAM.Core/Query/FullTypeName.cs
+++ SAM/SAM.Core/Query/FullTypeName.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace SAM.Core
 {
@@ -17,6 +18,22 @@ namespace SAM.Core
         {
             if (type == null)
                 return null;
+
+            if(type.IsPrimitive)
+                return type.FullName;
+
+            if(type.IsGenericType)
+            {
+                Type[] types_Generic = type.GetGenericArguments();
+                if(types_Generic != null && types_Generic.Length != 0)
+                {
+                    List<string> typeNames = new List<string>();
+                    foreach (Type type_Generic in types_Generic)
+                        typeNames.Add(string.Format("[{0}]", FullTypeName(type_Generic)));
+
+                    return string.Format("{0}.{1}[{2}],{3}", type.Namespace, type.UnderlyingSystemType.Name, string.Join(",", typeNames), type.Assembly.GetName().Name);
+                }
+            }
 
             return string.Format("{0},{1}", type.FullName, type.Assembly.GetName().Name);
         }
