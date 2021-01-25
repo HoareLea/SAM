d9ce659048c03412d495e832d24e278560f87260
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-25 08:24:10 +0000

    Bitmap Processing Added

---
+++ SAM/SAM.Core/Modify/DeleteObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SAM.Core
+{
+    public static partial class Modify
+    {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr intPtr);
+    }
+}
