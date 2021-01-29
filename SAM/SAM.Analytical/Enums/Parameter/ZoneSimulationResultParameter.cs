33c52cd503c0e71fa30a2163de86e3213a34d138
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-29 09:01:48 +0000

    ZoneSimulationResultParameters Added

--- SAM/SAM.Analytical/Enums/Parameter/ZoneSimulationResultParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/ZoneSimulationResultParameter.cs
@@ -11,6 +11,15 @@ namespace SAM.Analytical
         [ParameterProperties("Occupancy", "Occupancy [p]"), DoubleParameterValue(0)] Occupancy,
         [ParameterProperties("Max Sensible Load", "Max Sensible Load"), DoubleParameterValue(0)] MaxSensibleLoad,
         [ParameterProperties("Max Sensible Load Index", "Max Sensible Load Index"), IntegerParameterValue(0, 8759)] MaxSensibleLoadIndex,
+        [ParameterProperties("Air Movement Gain", "Air Movement Gain [W]"), DoubleParameterValue()] AirMovementGain,
+        [ParameterProperties("Building Heat Transfer", "Building Heat Transfer [W]"), DoubleParameterValue()] BuildingHeatTransfer,
+        [ParameterProperties("Equipment Sensible Gain", "Equipment Sensible Gain [W]"), DoubleParameterValue()] EquipmentSensibleGain,
+        [ParameterProperties("Glazing External Conduction", "Glazing External Conduction [W]"), DoubleParameterValue()] GlazingExternalConduction,
+        [ParameterProperties("Lighting Gain", "Lighting Gain [W]"), DoubleParameterValue()] LightingGain,
+        [ParameterProperties("Infiltration Gain", "Infiltration Gain [W]"), DoubleParameterValue()] InfiltrationGain,
+        [ParameterProperties("Occupancy Sensible Gain", "Occupancy Sensible Gain [W]"), DoubleParameterValue()] OccupancySensibleGain,
+        [ParameterProperties("Opaque External Conduction", "Opaque External Conduction [W]"), DoubleParameterValue()] OpaqueExternalConduction,
+        [ParameterProperties("Solar Gain", "Solar Gain [W]"), DoubleParameterValue()] SolarGain,
 
         [ParameterProperties("Simulation Type", "Simulation Type"), ParameterValue(Core.ParameterType.String)] SimulationType,
     }
