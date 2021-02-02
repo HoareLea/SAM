9e697b5de06cfdec592304463ae0d3ccc589c8fb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-02 16:40:24 +0000

    Expression and ExpressionVariable Implemented

---
+++ SAM/SAM.Core/Classes/Expression/Expression.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace SAM.Core
+{
+    public class Expression : IJSAMObject
+    {
+        private string text;
+        
+        public Expression(string text)
+        {
+            this.text = text;
+        }
+
+        public Expression(Expression expression)
+        {
+            text = expression?.text;
+        }
+
+        public Expression(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+        }
+
+        public List<ExpressionVariable> GetExpressionVariables()
+        {
+            if (text == null)
+                return null;
+
+            List<ExpressionVariable> result = new List<ExpressionVariable>();
+            List<string> texts = Query.Texts(text, true, '[', ']');
+            if (texts != null && texts.Count > 0)
+                result = texts.ConvertAll(x => new ExpressionVariable(x));
+
+            return result;
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+                return false;
+
+            if (jObject.ContainsKey("Text"))
+                text = jObject.Value<string>("Text");
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Query.FullTypeName(this));
+
+            if (text != null)
+                jObject.Add("Text", text);
+
+            return jObject;
+        }
+
+        public static bool operator ==(Expression expression, object @object)
+        {
+            if (ReferenceEquals(expression, null) || ReferenceEquals(expression.text, null))
+                return ReferenceEquals(@object, null) ? true : false;
+
+            Expression expression_Temp = @object as Expression;
+            if (expression_Temp == null)
+                return false;
+
+            return expression.text.Equals(expression_Temp.text);
+        }
+
+        public static bool operator !=(Expression expression, object @object)
+        {
+            return !(expression == @object);
+        }
+
+        public static implicit operator string(Expression expression) => expression?.text;
+
+        public static implicit operator Expression(string @string) => new Expression(@string);
+    }
+}
---
+++ SAM/SAM.Core/Classes/Expression/ExpressionVariable.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace SAM.Core
+{
+    public class ExpressionVariable : IJSAMObject
+    {
+        private string text;
+        
+        public ExpressionVariable(string text)
+        {
+            this.text = text;
+        }
+
+        public ExpressionVariable(ExpressionVariable expressionVariable)
+        {
+            text = expressionVariable?.text;
+        }
+
+        public ExpressionVariable(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+        }
+
+        public bool TryGetProperties(out string typeName, out string name)
+        {
+            typeName = null;
+            name = null;
+
+            if (text == null)
+                return false;
+
+            if (text.IndexOf('[') == -1)
+            {
+                name = text;
+                return true;
+            }
+
+            List<string> texts = Query.Texts(Text, '[', ']');
+            if(texts == null || texts.Count == 0)
+            {
+                name = text;
+                return true;
+            }
+
+            name = texts[0];
+
+            int length = text.IndexOf(texts[0]) - 1;
+            if (length < 1)
+                typeName = string.Empty;
+            else
+                typeName = text.Substring(0, length);
+
+            return true;
+        }
+
+        public string Name
+        {
+            get
+            {
+                if (!TryGetProperties(out string typeName, out string name))
+                    return null;
+
+                return name;
+            }
+        }
+
+        public string TypeName
+        {
+            get
+            {
+                if (!TryGetProperties(out string typeName, out string name))
+                    return null;
+
+                return typeName;
+            }
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if (jObject == null)
+                return false;
+
+            if (jObject.ContainsKey("Text"))
+                text = jObject.Value<string>("Text");
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Query.FullTypeName(this));
+
+            if (text != null)
+                jObject.Add("Text", text);
+
+            return jObject;
+        }
+
+        public static bool operator ==(ExpressionVariable expressionVariable, object @object)
+        {
+            if (ReferenceEquals(expressionVariable, null) || ReferenceEquals(expressionVariable.text, null))
+                return ReferenceEquals(@object, null) ? true : false;
+
+            ExpressionVariable expression_Temp = @object as ExpressionVariable;
+            if (expression_Temp == null)
+                return false;
+
+            return expressionVariable.text.Equals(expression_Temp.text);
+        }
+
+        public static bool operator !=(ExpressionVariable expressionVariable, object @object)
+        {
+            return !(expressionVariable == @object);
+        }
+
+        public static implicit operator string(ExpressionVariable expressionVariable) => expressionVariable?.text;
+
+        public static implicit operator ExpressionVariable(string @string) => new ExpressionVariable(@string);
+    }
+}
