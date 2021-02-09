d7b18f6fc35b49c4913472d65d029a2ea4d2eb1d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-09 08:03:40 +0000

    ZoneType Query Added

---
+++ SAM/SAM.Analytical/Query/ZoneType.cs
@@ -0,0 +1,17 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static ZoneType ZoneType(this Zone zone)
+        {
+            if (zone == null)
+                return Analytical.ZoneType.Undefined;
+
+
+            if (!zone.TryGetValue(ZoneParameter.ZoneCategory, out string category) || string.IsNullOrWhiteSpace(category))
+                return Analytical.ZoneType.Undefined;
+
+            return Core.Query.Enum<ZoneType>(category);
+        }
+    }
+}
