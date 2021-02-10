a2fa1f3055947e05e98493cf2ebbe928f5f777b1
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-10 08:55:42 +0000

    AddObject for Analytical Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalAddObject.cs
@@ -0,0 +1,134 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalAddObject : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("069a51b6-9db4-4bbd-8461-6709d12b162f");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalAddObject()
+          : base("SAMAnalytical.AddObject", "SAMAnalytical.AddObject",
+              "Add Object to AdjacencyCluster or AnalyticalModel",
+              "SAM", "Core")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_analytical", NickName = "_analytical", Description = "SAM Analytical AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_object", NickName = "_object", Description = "Object to be added", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "Analytical", NickName = "Analytical", Description = "SAM Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+            
+            SAMObject sAMObject = null;
+            index = Params.IndexOfInputParam("_analytical");
+            if (index == -1 || !dataAccess.GetData(index, ref sAMObject) || sAMObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            SAMObject sAMObject_Object = null;
+            index = Params.IndexOfInputParam("_object");
+            if(index != -1 || dataAccess.GetData(index, ref sAMObject_Object) || sAMObject_Object == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if (sAMObject is AnalyticalModel)
+            {
+                AnalyticalModel analyticalModel = new AnalyticalModel((AnalyticalModel)sAMObject);
+                if(sAMObject_Object is IMaterial)
+                {
+                    analyticalModel.AddMaterial((IMaterial)sAMObject_Object);
+                }
+                else if (sAMObject_Object is Profile)
+                {
+                    analyticalModel.AddProfile((Profile)sAMObject_Object);
+                }
+                else
+                {
+                    AdjacencyCluster adjacencyCluster = ((AnalyticalModel)sAMObject).AdjacencyCluster;
+                    if (adjacencyCluster.IsValid(sAMObject_Object))
+                        adjacencyCluster.AddObject(sAMObject_Object);
+
+                    analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster);
+                }
+
+                sAMObject = analyticalModel;
+            }
+            else if (sAMObject is AdjacencyCluster)
+            {
+                AdjacencyCluster adjacencyCluster = new AdjacencyCluster((AdjacencyCluster)sAMObject);
+                if (adjacencyCluster.IsValid(sAMObject_Object))
+                    adjacencyCluster.AddObject(sAMObject_Object);
+
+                sAMObject = adjacencyCluster;
+            }
+            else
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            index = Params.IndexOfOutputParam("Analytical");
+            if(index != -1)
+                dataAccess.SetData(index, sAMObject);
+        }
+    }
+}
