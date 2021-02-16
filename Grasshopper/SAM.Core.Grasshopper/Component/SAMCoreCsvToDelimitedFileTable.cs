45793033e3c45f4a5ca0b84a79b531b7cf6040da
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-16 16:26:10 +0000

    CsvToDelimitedFileTable Grasshopper Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper/Component/SAMCoreCsvToDelimitedFileTable.cs
@@ -0,0 +1,95 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper
+{
+    public class SAMCoreCsvToDelimitedFileTable : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("f22b038c-1b26-4371-980d-d894a4b81eea");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Get;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        private GH_OutputParamManager outputParamManager;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMCoreCsvToDelimitedFileTable()
+          : base("Csv.ToDelimitedFileTable", "Csv.ToDelimitedFileTable",
+              "Converts csv text or file to DelimitedFileTable",
+              "SAM", "Core")
+        {
+        }
+
+        
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "_csv", NickName = "_csv", Description = "file path to csv text", Access = GH_ParamAccess.item }, ParamVisibility.Binding));                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooDelimitedFileTableParam() { Name = "DelimitedFileTable", NickName = "DelimitedFileTable", Description = "SAM Core DelimitedFileTable", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            string csvOrPath = null;
+            index = Params.IndexOfInputParam("_csv");
+            if (index == -1 || !dataAccess.GetData(3, ref csvOrPath) || string.IsNullOrWhiteSpace(csvOrPath))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            DelimitedFileTable delimitedFileTable = null;
+            if (Core.Query.ValidFilePath(csvOrPath))
+            {
+                delimitedFileTable = new Core.DelimitedFileTable(new Core.DelimitedFileReader(Core.DelimitedFileType.Csv, csvOrPath));
+            }
+            else
+            {
+                string[] lines = csvOrPath.Split('\n');
+                delimitedFileTable = new DelimitedFileTable(new Core.DelimitedFileReader(Core.DelimitedFileType.Csv, lines));
+            }
+
+            index = Params.IndexOfOutputParam("DelimitedFileTable");
+            if (index != -1)
+                dataAccess.SetData(index, new GooDelimitedFileTable(delimitedFileTable));
+        }
+    }
+}
