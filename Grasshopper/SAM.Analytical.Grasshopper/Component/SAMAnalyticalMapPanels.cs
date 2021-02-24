29fc71a110ed5ef9a8ab9a40030800e18acdba17
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-24 14:01:54 +0000

    MapPanels Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalMapPanels.cs
@@ -0,0 +1,158 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalMapPanels : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("35570dc9-5267-4ec0-b340-7ced117ffa7b");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalMapPanels()
+          : base("SAMAnalytical.MapPanels", "SAMAnalytical.MapPanels",
+              "Map Panels for AnalyticalModel",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                global::Grasshopper.Kernel.Parameters.Param_GenericObject genericObject = new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_analytical", NickName = "_analytical", Description = "Analytical Object", Access = GH_ParamAccess.item };
+                result.Add(new GH_SAMParam(genericObject, ParamVisibility.Binding));
+
+
+                GooPanelParam gooPanelParam = new GooPanelParam() { Name = "_panels", NickName = "_panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list};
+                result.Add(new GH_SAMParam(gooPanelParam, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number number;
+
+                number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "minArea_", NickName = "minArea_", Description = "Minimal Area", Access = GH_ParamAccess.item};
+                number.SetPersistentData(Tolerance.MacroDistance);
+                result.Add(new GH_SAMParam(number, ParamVisibility.Voluntary));
+
+                number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "maxDistance_", NickName = "maxDistance_", Description = "Maximal Distance", Access = GH_ParamAccess.item };
+                number.SetPersistentData(0.01);
+                result.Add(new GH_SAMParam(number, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "Analytical", NickName = "Analytical", Description = "SAM Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "Panels", NickName = "Panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_panels");
+            List<Panel> panels = new List<Panel>();
+            if (index == -1 || !dataAccess.GetDataList(index, panels) || panels == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("minArea_");
+            double minArea = Tolerance.MacroDistance;
+            if(index != -1)
+            {
+                double value = double.NaN;
+                if (dataAccess.GetData(index, ref value) && !double.IsNaN(value))
+                    minArea = value;
+            }
+
+            index = Params.IndexOfInputParam("maxDistance_");
+            double maxDistance = 0.01;
+            if (index != -1)
+            {
+                double value = double.NaN;
+                if (dataAccess.GetData(index, ref value) && !double.IsNaN(value))
+                    maxDistance = value;
+            }
+
+            index = Params.IndexOfInputParam("_analytical");
+            SAMObject sAMObject= null;
+            if (index == -1 || !dataAccess.GetData(index, ref sAMObject) || sAMObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            AdjacencyCluster adjacencyCluster = null;
+            if(sAMObject is AdjacencyCluster)
+            {
+                adjacencyCluster = new AdjacencyCluster((AdjacencyCluster)sAMObject);
+            }
+            else if(sAMObject is AnalyticalModel)
+            {
+                adjacencyCluster = ((AnalyticalModel)sAMObject).AdjacencyCluster;
+            }
+
+            if(adjacencyCluster != null)
+            {
+                List<Panel> panels_Result = adjacencyCluster.MapPanels(panels, minArea, maxDistance);
+
+                if (sAMObject is AdjacencyCluster)
+                    sAMObject = adjacencyCluster;
+                else if (sAMObject is AnalyticalModel)
+                    sAMObject = new AnalyticalModel((AnalyticalModel)sAMObject, adjacencyCluster);
+
+                index = Params.IndexOfOutputParam("Panels");
+                if (index != -1)
+                    dataAccess.SetDataList(index, panels_Result?.ToList().ConvertAll(x => new GooPanel(x)));
+            }
+
+            index = Params.IndexOfOutputParam("Analytical");
+            if (index != -1)
+                dataAccess.SetData(index, sAMObject);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/MapPanels.cs
@@ -0,0 +1,66 @@
+﻿using SAM.Core;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Panel> MapPanels(this AdjacencyCluster adjacencyCluster, IEnumerable<Panel> panels, double minArea = Tolerance.MacroDistance, double maxDistance = Tolerance.MacroDistance)
+        {
+            if (adjacencyCluster == null || panels == null)
+                return null;
+
+            List<Panel> panels_AdjacencyCluster = adjacencyCluster.GetPanels();
+            if (panels_AdjacencyCluster == null)
+                return null;
+
+            List<Panel> result = new List<Panel>();
+
+            if (panels.Count() == 0 || panels_AdjacencyCluster.Count == 0)
+                return result;
+
+            List<Tuple<Point3D, Panel>> tuples = new List<Tuple<Point3D, Panel>>();
+            foreach(Panel panel_AdjacencyCluster in panels_AdjacencyCluster)
+            {
+                Point3D point3D = panel_AdjacencyCluster?.GetInternalPoint3D();
+                if (point3D == null)
+                    continue;
+
+                tuples.Add(new Tuple<Point3D, Panel>(point3D, panel_AdjacencyCluster));
+            }
+
+            foreach(Panel panel in panels)
+            {
+                Face3D face3D = panel?.GetFace3D();
+                if (face3D == null)
+                    continue;
+
+                List<Panel> panels_Panel = new List<Panel>();
+                foreach(Tuple<Point3D, Panel> tuple in tuples)
+                {
+                    double distance = face3D.Distance(tuple.Item1);
+                    if (distance <= maxDistance)
+                        panels_Panel.Add(tuple.Item2);
+                }
+
+                if (panels_Panel == null || panels_Panel.Count == 0)
+                    continue;
+
+                for(int i =0; i < panels_Panel.Count; i++)
+                {
+                    Panel panel_Panel = panels_Panel[i];
+
+                    panel_Panel = new Panel(panel_Panel.Guid, panel, panel_Panel.GetFace3D(), null, true, minArea, maxDistance);
+                    adjacencyCluster.AddObject(panel_Panel);
+                    result.Add(panel_Panel);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
