0381e46bc3012931d3e29b40e45fb2d9752a1cce
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-24 13:06:23 +0000

    GetShellsByPoint3D Component Added for Grasshopper

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Component/GeometryGetShellsByPoint3D.cs
@@ -0,0 +1,160 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper.Properties;
+using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public class GeometryGetShellsByPoint3D : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("5425d027-18d3-4694-ac5c-c995f8302231");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Geometry;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public GeometryGetShellsByPoint3D()
+          : base("Geometry.GetShellsByPoint3D", "Geometry.GetShellsByPoint3D",
+              "Gets Shell By Point3D ",
+              "SAM", "Geometry")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                global::Grasshopper.Kernel.Parameters.Param_GenericObject gerenricObject;
+
+                gerenricObject = new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_shells", NickName = "_shells", Description = "SAM Geometry Shells", Access = GH_ParamAccess.list };
+                gerenricObject.DataMapping = GH_DataMapping.Flatten;
+                result.Add(new GH_SAMParam(gerenricObject, ParamVisibility.Binding));
+
+                gerenricObject = new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_point", NickName = "_Point", Description = "SAM Geometry Point2D", Access = GH_ParamAccess.item };
+                result.Add(new GH_SAMParam(gerenricObject, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number number;
+
+                number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "silverSpacing_", NickName = "silverSpacing_", Description = "Silver Spacing", Access = GH_ParamAccess.item };
+                number.SetPersistentData(Core.Tolerance.MacroDistance);
+                result.Add(new GH_SAMParam(number, ParamVisibility.Voluntary));
+
+                number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "tolerance_", NickName = "tolerance_", Description = "Tolerance", Access = GH_ParamAccess.item };
+                number.SetPersistentData(Core.Tolerance.Distance);
+                result.Add(new GH_SAMParam(number, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooSAMGeometryParam() { Name = "Shells", NickName = "Shells", Description = "SAM Geometry Shells", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("_geometries");
+            List<GH_ObjectWrapper> objectWrappers = new List<GH_ObjectWrapper>();
+            if (index == -1 || !dataAccess.GetDataList(index, objectWrappers) || objectWrappers == null || objectWrappers.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Shell> shells = new List<Shell>();
+            foreach (GH_ObjectWrapper objectWrapper_Temp in objectWrappers)
+            {
+                if (Query.TryGetSAMGeometries(objectWrapper_Temp, out List<Shell> shells_Temp) && shells_Temp != null && shells_Temp.Count > 0)
+                    shells.AddRange(shells_Temp);
+            }
+
+            if (shells.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_point");
+            GH_ObjectWrapper objectWrapper = null;
+            if (index == -1 || !dataAccess.GetData(index, ref objectWrapper) || objectWrapper == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Point3D point3D = null;
+            if (Query.TryGetSAMGeometries(objectWrapper, out List<Point3D> point3Ds) && point3Ds != null && point3Ds.Count > 0)
+                point3D = point3Ds.FirstOrDefault();
+
+            if (point3D is null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("tolerance_");
+            double tolerance = Core.Tolerance.Distance;
+            if (index != -1)
+            {
+                double tolerance_Temp = double.NaN;
+                if (!dataAccess.GetData(index, ref tolerance_Temp) && !double.IsNaN(tolerance_Temp))
+                    tolerance = tolerance_Temp;
+            }
+
+            index = Params.IndexOfInputParam("silverSpacing_");
+            double silverSpacing = Core.Tolerance.MacroDistance;
+            if (index != -1)
+            {
+                double silverSpacing_Temp = double.NaN;
+                if (!dataAccess.GetData(index, ref silverSpacing_Temp) && !double.IsNaN(silverSpacing_Temp))
+                    silverSpacing = silverSpacing_Temp;
+            }
+
+            List<Shell> result = shells.FindAll(x => x.On(point3D, tolerance) || x.Inside(point3D, silverSpacing, tolerance));
+
+            index = Params.IndexOfInputParam("Shells");
+            if (index != -1)
+                dataAccess.SetDataList(index, result.ConvertAll(x => new GooSAMGeometry(x)));
+        }
+    }
+}
---
+++ Grasshopper/SAM.Geometry.Grasshopper/Query/TryGetSAMGeometry.cs
@@ -0,0 +1,58 @@
+﻿using Grasshopper.Kernel.Types;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public static partial class Query
+    {
+        public static bool TryGetSAMGeometries<T>(this GH_ObjectWrapper objectWrapper, out List<T> sAMGeometries) where T : SAMGeometry
+        {
+            sAMGeometries = null;
+
+            if (objectWrapper == null || objectWrapper.Value == null)
+                return false;
+
+            if (objectWrapper.Value is T)
+            {
+                sAMGeometries = new List<T>() { (T)objectWrapper.Value };
+                return true;
+            }
+
+
+            if (objectWrapper.Value is GooSAMGeometry)
+            {
+                ISAMGeometry sAMGeometry = ((GooSAMGeometry)objectWrapper.Value).Value;
+                if(sAMGeometry is T)
+                {
+                    sAMGeometries = new List<T>() { (T)sAMGeometry };
+                    return true;
+                }
+            }
+
+
+            if (objectWrapper.Value is IGH_GeometricGoo)
+            {
+                object @object = Convert.ToSAM(objectWrapper.Value as dynamic);
+                if (@object is IEnumerable)
+                {
+                    IEnumerable<ISAMGeometry> sAMGeometries_Temp = ((IEnumerable)@object).Cast<ISAMGeometry>();
+                    if(sAMGeometries_Temp != null && sAMGeometries_Temp.Count() > 0)
+                    {
+                        sAMGeometries = sAMGeometries_Temp.ToList().FindAll(x => x is T).Cast<T>().ToList();
+                        return sAMGeometries.Count > 0;
+                    }
+
+                }
+                else if(@object is T)
+                {
+                    sAMGeometries = new List<T>() { (T)@object };
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
