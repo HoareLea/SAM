2ffb1fcc013baa03713419dc56fed679358b113a
Michal Dengusiak <tassimulation@gmail.com>
2021-03-08 12:54:20 +0000

    Update Face2Ds.cs

--- SAM/SAM.Geometry/Convert/ToSAM/Face2Ds.cs
+++ SAM/SAM.Geometry/Convert/ToSAM/Face2Ds.cs
@@ -11,14 +11,29 @@ namespace SAM.Geometry
             if (multiPolygon == null || multiPolygon.IsEmpty)
                 return null;
 
-            List<Face2D> result = new List<Face2D>();
-            foreach (Polygon polygon in multiPolygon)
-            {
-                Face2D face2D = polygon?.ToSAM(tolerance);
-                if (face2D == null)
-                    continue;
+            IEnumerable<NetTopologySuite.Geometries.Geometry> geometries = multiPolygon.Geometries;
+            if (geometries == null)
+                return null;
 
-                result.Add(face2D);
+            List<Face2D> result = new List<Face2D>();
+            foreach (NetTopologySuite.Geometries.Geometry geometry in multiPolygon.Geometries)
+            {
+                if(geometry is Polygon)
+                {
+                    Face2D face2D = ((Polygon)geometry).ToSAM(tolerance);
+                    if (face2D == null)
+                        continue;
+
+                    result.Add(face2D);
+                }
+                else if(geometry is MultiPolygon)
+                {
+                    List<Face2D> face2Ds = ((MultiPolygon)geometry).ToSAM(tolerance);
+                    if (face2Ds == null)
+                        continue;
+
+                    result.AddRange(face2Ds);
+                }
             }
 
             return result;
