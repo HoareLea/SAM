7a47a3d3a428d91913dcdcb680ef72a14b7e1f1b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-03-18 14:26:17 +0000

    AlignPanelsByElevation Component Added

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalAlignPanelsByElevation.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalAlignPanelsByElevation.cs
@@ -7,12 +7,12 @@ using System.Collections.Generic;
 
 namespace SAM.Analytical.Grasshopper
 {
-    public class SAMAnalyticalAlignPanels : GH_SAMVariableOutputParameterComponent
+    public class SAMAnalyticalAlignPanelsByElevation : GH_SAMVariableOutputParameterComponent
     {
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
-        public override Guid ComponentGuid => new Guid("68f43a0e-abb0-40ec-9032-a2195366ceb1");
+        public override Guid ComponentGuid => new Guid("6cdf46cb-5e46-407d-847d-0e12a9b2545a");
 
         /// <summary>
         /// The latest version of this component
@@ -29,9 +29,9 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// Initializes a new instance of the SAM_point3D class.
         /// </summary>
-        public SAMAnalyticalAlignPanels()
-          : base("SAMAnalytical.AlignPanels", "SAMAnalytical.AlignPanels",
-              "Align Panels",
+        public SAMAnalyticalAlignPanelsByElevation()
+          : base("SAMAnalytical.AlignPanelsByElevation", "SAMAnalytical.AlignPanelsByElevation",
+              "Align Panels By Upper and Lower Elevation",
               "SAM", "Analytical")
         {
         }
@@ -51,14 +51,10 @@ namespace SAM.Analytical.Grasshopper
 
                 global::Grasshopper.Kernel.Parameters.Param_Number paramNumber;
 
-                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_elevation", NickName = "elevation", Description = "Elevation", Access = GH_ParamAccess.item };
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_upperElevation", NickName = "_upperElevation", Description = "Upper Elevation", Access = GH_ParamAccess.item };
                 result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
 
-                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_referenceElevation", NickName = "_referenceElevation", Description = "Reference Elevation", Access = GH_ParamAccess.item };
-                result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
-
-                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "maxDistance_", NickName = "maxDistance_", Description = "Max Distance", Access = GH_ParamAccess.item };
-                paramNumber.SetPersistentData(0.2);
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_lowerElevation", NickName = "_lowerElevation", Description = "Lower Elevation", Access = GH_ParamAccess.item };
                 result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
 
                 paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "tolerance_", NickName = "tolerance_", Description = "Tolerance", Access = GH_ParamAccess.item };
@@ -78,6 +74,8 @@ namespace SAM.Analytical.Grasshopper
             {
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
                 result.Add(new GH_SAMParam(new GooPanelParam() { Name = "Panels", NickName = "Panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "UpperPanels", NickName = "UpperPanels", Description = "SAM Analytical Upper Panels", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "LowerPanels", NickName = "LowerPanels", Description = "SAM Analytical Lower Panels", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
                 return result.ToArray();
             }
         }
@@ -94,29 +92,21 @@ namespace SAM.Analytical.Grasshopper
                 return;
             }
             
-            index = Params.IndexOfInputParam("_elevation");
-            double elevation = double.NaN;
-            if(index == -1 || !dataAccess.GetData(index, ref elevation) || double.IsNaN(elevation))
+            index = Params.IndexOfInputParam("_upperElevation");
+            double upperElevation = double.NaN;
+            if(index == -1 || !dataAccess.GetData(index, ref upperElevation) || double.IsNaN(upperElevation))
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Invalid Data");
                 return;
             }
 
-            index = Params.IndexOfInputParam("_referenceElevation");
-            double referenceElevation = double.NaN;
-            if (index == -1 || !dataAccess.GetData(index, ref referenceElevation) || double.IsNaN(referenceElevation))
+            index = Params.IndexOfInputParam("_lowerElevation");
+            double lowerElevation = double.NaN;
+            if (index == -1 || !dataAccess.GetData(index, ref lowerElevation) || double.IsNaN(lowerElevation))
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Invalid Data");
                 return;
             }
-
-            index = Params.IndexOfInputParam("maxDistance_");
-            double maxDistance = 0.2;
-            if (index != -1)
-                dataAccess.GetData(index, ref maxDistance);
-
-            if (double.IsNaN(maxDistance))
-                maxDistance = 0.2;
 
             index = Params.IndexOfInputParam("tolerance_");
             double tolerance = Tolerance.Distance;
@@ -126,9 +116,43 @@ namespace SAM.Analytical.Grasshopper
             if (double.IsNaN(tolerance))
                 tolerance = Tolerance.Distance;
 
-            Analytical.Modify.Align(panels, elevation, referenceElevation, maxDistance, SAM.Core.Tolerance.Angle, tolerance);
+            Geometry.Spatial.Plane plane_Upper = Geometry.Spatial.Plane.WorldXY.GetMoved(new Geometry.Spatial.Vector3D(0, 0, upperElevation)) as Geometry.Spatial.Plane;
+            Geometry.Spatial.Plane plane_Lower = Geometry.Spatial.Plane.WorldXY.GetMoved(new Geometry.Spatial.Vector3D(0, 0, lowerElevation)) as Geometry.Spatial.Plane;
 
-            dataAccess.SetDataList(0, panels?.ConvertAll(x => new GooPanel(x)));
+            List<Panel> panels_Result = new List<Panel>();
+            List<Panel> panels_Upper = new List<Panel>();
+            List<Panel> panels_Lower = new List<Panel>();
+            for (int i=0; i < panels.Count;i++)
+            {
+                if(panels[i] == null)
+                    continue;
+
+                Panel panel_Temp = new Panel(panels[i]);
+
+                panel_Temp = Analytical.Modify.Align(panel_Temp, plane_Upper, Tolerance.Angle, tolerance);
+                panel_Temp = Analytical.Modify.Align(panel_Temp, plane_Lower, Tolerance.Angle, tolerance);
+
+                List<Panel> panels_Temp = Analytical.Query.Cut(panel_Temp, new Geometry.Spatial.Plane[] { plane_Upper, plane_Lower });
+                if (panels_Temp == null || panels_Temp.Count == 0)
+                    continue;
+
+                foreach(Panel panel_Result in panels_Temp)
+                {
+                    Geometry.Spatial.Point3D point3D = panel_Result.GetInternalPoint3D();
+                    if (Geometry.Spatial.Query.Above(plane_Upper, point3D))
+                        panels_Upper.Add(panel_Result);
+                    else if (Geometry.Spatial.Query.Below(plane_Lower, point3D))
+                        panels_Lower.Add(panel_Result);
+                    else
+                        panels_Result.Add(panel_Result);
+                }
+            }
+
+           
+
+            dataAccess.SetDataList(0, panels_Result?.ConvertAll(x => new GooPanel(x)));
+            dataAccess.SetDataList(1, panels_Upper?.ConvertAll(x => new GooPanel(x)));
+            dataAccess.SetDataList(2, panels_Lower?.ConvertAll(x => new GooPanel(x)));
         }
     }
 }
