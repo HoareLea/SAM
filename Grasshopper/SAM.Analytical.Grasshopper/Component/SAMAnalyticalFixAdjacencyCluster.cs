d459513d7466c5ced539ea7cae0d3a771a96b0e7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-03-23 15:01:33 +0000

    Fix AdjacencyCluster Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalFixAdjacencyCluster.cs
@@ -0,0 +1,87 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalFixAdjacencyCluster : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("4b43d355-6ca0-40de-b0df-9da29bede2a7");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalFixAdjacencyCluster()
+          : base("SAMAnalytical.FixAdjacencyCluster", "SAMAnalytical.FixAdjacencyCluster",
+              "Fix AdjacencyCluster",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                GooAdjacencyClusterParam adjacencyClusterParam = new GooAdjacencyClusterParam() { Name = "_adjacencyCluster", NickName = "_adjacencyCluster", Description = "SAM Analytical AdjacencyCluster", Access = GH_ParamAccess.item };
+                adjacencyClusterParam.DataMapping = GH_DataMapping.Flatten;
+                result.Add(new GH_SAMParam(adjacencyClusterParam, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAdjacencyClusterParam() { Name = "AdjacencyCluster", NickName = "AdjacencyCluster", Description = "SAM Analytical AdjacencyCluster", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("_adjacencyCluster");
+            AdjacencyCluster adjacencyCluster = null;
+            if (index == -1 || !dataAccess.GetData(index, ref adjacencyCluster) || adjacencyCluster == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Invalid Data");
+                return;
+            }
+
+            adjacencyCluster = new AdjacencyCluster(adjacencyCluster);
+            adjacencyCluster.FixAdjacencyCluster();
+
+            dataAccess.SetData(0, new GooAdjacencyCluster(adjacencyCluster));
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/FixAdjacencyCluster.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Panel> FixAdjacencyCluster(this AdjacencyCluster adjacencyCluster)
+        {
+            if (adjacencyCluster == null)
+                return null;
+
+            List<Construction> constructions = adjacencyCluster.GetConstructions();
+            if (constructions == null || constructions.Count == 0)
+                return null;
+
+            Dictionary<string, Construction> dictionary = new Dictionary<string, Construction>();
+            List<Panel> result = new List<Panel>();
+            foreach(Construction construction in constructions)
+            {
+                string name = construction.Name;
+                if (string.IsNullOrWhiteSpace(name))
+                    continue;
+
+                List<Panel> panels = adjacencyCluster.GetPanels(construction);
+                panels?.RemoveAll(x => x.PanelGroup != PanelGroup.Wall);
+                if (panels == null || panels.Count == 0)
+                    continue;
+
+                List<bool> externals = panels.ConvertAll(x => adjacencyCluster.External(x));
+
+                IEnumerable<bool> externals_Unique = externals.Distinct();
+                if (externals_Unique == null || externals_Unique.Count() <= 1)
+                    continue;
+
+                for (int i = 0; i < panels.Count; i++)
+                {
+                    Panel panel = panels[i];
+                    bool external = externals[i];
+
+                    PanelType panelType = panel.PanelType;
+                    if (external == panelType.External())
+                        continue;
+
+                    string name_New = null;
+                    if(external)
+                    {
+                        if (name.StartsWith("SIM_INT_GLZ"))
+                            name_New = "SIM_EXT_GLZ" + name.Substring(("SIM_INT_GLZ").Length);
+                        else if (name.StartsWith("SIM_INT_SLD_Core"))
+                            name_New = "SIM_EXT_GRD" + name.Substring(("SIM_INT_SLD_Core").Length);
+                        else if (name.StartsWith("SIM_INT_SLD_Core"))
+                            name_New = "SIM_EXT_SLD" + name.Substring(("SIM_INT_SLD_Core").Length);
+
+                    }
+                    else
+                    {
+                        if (name.StartsWith("SIM_EXT_GLZ"))
+                            name_New = "SIM_INT_GLZ" + name.Substring(("SIM_EXT_GLZ").Length);
+                        else if (name.StartsWith("SIM_EXT_GRD"))
+                            name_New = "SIM_INT_SLD_Core" + name.Substring(("SIM_EXT_GRD").Length);
+                        else if (name.StartsWith("SIM_EXT_SLD"))
+                            name_New = "SIM_INT_SLD_Core" + name.Substring(("SIM_EXT_SLD").Length);
+                    }
+
+                    if (string.IsNullOrWhiteSpace(name_New))
+                        continue;
+
+                    if (!dictionary.TryGetValue(name_New, out Construction construction_New) || construction_New == null)
+                        construction_New = new Construction(construction, name_New);
+
+                    Panel panel_New = new Panel(panel, construction_New);
+                    if (external)
+                        panel_New = new Panel(panel_New, PanelType.WallExternal);
+                    else
+                        panel_New = new Panel(panel_New, PanelType.WallInternal);
+
+                    result.Add(panel_New);
+                }
+            }
+
+            if (result != null)
+                result.ForEach(x => adjacencyCluster.AddObject(x));
+
+            return result;
+        }
+    }
+}
