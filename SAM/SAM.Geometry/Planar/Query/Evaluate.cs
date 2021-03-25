7f2bd094140b208fd203f1434f3efe0c5f5ea2e0
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-03-25 08:26:00 +0000

    Evaluate Query Added for PolynomialEquation

---
+++ SAM/SAM.Geometry/Planar/Query/Evaluate.cs
@@ -0,0 +1,19 @@
+﻿using SAM.Math;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Query
+    {
+        public static Point2D Evaluate(this PolynomialEquation polynominalEquation, double x)
+        {
+            if (polynominalEquation == null || double.IsNaN(x))
+                return null;
+
+            double y = polynominalEquation.Evaluate(x);
+            if (double.IsNaN(y))
+                return null;
+
+            return new Point2D(x, y);
+        }
+   }
+}
