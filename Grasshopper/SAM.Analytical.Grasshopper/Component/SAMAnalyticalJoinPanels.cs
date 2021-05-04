ed5da5ac1fcbc1e5cd759e0dad11de105af2b305
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-05-04 15:32:05 +0100

    Join Method for Panels Updated

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalJoinPanels.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalJoinPanels.cs
@@ -1,17 +1,14 @@
 ﻿using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
 using SAM.Analytical.Grasshopper.Properties;
 using SAM.Core;
 using SAM.Core.Grasshopper;
-using SAM.Geometry.Grasshopper;
-using SAM.Geometry.Spatial;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SAM.Analytical.Grasshopper
 {
-    public class SAMAnalyticalJoinPanels : GH_SAMComponent
+    public class SAMAnalyticalJoinPanels : GH_SAMVariableOutputParameterComponent
     {
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
@@ -21,7 +18,7 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.0";
+        public override string LatestComponentVersion => "1.0.1";
 
         /// <summary>
         /// Provides an Icon for the component.
@@ -41,21 +38,47 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
-        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        protected override GH_SAMParam[] Inputs
         {
-            inputParamManager.AddParameter(new GooPanelParam(), "_panels", "_panels", "SAM Analytical Panels", GH_ParamAccess.list);
-            inputParamManager.AddNumberParameter("_maxDistance_", "_maxDistance_", "Maximum Distance to Adjust Panels", GH_ParamAccess.item, 0.52);
-            inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                GooPanelParam panelParam = new GooPanelParam() { Name = "_panels", NickName = "_panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list };
+                panelParam.DataMapping = GH_DataMapping.Flatten;
+                result.Add(new GH_SAMParam(panelParam, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number paramNumber;
+
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_elevation", NickName = "elevation", Description = "Elevation", Access = GH_ParamAccess.item };
+                result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
+
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "distance_", NickName = "distance_", Description = "Distance", Access = GH_ParamAccess.item };
+                paramNumber.SetPersistentData(0.2);
+                result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
+
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "tolerance_", NickName = "tolerance_", Description = "Tolerance", Access = GH_ParamAccess.item };
+                paramNumber.SetPersistentData(Tolerance.Distance);
+                result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
         }
 
         /// <summary>
         /// Registers all the output parameters for this component.
         /// </summary>
-        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        protected override GH_SAMParam[] Outputs
         {
-            outputParamManager.AddParameter(new GooPanelParam(), "Panels", "Panels", "SAM Analytical Panels", GH_ParamAccess.list);
-            outputParamManager.AddParameter(new GooPanelParam(), "Changed", "Changed", "Changed SAM Analytical Panels", GH_ParamAccess.list);
-            outputParamManager.AddParameter(new GooPanelParam(), "Removed", "Removed", "Removed SAM Analytical Panels", GH_ParamAccess.list);
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "panels", NickName = "panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "extendedPanels", NickName = "extendedPanels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "trimmedPanels", NickName = "trimmedPanels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "segment3Ds", NickName = "segment3Ds", Description = "SAM Geometry Segment3Ds", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                return result.ToArray();
+            }
         }
 
         /// <summary>
@@ -66,51 +89,57 @@ namespace SAM.Analytical.Grasshopper
         /// </param>
         protected override void SolveInstance(IGH_DataAccess dataAccess)
         {
-            bool run = false;
-            if (!dataAccess.GetData(2, ref run))
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
-                return;
-            }
+            int index = -1;
 
-            if (!run)
-                return;
-
+            index = Params.IndexOfInputParam("_panels");
             List<Panel> panels = new List<Panel>();
-            if (!dataAccess.GetDataList(0, panels))
+            if (index == -1 || !dataAccess.GetDataList(index, panels) || panels == null)
             {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Invalid Data");
                 return;
             }
 
-            double maxDistance = double.NaN;
-            if (!dataAccess.GetData(1, ref maxDistance))
-                return;
-
-            List<Panel> panels_Result = panels.ConvertAll(x => new Panel(x));
-
-            List<Guid> guids = panels_Result.Join(maxDistance);
-            if (guids == null)
-                return;
-
-            List<Panel> panels_Removed = new List<Panel>();
-            List<Panel> panels_Changed = new List<Panel>();
-
-            foreach(Guid guid in guids)
+            index = Params.IndexOfInputParam("_elevation");
+            double elevation = double.NaN;
+            if (index == -1 || !dataAccess.GetData(index, ref elevation) || double.IsNaN(elevation))
             {
-                Panel panel = panels_Result.Find(x => x.Guid == guid);
-                if (panel == null)
-                {
-                    panels_Removed.Add(new Panel(panels.Find(x => x.Guid == guid)));
-                    continue;
-                }
-
-                panels_Changed.Add(panel);
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Invalid Data");
+                return;
             }
 
-            dataAccess.SetDataList(0, panels_Result.ConvertAll(x => new GooPanel(x)));
-            dataAccess.SetDataList(1, panels_Changed.ConvertAll(x => new GooPanel(x)));
-            dataAccess.SetDataList(2, panels_Removed.ConvertAll(x => new GooPanel(x)));
+            index = Params.IndexOfInputParam("distance_");
+            double distance = 0.2;
+            if (index != -1)
+                dataAccess.GetData(index, ref distance);
+
+            if (double.IsNaN(distance))
+                distance = 0.2;
+
+            index = Params.IndexOfInputParam("tolerance_");
+            double tolerance = Tolerance.Distance;
+            if (index != -1)
+                dataAccess.GetData(index, ref tolerance);
+
+            if (double.IsNaN(tolerance))
+                tolerance = Tolerance.Distance;
+
+            Analytical.Modify.Join(panels, elevation, distance, out List<Panel> panels_Extended, out List<Panel> panels_Trimmed, out List<Geometry.Spatial.Segment3D> segment3Ds, Tolerance.MacroDistance, Tolerance.Angle, tolerance);
+
+            index = Params.IndexOfOutputParam("panels");
+            if (index != -1)
+                dataAccess.SetDataList(index, panels?.ConvertAll(x => new GooPanel(x)));
+
+            index = Params.IndexOfOutputParam("extendedPanels");
+            if (index != -1)
+                dataAccess.SetDataList(index, panels_Extended?.ConvertAll(x => new GooPanel(x)));
+
+            index = Params.IndexOfOutputParam("trimmedPanels");
+            if (index != -1)
+                dataAccess.SetDataList(index, panels_Trimmed?.ConvertAll(x => new GooPanel(x)));
+
+            index = Params.IndexOfOutputParam("segment3Ds");
+            if (index != -1)
+                dataAccess.SetDataList(index, segment3Ds);
         }
     }
 }
