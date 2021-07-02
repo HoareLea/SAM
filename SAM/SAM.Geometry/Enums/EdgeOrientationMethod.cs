a21a5b7ae58fe156563b4e05fc1c1f2ed3d54587
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-02 15:23:00 +0100

    EdgeOrientationMethod Enum Added

---
+++ SAM/SAM.Geometry/Enums/EdgeOrientationMethod.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Geometry
+{
+    [Description("Edge Orientation Method")]
+    public enum EdgeOrientationMethod
+    {
+        [Description("Undefined")] Undefined,
+        [Description("Similar")] Similar,
+        [Description("Opposite")] Opposite,
+    }
+}
