8f29659c10fb1366a0f9644a1541b92bc4dafd73
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-05 15:53:56 +0100

    MergeOverlapApertures Grasshopper Component Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalMergeOverlapApertures.cs
@@ -0,0 +1,146 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalMergeOverlapApertures : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("976cc056-f7aa-4384-a479-d92da090d8a3");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalMergeOverlapApertures()
+          : base("SAMAnalytical.MergeOverlapApertures", "SAMAnalytical.MergeOverlapApertures",
+              "Merge Overlap SAM Analytical Apertures",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            int index;
+
+            index = inputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "_analyticalObject", "_analyticalObject", "SAM Analytical Object such as AdjacencyCluster, Panel or AnalyticalModel", GH_ParamAccess.list);
+            inputParamManager[index].DataMapping = GH_DataMapping.Flatten;
+
+            inputParamManager.AddBooleanParameter("_validateConstruction_", "_validateConstruction_", "Validate Construction - Merge Only Apertures with the Same ApertureConstruction", GH_ParamAccess.item, false);
+            inputParamManager.AddNumberParameter("_minArea_", "_minArea_", "Minimal Area for Aperture", GH_ParamAccess.item, Tolerance.MacroDistance);
+            inputParamManager.AddNumberParameter("_tolerance_", "_tolerance_", "Tolerance", GH_ParamAccess.item, Tolerance.Distance);
+            inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "AnalyticalObject", "AnalyticalObject", "SAM Analytical Object", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(4, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            if (!run)
+                return;
+
+            bool validateConstruction = false;
+            if (!dataAccess.GetData(1, ref validateConstruction))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double minArea = Tolerance.MacroDistance;
+            if (!dataAccess.GetData(2, ref minArea))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double tolerance = Tolerance.Distance;
+            if (!dataAccess.GetData(3, ref tolerance))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<SAMObject> sAMObjects = new List<SAMObject>();
+            if (!dataAccess.GetDataList(0, sAMObjects) || sAMObjects == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Panel> panels = sAMObjects.ConvertAll(x => x as Panel);
+            panels.RemoveAll(x => x == null);
+
+            List<AdjacencyCluster> adjacencyClusters = sAMObjects.ConvertAll(x => x as AdjacencyCluster);
+            adjacencyClusters.RemoveAll(x => x == null);
+
+            List<AnalyticalModel> analyticalModels = sAMObjects.ConvertAll(x => x as AnalyticalModel);
+            analyticalModels.RemoveAll(x => x == null);
+
+            panels = Analytical.Query.MergeOverlapApertures(panels, validateConstruction, minArea, tolerance);
+
+            if (panels != null)
+            {
+                foreach (Panel panel in panels)
+                {
+                    if (panel == null)
+                        continue;
+
+                    if (panel.GetArea() < Tolerance.MacroDistance)
+                        AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, string.Format("Area of panel {0} [Guid: {1}] is below {2}", panel.Name, panel.Guid, Tolerance.MacroDistance));
+                }
+            }
+
+            adjacencyClusters = adjacencyClusters.ConvertAll(x => Analytical.Query.MergeOverlapApertures(x, validateConstruction, minArea, tolerance));
+            analyticalModels = analyticalModels.ConvertAll(x => Analytical.Query.MergeOverlapApertures(x, validateConstruction, minArea, tolerance));
+
+            List<SAMObject> result = new List<SAMObject>();
+            if (panels != null)
+                result.AddRange(panels.Cast<SAMObject>());
+
+            if (adjacencyClusters != null)
+                result.AddRange(adjacencyClusters.Cast<SAMObject>());
+
+            if (analyticalModels != null)
+                result.AddRange(analyticalModels.Cast<SAMObject>());
+
+            dataAccess.SetDataList(0, result);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Query/MergeCoplanarApertures.cs
@@ -0,0 +1,210 @@
+﻿using SAM.Geometry.Spatial;
+using SAM.Geometry.Planar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static List<Panel> MergeOverlapApertures(this IEnumerable<Panel> panels, bool validateConstruction = true, double minArea = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            if(panels == null)
+            {
+                return null;
+            }
+
+            return panels.ToList().ConvertAll(x => MergeOverlapApertures(x, validateConstruction, minArea, tolerance));
+        }
+
+        public static AdjacencyCluster MergeOverlapApertures(this AdjacencyCluster adjacencyCluster, bool validateConstruction = true, double minArea = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            List<Panel> panels = adjacencyCluster?.GetPanels();
+            if (panels == null)
+                return null;
+
+            panels = MergeOverlapApertures(panels, validateConstruction, minArea, tolerance);
+
+            AdjacencyCluster result = new AdjacencyCluster(adjacencyCluster);
+
+            if (panels != null && panels.Count != 0)
+            {
+                panels.ForEach(x => result.AddObject(x));
+            }
+
+            return result;
+        }
+
+        public static AnalyticalModel MergeOverlapApertures(this AnalyticalModel analyticalModel, bool validateConstruction = true, double minArea = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            AdjacencyCluster adjacencyCluster = analyticalModel?.AdjacencyCluster;
+            if (adjacencyCluster == null)
+                return null;
+
+            adjacencyCluster = MergeOverlapApertures(adjacencyCluster, validateConstruction, minArea, tolerance);
+
+            return new AnalyticalModel(analyticalModel, adjacencyCluster);
+        }
+
+        public static Panel MergeOverlapApertures(this Panel panel, bool validateConstruction = true, double minArea = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            if (panel == null)
+                return null;
+
+            Panel panel_New = new Panel(panel);
+
+            List<Aperture> apertures = panel_New.Apertures;
+            if (apertures == null || apertures.Count < 2)
+            {
+                return panel_New;
+            }
+
+            List<List<Aperture>> aperturesList = new List<List<Aperture>>();
+            if(validateConstruction)
+            {
+                Dictionary<string, List<Aperture>> dictionary = new Dictionary<string, List<Aperture>>();
+                foreach(Aperture aperture in apertures)
+                {
+                    ApertureConstruction apertureConstruction = aperture.ApertureConstruction;
+                    string name = apertureConstruction?.Name;
+                    if (name == null)
+                        name = string.Empty;
+
+                    if(!dictionary.TryGetValue(name, out List<Aperture> apertures_Temp))
+                    {
+                        apertures_Temp = new List<Aperture>();
+                        dictionary[name] = apertures_Temp;
+                    }
+
+                    apertures_Temp.Add(aperture);
+
+                }
+
+                foreach (List<Aperture> apertures_Temp in dictionary.Values)
+                    aperturesList.Add(apertures_Temp);
+            }
+            else
+            {
+                aperturesList.Add(apertures);
+            }
+
+
+            foreach(List<Aperture> apertures_Temp in aperturesList)
+            {
+                List<Aperture> apertures_Merged = MergeOverlapApertures(apertures_Temp, minArea, tolerance);
+                if(apertures_Merged == null || apertures_Merged.Count == 0)
+                {
+                    continue;
+                }
+
+                panel_New.RemoveApertures();
+
+                foreach(Aperture aperture_New in apertures_Merged)
+                {
+                    panel_New.AddAperture(aperture_New);
+                }
+            }
+
+            return panel_New;
+
+        }
+
+        public static List<Aperture> MergeOverlapApertures(this IEnumerable<Aperture> apertures, double minArea = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            if(apertures == null)
+            {
+                return null;
+            }
+
+
+            List<Tuple<Aperture, Face3D, Point3D>> tuples = new List<Tuple<Aperture, Face3D, Point3D>>();
+            foreach(Aperture aperture in apertures)
+            {
+                Face3D face3D = aperture?.GetFace3D();
+                if(face3D == null || !face3D.IsValid())
+                {
+                    continue;
+                }
+
+                if(face3D.GetArea() < minArea)
+                {
+                    continue;
+                }
+
+                Point3D internalPoint3D = face3D.InternalPoint3D(tolerance);
+                if (internalPoint3D == null || !internalPoint3D.IsValid())
+                {
+                    continue;
+                }
+
+                tuples.Add(new Tuple<Aperture, Face3D, Point3D>(aperture, face3D, internalPoint3D));
+            }
+
+            List<Aperture> result = new List<Aperture>();
+            while(tuples.Count != 0)
+            {
+                Plane plane = tuples[0].Item2.GetPlane();
+
+                List<Tuple<Aperture, Face3D, Point3D>> tuples_Coplanar = tuples.FindAll(x => x.Item2.GetPlane().Coplanar(plane, tolerance));
+                if(tuples_Coplanar == null || tuples_Coplanar.Count == 0)
+                {
+                    result.Add(tuples[0].Item1);
+                    tuples.RemoveAt(0);
+                    continue;
+                }
+
+                tuples_Coplanar.ForEach(x => tuples.Remove(x));
+
+                if(tuples_Coplanar.Count == 1)
+                {
+                    result.Add(tuples_Coplanar[0].Item1);
+                    continue;
+                }
+
+                List<Face2D> face2Ds_Union = new List<Face2D>();
+                foreach(Tuple<Aperture, Face3D, Point3D> tuple in tuples_Coplanar)
+                {
+                    Face2D face2D = plane.Convert(tuple.Item2);
+
+                    List<Face2D> face2Ds_Intersection = new List<Face2D>() { face2D };
+                    foreach(Face2D face2D_Union in face2Ds_Union)
+                    {
+                        List<Face2D> face2Ds_Intersection_Temp = new List<Face2D>();
+                        foreach (Face2D face2D_Intersection in face2Ds_Intersection)
+                        {
+                            List<Face2D> face2Ds_Difference = face2D_Intersection.Difference(face2D_Union, tolerance);
+                            if(face2Ds_Difference != null && face2Ds_Difference.Count != 0)
+                            {
+                                face2Ds_Intersection_Temp.AddRange(face2Ds_Difference);
+                            }
+                        }
+
+                        face2Ds_Intersection = face2Ds_Intersection_Temp;
+                    }
+
+                    ApertureConstruction apertureConstruction = tuple.Item1.ApertureConstruction;
+
+                    foreach (Face2D face2D_Intersection in face2Ds_Intersection)
+                    {
+                        if (face2D_Intersection.GetArea() < tolerance)
+                        {
+                            continue;
+                        }
+
+                        Face3D face3D = plane.Convert(face2D_Intersection);
+
+                        Aperture aperture = new Aperture(apertureConstruction, face3D);
+                        result.Add(aperture);
+
+                        face2Ds_Union.Add(face2D_Intersection);
+                    }
+
+                    face2Ds_Union = face2Ds_Union.Union(tolerance);
+                }
+            }
+
+            return result;
+        }
+    }
+}
