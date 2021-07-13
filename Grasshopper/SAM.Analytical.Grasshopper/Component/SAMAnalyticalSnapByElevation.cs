d02ef7c12bb3062686afecc0eb06f4bb55e89dd4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-13 12:58:03 +0100

    SnapByElevation Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalSnapByElevation.cs
@@ -0,0 +1,95 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalSnapByElevation : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("ad6aa954-4861-4248-9f73-fecfff2ca796");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalSnapByElevation()
+          : base("SAMAnalytical.SnapByElevation", "SAMAnalytical.SnapByElevation",
+              "Snap By Elevation",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooPanelParam(), "_panels", "_panels", "SAM Analytical Panels", GH_ParamAccess.list);
+            inputParamManager.AddNumberParameter("_elevation", "_elevation", "elevation", GH_ParamAccess.item);
+            inputParamManager.AddNumberParameter("_minTolerance_", "_minTolerance_", "Minimal Tolerance", GH_ParamAccess.item, Core.Tolerance.MicroDistance);
+            inputParamManager.AddNumberParameter("_maxTolerance", "_maxTolerance", "Maximal Tolerance", GH_ParamAccess.item, Core.Tolerance.MacroDistance);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooPanelParam(), "Panels", "Panels", "SAM Analytical Panels", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            List<Panel> panels = new List<Panel>();
+            if (!dataAccess.GetDataList(0, panels))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double elevation = double.NaN;
+            if (!dataAccess.GetData(1, ref elevation) || double.IsNaN(elevation))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double minTolerance = double.NaN;
+            if (!dataAccess.GetData(2, ref minTolerance) || double.IsNaN(minTolerance))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            double maxTolerance = double.NaN;
+            if (!dataAccess.GetData(3, ref maxTolerance) || double.IsNaN(maxTolerance))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Panel> result = Analytical.Query.SnapByElevation(panels, elevation, maxTolerance, minTolerance);
+            dataAccess.SetDataList(0, result.ConvertAll(x => new GooPanel(x)));
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Query/SnapByElevation.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using SAM.Geometry.Spatial;
+using SAM.Geometry.Planar;
+using System;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static List<Panel> SnapByElevation(this IEnumerable<Panel> panels, double elevation, double maxTolerance = Core.Tolerance.MacroDistance, double minTolerance = Core.Tolerance.MicroDistance)
+        {
+            if(panels == null || double.IsNaN(elevation))
+            {
+                return null;
+            }
+
+            Plane plane = Plane.WorldXY.GetMoved(Vector3D.WorldZ * elevation) as Plane;
+
+            Dictionary<Panel, List<ISegmentable2D>> dictionary = panels.SectionDictionary<ISegmentable2D>(plane, maxTolerance);
+            if(dictionary == null)
+            {
+                return null;
+            }
+
+            List<Tuple<Panel, List<Segment2D>>> tuples = new List<Tuple<Panel, List<Segment2D>>>();
+            List<Tuple<Point2D, Segment2D, bool>> tuples_Point2D = new List<Tuple<Point2D, Segment2D, bool>>();
+            foreach(KeyValuePair<Panel, List<ISegmentable2D>> keyValuePair in dictionary)
+            {
+                if(keyValuePair.Key == null || keyValuePair.Value == null || keyValuePair.Value.Count == 0)
+                {
+                    continue;
+                }
+
+                List<Segment2D> segment2Ds = new List<Segment2D>();
+                foreach(ISegmentable2D segmentable2D in keyValuePair.Value)
+                {
+                    List<Segment2D> segment2Ds_Temp = segmentable2D?.GetSegments();
+                    if(segment2Ds_Temp == null || segment2Ds_Temp.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    segment2Ds.AddRange(segment2Ds_Temp);
+                }
+
+                foreach (Segment2D segment2D in segment2Ds)
+                {
+                    tuples_Point2D.Add(new Tuple<Point2D, Segment2D, bool>(segment2D[0], segment2D, true));
+                    tuples_Point2D.Add(new Tuple<Point2D, Segment2D, bool>(segment2D[1], segment2D, false));
+                }
+
+                tuples.Add(new Tuple<Panel, List<Segment2D>>(keyValuePair.Key, segment2Ds));
+            }
+
+            List<Tuple<Point2D, Segment2D, bool>> tuples_Point2D_Snap = new List<Tuple<Point2D, Segment2D, bool>>();
+            while (tuples_Point2D.Count > 0)
+            {
+                List<Tuple<Point2D, Segment2D, bool>> tuples_Point2D_Temp = tuples_Point2D.FindAll(x => x.Item1.Distance(tuples_Point2D[0].Item1) <= maxTolerance);
+                tuples_Point2D_Temp.ForEach(x => tuples_Point2D.Remove(x));
+                if(tuples_Point2D_Temp.Count < 2)
+                {
+                    continue;
+                }
+
+                bool equals = true;
+                for(int i= 1; i < tuples_Point2D_Temp.Count; i++)
+                {
+                    if(!tuples_Point2D_Temp[0].Item1.AlmostEquals(tuples_Point2D_Temp[i].Item1, minTolerance))
+                    {
+                        equals = false;
+                        break;
+                    }
+                }
+
+                if(equals)
+                {
+                    continue;
+                }
+
+                List<Point2D> point2Ds = tuples_Point2D_Temp.ConvertAll(x => x.Item1);
+                point2Ds.Add(point2Ds.Average());
+
+                List<Tuple<Point2D, double>> tuples_Weight = new List<Tuple<Point2D, double>>();
+                foreach(Point2D point2D in point2Ds)
+                {
+                    List<Tuple<Segment2D, double>> tuples_Segment2D_Weight = new List<Tuple<Segment2D, double>>();
+
+                    foreach(Tuple<Point2D, Segment2D, bool> tuple in tuples_Point2D_Temp)
+                    {
+                        Segment2D segment2D = null;
+                        if(tuple.Item3)
+                        {
+                            segment2D = new Segment2D(point2D, segment2D[1]);
+                        }
+                        else
+                        {
+                            segment2D = new Segment2D(segment2D[0], point2D);
+                        }
+
+                        tuples_Segment2D_Weight.Add(new Tuple<Segment2D, double> (segment2D, segment2D.GetLength() * segment2D.Direction.SmallestAngle(tuple.Item2.Direction)));
+                    }
+
+                    tuples_Weight.Add(new Tuple<Point2D, double>(point2D, tuples_Segment2D_Weight.ConvertAll(x => x.Item2).Sum()));
+                }
+
+                tuples_Weight.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+
+                Point2D point2D_Snap = tuples_Weight[0].Item1;
+                
+                foreach (Tuple<Point2D, Segment2D, bool> tuple in tuples_Point2D_Temp)
+                {
+                    tuples_Point2D_Snap.Add(new Tuple<Point2D, Segment2D, bool>(point2D_Snap, tuple.Item2, tuple.Item3));
+                }
+            }
+
+            List<Panel> result = new List<Panel>();
+            foreach(Tuple<Panel, List<Segment2D>> tuple in tuples)
+            {
+                List<Tuple<Point2D, Segment2D, bool>> tuples_Point2D_Snap_Panel = new List<Tuple<Point2D, Segment2D, bool>>();
+                foreach (Segment2D segment2D in tuple.Item2)
+                {
+                    tuples_Point2D_Snap_Panel.Add(tuples_Point2D_Snap.Find(x => x.Item2 == segment2D));
+                }
+
+                if (tuples_Point2D_Snap_Panel.TrueForAll(x => x == null))
+                {
+                    result.Add(tuple.Item1);
+                    continue;
+                }
+
+                for(int i=0; i < tuple.Item2.Count; i++)
+                {
+                    Segment2D segment2D = tuple.Item2[i];
+                    if (tuples_Point2D_Snap_Panel[i] != null)
+                    {
+                        if (tuples_Point2D_Snap_Panel[i].Item3)
+                        {
+                            segment2D = new Segment2D(tuples_Point2D_Snap_Panel[i].Item1, segment2D[1]);
+                        }
+                        else
+                        {
+                            segment2D = new Segment2D(segment2D[0], tuples_Point2D_Snap_Panel[i].Item1);
+                        }
+                    }
+
+                    Guid guid = tuple.Item1.Guid;
+                    if(result.Find(x => x.Guid == guid) != null)
+                    {
+                        guid = Guid.NewGuid();
+                    }
+
+                    BoundingBox3D boundingBox3D = tuple.Item1.GetBoundingBox();
+                    Vector3D vector3D = Vector3D.WorldZ * boundingBox3D.Height;
+
+                    Segment3D segment3D = plane.Convert(segment2D);
+
+                    Panel panel = Create.Panel(guid, tuple.Item1, new PlanarBoundary3D(new Polygon3D(new Point3D[] { segment3D[0], segment3D[1], segment3D[1].GetMoved(vector3D) as Point3D, segment3D[0].GetMoved(vector3D) as Point3D })));
+                    result.Add(panel);
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
