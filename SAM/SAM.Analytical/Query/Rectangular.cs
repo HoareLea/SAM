0e44f26faaced297ae7dac9ed60447fdb5a9b0ae
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-14 08:45:24 +0100

    Rectangular Query Added for Panel and Aperture

---
+++ SAM/SAM.Analytical/Query/Rectangular.cs
@@ -0,0 +1,25 @@
+﻿using SAM.Geometry.Spatial;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static bool Rectangular(this Panel panel, double tolerance = Core.Tolerance.Distance)
+        {
+            Face3D face3D = panel?.GetFace3D();
+            if (face3D == null || !face3D.IsValid())
+                return false;
+
+            return Geometry.Spatial.Query.Rectangular(face3D, tolerance);
+        }
+
+        public static bool Rectangular(this Aperture aperture, double tolerance = Core.Tolerance.Distance)
+        {
+            Face3D face3D = aperture?.GetFace3D();
+            if (face3D == null || !face3D.IsValid())
+                return false;
+
+            return Geometry.Spatial.Query.Rectangular(face3D, tolerance);
+        }
+    }
+}
