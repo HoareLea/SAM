bcf0cae3c3afca58322a022f709713dc80c152cc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-07-14 11:52:36 +0100

    Create Panel Method Added

--- SAM/SAM.Analytical/Create/Panel.cs
+++ SAM/SAM.Analytical/Create/Panel.cs
@@ -31,6 +31,35 @@ namespace SAM.Analytical
             return new Panel(panel.Guid, panel, new Face3D(polygon3D));
         }
 
+        public static Panel Panel(this Construction construction, Segment3D segment3D, Vector3D vector3D, PanelType panelType = PanelType.Undefined)
+        {
+            if(vector3D == null || !vector3D.IsValid() || segment3D == null || !segment3D.IsValid())
+            {
+                return null;
+            }
+            
+            Face3D face3D = new Face3D(new Polygon3D(new Point3D[] { segment3D[0], segment3D[1], segment3D[1].GetMoved(vector3D) as Point3D, segment3D[0].GetMoved(vector3D) as Point3D }));
+
+            if(panelType == PanelType.Undefined)
+            {
+                if(construction != null && construction.TryGetValue(ConstructionParameter.DefaultPanelType, out string text))
+                {
+                    panelType = Query.PanelType(text, false);
+                }
+
+                if(panelType == PanelType.Undefined)
+                {
+                    Vector3D normal = face3D?.GetPlane()?.Normal;
+                    if(normal != null)
+                    {
+                        panelType = normal.PanelType();
+                    }
+                }
+            }
+
+            return Panel(construction, panelType, face3D);
+        }
+
         public static Panel Panel(Construction construction, PanelType panelType, Face3D face3D)
         {
             if(face3D == null || panelType == PanelType.Undefined)
@@ -50,7 +79,7 @@ namespace SAM.Analytical
 
             return new Panel(guid, panel, face3D, apertures, trimGeometry, minArea, maxDistance);
         }
-
+        
         public static Panel Panel(Panel panel)
         {
             if(panel == null)
