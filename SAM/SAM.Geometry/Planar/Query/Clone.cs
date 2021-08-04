c7de5862e31c51eea1b34532632c4f67de510058
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-08-04 08:21:47 +0100

    Clone Query Updated

--- SAM/SAM.Geometry/Planar/Query/Clone.cs
+++ SAM/SAM.Geometry/Planar/Query/Clone.cs
@@ -12,5 +12,23 @@ namespace SAM.Geometry.Planar
 
             return result;
         }
+
+        public static List<Face2D> Clone(this IEnumerable<Face2D> face2Ds)
+        {
+            List<Face2D> result = new List<Face2D>();
+            foreach (Face2D face2D in face2Ds)
+                result.Add(new Face2D(face2D));
+
+            return result;
+        }
+
+        public static List<Polygon2D> Clone(this IEnumerable<Polygon2D> polygon2Ds)
+        {
+            List<Polygon2D> result = new List<Polygon2D>();
+            foreach (Polygon2D polygon2D in polygon2Ds)
+                result.Add(new Polygon2D(polygon2D));
+
+            return result;
+        }
     }
 }
