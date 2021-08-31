e790028ec4c4ebe071e082db273343f2561db0f3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-08-31 19:24:14 +0100

    RemoveOverlapApertures Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalRemoveOverlapApertures.cs
@@ -0,0 +1,153 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalRemoveOverlapApertures : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("c0512da8-82c6-4bb7-a660-369c5589063b");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalRemoveOverlapApertures()
+          : base("SAMAnalytical.RemoveOverlapApertures", "SAMAnalytical.RemoveOverlapApertures",
+              "Remove Overlap Apertures",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            int index;
+
+            index = inputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "_analyticalObject", "_analyticalObject", "SAM Analytical Object such as AdjacencyCluster, Panel or AnalyticalModel", GH_ParamAccess.list);
+            inputParamManager[index].DataMapping = GH_DataMapping.Flatten;
+
+            inputParamManager.AddNumberParameter("_tolerance_", "_tolerance_", "Tolerance", GH_ParamAccess.item, Tolerance.Distance);
+            inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddParameter(new GooSAMObjectParam<SAMObject>(), "analyticalObject", "analyticalObject", "SAM Analytical Object", GH_ParamAccess.list);
+            outputParamManager.AddParameter(new GooApertureParam(), "removedApertures", "removedApertures", "RemovedApertures", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            bool run = false;
+            if (!dataAccess.GetData(2, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+            if (!run)
+                return;
+
+            double tolerance = Tolerance.Distance;
+            if (!dataAccess.GetData(1, ref tolerance))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<SAMObject> sAMObjects = new List<SAMObject>();
+            if (!dataAccess.GetDataList(0, sAMObjects) || sAMObjects == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Aperture> removedApertures = new List<Aperture>();
+
+            List<Panel> panels = sAMObjects.ConvertAll(x => x as Panel);
+            panels.RemoveAll(x => x == null);
+
+            List<AdjacencyCluster> adjacencyClusters = sAMObjects.ConvertAll(x => x as AdjacencyCluster);
+            adjacencyClusters.RemoveAll(x => x == null);
+
+            List<AnalyticalModel> analyticalModels = sAMObjects.ConvertAll(x => x as AnalyticalModel);
+            analyticalModels.RemoveAll(x => x == null);
+
+            Analytical.Modify.RemoveOverlapApertures(panels, out removedApertures, tolerance);
+
+            for(int i=0; i < adjacencyClusters.Count; i++)
+            {
+                adjacencyClusters[i] = new AdjacencyCluster(adjacencyClusters[i]);
+                Analytical.Modify.RemoveOverlapApertures(adjacencyClusters[i], out List<Aperture> removedApertures_Temp, tolerance);
+                if(removedApertures_Temp != null && removedApertures_Temp.Count > 0)
+                {
+                    if(removedApertures == null)
+                    {
+                        removedApertures = new List<Aperture>();
+                    }
+
+                    removedApertures.AddRange(removedApertures_Temp);
+                }
+            }
+
+            for (int i = 0; i < analyticalModels.Count; i++)
+            {
+                AdjacencyCluster adjacencyCluster = analyticalModels[i]?.AdjacencyCluster;
+                if (Analytical.Modify.RemoveOverlapApertures(adjacencyClusters[i], out List<Aperture> removedApertures_Temp, tolerance))
+                {
+                    if (removedApertures_Temp != null && removedApertures_Temp.Count > 0)
+                    {
+                        if (removedApertures == null)
+                        {
+                            removedApertures = new List<Aperture>();
+                        }
+
+                        removedApertures.AddRange(removedApertures_Temp);
+                    }
+
+                    analyticalModels[i] = new AnalyticalModel(analyticalModels[i], adjacencyCluster);
+                }
+            }
+
+            List<SAMObject> result = new List<SAMObject>();
+            if (panels != null)
+                result.AddRange(panels.Cast<SAMObject>());
+
+            if (adjacencyClusters != null)
+                result.AddRange(adjacencyClusters.Cast<SAMObject>());
+
+            if (analyticalModels != null)
+                result.AddRange(analyticalModels.Cast<SAMObject>());
+
+            dataAccess.SetDataList(0, result);
+            dataAccess.SetDataList(1, removedApertures?.ConvertAll(x => new GooAperture(x)));
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/RemoveOverlapApertures.cs
@@ -0,0 +1,159 @@
+﻿using SAM.Geometry.Spatial;
+using SAM.Geometry.Planar;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static bool RemoveOverlapApertures(this Panel panel, out List<Aperture> removedApertures, double tolerance = Core.Tolerance.Distance)
+        {
+            removedApertures = null;
+            
+            List<Aperture> apertures = panel?.Apertures;
+            if (apertures == null || apertures.Count < 2)
+            {
+                return false;
+            }
+
+            Plane plane = panel.Plane;
+            if(plane == null)
+            {
+                return false;
+            }
+
+            List<Tuple<BoundingBox2D, Face2D, Aperture>> tuples = new List<Tuple<BoundingBox2D, Face2D, Aperture>>();
+            foreach(Aperture aperture in apertures)
+            {
+                Face2D face2D = plane.Convert(plane.Project(aperture?.GetFace3D()));
+                if(face2D == null)
+                {
+                    continue;
+                }
+
+                tuples.Add(new Tuple<BoundingBox2D, Face2D, Aperture>(face2D.GetBoundingBox(), face2D, aperture));
+            }
+
+            if(tuples == null || tuples.Count == 0)
+            {
+                return false;
+            }
+
+            for(int i=0; i < tuples.Count; i++)
+            {
+                Tuple<BoundingBox2D, Face2D, Aperture> tuple_1 = tuples[i];
+
+                for (int j = 1; j < tuples.Count - 1; j++)
+                {
+                    Tuple<BoundingBox2D, Face2D, Aperture> tuple_2 = tuples[j];
+
+                    if(!tuple_1.Item1.InRange(tuple_2.Item1, tolerance))
+                    {
+                        continue;
+                    }
+
+                    List<Face2D> face2Ds = tuple_1.Item2.Intersection(tuple_2.Item2, tolerance);
+                    face2Ds?.RemoveAll(x => x == null || x.GetArea() < tolerance);
+                    if(face2Ds == null || face2Ds.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    removedApertures = new List<Aperture>();
+
+                    if(tuple_1.Item2.GetArea() > tuple_2.Item2.GetArea())
+                    {
+                        removedApertures.Add(tuple_2.Item3);
+                    }
+                    else
+                    {
+                        removedApertures.Add(tuple_1.Item3);
+                    }
+
+                    foreach(Aperture aperture in removedApertures)
+                    {
+                        panel.RemoveAperture(aperture.Guid);
+                    }
+                    
+                    RemoveOverlapApertures(panel, out List<Aperture> removedApertures_Temp, tolerance);
+                    if(removedApertures_Temp != null && removedApertures_Temp.Count > 0)
+                    {
+                        removedApertures.AddRange(removedApertures_Temp);
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool RemoveOverlapApertures(this List<Panel> panels, out List<Aperture> removedApertures, double tolerance = Core.Tolerance.Distance)
+        {
+            removedApertures = null;
+
+            if (panels == null || panels.Count == 0)
+            {
+                return false;
+            }
+
+            bool result = false;
+            for(int i=0; i < panels.Count; i++)
+            {
+                Panel panel = panels[i];
+                if(panel == null)
+                {
+                    continue;
+                }
+
+                panel = new Panel(panel);
+                if(!RemoveOverlapApertures(panel, out List<Aperture> removedApertures_Temp, tolerance))
+                {
+                    continue;
+                }
+
+                panels[i] = panel;
+                result = true;
+                
+                if(removedApertures_Temp == null || removedApertures_Temp.Count == 0)
+                {
+                    continue;
+                }
+
+                if (removedApertures == null)
+                {
+                    removedApertures = new List<Aperture>();
+                }
+
+                removedApertures.AddRange(removedApertures_Temp);
+            }
+
+            return result;
+
+        }
+
+        public static bool RemoveOverlapApertures(this AdjacencyCluster adjacencyCluster, out List<Aperture> removedApertures, double tolerance = Core.Tolerance.Distance)
+        {
+            removedApertures = null;
+
+            List<Panel> panels = adjacencyCluster?.GetPanels();
+            if(panels == null || panels.Count == 0)
+            {
+                return false;
+            }
+
+            if (!RemoveOverlapApertures(panels, out removedApertures, tolerance))
+            {
+                return false;
+            }
+
+            foreach(Panel panel in panels)
+            {
+                adjacencyCluster.AddObject(panel);
+            }
+
+            return true;
+        }
+    }
+}
