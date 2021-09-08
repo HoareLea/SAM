8a18b6183ab7a7080159074e46d668f49347e777
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-09-08 21:30:57 +0200

    Create Directory Method Added

---
+++ SAM/SAM.Core/Create/Directory.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SAM.Core
+{
+    public static partial class Create
+    {
+        public static bool Directory(this string directory)
+        {
+            if(string.IsNullOrWhiteSpace(directory))
+            {
+                return false;
+            }
+
+            string directory_Temp = directory;
+            if (!string.IsNullOrWhiteSpace(Path.GetExtension(directory_Temp)))
+            {
+                directory_Temp = Path.GetDirectoryName(directory_Temp);
+            }
+
+            List<string> directories = new List<string>();
+            directories.Add(directory_Temp);
+
+            directory_Temp = Path.GetDirectoryName(directory_Temp);
+            while(!string.IsNullOrWhiteSpace(directory_Temp))
+            {
+                directories.Add(directory_Temp);
+                directory_Temp = Path.GetDirectoryName(directory_Temp);
+            }
+
+            if(directories.Count < 2)
+            {
+                return false;
+            }
+
+            for(int i = directories.Count - 2; i >= 0; i--)
+            {
+                directory_Temp = directories[i];
+
+                if(System.IO.Directory.Exists(directory_Temp))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    DirectoryInfo directoryInfo = System.IO.Directory.CreateDirectory(directory_Temp);
+                    if(directoryInfo == null)
+                    {
+                        return false;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
