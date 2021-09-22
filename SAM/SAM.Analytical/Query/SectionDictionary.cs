2506277d90a157a7e5601134bce62096d3dedfdc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-09-22 16:06:30 +0100

    Performance Issue Fixed

--- SAM/SAM.Analytical/Query/SectionDictionary.cs
+++ SAM/SAM.Analytical/Query/SectionDictionary.cs
@@ -1,5 +1,8 @@
 ﻿using SAM.Geometry.Spatial;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SAM.Analytical
 {
@@ -10,47 +13,64 @@ namespace SAM.Analytical
             if (plane == null || panels == null)
                 return null;
 
-            Dictionary<Panel, List<T>> result = new Dictionary<Panel, List<T>>();
+            List<Tuple<Panel, List<T>>> tuples = new List<Tuple<Panel, List<T>>>();
             foreach(Panel panel in panels)
             {
-                Face3D face3D = panel?.GetFace3D();
-                if(face3D == null)
+                if(panel == null)
                 {
                     continue;
+                }
+
+                tuples.Add(new Tuple<Panel, List<T>>(panel, new List<T>()));
+            }
+
+            if(tuples.Count == 0)
+            {
+                return null;
+            }
+
+            Parallel.For(0, tuples.Count, (int i) => 
+            {
+                Panel panel = tuples[i].Item1;
+                
+                Face3D face3D = panel?.GetFace3D();
+                if (face3D == null)
+                {
+                    return;
                 }
 
                 PlanarIntersectionResult planarIntersectionResult = Geometry.Spatial.Create.PlanarIntersectionResult(plane, face3D, tolerance);
-                if(planarIntersectionResult == null || !planarIntersectionResult.Intersecting)
+                if (planarIntersectionResult == null || !planarIntersectionResult.Intersecting)
                 {
-                    continue;
+                    return;
                 }
 
-                if(typeof(ISAMGeometry3D).IsAssignableFrom(typeof(T)))
+                if (typeof(ISAMGeometry3D).IsAssignableFrom(typeof(T)))
                 {
                     List<ISAMGeometry3D> sAMGeometry3Ds = planarIntersectionResult.Geometry3Ds;
-                    if(sAMGeometry3Ds == null || sAMGeometry3Ds.Count == 0)
+                    if (sAMGeometry3Ds == null || sAMGeometry3Ds.Count == 0)
                     {
-                        continue;
+                        return;
                     }
 
                     List<T> sAMGeometries = new List<T>();
 
                     foreach (ISAMGeometry3D sAMGeometry3D in sAMGeometry3Ds)
                     {
-                        if(sAMGeometry3D is T)
+                        if (sAMGeometry3D is T)
                         {
                             sAMGeometries.Add((T)sAMGeometry3D);
                         }
                     }
 
-                    result[panel] = sAMGeometries;
+                    tuples[i].Item2.AddRange(sAMGeometries);
                 }
-                else if(typeof(Geometry.Planar.ISAMGeometry2D).IsAssignableFrom(typeof(T)))
+                else if (typeof(Geometry.Planar.ISAMGeometry2D).IsAssignableFrom(typeof(T)))
                 {
                     List<Geometry.Planar.ISAMGeometry2D> sAMGeometry2Ds = planarIntersectionResult.Geometry2Ds;
                     if (sAMGeometry2Ds == null || sAMGeometry2Ds.Count == 0)
                     {
-                        continue;
+                        return;
                     }
 
                     List<T> sAMGeometries = new List<T>();
@@ -63,8 +83,19 @@ namespace SAM.Analytical
                         }
                     }
 
-                    result[panel] = sAMGeometries;
+                    tuples[i].Item2.AddRange(sAMGeometries);
                 }
+            });
+
+            Dictionary<Panel, List<T>> result = new Dictionary<Panel, List<T>>();
+            foreach(Tuple<Panel, List<T>> tuple in tuples)
+            {
+                if(tuple.Item2 == null || tuple.Item2.Count == 0)
+                {
+                    continue;
+                }
+
+                result[tuple.Item1] = tuple.Item2;
             }
 
             return result;
