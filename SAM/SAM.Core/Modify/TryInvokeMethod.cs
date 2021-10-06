8eefdf41d29db18531504fabe23ed55b1a2119a8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-06 20:23:53 +0100

    TryInvokeMethod Implemented

---
+++ SAM/SAM.Core/Modify/TryInvokeMethod.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace SAM.Core
+{
+    public static partial class Modify
+    {
+        public static bool TryInvokeMethod(this object @object, string methodName, out object result, params object[] parameters)
+        {
+            result = null;
+
+            if(@object == null || string.IsNullOrWhiteSpace(methodName))
+            {
+                return false;
+            }
+
+            foreach (MethodInfo methodInfo in @object.GetType().GetMethods())
+            {
+                if (methodInfo?.Name != methodName)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    result = methodInfo.Invoke(@object, parameters);
+                    return true;
+                }
+                catch(Exception exception)
+                {
+                    result = null;
+                }
+            }
+
+            return false;
+        }
+    }
+}
