c9cae8ea2ae1779c0014b1afd3f5cedb16d52e27
Michal Dengusiak <tassimulation@gmail.com>
2021-10-06 21:58:59 +0100

    new modify function added (TryInvokeMethod)

---
+++ SAM/SAM.Core/Modify/TryInvokeDeclaredMethod.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SAM.Core
+{
+    public static partial class Modify
+    {
+        public static bool TryInvokeDeclaredMethod(dynamic @object, string methodName, out object result, params object[] parameters)
+        {
+
+            result = null;
+
+            if(@object == null || string.IsNullOrWhiteSpace(methodName))
+            {
+                return false;
+            }
+
+            IEnumerable<MethodInfo> methodInfos = null;
+            try
+            {
+                methodInfos = @object.GetType().DeclaredMethods;
+            }
+            catch
+            {
+                return false;
+            }
+
+            if(methodInfos == null)
+            {
+                return false;
+            }
+
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                if (methodInfo?.Name != methodName)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    result = methodInfo.Invoke(@object, parameters);
+                    return true;
+                }
+                catch(Exception exception)
+                {
+                    result = null;
+                }
+            }
+
+            return false;
+        }
+    }
+}
