50a0cf97a49f31de16c4067f5d52eb3f2f43322d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-10-28 00:21:47 +0200

    MinDimension Query Added for BoundingBox3D

---
+++ SAM/SAM.Geometry/Spatial/Query/MinDimension.cs
@@ -0,0 +1,16 @@
+﻿namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static double MinDimension(this BoundingBox3D boundingBox3D)
+        {
+            if(boundingBox3D == null)
+            {
+                return double.NaN;
+            }
+
+            return Math.Query.Min(boundingBox3D.Width, boundingBox3D.Height, boundingBox3D.Depth);
+
+        }
+    }
+}
