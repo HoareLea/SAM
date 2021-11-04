4d6f8c0af519674914550e51dcd4af0e20cfde06
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-11-04 17:44:39 +0000

    SimulationResults Added

---
+++ SAM/SAM.Analytical/Classes/New/SimulationResult/ArchitecturalModelSimulationResult.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using System;
+
+namespace SAM.Analytical
+{
+    public class ArchitecturalModelSimulationResult : Result
+    {
+        public ArchitecturalModelSimulationResult(string name, string source, string reference)
+            : base(name, source, reference)
+        {
+
+        }
+
+        public ArchitecturalModelSimulationResult(Guid guid, string name, string source, string reference)
+            : base(guid, name, source, reference)
+        {
+
+        }
+
+        public ArchitecturalModelSimulationResult(ArchitecturalModelSimulationResult architecturalModelSimulationResult)
+            : base(architecturalModelSimulationResult)
+        {
+
+        }
+
+        public ArchitecturalModelSimulationResult(Guid guid, ArchitecturalModelSimulationResult architecturalModelSimulationResult)
+            : base(guid, architecturalModelSimulationResult)
+        {
+
+        }
+
+        public ArchitecturalModelSimulationResult(JObject jObject)
+            : base(jObject)
+        {
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            if (!base.FromJObject(jObject))
+                return false;
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+           JObject jObject = base.ToJObject();
+            if (jObject == null)
+                return null;
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Classes/New/SimulationResult/PartitionSimulationResult.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+using System;
+
+namespace SAM.Analytical
+{
+    public class PartitionSimulationResult : Result
+    {
+        public PartitionSimulationResult(string name, string source, string reference)
+            : base(name, source, reference)
+        {
+
+        }
+
+        public PartitionSimulationResult(Guid guid, string name, string source, string reference)
+            : base(guid, name, source, reference)
+        {
+
+        }
+
+        public PartitionSimulationResult(PartitionSimulationResult partitionSimulationResult)
+            : base(partitionSimulationResult)
+        {
+
+        }
+
+        public PartitionSimulationResult(JObject jObject)
+            : base(jObject)
+        {
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            if (!base.FromJObject(jObject))
+                return false;
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+           JObject jObject = base.ToJObject();
+            if (jObject == null)
+                return null;
+
+            return jObject;
+        }
+    }
+}
