b11331fec179a34bd02cddf531eb0cf7938ee2f5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-11-10 15:18:43 +0000

    MapZones, UpdateSpace and UpdateZone

---
+++ SAM/SAM.Analytical/Modify/New/MapZones.cs
@@ -0,0 +1,51 @@
+﻿using SAM.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Zone> MapZones(this ArchitecturalModel architecturalModel, TextMap textMap)
+        {
+            if(textMap == null || architecturalModel == null)
+            {
+                return null;
+            }
+
+            IEnumerable<string> zoneCategories = textMap.Keys;
+            if (zoneCategories == null || zoneCategories.Count() == 0)
+                return null;
+
+            List<Space> spaces = architecturalModel.GetSpaces();
+            if (spaces == null || spaces.Count == 0)
+                return null;
+
+            List<Zone> result = new List<Zone>();
+            foreach (string zoneCategory in zoneCategories)
+            {
+                List<string> parameterNames = textMap.GetValues(zoneCategory);
+                if (parameterNames == null || parameterNames.Count == 0)
+                    continue;
+
+                foreach (string parameterName in parameterNames)
+                {
+                    Dictionary<string, List<Space>> dictionary = Core.Query.Dictionary<string, Space>(spaces, parameterName);
+                    if (dictionary == null || dictionary.Count == 0)
+                        continue;
+
+                    foreach (KeyValuePair<string, List<Space>> keyValuePair in dictionary)
+                    {
+                        Zone zone = architecturalModel.UpdateZone(keyValuePair.Key, zoneCategory, keyValuePair.Value?.ToArray());
+                        if (zone == null)
+                            continue;
+
+                        result.Add(zone);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/New/UpdateSpace.cs
@@ -0,0 +1,64 @@
+﻿using SAM.Geometry.Spatial;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static bool UpdateSpace(this ArchitecturalModel architecturalModel, Space space, double silverSpacing = Core.Tolerance.MacroDistance, double tolerance = Core.Tolerance.Distance)
+        {
+            if (architecturalModel == null || space == null)
+                return false;
+
+            Point3D point3D = space.Location;
+            List<Space> spaces = architecturalModel.GetSpaces();
+            if (spaces == null || spaces.Count == 0)
+                return false;
+
+            Dictionary<Space, Shell> dictionary = new Dictionary<Space, Shell>();
+            foreach(Space space_Temp in spaces)
+            {
+                Shell shell = architecturalModel.GetShell(space_Temp);
+                if (shell == null)
+                    continue;
+
+                if (shell.InRange(point3D, tolerance) || shell.Inside(point3D, tolerance))
+                    dictionary[space_Temp] = shell;
+            }
+
+            if (dictionary.Count == 0)
+                return false;
+
+            Space space_Result = null;
+
+            if(dictionary.Count > 1)
+            {
+                point3D = point3D.GetMoved(Vector3D.WorldZ * silverSpacing) as Point3D;
+                foreach (KeyValuePair<Space, Shell> keyValuePair in dictionary)
+                {
+                    Shell shell = keyValuePair.Value;
+                    if (shell.InRange(point3D, tolerance) || shell.Inside(point3D, tolerance))
+                    {
+                        space_Result = keyValuePair.Key;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                space_Result = dictionary.Keys.First();
+            }
+
+
+            if (space_Result == null)
+                return false;
+
+            List<IPartition> partitions = architecturalModel.GetPartitions(space_Result);
+            architecturalModel.RemoveObject(space_Result);
+
+            architecturalModel.Add(space, partitions);
+            return true;
+        }
+    }
+}
