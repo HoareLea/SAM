fcb7b31266dcc333c3ccce997b1a2e9f44a74241
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-11-24 08:59:51 +0100

    Parse Int and Double Methods Implemented

---
+++ SAM/SAM.Core/Query/ParseDouble.cs
@@ -0,0 +1,15 @@
+﻿namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static double ParseDouble(this string value, double @default = default)
+        {
+            if(!TryParseDouble(value, out double result))
+            {
+                result = @default;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SAM/SAM.Core/Query/TryParseInt.cs
@@ -0,0 +1,18 @@
+﻿namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static bool TryParseInt(this string value, out int result)
+        {
+            result = default;
+
+            if(TryParseDouble(value, out double @double))
+            {
+                result = System.Convert.ToInt32(@double);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
