6e83bac972ce2fca8d51fa660434e56eba1d7ef3
Michal Dengusiak <tassimulation@gmail.com>
2021-12-03 16:56:16 +0000

    add SpaceSimulationResultParameter

---
+++ SAM/SAM.Analytical/Query/SpecificDesignLoad.cs
@@ -0,0 +1,22 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double SpecificDesignLoad(this SpaceSimulationResult spaceSimulationResult)
+        {
+            if (spaceSimulationResult == null)
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.DesignLoad, out double load) || double.IsNaN(load))
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.Area, out double area) || double.IsNaN(area))
+                return double.NaN;
+
+            if (area == 0)
+                return 0;
+
+            return load / area;
+        }
+    }
+}
