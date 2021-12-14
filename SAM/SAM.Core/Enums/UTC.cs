ad68ce2b33924e756d5792679f9bd247439147bb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-12-14 13:51:55 +0000

    UTC Enum Implemented

---
+++ SAM/SAM.Core/Enums/UTC.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Core
+{
+    /// <summary>
+    /// Coordinated Universal Time (UTC) Time Zones
+    /// </summary>
+    public enum UTC
+    {
+        [Description("Undefined")] Undefined,
+        [Description("UTC−12:00")] Minus1200,
+        [Description("UTC−11:00")] Minus1100,
+        [Description("UTC−10:00")] Minus1000,
+        [Description("UTC−09:30")] Minus0930,
+        [Description("UTC−09:00")] Minus0900,
+        [Description("UTC−08:00")] Minus0800,
+        [Description("UTC−07:00")] Minus0700,
+        [Description("UTC−06:00")] Minus0600,
+        [Description("UTC−05:00")] Minus0500,
+        [Description("UTC−04:00")] Minus0400,
+        [Description("UTC−03:30")] Minus0330,
+        [Description("UTC−03:00")] Minus0300,
+        [Description("UTC−02:00")] Minus0200,
+        [Description("UTC−01:00")] Minus0100,
+        [Description("UTC±00:00")] PlusMinus0000,
+        [Description("UTC+01:00")] Plus0100,
+        [Description("UTC+02:00")] Plus0200,
+        [Description("UTC+03:00")] Plus0300,
+        [Description("UTC+03:30")] Plus0330,
+        [Description("UTC+04:00")] Plus0400,
+        [Description("UTC+04:30")] Plus0430,
+        [Description("UTC+05:00")] Plus0500,
+        [Description("UTC+05:30")] Plus0530,
+        [Description("UTC+05:45")] Plus0545,
+        [Description("UTC+06:00")] Plus0600,
+        [Description("UTC+06:30")] Plus0630,
+        [Description("UTC+07:00")] Plus0700,
+        [Description("UTC+08:00")] Plus0800,
+        [Description("UTC+08:45")] Plus0845,
+        [Description("UTC+09:00")] Plus0900,
+        [Description("UTC+09:30")] Plus0930,
+        [Description("UTC+10:00")] Plus1000,
+        [Description("UTC+10:30")] Plus1030,
+        [Description("UTC+11:00")] Plus1100,
+        [Description("UTC+12:00")] Plus1200,
+        [Description("UTC+12:45")] Plus1245,
+        [Description("UTC+13:00")] Plus1300,
+        [Description("UTC+14:00")] Plus1400
+    }
+}
---
+++ SAM/SAM.Core/Query/UTC.cs
@@ -0,0 +1,159 @@
+﻿namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static UTC UTC(double value)
+        {
+            if(double.IsNaN(value))
+            {
+                return Core.UTC.Undefined;
+            }
+
+            switch(value)
+            {
+                case -12.0:
+                    return Core.UTC.Minus1200;
+
+                case -11.0:
+                    return Core.UTC.Minus1100;
+
+                case -10.0:
+                    return Core.UTC.Minus1000;
+
+                case -09.5:
+                    return Core.UTC.Minus0930;
+
+                case -08.0:
+                    return Core.UTC.Minus0800;
+
+                case -07.0:
+                    return Core.UTC.Minus0700;
+
+                case -06.0:
+                    return Core.UTC.Minus0600;
+
+                case -05.0:
+                    return Core.UTC.Minus0500;
+
+                case -04.0:
+                    return Core.UTC.Minus0400;
+
+                case -03.5:
+                    return Core.UTC.Minus0330;
+
+                case -03.0:
+                    return Core.UTC.Minus0300;
+
+                case -02.0:
+                    return Core.UTC.Minus0200;
+
+                case -01.0:
+                    return Core.UTC.Minus0100;
+
+                case 0.0:
+                    return Core.UTC.PlusMinus0000;
+
+                case 1.0:
+                    return Core.UTC.Plus0100;
+
+                case 2.0:
+                    return Core.UTC.Plus0200;
+
+                case 3.0:
+                    return Core.UTC.Plus0300;
+
+                case 3.5:
+                    return Core.UTC.Plus0330;
+
+                case 4.0:
+                    return Core.UTC.Plus0400;
+
+                case 4.5:
+                    return Core.UTC.Plus0430;
+
+                case 5.0:
+                    return Core.UTC.Plus0500;
+
+                case 5.5:
+                    return Core.UTC.Plus0530;
+
+                case 5.75:
+                    return Core.UTC.Plus0545;
+
+                case 6.0:
+                    return Core.UTC.Plus0600;
+
+                case 6.5:
+                    return Core.UTC.Plus0630;
+
+                case 7.0:
+                    return Core.UTC.Plus0700;
+
+                case 8.0:
+                    return Core.UTC.Plus0800;
+
+                case 8.75:
+                    return Core.UTC.Plus0845;
+
+                case 9.0:
+                    return Core.UTC.Plus0900;
+
+                case 9.5:
+                    return Core.UTC.Plus0930;
+
+                case 10.0:
+                    return Core.UTC.Plus1000;
+
+                case 10.5:
+                    return Core.UTC.Plus1030;
+
+                case 11.0:
+                    return Core.UTC.Plus1100;
+
+                case 12.0:
+                    return Core.UTC.Plus1200;
+
+                case 12.75:
+                    return Core.UTC.Plus1245;
+
+                case 13.0:
+                    return Core.UTC.Plus1300;
+
+                case 14.0:
+                    return Core.UTC.Plus1400;
+            }
+
+            return Core.UTC.Undefined;
+        }
+
+        public static UTC UTC(string value)
+        {
+            if(string.IsNullOrWhiteSpace(value))
+            {
+                return Core.UTC.Undefined;
+            }
+
+            if(Core.UTC.Undefined.ToString().ToUpper().Equals(value.ToUpper().Trim()))
+            {
+                return Core.UTC.Undefined;
+            }
+            
+            UTC result = Enum<UTC>(value);
+            if(result != Core.UTC.Undefined)
+            {
+                return result;
+            }
+
+            if(TryConvert(value, out double @double))
+            {
+                result = UTC(@double);
+                if(result != Core.UTC.Undefined)
+                {
+                    return result;
+                }
+            }
+
+            return Core.UTC.Undefined;
+        }
+    }
+}
