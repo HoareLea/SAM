2e0d4f9cb4c19447d8df9b620dfd7039ad487b04
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-12-20 15:07:26 +0000

    GooDesignDay Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Classes/GooWeatherYear.cs
@@ -0,0 +1,60 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class GooDesignDay : GooJSAMObject<DesignDay>
+    {
+        public GooDesignDay()
+            : base()
+        {
+        }
+
+        public GooDesignDay(DesignDay designDay)
+            : base(designDay)
+        {
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new GooDesignDay(Value);
+        }
+    }
+
+    public class GooDesignDayParam : GH_PersistentParam<GooDesignDay>, IGH_PreviewObject
+    {
+        public override Guid ComponentGuid => new Guid("16fcd325-054c-4dd9-b668-4d8589562c34");
+
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        bool IGH_PreviewObject.Hidden { get; set; }
+
+        bool IGH_PreviewObject.IsPreviewCapable => !VolatileData.IsEmpty;
+
+        BoundingBox IGH_PreviewObject.ClippingBox => Preview_ComputeClippingBox();
+
+        void IGH_PreviewObject.DrawViewportMeshes(IGH_PreviewArgs args) => Preview_DrawMeshes(args);
+
+        void IGH_PreviewObject.DrawViewportWires(IGH_PreviewArgs args) => Preview_DrawWires(args);
+
+        public GooDesignDayParam()
+            : base(typeof(DesignDay).Name, typeof(DesignDay).Name, typeof(DesignDay).FullName.Replace(".", " "), "Params", "SAM")
+        {
+        }
+
+        protected override GH_GetterResult Prompt_Plural(ref List<GooDesignDay> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override GH_GetterResult Prompt_Singular(ref GooDesignDay value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
