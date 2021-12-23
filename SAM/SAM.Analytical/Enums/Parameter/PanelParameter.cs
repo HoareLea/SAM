3583ee2e46306562c56dde5ac350879a19c19f97
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-12-23 09:54:05 +0000

    New parameters Added to Panel

--- SAM/SAM.Analytical/Enums/Parameter/PanelParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/PanelParameter.cs
@@ -9,5 +9,13 @@ namespace SAM.Analytical
         [ParameterProperties("Transparent", "Transparent"), ParameterValue(Core.ParameterType.Boolean)] Transparent,
         [ParameterProperties("Color", "Color"), ParameterValue(Core.ParameterType.Color)] Color,
         [ParameterProperties("Thermal Transmittance", "Thermal Transmittance (U-Value) [W/m^2*K]"), DoubleParameterValue()] ThermalTransmittance,
+        [ParameterProperties("Light Transmittance", "Light Transmittance"), DoubleParameterValue(0, 1)] LightTransmittance,
+        [ParameterProperties("Light Reflectance", "Light Reflectance"), DoubleParameterValue(0, 1)] LightReflectance,
+        [ParameterProperties("Direct Solar Energy Transmittance", "Direct Solar Energy Transmittance"), DoubleParameterValue(0, 1)] DirectSolarEnergyTransmittance,
+        [ParameterProperties("Direct Solar Energy Reflectance", "Direct Solar Energy Reflectance"), DoubleParameterValue(0, 1)] DirectSolarEnergyReflectance,
+        [ParameterProperties("Direct Solar Energy Absorptance", "Direct Solar Energy Absorptance"), DoubleParameterValue(0, 1)] DirectSolarEnergyAbsorptance,
+        [ParameterProperties("Total Solar Energy Transmittance", "Total Solar Energy Transmittance"), DoubleParameterValue(0, 1)] TotalSolarEnergyTransmittance,
+        [ParameterProperties("Pilkington Shading Short Wavelength Coefficient", "Pilkington Shading Short Wavelength Coefficient"), DoubleParameterValue(0, 1)] PilkingtonShadingShortWavelengthCoefficient,
+        [ParameterProperties("Pilkington Shading Long Wavelength Coefficient", "Pilkington Shading Long Wavelength Coefficient"), DoubleParameterValue(0, 1)] PilkingtonShadingLongWavelengthCoefficient,
     }
 }
