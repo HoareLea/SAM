e16eab3ffd79162ab1200e452cb3f3d226057a19
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-03 15:11:32 +0000

    Segmant3Ds Query Added foe ISegmentable3Ds

---
+++ SAM/SAM.Geometry/Spatial/Query/Segment3Ds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Geometry.Spatial
+{
+    public static partial class Query
+    {
+        public static List<Segment3D> Segment3Ds(this IEnumerable<ISegmentable3D> segmentable3Ds)
+        {
+            if(segmentable3Ds == null)
+            {
+                return null;
+            }
+
+            List<Segment3D> result = new List<Segment3D>();
+            foreach(ISegmentable3D segmentable3D in segmentable3Ds)
+            {
+                List<Segment3D> segment3Ds = segmentable3D?.GetSegments();
+                if(segment3Ds == null)
+                {
+                    continue;
+                }
+
+                foreach(Segment3D segment3D in segment3Ds)
+                {
+                    if(segment3D == null)
+                    {
+                        continue;
+                    }
+
+                    result.Add(segment3D);
+                }
+            }
+
+            return result;
+        }
+    }
+}
