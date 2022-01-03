224e726a929d8118c442135f29de0ae7dc9b5e6f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-03 11:40:47 +0000

    IFace2DObject Implemented

---
+++ SAM/SAM.Geometry/Planar/Interfaces/IBoundable2DObject.cs
@@ -0,0 +1,7 @@
+﻿namespace SAM.Geometry.Planar
+{
+    public interface IBoundable2DObject : ISAMGeometry2DObject
+    {
+        BoundingBox2D GetBoundingBox(double offset = 0);
+    }
+}
---
+++ SAM/SAM.Geometry/Planar/Interfaces/IFace2DObject.cs
@@ -0,0 +1,7 @@
+﻿namespace SAM.Geometry.Planar
+{
+    public interface IFace2DObject : IBoundable2DObject
+    {
+        Face2D Face2D { get; }
+    }
+}
