c001777508dc92622dd0e240ce37f667dac9551f
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-06 15:27:50 +0000

    WaitToUnlock Query Implemented

---
+++ SAM/SAM.Core/Query/WaitToUnlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static bool WaitToUnlock(string path, int waitTime = 1000, int count = 10)
+        {
+            if(string.IsNullOrWhiteSpace(path) || !System.IO.File.Exists(path))
+            {
+                return false;
+            }
+
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(path);
+
+            bool result = false;
+
+            int i = 0;
+            while(i <= count)
+            {
+                if(!fileInfo.Locked())
+                {
+                    result = true;
+                    break;
+                }
+                System.Threading.Thread.Sleep(waitTime);
+            }
+
+            return result;
+        }
+    }
+}
