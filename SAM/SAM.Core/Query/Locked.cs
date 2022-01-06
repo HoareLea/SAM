679062e75c754d9b4cf75666b6c04a7a7a5d8315
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-06 10:43:13 +0000

    Locked File Query Added

---
+++ SAM/SAM.Core/Query/Locked.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static bool Locked(this FileInfo fileInfo)
+        {
+            try
+            {
+                using (FileStream fileStream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    fileStream.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+}
