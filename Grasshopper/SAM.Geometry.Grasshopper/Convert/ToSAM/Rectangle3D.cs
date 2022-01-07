53550f7057c39f2c9eda3a1889f5ea038ccf3b0e
Michal Dengusiak <tassimulation@gmail.com>
2022-01-07 09:17:14 +0000

    added restangular

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Convert/ToSAM/Rectangle3D.cs
@@ -0,0 +1,12 @@
+﻿using Grasshopper.Kernel.Types;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public static partial class Convert
+    {
+        public static Spatial.Rectangle3D ToSAM(this GH_Rectangle rectangle)
+        {
+            return Rhino.Convert.ToSAM(rectangle.Value);
+        }
+    }
+}
---
+++ Rhino/SAM.Geometry.Rhino/Convert/ToSAM/Rectangle3D.cs
@@ -0,0 +1,23 @@
+﻿namespace SAM.Geometry.Rhino
+{
+    public static partial class Convert
+    {
+        public static Spatial.Rectangle3D ToSAM(this global::Rhino.Geometry.Rectangle3d rectangle3d)
+        {
+            if(!rectangle3d.IsValid)
+            {
+                return null;
+            }
+
+            Spatial.Plane plane = rectangle3d.Plane.ToSAM();
+            if(plane == null)
+            {
+                return null;
+            }
+
+            Planar.Rectangle2D rectangle2D = new Planar.Rectangle2D(new Planar.Point2D(rectangle3d.X.Min, rectangle3d.Y.Min), rectangle3d.Width, rectangle3d.Height);
+
+            return new Spatial.Rectangle3D(plane, rectangle2D);
+        }
+    }
+}
