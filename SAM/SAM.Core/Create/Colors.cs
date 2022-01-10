826fe18a04c7e5ae919865d51afc25b61b128e84
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-01-10 10:49:35 +0000

    Create Colors Method Added

---
+++ SAM/SAM.Core/Create/Colors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace SAM.Core
+{
+    public static partial class Create
+    {
+        public static List<Color> Colors(this Color color, int count, double minBrightness = 0, double maxBrightness = 1)
+        {
+            if(color == Color.Empty || count < 1)
+            {
+                return null;
+            }
+
+            Color color_1 = Query.Brighten(color, maxBrightness);
+            Color color_2 = Query.Brighten(color, minBrightness);
+
+            return Query.Lerps(color_1, color_2, count);
+        }
+    }
+}
