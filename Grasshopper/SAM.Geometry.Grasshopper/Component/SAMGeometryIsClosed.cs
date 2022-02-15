229067e41dd4258ee068689a50dcce50166c26ad
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-02-15 14:19:27 +0000

    IsClosed Grasshopper Component Added

---
+++ Grasshopper/SAM.Geometry.Grasshopper/Component/SAMGeometryIsClosed.cs
@@ -0,0 +1,115 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper.Properties;
+using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Geometry.Grasshopper
+{
+    public class SAMGeometryIsClosed : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("594419a2-307a-4694-95c4-2fdd588ca6e5");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Geometry;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMGeometryIsClosed()
+          : base("SAMGeometry.IsClosed", "SAMGeometry.IsClosed",
+              "Shell Is Closed",
+              "SAM", "Geometry")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                global::Grasshopper.Kernel.Parameters.Param_GenericObject gerenricObject;
+
+                gerenricObject = new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_shell", NickName = "_shell", Description = "SAM Geometry Shell", Access = GH_ParamAccess.item };
+                result.Add(new GH_SAMParam(gerenricObject, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number number;
+
+                number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "tolerance_", NickName = "tolerance_", Description = "Tolerance", Access = GH_ParamAccess.item };
+                number.SetPersistentData(Core.Tolerance.MacroDistance);
+                result.Add(new GH_SAMParam(number, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "isClosed", NickName = "isClosed", Description = "Is Shell Colsed", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("_shell");
+            GH_ObjectWrapper objectWrapper = null;
+            if (index == -1 || !dataAccess.GetData(index, ref objectWrapper) || objectWrapper == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if (!Query.TryGetSAMGeometries(objectWrapper, out List<Shell> shells) || shells == null || shells.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("tolerance_");
+            double tolerance = Core.Tolerance.MacroDistance;
+            if (index != -1)
+            {
+                double tolerance_Temp = double.NaN;
+                if (!dataAccess.GetData(index, ref tolerance_Temp) && !double.IsNaN(tolerance_Temp))
+                    tolerance = tolerance_Temp;
+            }
+
+            index = Params.IndexOfOutputParam("shells");
+            if (index != -1)
+                dataAccess.SetData(index, shells?.FirstOrDefault()?.IsClosed(tolerance));
+        }
+    }
+}
