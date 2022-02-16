4b224806d5ce4b752d7fa3c6f1a7f60ebe91756c
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-02-16 15:11:53 +0000

    Bug Fix for ViewFiled Query

--- SAM/SAM.Geometry/Spatial/Create/LinkedFace3Ds.cs
+++ SAM/SAM.Geometry/Spatial/Create/LinkedFace3Ds.cs
@@ -5,10 +5,10 @@ namespace SAM.Geometry.Spatial
 {
     public static partial class Create
     {
-        public static List<LinkedFace3D> LinkedFace3Ds(this LinkedFace3D solarFace, Vector3D vector3D, IEnumerable<Polygon2D> polygon2Ds, Plane plane, double tolerance = Core.Tolerance.Distance)
+        public static List<LinkedFace3D> LinkedFace3Ds(this LinkedFace3D solarFace, Vector3D vector3D, IEnumerable<Face2D> face2Ds, Plane plane, double tolerance = Core.Tolerance.Distance)
         {
-            List<Polygon2D> polygon2Ds_SolarFace = Planar.Query.Union(polygon2Ds, tolerance);
-            if (polygon2Ds_SolarFace == null || polygon2Ds_SolarFace.Count == 0)
+            List<Face2D> face2Ds_SolarFace = Planar.Query.Union(face2Ds, tolerance);
+            if (face2Ds_SolarFace == null || face2Ds_SolarFace.Count == 0)
             {
                 return null;
             }
@@ -18,31 +18,31 @@ namespace SAM.Geometry.Spatial
             Face2D face2D = plane_SolarFace.Convert(face3D_SolarFace);
 
             List<LinkedFace3D> result = new List<LinkedFace3D>();
-            foreach (Polygon2D polygon2D_SolarFace in polygon2Ds_SolarFace)
+            foreach (Face2D face2D_SolarFace in face2Ds_SolarFace)
             {
-                Polygon3D polygon3D_Shade = plane.Convert(polygon2D_SolarFace);
+                Face3D face3D_Shade = plane.Convert(face2D_SolarFace);
 
-                polygon3D_Shade = plane_SolarFace.Project(polygon3D_Shade, vector3D, tolerance);
-                if (polygon3D_Shade == null || !polygon3D_Shade.IsValid())
+                face3D_Shade = plane_SolarFace.Project(face3D_Shade, vector3D, tolerance);
+                if (face3D_Shade == null || !face3D_Shade.IsValid())
                 {
                     continue;
                 }
 
-                Polygon2D polygon2D_Shade = plane_SolarFace.Convert(polygon3D_Shade);
-                if (polygon2D_Shade == null)
+                Face2D face2D_Shade = plane_SolarFace.Convert(face3D_Shade);
+                if (face2D_Shade == null)
                 {
                     continue;
                 }
 
-                List<Face2D> face2Ds_Shade = Planar.Query.Intersection(face2D, new Face2D(polygon2D_Shade));
+                List<Face2D> face2Ds_Shade = Planar.Query.Intersection(face2D, face2D_Shade);
                 if (face2Ds_Shade == null || face2Ds_Shade.Count == 0)
                 {
                     continue;
                 }
 
-                foreach (Face2D face2D_Shade in face2Ds_Shade)
+                foreach (Face2D face2D_Shade_Temp in face2Ds_Shade)
                 {
-                    result.Add(new LinkedFace3D(solarFace.Guid, plane_SolarFace.Convert(face2D_Shade)));
+                    result.Add(new LinkedFace3D(solarFace.Guid, plane_SolarFace.Convert(face2D_Shade_Temp)));
                 }
             }
 
