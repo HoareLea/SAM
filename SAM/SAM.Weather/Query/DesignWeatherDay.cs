4a1b89d06498bcb25322186f6424a478eb78fc86
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-02-23 08:47:56 +0000

    DesignWeatherDay Query Added

---
+++ SAM/SAM.Weather/Query/DesignWeatherDay.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Weather
+{
+    public static partial class Query
+    {
+        public static WeatherDay DesignWeatherDay(this WeatherData weatherData)
+        {
+            return DesignWeatherDay(weatherData?.WeatherDays());
+        }
+
+        public static WeatherDay DesignWeatherDay(this WeatherYear weatherYear)
+        {
+            return DesignWeatherDay(weatherYear?.WeatherDays);
+        }
+
+        public static WeatherDay DesignWeatherDay(this IEnumerable<WeatherDay> weatherDays)
+        {
+            if (weatherDays == null || weatherDays.Count() == 0)
+            {
+                return null;
+            }
+
+            double dryBulbTemperature_Max = double.MinValue;
+            double solarRadiation_Max = double.MinValue;
+            WeatherDay weatherDay_DryBulbTemperature = null;
+            WeatherDay weatherDay_SolarRadiation = null;
+            foreach (WeatherDay weatherDay in weatherDays)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    if (weatherDay.TryGetValue(WeatherDataType.DryBulbTemperature, i, out double dryBulbTemperature) && dryBulbTemperature > dryBulbTemperature_Max)
+                    {
+                        weatherDay_DryBulbTemperature = weatherDay;
+                        dryBulbTemperature_Max = dryBulbTemperature;
+                    }
+
+                    double solarRadiation = weatherDay.CalculatedGlobalRadiation(i);
+                    if (!double.IsNaN(solarRadiation) && solarRadiation > solarRadiation_Max)
+                    {
+                        weatherDay_SolarRadiation = weatherDay;
+                        solarRadiation_Max = solarRadiation;
+                    }
+                }
+            }
+
+            if (weatherDay_DryBulbTemperature == null)
+            {
+                return null;
+            }
+
+            WeatherDay result = new WeatherDay(weatherDay_DryBulbTemperature);
+            if (weatherDay_SolarRadiation != null)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    if (weatherDay_SolarRadiation.Contains(WeatherDataType.DiffuseSolarRadiation))
+                    {
+                        result[WeatherDataType.DiffuseSolarRadiation][i] = weatherDay_SolarRadiation[WeatherDataType.DiffuseSolarRadiation][i];
+                    }
+
+                    if (weatherDay_SolarRadiation.Contains(WeatherDataType.DirectSolarRadiation))
+                    {
+                        result[WeatherDataType.DirectSolarRadiation][i] = weatherDay_SolarRadiation[WeatherDataType.DirectSolarRadiation][i];
+                    }
+
+                    if (weatherDay_SolarRadiation.Contains(WeatherDataType.GlobalSolarRadiation))
+                    {
+                        result[WeatherDataType.GlobalSolarRadiation][i] = weatherDay_SolarRadiation[WeatherDataType.GlobalSolarRadiation][i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SAM/SAM.Weather/Query/WeatherDays.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Weather
+{
+    public static partial class Query
+    {
+        public static List<WeatherDay> WeatherDays(this WeatherData weatherData)
+        {
+            if (weatherData == null)
+                return null;
+
+            List<WeatherYear> weatherYears = weatherData?.WeatherYears;
+            if (weatherYears == null || weatherYears.Count == 0)
+            {
+                return null;
+            }
+
+
+            List<WeatherDay> result = new List<WeatherDay>();
+            foreach (WeatherYear weatherYear in weatherYears)
+            {
+                List<WeatherDay> weatherDays_Temp = weatherYear?.WeatherDays;
+                if(weatherDays_Temp == null || weatherDays_Temp.Count == 0)
+                {
+                    continue;
+                }
+
+                result.AddRange(weatherDays_Temp);
+            }
+
+            return result;
+        }
+    }
+}
