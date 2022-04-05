9bd82fc614b93e20a5d43adb9f110fbe1d6e64f4
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-05 17:01:01 +0100

    MaxThickness Query Added for ApertureConstruction

---
+++ SAM/SAM.Analytical/Query/MaxThickness.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double MaxThickness(this ApertureConstruction apertureConstruction)
+        {
+            if(apertureConstruction == null)
+            {
+                return double.NaN;
+            }
+
+            List<ConstructionLayer> constructionLayers_Pane = apertureConstruction.PaneConstructionLayers;
+            List<ConstructionLayer> constructionLayers_Frame = apertureConstruction.PaneConstructionLayers;
+            if (constructionLayers_Pane == null && constructionLayers_Frame == null)
+            {
+                return double.NaN;
+            }
+
+            double result = 0;
+            if(constructionLayers_Pane != null)
+            {
+                result = constructionLayers_Pane.ConvertAll(x => x.Thickness).Sum();
+            }
+
+            if (constructionLayers_Frame != null)
+            {
+                double thickness = constructionLayers_Frame.ConvertAll(x => x.Thickness).Sum();
+                result = System.Math.Max(result, thickness);
+            }
+
+            return result;
+        }
+    }
+}
