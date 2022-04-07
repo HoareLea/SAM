261edaeb01c0ec4e1048dcd6aa8afd6cb9df10df
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-07 13:41:46 +0100

    Bug Fixed for ApertureConstructionParameter

--- SAM/SAM.Analytical/Enums/Parameter/ApertureConstructionParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/ApertureConstructionParameter.cs
@@ -6,7 +6,7 @@ namespace SAM.Analytical
     [AssociatedTypes(typeof(ApertureConstruction)), Description("ApertureConstruction Parameter")]
     public enum ApertureConstructionParameter
     {
-        [ParameterProperties("Dafault Panel Type", "Dafault Panel Type"), ParameterValue(Core.ParameterType.String)] DefaultPanelType,
+        [ParameterProperties("Default Panel Type", "Default Panel Type"), ParameterValue(Core.ParameterType.String)] DefaultPanelType,
         [ParameterProperties("Description", "Description"), ParameterValue(Core.ParameterType.String)] Description,
         [ParameterProperties("Transparent", "Transparent"), ParameterValue(Core.ParameterType.Boolean)] Transparent,
         [ParameterProperties("Color", "Color"), ParameterValue(Core.ParameterType.Color)] Color,
