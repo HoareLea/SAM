319c3b48c8d6f0b4b14c4328a84db33b026cbd50
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-04-08 09:40:24 +0100

    Unnecessary Material Parameters Removed

--- SAM/SAM.Analytical/Enums/Parameter/MaterialParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/MaterialParameter.cs
@@ -6,8 +6,8 @@ namespace SAM.Analytical
     [AssociatedTypes(typeof(Core.Material)), Description("Material Parameter")]
     public enum MaterialParameter
     {
-        [ParameterProperties("Type Name", "Type Name"), ParameterValue(Core.ParameterType.String)] TypeName,
-        [ParameterProperties("Description", "Description"), ParameterValue(Core.ParameterType.String)] Description,
+        //[ParameterProperties("Type Name", "Type Name"), ParameterValue(Core.ParameterType.String)] TypeName,
+        //[ParameterProperties("Description", "Description"), ParameterValue(Core.ParameterType.String)] Description,
         [ParameterProperties("Vapour Diffusion Factor", "Vapour Diffusion Factor"), DoubleParameterValue(0)] VapourDiffusionFactor,
     }
 }
