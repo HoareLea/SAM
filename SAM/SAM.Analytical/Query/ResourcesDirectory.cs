7d082016d4ced44501f7b0121a06ebdfc0d26641
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-04 10:32:30 +0100

    ResourcesDIrectory Query Added for SAM Analytical

---
+++ SAM/SAM.Analytical/Query/ResourcesDirectory.cs
@@ -0,0 +1,10 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static string ResourcesDirectory()
+        {
+            return Core.Query.ResourcesDirectory(Core.ActiveSetting.Setting, typeof(ActiveSetting).Assembly);
+        }
+    }
+}
