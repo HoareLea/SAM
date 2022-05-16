0592e956ec2a066385c09386dc47bfc9183e02e0
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-16 16:06:02 +0100

    Systems Query Added for AdjacencyCluster and AnalyticalModel

---
+++ SAM/SAM.Analytical/Query/Systems.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static List<T> Systems<T>(this AdjacencyCluster adjacencyCluster, Space space) where T : Core.ISystem
+        {
+            if(adjacencyCluster == null || space == null)
+            {
+                return null;
+            }
+
+            return adjacencyCluster.GetRelatedObjects<T>(space);
+        }
+
+        public static List<T> Systems<T>(this AnalyticalModel analyticalModel, Space space) where T : Core.ISystem
+        {
+            if (analyticalModel == null || space == null)
+            {
+                return null;
+            }
+
+            return analyticalModel.AdjacencyCluster?.GetRelatedObjects<T>(space);
+        }
+    }
+}
