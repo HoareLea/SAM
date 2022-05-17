86c5d0fdb676d6c5f5155e9bdbbcba9438ab77df
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-17 11:06:41 +0100

    Missing Queries Added

---
+++ SAM/SAM.Analytical/Query/SpecificHeatTransferGain.cs
@@ -0,0 +1,29 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double SpecificBuildingHeatTransferGain(this SpaceSimulationResult spaceSimulationResult)
+        {
+            if (spaceSimulationResult == null)
+                return double.NaN;
+
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.BuildingHeatTransfer, out double buildingHeatTransfer) || double.IsNaN(buildingHeatTransfer))
+            {
+                return double.NaN;
+            }
+
+            double area = double.NaN;
+            if (!spaceSimulationResult.TryGetValue(SpaceSimulationResultParameter.Area, out area) || double.IsNaN(area))
+            {
+                return double.NaN;
+            }
+
+            if (area == 0)
+            {
+                return 0;
+            }
+
+            return buildingHeatTransfer / area;
+        }
+    }
+}
