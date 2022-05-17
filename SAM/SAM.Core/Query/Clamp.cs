340b494861a7e8d38ae39fb04520b2136b050ad2
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-17 15:35:20 +0100

    Clamp Range Implemented

--- SAM/SAM.Core/Query/Clamp.cs
+++ SAM/SAM.Core/Query/Clamp.cs
@@ -3,25 +3,35 @@
     public static partial class Query
     {
         /// <summary>
-        /// Limits given value to top and bottom
+        /// Limits given value to min and max
         /// </summary>
         /// <param name="value">Value to be clamped</param>
-        /// <param name="bottom">Bottom Value</param>
-        /// <param name="top">Top Value</param>
-        /// <returns></returns>
-        public static double Clamp(this double value, double bottom, double top)
+        /// <param name="min">Minimal Value</param>
+        /// <param name="max">Maximal Value</param>
+        /// <returns>Clamp value</returns>
+        public static T Clamp<T>(this T value, T min, T max)
         {
-            if (value < bottom)
+            if ((dynamic)value < (dynamic)min)
             {
-                return bottom;
+                return min;
             }
 
-            if (value > top)
+            if ((dynamic)value > (dynamic)max)
             {
-                return top;
+                return max;
             }
 
             return value;
+        }
+
+        public static Range<T> Clamp<T>(this Range<T> range, T min, T max)
+        {
+            if(range == null)
+            {
+                return null;
+            }
+
+            return new Range<T>(Clamp(range.Min, min, max), Clamp(range.Max, min, max));
         }
     }
 }
