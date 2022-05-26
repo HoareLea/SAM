b1cd39eddc7aea70a792e138a54fc53bc7b5f258
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-05-26 15:04:22 +0100

    Units Added for WeatherDataType Enum

--- SAM/SAM.Weather/Enums/WeatherDataType.cs
+++ SAM/SAM.Weather/Enums/WeatherDataType.cs
@@ -13,51 +13,51 @@ namespace SAM.Weather
         /// <summary>
         /// Global Solar Radiation [W/m2]
         /// </summary>
-        [Description("Global Solar Radiation")] GlobalSolarRadiation,
+        [Description("Global Solar Radiation [W/m2]")] GlobalSolarRadiation,
         
         /// <summary>
         /// Diffuse Solar Radiation [W/m2]
         /// </summary>
-        [Description("Diffuse Solar Radiation")] DiffuseSolarRadiation,
+        [Description("Diffuse Solar Radiation [W/m2]")] DiffuseSolarRadiation,
 
         /// <summary>
         /// Direct Solar Radiation [W/m2]
         /// </summary>
-        [Description("Direct Solar Radiation")] DirectSolarRadiation,
+        [Description("Direct Solar Radiation [W/m2]")] DirectSolarRadiation,
 
         /// <summary>
         /// Cloud Cover [0-1]
         /// </summary>
-        [Description("Cloud Cover")] CloudCover,
+        [Description("Cloud Cover [0-1]")] CloudCover,
 
         /// <summary>
         /// Dry Bulb Temperature [C]
         /// </summary>
-        [Description("Dry Bulb Temperature")] DryBulbTemperature,
+        [Description("Dry Bulb Temperature [°C]")] DryBulbTemperature,
 
         /// <summary>
         /// Wet Bulb Temperature (Dew Point Temperature) [C]
         /// </summary>
-        [Description("Wet Bulb Temperature")] WetBulbTemperature,
+        [Description("Wet Bulb Temperature [°C]")] WetBulbTemperature,
 
         /// <summary>
         /// Relative Humidity [%]
         /// </summary>
-        [Description("Relative Humidity")] RelativeHumidity,
+        [Description("Relative Humidity [%]")] RelativeHumidity,
 
         /// <summary>
         /// Wind Speed [m/s]
         /// </summary>
-        [Description("Wind Speed")] WindSpeed,
+        [Description("Wind Speed [m/s]")] WindSpeed,
 
         /// <summary>
         /// Wind Direction [Degree]
         /// </summary>
-        [Description("Wind Direction")] WindDirection,
+        [Description("Wind Direction [°]")] WindDirection,
 
         /// <summary>
         /// Atmospheric Pressure [Pa]
         /// </summary>
-        [Description("Atmospheric Pressure")] AtmosphericPressure,
+        [Description("Atmospheric Pressure [Pa]")] AtmosphericPressure,
     }
 }
