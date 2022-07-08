23f83fffd4273f20adc51cdd6465364e854e7e7a
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-07-08 09:50:21 +0100

    Calculate_ByMaxStep Implemented

--- SAM/SAM.Core/Query/Calculate.cs
+++ SAM/SAM.Core/Query/Calculate.cs
@@ -32,7 +32,7 @@ namespace SAM.Core
 
             count = 0;
 
-            if(value == value_Min)
+            if (value == value_Min)
             {
                 return value_Min;
             }
@@ -99,58 +99,51 @@ namespace SAM.Core
             return Calculate(func, value, start, stop, out int count, out double calculationValue, maxCount, tolerance);
         }
 
-        private static double Calculate_WIP(this Func<double, double> func, double value, double start, double stop, out int count, out double calculationValue, int maxCount = 100, double tolerance = Core.Tolerance.MacroDistance)
+        public static double Calculate_ByMaxStep(this Func<double, double> func, double value, double start, double maxStep, out int count, out double calculationValue, int maxCount = 100, double tolerance = Core.Tolerance.MacroDistance)
         {
             calculationValue = double.NaN;
             count = -1;
 
-            if (func == null || double.IsNaN(start) || double.IsNaN(stop))
+            if (func == null || double.IsNaN(start) || double.IsNaN(maxStep))
             {
                 return double.NaN;
             }
 
-            double min = Math.Min(start, stop);
-            double max = Math.Max(start, stop);
-
-            double value_Min_Temp = func.Invoke(min);
-            double value_Max_Temp = func.Invoke(max);
-
-            int sign = Math.Sign(value_Max_Temp - value_Min_Temp);
-
-            double value_Min = Math.Min(value_Min_Temp, value_Max_Temp);
-            double value_Max = Math.Max(value_Min_Temp, value_Max_Temp);
-
-            if (value < value_Min || value > value_Max)
+            double value_Start = func.Invoke(start);
+            if (double.IsNaN(value_Start))
             {
                 return double.NaN;
             }
 
-            count = 0;
-
-            if (value == value_Min)
+            if (Math.Abs(value - value_Start) <= tolerance)
             {
-                return value_Min;
+                count = 0;
+                return value_Start;
             }
 
-            if (value == value_Max)
+            double step = maxStep;
+            double result = start + step;
+
+            double value_Next = func.Invoke(result);
+            if (double.IsNaN(value_Next))
             {
-                return value_Max;
+                return double.NaN;
             }
 
-            double difference = (max - min) / 2;
-            double result = min + difference;
+            if (Math.Abs(value - value_Next) <= tolerance)
+            {
+                count = 0;
+                return value_Next;
+            }
 
+            int sign = Math.Sign(value_Next - value_Start);
             int sign_Temp = 0;
 
+            count = 0;
             for (int i = 1; i <= maxCount; i++)
             {
                 count = i;
-
-                if (double.IsNaN(result))
-                {
-                    return double.NaN;
-                }
-
+                
                 calculationValue = func.Invoke(result);
                 if (double.IsNaN(calculationValue))
                 {
@@ -164,29 +157,33 @@ namespace SAM.Core
 
                 if (sign_Temp == 0)
                 {
-                    difference = difference / 2;
                     sign_Temp = Math.Sign(value - calculationValue) * sign;
-                    result = result + (difference * sign_Temp);
+                    result = result + (step * sign_Temp);
                 }
                 else
                 {
                     int sign_Temp_New = Math.Sign(value - calculationValue) * sign;
                     if (sign_Temp == sign_Temp_New)
                     {
-                        result = result + difference * sign_Temp_New;
+                        result = result + step * sign_Temp_New;
                         continue;
                     }
                     else
                     {
                         sign_Temp = sign_Temp_New;
-                        difference = difference / 2;
-                        result = result + (difference * sign_Temp);
+                        step = step / 2;
+                        result = result + (step * sign_Temp);
                     }
 
                 }
             }
 
             return result;
+        }
+
+        public static double Calculate_ByMaxStep(this Func<double, double> func, double value, double start, double maxStep, int maxCount = 100, double tolerance = Core.Tolerance.MacroDistance)
+        {
+            return Calculate_ByMaxStep(func, value, start, maxStep, out int count, out double calculationValue, maxCount, tolerance);
         }
     }
 }
