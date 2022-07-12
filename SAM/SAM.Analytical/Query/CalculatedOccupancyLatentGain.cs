aafff2bc6f63f6000345f86cda7d8439ec103990
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-07-12 14:51:52 +0100

    CalculatedOccupancyLatentGain Query Implemented

---
+++ SAM/SAM.Analytical/Query/CalculatedOccupancyLatentGain.cs
@@ -0,0 +1,27 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        public static double CalculatedOccupancyLatentGain(this Space space)
+        {
+            if (space == null)
+                return double.NaN;
+
+            InternalCondition internalCondition = space.InternalCondition;
+            if (internalCondition == null)
+                return double.NaN;
+
+            double gain_1 = double.NaN;
+            if(internalCondition.TryGetValue(InternalConditionParameter.OccupancyLatentGainPerPerson, out gain_1) && !double.IsNaN(gain_1))
+            {
+                double occupancy = space.CalculatedOccupancy();
+                if(!double.IsNaN(occupancy))
+                {
+                    gain_1 = gain_1 * occupancy;
+                }
+            }
+
+            return gain_1;
+        }
+    }
+}
