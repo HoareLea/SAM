302f4fd4ed3ddc730269f6bffc2e31a0643647a4
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-07-18 09:02:06 +0100

    UserSAMTemporaryDirectory Implemented

---
+++ SAM/SAM.Core/Query/UserSAMTemporaryDirectory.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static string UserSAMTemporaryDirectory()
+        {
+            return Path.Combine(Path.GetTempPath(), "SAM");
+        }
+    }
+}
