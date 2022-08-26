81bb24dd416cf4cb0782d41af784c4c7a5bc22a0
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-08-26 13:19:13 +0100

    AddPanels Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalAddPanels.cs
@@ -0,0 +1,158 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalAddPanels : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("5ae3e189-02d6-43de-ab1f-cccb2c9055d0");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.4";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalAddPanels()
+          : base("SAMAnalytical.AddPanels", "SAMAnalytical.AddPanels",
+              "Add Panels to Analytical Model or AdjacencyCluster",
+              "SAM WIP", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "_analytical", NickName = "_analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject { Name = "_face3Ds", NickName = "_face3Ds", Description = "SAM Geometry Face3Ds", Access = GH_ParamAccess.list}, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooConstructionParam() { Name = "construction_", NickName = "construction_", Description = "SAM Analytical Construction", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new GooSpaceParam() { Name = "spaces_", NickName = "spaces_", Description = "SAM Analytical Spaces", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject { Name = "analytical", NickName = "analytical", Description = "SAM Analytical", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "panels ", NickName = "panels", Description = "SAM Analytical Panels", Access = GH_ParamAccess.list}, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("_analytical");
+            SAMObject sAMObject = null;
+            if (index == -1 || !dataAccess.GetData(index, ref sAMObject) || sAMObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+
+            index = Params.IndexOfInputParam("_face3Ds");
+            List<GH_ObjectWrapper> objectWrappers = new List<GH_ObjectWrapper>();
+            if (index == -1 || !dataAccess.GetDataList(index, objectWrappers) || objectWrappers == null || objectWrappers.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            Construction construction = null;
+            index = Params.IndexOfInputParam("construction_");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref construction);
+            }
+
+            List<Face3D> face3Ds = new List<Face3D>();
+
+            foreach(GH_ObjectWrapper objectWrapper in objectWrappers)
+            {
+                if(Geometry.Grasshopper.Query.TryGetSAMGeometries(objectWrapper, out List<Face3D> face3Ds_Temp) && face3Ds_Temp != null)
+                {
+                    face3Ds.AddRange(face3Ds_Temp);
+                }
+            }
+
+            List<Space> spaces = null;
+            index = Params.IndexOfInputParam("spaces_");
+            if(index != -1)
+            {
+                List<Space> spaces_Temp = new List<Space>();
+
+                if (dataAccess.GetDataList(index, spaces_Temp) && spaces_Temp != null && spaces_Temp.Count != 0)
+                {
+                    spaces = spaces_Temp;
+                }
+            }
+
+            List<Panel> panels = null;
+
+            AdjacencyCluster adjacencyCluster = sAMObject is AnalyticalModel ? ((AnalyticalModel)sAMObject).AdjacencyCluster : sAMObject as AdjacencyCluster;
+            if(adjacencyCluster != null)
+            {
+                if(sAMObject is AdjacencyCluster)
+                {
+                    adjacencyCluster = new AdjacencyCluster(adjacencyCluster);
+                }
+
+                panels = adjacencyCluster.AddPanels(face3Ds, construction, spaces, Tolerance.MacroDistance, Tolerance.Angle, Tolerance.Distance, Tolerance.MacroDistance);
+                if (sAMObject is AnalyticalModel)
+                {
+                    sAMObject = new AnalyticalModel((AnalyticalModel)sAMObject, adjacencyCluster);
+                }
+                else
+                {
+                    sAMObject = adjacencyCluster;
+                }
+            }
+
+            index = Params.IndexOfOutputParam("analytical");
+            if (index != -1)
+                dataAccess.SetData(index, sAMObject);
+
+            index = Params.IndexOfOutputParam("panels");
+            if (index != -1)
+                dataAccess.SetDataList(index, panels);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/AddPanels.cs
@@ -0,0 +1,150 @@
+﻿using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Panel> AddPanels(this AdjacencyCluster adjacencyCluster, IEnumerable<Face3D> face3Ds, Construction construction, IEnumerable<Space> spaces = null, double silverSpacing = Core.Tolerance.MacroDistance, double tolerance_Angle = Core.Tolerance.Angle, double tolerance_Distance = Core.Tolerance.Distance, double tolerance_Snap = Core.Tolerance.MacroDistance)
+        {
+            if(adjacencyCluster == null || face3Ds == null)
+            {
+                return null;
+            }
+
+            List<Space> spaces_Temp = spaces == null ? adjacencyCluster.GetSpaces() : new List<Space>(spaces);
+            if(spaces_Temp == null || spaces_Temp.Count == 0)
+            {
+                return null;
+            }
+
+            List<Tuple<Face3D, BoundingBox3D>> tuples = face3Ds.ToList().FindAll(x => x != null).ConvertAll(x => new Tuple<Face3D, BoundingBox3D>(x, x.GetBoundingBox()));
+            BoundingBox3D boundingBox3D = new BoundingBox3D(tuples.ConvertAll(x => x.Item2));
+
+
+            List<Panel> result = new List<Panel>();
+            foreach(Space space in spaces_Temp)
+            {
+                Shell shell = adjacencyCluster.Shell(space);
+
+                BoundingBox3D boundingBox3D_Shell = shell?.GetBoundingBox();
+                if(boundingBox3D_Shell == null)
+                {
+                    continue;
+                }
+
+                if(!boundingBox3D.InRange(boundingBox3D_Shell, tolerance_Snap))
+                {
+                    continue;
+                }
+
+                List<Face3D> face3Ds_Temp = tuples.FindAll(x => x.Item2.InRange(boundingBox3D_Shell, tolerance_Snap))?.ConvertAll(x => x.Item1);
+                if(face3Ds_Temp == null || face3Ds_Temp.Count == 0)
+                {
+                    continue;
+                }
+
+                List<Shell> shells_Split = Geometry.Spatial.Query.Split(shell, face3Ds_Temp, tolerance_Snap, tolerance_Angle, tolerance_Distance);
+                if(shells_Split == null || shells_Split.Count < 2)
+                {
+                    continue;
+                }
+
+                List<Panel> panels = adjacencyCluster.GetPanels(space);
+
+                PanelType panelType = PanelType.Air;
+                if(construction != null)
+                {
+                    panelType = construction.PanelType();
+                }
+
+                if(panelType == PanelType.Undefined)
+                {
+                    panelType = PanelType.Air;
+                }
+                else if(panelType == PanelType.Wall)
+                {
+                    panelType = PanelType.WallInternal;
+                }
+                else if(panelType == PanelType.Floor)
+                {
+                    panelType = PanelType.FloorInternal;
+                }
+
+                List<Panel> panels_New = new List<Panel>();
+                for(int i = 0; i < shells_Split.Count; i++)
+                {
+                    Shell shell_Split = shells_Split[i];
+
+                    Point3D point3D = shell_Split.CalculatedInternalPoint3D(tolerance_Snap, tolerance_Distance);
+                    if (point3D == null)
+                    {
+                        continue;
+                    }
+                    string name = space.Name;
+                    if(name != null)
+                    {
+                        name += string.Format(" {0}", i + 1);
+                    }
+
+                    Space space_Shell = new Space(Guid.NewGuid(), space, name, point3D);
+
+                    adjacencyCluster.AddObject(space_Shell);
+
+                    List<Face3D> face3Ds_Shell = shell_Split.Face3Ds;
+                    foreach(Face3D face3D_Shell in face3Ds_Shell)
+                    {
+                        Point3D point3D_Face3D = face3D_Shell.GetInternalPoint3D(tolerance_Distance);
+
+                        Panel panel = panels.Find(x => x.Face3D.On(point3D_Face3D, tolerance_Snap));
+                        List<Space> spaces_Panel = null;
+                        if (panel == null)
+                        {
+                            panel = panels_New.Find(x => x.Face3D.On(point3D_Face3D, tolerance_Snap));
+                            if (panel == null)
+                            {
+                                panel = Create.Panel(construction, panelType, face3D_Shell);
+                                panels_New.Add(panel);
+                            }
+                        }
+                        else
+                        {
+                            spaces_Panel = adjacencyCluster.GetSpaces(panel);
+                            spaces_Panel?.RemoveAll(x => x != null && x.Guid == space.Guid);
+
+                            panel = Create.Panel(Guid.NewGuid(), panel, new PlanarBoundary3D(face3D_Shell));
+                        }
+
+                        if(spaces_Panel == null)
+                        {
+                            spaces_Panel = new List<Space>();
+                        }
+
+                        spaces_Panel.Add(space_Shell);
+
+                        adjacencyCluster.AddObject(panel);
+
+                        foreach(Space space_Panel in spaces_Panel)
+                        {
+                            adjacencyCluster.AddRelation(space_Panel, panel);
+                        }
+                    }
+                }
+
+                result.AddRange(panels_New);
+
+                foreach(Panel panel in panels)
+                {
+                    adjacencyCluster.RemoveObject<Panel>(panel.Guid);
+                }
+
+                adjacencyCluster.RemoveObject<Space>(space.Guid);
+            }
+
+            return result;
+        }
+    }
+}
