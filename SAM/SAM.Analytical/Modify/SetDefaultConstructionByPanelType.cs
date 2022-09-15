54fb0836ec862e04ada243e2980e7528c0f73297
michaldengusiak <tassimulation@gmail.com>
2022-09-15 14:57:30 +0100

    Update SetDefaultConstructionByPanelType.cs

--- SAM/SAM.Analytical/Modify/SetDefaultConstructionByPanelType.cs
+++ SAM/SAM.Analytical/Modify/SetDefaultConstructionByPanelType.cs
@@ -30,120 +30,133 @@ namespace SAM.Analytical
 
             foreach (Panel panel in panels)
             {
-                Construction construction = panel.Construction;
-                if (construction == null)
-                    continue;
-                
                 PanelType panelType = panel.PanelType;
+                if(panelType == PanelType.Undefined)
+                {
+                    continue;
+                }
 
                 bool update = false;
 
-                double elevation = double.NaN;
-
-                switch (panelType)
+                Construction construction = panel.Construction;
+                if (construction == null)
                 {
-                    case PanelType.Air:
-                        update = construction != null;
-                        break;
-                    case PanelType.Ceiling:
-                        panelType = Query.PanelType(panel.Normal);
-                        switch (panelType.PanelGroup())
-                        {
-                            case PanelGroup.Wall:
-                                update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
-                                panelType = PanelType.WallExternal;
-                                break;
-                            case PanelGroup.Floor:
-                                update = !construction.Name.StartsWith(floorExposedConstructionPrefix);
-                                panelType = PanelType.FloorExposed;
-                                break;
-                            case PanelGroup.Roof:
-                                update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
-                                panelType = PanelType.Roof;
-                                break;
-                        }
-                        break;
-                    case PanelType.CurtainWall:
-                        update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
-                        panelType = PanelType.Roof;
-                        break;
-                    case PanelType.Wall:
-                        elevation = Geometry.Spatial.Query.MaxElevation(panel);
+                    if(panelType != PanelType.Air)
+                    {
+                        update = true;
+                    }
+                }
+                else
+                {
+                    double elevation = double.NaN;
 
-                        if (elevation <= 0)
-                        {
+                    switch (panelType)
+                    {
+                        case PanelType.Air:
+                            update = construction != null;
+                            break;
+                        case PanelType.Ceiling:
+                            panelType = Query.PanelType(panel.Normal);
+                            switch (panelType.PanelGroup())
+                            {
+                                case PanelGroup.Wall:
+                                    update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                    panelType = PanelType.WallExternal;
+                                    break;
+                                case PanelGroup.Floor:
+                                    update = !construction.Name.StartsWith(floorExposedConstructionPrefix);
+                                    panelType = PanelType.FloorExposed;
+                                    break;
+                                case PanelGroup.Roof:
+                                    update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
+                                    panelType = PanelType.Roof;
+                                    break;
+                            }
+                            break;
+                        case PanelType.CurtainWall:
                             update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
-                            panelType = PanelType.UndergroundWall;
-                        }
-                        else
-                        {
-                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
-                            panelType = PanelType.WallExternal;
-                        }
-                        break;
-                    case PanelType.WallExternal:
-                        update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
-                        break;
-                    case PanelType.WallInternal:
-                        update = !panel.Construction.Name.StartsWith(wallInternalConstructionPrefix);
-                        break;
-                    case PanelType.Roof:
-                        update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
-                        break;
-                    case PanelType.Floor:
-                        PanelType panelType_Normal = Query.PanelType(panel.Normal);
-                        if (panelType_Normal == PanelType.Floor)
-                        {
+                            panelType = PanelType.Roof;
+                            break;
+                        case PanelType.Wall:
                             elevation = Geometry.Spatial.Query.MaxElevation(panel);
 
-                            if (elevation == 0)
+                            if (elevation <= 0)
                             {
-                                update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
-                                panelType = PanelType.SlabOnGrade;
-                            }
-                            else if (elevation < 0)
-                            {
-                                update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
-                                panelType = PanelType.SlabOnGrade;
+                                update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                panelType = PanelType.UndergroundWall;
                             }
                             else
                             {
-                                panelType = PanelType.FloorExposed;
+                                update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                panelType = PanelType.WallExternal;
                             }
-                        }
-                        else if (panelType_Normal == PanelType.Roof)
-                        {
-                            panelType = PanelType.Roof;
-                        }
-                        break;
-                    case PanelType.SlabOnGrade:
-                        update = construction.Name.Equals(floorConstructionName) || !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
-                        break;
-                    case PanelType.UndergroundSlab:
-                        update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
-                        break;
-                    case PanelType.FloorInternal:
-                        update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
-                        break;
-                    case PanelType.UndergroundCeiling:
-                        update = !construction.Name.StartsWith(floorInternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
-                        break;
-                    case PanelType.FloorExposed:
-                        update = !construction.Name.StartsWith(floorExposedConstructionPrefix) || construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
-                        break;
-                    case PanelType.FloorRaised:
-                        update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
-                        break;
-                    case PanelType.Shade:
-                        update = !construction.Name.Contains("_SHD_");
-                        break;
-                    case PanelType.UndergroundWall:
-                        update = !construction.Name.StartsWith(wallExternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
-                        break;
+                            break;
+                        case PanelType.WallExternal:
+                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                            break;
+                        case PanelType.WallInternal:
+                            update = !panel.Construction.Name.StartsWith(wallInternalConstructionPrefix);
+                            break;
+                        case PanelType.Roof:
+                            update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
+                            break;
+                        case PanelType.Floor:
+                            PanelType panelType_Normal = Query.PanelType(panel.Normal);
+                            if (panelType_Normal == PanelType.Floor)
+                            {
+                                elevation = Geometry.Spatial.Query.MaxElevation(panel);
+
+                                if (elevation == 0)
+                                {
+                                    update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
+                                    panelType = PanelType.SlabOnGrade;
+                                }
+                                else if (elevation < 0)
+                                {
+                                    update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
+                                    panelType = PanelType.SlabOnGrade;
+                                }
+                                else
+                                {
+                                    panelType = PanelType.FloorExposed;
+                                }
+                            }
+                            else if (panelType_Normal == PanelType.Roof)
+                            {
+                                panelType = PanelType.Roof;
+                            }
+                            break;
+                        case PanelType.SlabOnGrade:
+                            update = construction.Name.Equals(floorConstructionName) || !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.UndergroundSlab:
+                            update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.FloorInternal:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
+                            break;
+                        case PanelType.UndergroundCeiling:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
+                            break;
+                        case PanelType.FloorExposed:
+                            update = !construction.Name.StartsWith(floorExposedConstructionPrefix) || construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.FloorRaised:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
+                            break;
+                        case PanelType.Shade:
+                            update = !construction.Name.Contains("_SHD_");
+                            break;
+                        case PanelType.UndergroundWall:
+                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
+                            break;
+                    }
                 }
 
                 if (!update)
+                {
                     continue;
+                }
 
                 construction = Query.DefaultConstruction(panelType);
                 Panel panel_New = new Panel(panel, construction);
