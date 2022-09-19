546ea663086eb3c0ffa8ef839ad956948d1d26e9
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-09-19 10:44:05 +0100

    TryConvert to Segment3D Implemented

--- SAM/SAM.Geometry/Spatial/Query/TryConvert.cs
+++ SAM/SAM.Geometry/Spatial/Query/TryConvert.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SAM.Geometry.Spatial
 {
@@ -48,6 +49,45 @@ namespace SAM.Geometry.Spatial
                 if(sAMGeometry3D is IFace3DObject)
                 {
                     sAMGeometry3Ds = new List<T>() { (T)(object)((IFace3DObject)sAMGeometry3D).Face3D };
+                }
+            }
+
+            if(typeof(T) == typeof(Segment3D))
+            {
+                if (sAMGeometry3D is Face3D)
+                {
+                    Face3D face3D = (Face3D)sAMGeometry3D;
+                    List<IClosedPlanar3D> closedPlanar3Ds = face3D.GetEdge3Ds();
+                    if (closedPlanar3Ds == null)
+                    {
+                        return true;
+                    }
+
+                    sAMGeometry3Ds = new List<T>();
+
+                    foreach (IClosedPlanar3D closedPlanar3D in closedPlanar3Ds)
+                    {
+                        if(closedPlanar3D == null)
+                        {
+                            continue;
+                        }
+
+                        ISegmentable3D segmentable3D = closedPlanar3D as ISegmentable3D;
+                        if(segmentable3D == null)
+                        {
+                            return false;
+                        }
+
+                        sAMGeometry3Ds.AddRange(segmentable3D.GetSegments().Cast<T>());
+                    }
+
+                    return true;
+                }
+
+                if(typeof(ISegmentable3D).IsAssignableFrom(sAMGeometry3D.GetType()))
+                {
+                    ISegmentable3D segmentable3D = sAMGeometry3D as ISegmentable3D;
+                    sAMGeometry3Ds.AddRange(segmentable3D.GetSegments().Cast<T>());
                 }
             }
 
