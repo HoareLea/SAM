16601025e13fcd4b5052f15976ba26f80f52a77c
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-09-20 11:28:30 +0100

    Page Enums Added

---
+++ SAM/SAM.Core/Enums/PageOrientation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Core
+{
+    [Description("Page Orientation")]
+    public enum PageOrientation
+    {
+        [Description("Undefined")] Undefined,
+        [Description("Portrait")] Portrait,
+        [Description("Landscape")] Landscape,
+    }
+}
---
+++ SAM/SAM.Core/Enums/PageSize.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SAM.Core
+{
+    [Description("Page Size")]
+    public enum PageSize
+    {
+        [Description("Undefined")] Undefined,
+        [Description("A4")] A4,
+        [Description("A3")] A3,
+    }
+}
