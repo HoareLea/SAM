c8fc5d135ace4aaf01722f5c889e1468bc7c9a8d
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-10-11 10:51:06 +0100

    OpeningProperties Added WIP

---
+++ SAM/SAM.Analytical/Classes/OpeningProperties.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using SAM.Core;
+
+namespace SAM.Analytical
+{
+    /// <summary>
+    /// Opening Properties
+    /// </summary>
+    public class OpeningProperties : IJSAMObject
+    {
+        public double OpenableArea { get; set; }
+
+        public OpeningProperties()
+        {
+
+        }
+
+        public OpeningProperties(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public OpeningProperties(OpeningProperties openingProperties)
+        {
+            if(openingProperties != null)
+            {
+                OpenableArea = openingProperties.OpenableArea;
+            }
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if(jObject == null)
+            {
+                return false;
+            }
+
+            if(jObject.ContainsKey("OpenableArea"))
+            {
+                OpenableArea = jObject.Value<double>("OpenableArea");
+            }
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+
+            if(!double.IsNaN(OpenableArea))
+            {
+                jObject.Add("OpenableArea", OpenableArea);
+            }
+
+            return jObject;
+        }
+    }
+}
