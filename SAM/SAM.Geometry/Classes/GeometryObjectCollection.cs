5fc23f60f2bef0c9b5754338f1e16e5d1dd6dad9
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-10-25 16:27:55 +0100

    GeometryObjectCollection and GeometryObjectModel Implemented

---
+++ SAM/SAM.Geometry/Classes/GeometryObjectCollection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SAM.Geometry.Classes
+{
+    public class GeometryObjectCollection : IEnumerable<ISAMGeometryObject>
+    {
+        private List<ISAMGeometryObject> sAMGeometryObjects;
+
+        public GeometryObjectCollection()
+        {
+            sAMGeometryObjects = new List<ISAMGeometryObject>();
+        }
+
+        public GeometryObjectCollection(IEnumerable<ISAMGeometryObject> sAMGeometryObjects)
+        {
+            if (sAMGeometryObjects != null)
+            {
+                sAMGeometryObjects = new List<ISAMGeometryObject>(sAMGeometryObjects);
+            }
+        }
+
+        public GeometryObjectCollection(ISAMGeometryObject sAMGeometryObject)
+        {
+            sAMGeometryObjects = new List<ISAMGeometryObject>() { sAMGeometryObject };
+        }
+
+        public void Add(ISAMGeometryObject sAMGeometryObject)
+        {
+            sAMGeometryObjects.Add(sAMGeometryObject);
+        }
+
+        public IEnumerator<ISAMGeometryObject> GetEnumerator()
+        {
+            return sAMGeometryObjects?.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return sAMGeometryObjects?.GetEnumerator();
+        }
+    }
+}
---
+++ SAM/SAM.Geometry/Classes/GeometryObjectModel.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SAM.Geometry.Classes
+{
+    public class GeometryObjectModel : Core.SAMModel
+    {
+        private GeometryObjectCollection geometryObjectCollection;
+
+        public GeometryObjectModel(GeometryObjectModel geometryObjectModel)
+        {
+            if(geometryObjectModel?.geometryObjectCollection != null)
+            {
+                geometryObjectCollection = new GeometryObjectCollection();
+                foreach(ISAMGeometryObject sAMGeometryObject in geometryObjectModel.geometryObjectCollection)
+                {
+                    geometryObjectCollection.Add(sAMGeometryObject);
+                }
+            }
+        }
+
+        public GeometryObjectModel(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject result = base.ToJObject();
+            if(result == null)
+            {
+                result = new JObject();
+            }
+
+            if(geometryObjectCollection != null)
+            {
+                JArray jArray = new JArray();
+
+                foreach (ISAMGeometryObject sAMGeometryObject in geometryObjectCollection)
+                {
+                    if(sAMGeometryObject == null)
+                    {
+                        continue;
+                    }
+
+                    jArray.Add(sAMGeometryObject.ToJObject());
+                }
+
+                result.Add("GeometryObjects", jArray);
+            }
+            return result;
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            if(!base.FromJObject(jObject))
+            {
+                return false;
+            }
+
+            if(jObject.ContainsKey("GeometryObjects"))
+            {
+                JArray jArray = jObject.Value<JArray>("GeometryObjects");
+
+                geometryObjectCollection = new GeometryObjectCollection();
+                foreach (JObject jObject_GeometryObject in jArray)
+                {
+                    ISAMGeometryObject sAMGeometryObject = Core.Create.IJSAMObject<ISAMGeometryObject>(jObject_GeometryObject);
+                    if(sAMGeometryObject != null)
+                    {
+                        geometryObjectCollection.Add(sAMGeometryObject);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
