cc8dedd65cac34dbdd0011401da2b080396f1f43
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2022-10-26 21:58:56 +0200

    Ping Added

---
+++ SAM/SAM.Core/Query/Ping.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.NetworkInformation;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static bool Ping(string hostNameOrAddress = "https://www.google.com/", int timeout = 1500)
+        {
+            Ping ping = null;
+
+            bool result = false;
+            try
+            {
+                ping = new Ping();
+                PingReply pingReply = ping.Send(hostNameOrAddress);
+                result = pingReply.Status == IPStatus.Success;
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if(ping != null)
+                {
+                    ping.Dispose();
+                }
+            }
+
+            return result;
+        }
+    }
+}
