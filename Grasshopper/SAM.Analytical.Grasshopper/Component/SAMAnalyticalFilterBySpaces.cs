e47b51f619332468335e4864365bb2d3ab7732e8
michaldengusiak <tassimulation@gmail.com>
2022-11-28 11:37:09 +0000

    Update SAMAnalyticalFilterBySpaces.cs

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalFilterBySpaces.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalFilterBySpaces.cs
@@ -28,7 +28,7 @@ namespace SAM.Analytical.Grasshopper
         /// </summary>
         public SAMAnalyticalFilterBySpaces()
           : base("SAMAnalytical.FilterBySpaces", "SAMAnalytical.FilterBySpaces",
-              "Filters AdjacencyCluster by Spaces \n*This is used to create AdjCluster from few selected sapces",
+              "Filters AdjacencyCluster by Spaces \n*This is used to create AdjCluster from few selected sapces\nEach internal panel without adjacent space will be set as Adiabatic",
               "SAM", "Analytical")
         {
         }
