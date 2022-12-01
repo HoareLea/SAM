fb8240e8fd959420b6bdddd8e01c60b0ecf5bac8
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-12-01 16:25:30 +0000

    Color Parameter Added to Zone

--- SAM/SAM.Analytical/Enums/Parameter/ZoneParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/ZoneParameter.cs
@@ -6,6 +6,8 @@ namespace SAM.Analytical
     [AssociatedTypes(typeof(Zone)), Description("Analytical Zone Parameter")]
     public enum ZoneParameter
     {
+        [ParameterProperties("Color", "Color"), ParameterValue(Core.ParameterType.Color)] Color,
+
         [ParameterProperties("Zone Category", "Zone Category"), ParameterValue(Core.ParameterType.String)] ZoneCategory,
     }
 }
