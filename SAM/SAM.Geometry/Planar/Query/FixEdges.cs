8b9bf57bb86322e50200e992315b4e0ae234d1f5
Ziolkowski <jakubziolkowski@digiproject.uk>
2022-12-02 11:38:00 +0000

    Bug Fix for FixEdges Added

--- SAM/SAM.Geometry/Planar/Query/FixEdges.cs
+++ SAM/SAM.Geometry/Planar/Query/FixEdges.cs
@@ -69,7 +69,7 @@ namespace SAM.Geometry.Planar
                     }
                     else
                     {
-                        face2Ds.Sort((x, y) => y.ExternalEdge2D.GetArea().CompareTo(x.ExternalEdge2D.GetArea()));
+                        face2Ds.Sort((x, y) => y.ExternalEdge2D.GetBoundingBox().GetArea().CompareTo(x.ExternalEdge2D.GetBoundingBox().GetArea()));
                         IClosed2D externalEdge = face2Ds[0].ExternalEdge2D;
                         face2Ds.RemoveAt(0);
                         List<IClosed2D> internalEdges_Temp = new List<IClosed2D>();
