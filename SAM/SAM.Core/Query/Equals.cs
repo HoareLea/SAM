d30bca5442de342f10900268260ff45d548fe0c1
michaldengusiak <tassimulation@gmail.com>
2023-01-02 10:13:23 +0000

    add equal

---
+++ SAM/SAM.Core/Query/Equals.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static bool Equals(this Color value_1, Color value_2)
+        {
+            return value_1.R == value_2.R && value_1.G == value_2.G && value_1.B == value_2.B && value_1.A == value_2.A;
+        }
+    }
+}
