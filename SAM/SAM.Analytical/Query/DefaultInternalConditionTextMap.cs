7eb95e71ce05d653753e2aec7d49f1e498e3cfae
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-01-04 19:35:08 +0000

    DefaultInternalConditionTextMap Implemented

---
+++ SAM/SAM.Analytical/Query/DefaultInternalConditionTextMap.cs
@@ -0,0 +1,17 @@
+﻿using SAM.Core;
+
+namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Returns Default SAM Analytical InternalCondition TextMap
+        /// </summary>
+        /// <returns name="TextMap">Default SAM Analytical InternalCondition TextMap</returns>
+        /// <search>Default SAM Analytical InternalCondition, IC, InternalCondition, TextMap</search> 
+        public static TextMap DefaultInternalConditionTextMap()
+        {
+            return ActiveSetting.Setting.GetValue<TextMap>(AnalyticalSettingParameter.InternalConditionTextMap);
+        }
+    }
+}
