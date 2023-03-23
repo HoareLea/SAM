039d40338c2dbd5e9948e6ba981c4f2e7c4db5ad
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-03-23 08:52:12 +0000

    Query.ActiveObject Implemented (Marshal.GetActiveObject)

---
+++ SAM/SAM.Core/Query/ActiveObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        internal const string OLEAUT32 = "oleaut32.dll";
+        internal const string OLE32 = "ole32.dll";
+
+        /// <summary>
+        /// Replacement for System.Runtime.InteropServices.Marshal.GetActiveObject(string progID)
+        /// Source: https://stackoverflow.com/questions/58010510/no-definition-found-for-getactiveobject-from-system-runtime-interopservices-mars/65496277#65496277
+        /// </summary>
+        /// <param name="progID">program ID</param>
+        /// <returns>Object</returns>
+        [System.Security.SecurityCritical]  // auto-generated_required
+        public static object ActiveObject(string progID)
+        {
+            object obj = null;
+            Guid clsid;
+
+            // Call CLSIDFromProgIDEx first then fall back on CLSIDFromProgID if
+            // CLSIDFromProgIDEx doesn't exist.
+            try
+            {
+                CLSIDFromProgIDEx(progID, out clsid);
+            }
+            catch (Exception)
+            {
+                CLSIDFromProgID(progID, out clsid);
+            }
+
+            GetActiveObject(ref clsid, IntPtr.Zero, out obj);
+            return obj;
+        }
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+        [DllImport(OLE32, PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] string progId, out Guid clsid);
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLE32, PreserveSig = false)]
+        [DllImport(OLE32, PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void CLSIDFromProgID([MarshalAs(UnmanagedType.LPWStr)] string progId, out Guid clsid);
+
+        //[DllImport(Microsoft.Win32.Win32Native.OLEAUT32, PreserveSig = false)]
+        [DllImport(OLEAUT32, PreserveSig = false)]
+        [ResourceExposure(ResourceScope.None)]
+        [SuppressUnmanagedCodeSecurity]
+        [System.Security.SecurityCritical]  // auto-generated
+        private static extern void GetActiveObject(ref Guid rclsid, IntPtr reserved, [MarshalAs(UnmanagedType.Interface)] out object ppunk);
+
+    }
+}
