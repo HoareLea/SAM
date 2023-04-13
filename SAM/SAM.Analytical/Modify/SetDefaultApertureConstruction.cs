f030ab56c4912d5205e8d97396ce2cd9e7c2fd22
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-04-13 12:42:29 +0200

    SetDefaultApertureConstruction Implemented

---
+++ SAM/SAM.Analytical/Modify/SetDefaultApertureConstruction.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static IEnumerable<Aperture> SetDefaultApertureConstruction(this AdjacencyCluster adjacencyCluster, IEnumerable<Guid> guids = null)
+        {
+            List<Panel> panels = adjacencyCluster?.GetPanels();
+            if(panels == null || panels.Count == 0)
+            {
+                return null;
+            }
+
+
+            List<Aperture> result = new List<Aperture>();
+            foreach(Panel panel in panels)
+            {
+                List<Aperture> apertures = panel?.Apertures;
+                if(apertures == null || apertures.Count == 0)
+                {
+                    continue;
+                }
+
+                PanelType panelType = panel.PanelType;
+
+                List<Tuple<Aperture, ApertureConstruction>> tuples = new List<Tuple<Aperture, ApertureConstruction>>();
+                foreach(Aperture aperture in apertures)
+                {
+                    if(aperture == null)
+                    {
+                        continue;
+                    }
+
+                    if (guids != null && guids.Count() != 0 && !guids.Contains(aperture.Guid))
+                    {
+                        continue;
+                    }
+
+                    ApertureConstruction apertureConstruction = Query.DefaultApertureConstruction(panel, aperture.ApertureType);
+                    if(apertureConstruction == null)
+                    {
+                        continue;
+                    }
+
+                    if(aperture.Name != null && apertureConstruction.Name == aperture.Name)
+                    {
+                        continue;
+                    }
+
+                    tuples.Add(new Tuple<Aperture, ApertureConstruction>(aperture, apertureConstruction));
+                }
+
+                if(tuples == null || tuples.Count == 0)
+                {
+                    continue;
+                }
+
+                Panel panel_New = new Panel(panel);
+
+                foreach(Tuple<Aperture, ApertureConstruction> tuple in tuples)
+                {
+                    Aperture aperture = new Aperture(tuple.Item1, tuple.Item2);
+                    if(aperture == null)
+                    {
+                        continue;
+                    }
+
+                    if(panel.RemoveAperture(aperture.Guid))
+                    {
+                        panel.AddAperture(aperture);
+                        result.Add(aperture);
+                    }
+                }
+
+                adjacencyCluster.AddObject(panel_New);
+            }
+
+            return result;
+        }
+    }
+}
