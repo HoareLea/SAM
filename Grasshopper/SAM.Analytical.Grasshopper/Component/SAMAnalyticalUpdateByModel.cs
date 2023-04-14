7f8ca4dce835afbcfb5b0c8e5c6dc377f006a6c5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-04-14 17:29:10 +0200

    UpdateByModel Grasshopper Component Implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalUpdateByModel.cs
@@ -0,0 +1,158 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalUpdateByModel : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("3d09900f-8fd4-4c35-9727-ab0d910473af");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalUpdateByModel()
+          : base("SAMAnalytical.UpdateByModel", "SAMAnalytical.UpdateByModel",
+              "Update By Model",
+              "SAM WIP", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analytical", NickName = "_analytical", Description = "Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_oldAnalytical", NickName = "_oldAnalytical", Description = "Old Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooGroupParam() { Name = "_zones_", NickName = "_zones_", Description = "Zones", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "analytical", NickName = "analytical", Description = "SAM Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooGroupParam() { Name = "zones", NickName = "zones", Description = "SAM Analytical Zones", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_analytical");
+            IAnalyticalObject analyticalObject_Destination = null;
+            if (index == -1 || !dataAccess.GetData(index, ref analyticalObject_Destination) || analyticalObject_Destination == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster_Destination = null;
+            if(analyticalObject_Destination is AdjacencyCluster)
+            {
+                adjacencyCluster_Destination = new AdjacencyCluster((AdjacencyCluster)analyticalObject_Destination);
+            }
+            else if(analyticalObject_Destination is AnalyticalModel)
+            {
+                adjacencyCluster_Destination = ((AnalyticalModel)analyticalObject_Destination).AdjacencyCluster;
+                if(adjacencyCluster_Destination != null)
+                {
+                    adjacencyCluster_Destination = new AdjacencyCluster(adjacencyCluster_Destination);
+                }
+            }
+
+            index = Params.IndexOfInputParam("_oldAnalytical");
+            IAnalyticalObject analyticalObject_Source = null;
+            if (index == -1 || !dataAccess.GetData(index, ref analyticalObject_Source) || analyticalObject_Source == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster_Source = null;
+            if (adjacencyCluster_Source is AdjacencyCluster)
+            {
+                adjacencyCluster_Source = new AdjacencyCluster((AdjacencyCluster)analyticalObject_Source);
+            }
+            else if (analyticalObject_Destination is AnalyticalModel)
+            {
+                adjacencyCluster_Source = ((AnalyticalModel)analyticalObject_Source).AdjacencyCluster;
+                if (adjacencyCluster_Source != null)
+                {
+                    adjacencyCluster_Source = new AdjacencyCluster(adjacencyCluster_Source);
+                }
+            }
+
+
+            index = Params.IndexOfInputParam("_zones_");
+            List<Zone> zones = new List<Zone>();
+            if (index == -1 || !dataAccess.GetDataList(index, zones) || zones == null)
+            {
+                zones = adjacencyCluster_Source.GetObjects<Zone>();
+            }
+
+            zones = adjacencyCluster_Destination.CopyZones(adjacencyCluster_Source, zones?.ConvertAll(x => x.Guid));
+
+            if (analyticalObject_Destination is AdjacencyCluster)
+            {
+                analyticalObject_Destination = adjacencyCluster_Destination;
+            }
+            else if (analyticalObject_Destination is AnalyticalModel)
+            {
+                analyticalObject_Destination = new AnalyticalModel((AnalyticalModel)analyticalObject_Destination, adjacencyCluster_Destination);
+            }
+
+            index = Params.IndexOfOutputParam("analytical");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, analyticalObject_Destination);
+            }
+
+            index = Params.IndexOfOutputParam("zones");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, zones);
+            }
+
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/CopyZones.cs
@@ -0,0 +1,72 @@
+﻿using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Zone> CopyZones(this AdjacencyCluster adjacencyCluster_Destination, AdjacencyCluster adjacencyCluster_Source, IEnumerable<Guid> zoneGuids)
+        {
+            if(adjacencyCluster_Destination == null || adjacencyCluster_Source == null || zoneGuids == null)
+            {
+                return null;
+            }
+
+            List<Zone> result = new List<Zone>();
+
+            Dictionary<Space, Shell> dictionary_Destination = adjacencyCluster_Destination?.ShellDictionary();
+
+            foreach (Guid zoneGuid in zoneGuids)
+            {
+                Zone zone_Source = adjacencyCluster_Source.GetObject<Zone>(zoneGuid);
+                if(zone_Source == null)
+                {
+                    continue;
+                }
+
+                List<Space> spaces_Destination = new List<Space>();
+
+                if(dictionary_Destination != null)
+                {
+                    List<Space> spaces_Source = adjacencyCluster_Source.GetRelatedObjects<Space>(zone_Source);
+                    if (spaces_Source != null && spaces_Source.Count != 0)
+                    {
+                        foreach (Space space_Source in spaces_Source)
+                        {
+                            Point3D point3D = space_Source?.Location;
+                            if (point3D == null)
+                            {
+                                continue;
+                            }
+
+                            foreach (KeyValuePair<Space, Shell> keyValuePair in dictionary_Destination)
+                            {
+                                if (keyValuePair.Value.InRange(point3D) || keyValuePair.Value.Inside(point3D))
+                                {
+                                    spaces_Destination.Add(keyValuePair.Key);
+                                    break;
+                                }
+                            }
+
+
+                        }
+                    }
+                }
+
+                if(!zone_Source.TryGetValue(ZoneParameter.ZoneCategory, out string zoneCategory))
+                {
+                    zoneCategory = null;
+                }
+
+                Zone zone_Destination = adjacencyCluster_Destination.UpdateZone(zone_Source.Name, zoneCategory, spaces_Destination.ToArray());
+                if(zone_Destination != null)
+                {
+                    result.Add(zone_Destination);
+                }
+            }
+
+            return result;
+        }
+    }
+}
