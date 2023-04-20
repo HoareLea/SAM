a79b90ae1dcf0eee3d361c8f4c7c000f917a410a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-04-20 12:22:34 +0200

    TypeName Query Implemented

---
+++ SAM/SAM.Core/Query/TypeName.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static string TypeName(this object @object)
+        {
+            if(@object == null)
+            {
+                return null;
+            }
+
+            return Marshal.IsComObject(@object) ? COMObjectTypeName(@object) : @object.GetType().Name;
+        }
+    }
+}
