71b9ff0ace1aa2b19f1144266f48fbf28a5445a9
Ziolkowski <jakubziolkowski@digiproject.uk>
2023-05-09 16:14:41 +0100

    Comments Added to IJSAMObjects

--- SAM/SAM.Core/Create/IJSAMObjects.cs
+++ SAM/SAM.Core/Create/IJSAMObjects.cs
@@ -1,43 +1,62 @@
-﻿using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq; // Required for working with JSON
+using System.Collections.Generic; // Required for using List<>
+using System.Linq; // Required for using Enumerable.Repeat() and Enumerable.ToList()
+using System.Threading.Tasks; // Required for using Parallel.For()
 
 namespace SAM.Core
 {
     public static partial class Create
     {
+        // This method converts a JArray into a List of IJSAMObjects of type T
         public static List<T> IJSAMObjects<T>(this JArray jArray) where T : IJSAMObject
         {
             if (jArray == null)
+            {
                 return null;
+            }
 
+            // Initialize a List of type T with default values
             List<T> result = Enumerable.Repeat<T>(default, jArray.Count).ToList();
 
+            // Process each element of the JArray in parallel
             Parallel.For(0, jArray.Count, (int i) =>
             {
+                // Convert the current element to a JObject
                 JObject jObject = jArray[i] as JObject;
-                if(jObject == null)
+
+                // Skip if the current element is not a JObject
+                if (jObject == null)
                 {
                     return;
                 }
 
+                // Convert the current element to an IJSAMObject of type T and store it in the result List
                 result[i] = IJSAMObject<T>(jObject);
 
             });
 
+            // Return the List of IJSAMObjects of type T
             return result;
         }
 
+        // This method converts a JSON string into a List of IJSAMObjects of type T
         public static List<T> IJSAMObjects<T>(this string json) where T : IJSAMObject
         {
             if (string.IsNullOrEmpty(json))
+            {
                 return default;
+            }
 
+            // Convert the JSON string to a JArray
             JArray jArray = Query.JArray(json);
-            if (jArray == null)
-                return default;
 
+            // Return null if the JArray is null
+            if (jArray == null)
+            {
+                return null;
+            }
+
+            // Convert the JArray to a List of IJSAMObjects of type T using the IJSAMObjects<JObject> method
             return IJSAMObjects<T>(jArray);
         }
     }
