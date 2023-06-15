44865f5b9838c8d7373ecff14f2283ce174e379d
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-15 16:06:36 +0200

    AddLayer Method Added

---
+++ Rhino/SAM.Core.Rhino/Modify/AddLayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Rhino.DocObjects;
+using System.Linq;
+using Rhino.DocObjects.Tables;
+
+namespace SAM.Core.Rhino
+{
+    public static partial class Modify
+    {
+        public static Layer AddLayer(this LayerTable layerTable, string prefix)
+        {
+            if (layerTable == null)
+                return null;
+
+            List<int> indexes = new List<int>();
+            List<Layer> layers = Query.Layers(layerTable, prefix, TextComparisonType.StartsWith, true);
+            if(layers != null || layers.Count != 0)
+            {
+                foreach(Layer layer in layers)
+                {
+                    string text = layer.Name.Substring(prefix.Length);
+                    if(!string.IsNullOrEmpty(text))
+                    {
+                        int index = -1;
+                        if (int.TryParse(text, out index))
+                            indexes.Add(index);
+                    }
+                }
+            }
+
+            int next = 0;
+            if (indexes.Count != 0)
+                next = indexes.Max();
+
+            next++;
+
+            int index_Layer = layerTable.Add();
+            layerTable[index_Layer].Name = string.Format("{0}{1}", prefix, next);
+
+            return layerTable[index_Layer];
+
+        }
+    }
+}
--- Rhino/SAM.Core.Rhino/Modify/AddSAMLayer.cs
+++ Rhino/SAM.Core.Rhino/Modify/AddSAMLayer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Rhino.DocObjects;
-using System.Linq;
+﻿using Rhino.DocObjects;
 using Rhino.DocObjects.Tables;
 
 namespace SAM.Core.Rhino
@@ -9,38 +7,7 @@ namespace SAM.Core.Rhino
     {
         public static Layer AddSAMLayer(this LayerTable layerTable)
         {
-            if (layerTable == null)
-                return null;
-
-            string prefix = "SAM_";
-
-            List<int> indexes = new List<int>();
-            List<Layer> layers = Query.Layers(layerTable, prefix, TextComparisonType.StartsWith, true);
-            if(layers != null || layers.Count != 0)
-            {
-                foreach(Layer layer in layers)
-                {
-                    string text = layer.Name.Substring(prefix.Length);
-                    if(!string.IsNullOrEmpty(text))
-                    {
-                        int index = -1;
-                        if (int.TryParse(text, out index))
-                            indexes.Add(index);
-                    }
-                }
-            }
-
-            int next = 0;
-            if (indexes.Count != 0)
-                next = indexes.Max();
-
-            next++;
-
-            int index_Layer = layerTable.Add();
-            layerTable[index_Layer].Name = string.Format("{0}{1}", prefix, next);
-
-            return layerTable[index_Layer];
-
+            return AddLayer(layerTable, "SAM_");
         }
     }
 }
