70e19b7972a0958d6a3df751e5cfb983d41a2ebd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-28 12:39:51 +0200

    LogarithmicMeanTemperatureDifference Implemented

---
+++ SAM/SAM.Core/Query/LogarithmicMeanTemperatureDifference.cs
@@ -0,0 +1,25 @@
+﻿namespace SAM.Core
+{
+    public static partial class Query
+    {
+
+        public static double LogarithmicMeanTemperatureDifference(double enteringTemperature_1, double leavingTemperature_1, double enteringTemperature_2, double leavingTemperature_2)
+        {
+            if(double.IsNaN(enteringTemperature_1) || double.IsNaN(leavingTemperature_1) || double.IsNaN(enteringTemperature_2) || double.IsNaN(leavingTemperature_2))
+            {
+                return double.NaN;
+            }
+
+            double temperatureDifference_1 = leavingTemperature_1 - enteringTemperature_2;
+            if(temperatureDifference_1 == 0)
+            {
+                return double.NaN;
+            }
+
+            double temperatureDifference_2 = enteringTemperature_1 - leavingTemperature_2;
+
+            return (temperatureDifference_2 - temperatureDifference_1) / System.Math.Log(temperatureDifference_2 / temperatureDifference_1);
+
+        }
+    }
+}
