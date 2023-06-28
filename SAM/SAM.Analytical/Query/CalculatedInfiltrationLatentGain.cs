ec2d05de3e610994e34d376ea0ec7b0c8b340eb7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-06-28 09:53:27 +0200

    CalculatedInfiltrationLatentGain And CalculatedInfiltrationSensibleGain Queries Added

---
+++ SAM/SAM.Analytical/Query/CalculatedInfiltrationLatentGain.cs
@@ -0,0 +1,36 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Calculates Infiltration Latent Gain
+        /// </summary>
+        /// <param name="space">Space</param>
+        /// <param name="humidityRatio_Outside">Outside Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <param name="humidityRatio_Inside">Inside Humidity Ratio [kg_waterVapor/kg_dryAir]</param>
+        /// <param name="dryBulbTemperature_Inside">Inside Air Dry Bulb Temperature [C]</param>
+        /// <param name="density_Outside">Outside Air Density [kg/m3]</param>
+        /// <returns>Calculated Infiltration Latent Gain [W]</returns>
+        public static double CalculatedInfiltrationLatentGain(this Space space, double humidityRatio_Outside, double humidityRatio_Inside, double dryBulbTemperature_Inside, double density_Outside)
+        {
+            if(space == null || double.IsNaN(humidityRatio_Inside) || double.IsNaN(humidityRatio_Outside) || double.IsNaN(dryBulbTemperature_Inside) || double.IsNaN(density_Outside))
+            {
+                return double.NaN;
+            }
+            
+            double vapourizationLatentHeat = VapourizationLatentHeat(dryBulbTemperature_Inside);
+            if (double.IsNaN(vapourizationLatentHeat))
+            {
+                return double.NaN;
+            }
+
+            double calculatedInfiltrationMassAirFlow = CalculatedInfiltrationAirFlow(space) * density_Outside; //[kg/s]
+            if (double.IsNaN(calculatedInfiltrationMassAirFlow))
+            {
+                return double.NaN;
+            }
+
+            return calculatedInfiltrationMassAirFlow * (humidityRatio_Outside - humidityRatio_Inside) * (1.86 * dryBulbTemperature_Inside + vapourizationLatentHeat) * 1000;
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Query/CalculatedInfiltrationSensibleGain.cs
@@ -0,0 +1,35 @@
+﻿namespace SAM.Analytical
+{
+    public static partial class Query
+    {
+        /// <summary>
+        /// Calculates Infiltration Sensible Gain
+        /// </summary>
+        /// <param name="space">Space</param>
+        /// <param name="dryBulbTemperature_Outside">Outside Air Dry Bulb Temperature [C]</param>
+        /// <param name="dryBulbTemperature_Inside">Inside Air Dry Bulb Temperature [C]</param>
+        /// <param name="density_Outside">Outside Air Density [kg/m3]</param>
+        /// <returns>Calculated Infiltration Sensible Gain [W]</returns>
+        public static double CalculatedInfiltrationSensibleGain(this Space space, double dryBulbTemperature_Outside, double dryBulbTemperature_Inside, double density_Outside)
+        {
+            if(space == null|| double.IsNaN(dryBulbTemperature_Inside) || double.IsNaN(density_Outside))
+            {
+                return double.NaN;
+            }
+            
+            double vapourizationLatentHeat = VapourizationLatentHeat(dryBulbTemperature_Inside);
+            if (double.IsNaN(vapourizationLatentHeat))
+            {
+                return double.NaN;
+            }
+
+            double calculatedInfiltrationMassAirFlow = CalculatedInfiltrationAirFlow(space) * density_Outside; //[kg/s]
+            if (double.IsNaN(calculatedInfiltrationMassAirFlow))
+            {
+                return double.NaN;
+            }
+
+            return calculatedInfiltrationMassAirFlow * 1.005 * (dryBulbTemperature_Inside - dryBulbTemperature_Outside) * 1000;
+        }
+    }
+}
