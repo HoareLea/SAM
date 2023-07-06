8ada78bb4fb8f30f8aeca3a2431999ae35c385f7
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-07-06 09:21:05 +0200

    Create Vector2D by Angle Implemented

---
+++ SAM/SAM.Geometry/Planar/Create/Vector2D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Create
+    {
+        /// <summary>
+        /// Calculates unit vector for given angle [rad]
+        /// </summary>
+        /// <param name="angle">angle to X axis counted counterclockwise [rad]</param>
+        /// <returns>Unit Vector2D</returns>
+        public static Vector2D Vector2D(double angle)
+        {
+            if(double.IsNaN(angle) || double.IsInfinity(angle))
+            {
+                return null;
+            }
+
+            if(angle == 0)
+            {
+                return Planar.Vector2D.WorldX;
+            }
+
+            double xAngle = angle;
+            double yAngle = (System.Math.PI / 2) - angle;
+
+            return new Vector2D(System.Math.Cos(xAngle), System.Math.Cos(yAngle));
+        }
+    }
+}
