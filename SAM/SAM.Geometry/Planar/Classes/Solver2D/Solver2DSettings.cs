f0c355571e6a0445c9a2080cafee16c41b30b151
Maciej Dengusiak <maciejdengusiak@gmail.com>
2023-08-03 16:47:38 +0200

    Implemented Solver2D
    Class that arranges rectangles on a plane in 2 variants: around a point or along the polyline

---
+++ SAM/SAM.Geometry/Planar/Classes/Solver2D/Solver2DSettings.cs
@@ -0,0 +1,12 @@
+﻿using SAM.Core;
+
+namespace SAM.Geometry.Planar
+{
+    public class Solver2DSettings
+    {
+        public double moveDistance { get; set; } = 0.1;
+        public double maxStepPolyline { get; set; } = 200;
+
+        public double maxStepPoint { get; set; } = 10;
+    }
+}
