65650c71080ca9a772284ae488f8e745b1d4c356
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-08-08 17:25:06 +0200

    MixingSection and EmptySection Added

---
+++ SAM/SAM.Analytical/Classes/AnalyticalEquipment/EmptySection.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SAM.Analytical
+{
+    /// <summary>
+    /// Represents an empty section in the analytical domain
+    /// </summary>
+    public class EmptySection : SimpleEquipment
+    {
+        public EmptySection(string name)
+            : base(name)
+        {
+
+        }
+
+        public EmptySection(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public EmptySection(Filter filter)
+            : base(filter)
+        {
+
+        }
+
+        public EmptySection(Guid guid, string name)
+            : base(guid, name)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            if (!base.FromJObject(jObject))
+                return false;
+
+            // TODO: Implement specific deserialization logic for AirHandlingUnit properties
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject jObject = base.ToJObject();
+            if (jObject == null)
+                return null;
+
+            // TODO: Implement specific serialization logic for AirHandlingUnit properties
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Classes/AnalyticalEquipment/MixingSection.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SAM.Analytical
+{
+    /// <summary>
+    /// Represents an mixing section in the analytical domain
+    /// </summary>
+    public class MixingSection : SimpleEquipment
+    {
+        public MixingSection(string name)
+            : base(name)
+        {
+
+        }
+
+        public MixingSection(JObject jObject)
+            : base(jObject)
+        {
+
+        }
+
+        public MixingSection(Filter filter)
+            : base(filter)
+        {
+
+        }
+
+        public MixingSection(Guid guid, string name)
+            : base(guid, name)
+        {
+
+        }
+
+        public override bool FromJObject(JObject jObject)
+        {
+            if (!base.FromJObject(jObject))
+                return false;
+
+            // TODO: Implement specific deserialization logic for AirHandlingUnit properties
+
+            return true;
+        }
+
+        public override JObject ToJObject()
+        {
+            JObject jObject = base.ToJObject();
+            if (jObject == null)
+                return null;
+
+            // TODO: Implement specific serialization logic for AirHandlingUnit properties
+
+            return jObject;
+        }
+    }
+}
