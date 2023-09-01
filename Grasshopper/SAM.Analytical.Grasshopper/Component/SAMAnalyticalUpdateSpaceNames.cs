56be8de8463099a865a5fe1988b25da2cd2e59ec
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-09-01 13:07:48 +0200

    UpdateSpaceNames Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalUpdateSpaceNames.cs
@@ -0,0 +1,107 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalUpdateSpaceNames : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("b8d42e3b-ed13-41b9-b2b9-21c172f61b0b");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalUpdateSpaceNames()
+          : base("SAMAnalytical.UpdateSpaceNames", "SAMAnalytical.UpdateSpaceNames",
+              "Update Space Names",
+              "SAM", "Analytical")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analytical", NickName = "_analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "analytical", NickName = "analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_analytical");
+            IAnalyticalObject analyticalObject = null;
+            if(index == -1 || !dataAccess.GetData(index, ref analyticalObject) || analyticalObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if(analyticalObject is AnalyticalModel)
+            {
+                AnalyticalModel analyticalModel = new AnalyticalModel((AnalyticalModel)analyticalObject);
+                AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+                if(adjacencyCluster != null)
+                {
+                    adjacencyCluster.UpdateSpaceNames();
+                    
+                    analyticalObject = new AnalyticalModel(analyticalModel, adjacencyCluster);
+                }
+            }
+            else if(analyticalObject is AdjacencyCluster)
+            {
+                AdjacencyCluster adjacencyCluster = new AdjacencyCluster((AdjacencyCluster)analyticalObject);
+                adjacencyCluster.UpdateSpaceNames();
+                analyticalObject = adjacencyCluster;
+            }
+
+            index = Params.IndexOfOutputParam("analytical");
+            if (index != -1)
+                dataAccess.SetData(index, analyticalObject);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/UpdateSpaceNames.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static Dictionary<string, Space> UpdateSpaceNames(this AdjacencyCluster adjacencyCluster)
+        {
+            List<Space> spaces = adjacencyCluster?.GetSpaces();
+            if(spaces == null || spaces.Count == 0)
+            {
+                return null;
+            }
+
+            List<Guid> guids = spaces.ConvertAll(x => x.Guid);
+
+            Dictionary<string, Space> result = new Dictionary<string, Space>();
+
+            foreach (Guid guid in guids)
+            {
+                spaces = adjacencyCluster.GetSpaces();
+                
+                int spaceIndex = spaces.FindIndex(x => x.Guid == guid);
+                if(spaceIndex == -1)
+                {
+                    continue;
+                }
+
+                Space space = spaces[spaceIndex];
+                spaces.RemoveAt(spaceIndex);
+
+                string name = space.Name;
+                if(name == null)
+                {
+                    continue;
+                }
+
+                List<Space> spaces_Name = spaces.FindAll(x => x.Name == name);
+                if(spaces_Name == null || spaces_Name.Count == 0)
+                {
+                    continue;
+                }
+
+                int index = 1;
+                foreach (Space space_Name in spaces_Name)
+                {
+                    spaces = adjacencyCluster.GetSpaces();
+
+                    string name_New = string.Format("{0} {1}", name, index.ToString());
+                    while (spaces.Find(x => x.Name == name_New) != null)
+                    {
+                        index++;
+                        name_New = string.Format("{0} {1}", name, index.ToString());
+                    }
+
+                    Space space_New = new Space(space_Name, name_New, space_Name.Location);
+                    adjacencyCluster.AddObject(space_New);
+                    result[name_New] = space_New;
+                }
+            }
+            return result;
+
+        }
+    }
+}
