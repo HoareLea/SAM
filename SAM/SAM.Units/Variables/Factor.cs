e74f9c54a886c082412ae23e765b5566bca87763
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-09-15 15:47:54 +0200

    Factors Reorganised

--- SAM/SAM.Units/Variables/Factor.cs
+++ SAM/SAM.Units/Variables/Factor.cs
@@ -1,8 +1,10 @@
-﻿namespace SAM.Units
+﻿using System;
+
+namespace SAM.Units
 {
     public static class Factor
     {
-        public const double RadiansToDegrees = 180 / System.Math.PI;
+        public const double RadiansToDegrees = 180 / Math.PI;
         public const double DegreesToRadians = 1 / RadiansToDegrees;
 
         public const double MetersToFeet = 3.280839895;
@@ -14,5 +16,4 @@
         public const double PoundsPerInchToPascal = 6894.75728;
         public const double PascalToPoundsPerInch = 1 / PoundsPerInchToPascal;
     }
-
 }
