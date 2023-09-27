48138138ea5abef4165d71fac1a4ac72969a8001
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-09-27 11:37:45 +0100

    Line2D Create Method Implemented (WIP)

---
+++ SAM/SAM.Geometry/Planar/Create/Segment2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM.Geometry.Planar
+{
+    public static partial class Create
+    {
+        public static Segment2D Segment2D(this Line2D line2D, Rectangle2D rectangle2D)
+        {
+            if(line2D == null || rectangle2D == null)
+            {
+                return null;
+            }
+
+            throw new NotImplementedException();
+        }
+
+    
+    }
+}
