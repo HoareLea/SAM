993e404f6a72e0324b73dfe1411a34fc11372d27
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-10-04 15:45:33 +0200

    NCMName Implementation

---
+++ SAM/SAM.Analytical/Classes/NCM/NCMName.cs
@@ -0,0 +1,141 @@
+﻿using NetTopologySuite.Utilities;
+using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Geometry.Planar;
+using System;
+
+namespace SAM.Analytical
+{
+    public class NCMName : IJSAMObject
+    {
+        private string name;
+        private string version;
+        private string description;
+        private string group;
+
+        public NCMName(string name)
+        {
+            this.name = name;
+            version = null;
+            description = null;
+            group = null;
+        }
+
+        public NCMName(string name, string version, string description, string group)
+        {
+            this.name = name;
+            this.version = version;
+            this.description = description;
+            this.group = group;
+        }
+
+        public NCMName(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return version;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if(jObject == null)
+            {
+                return false;
+            }
+
+            if(jObject.ContainsKey("Name"))
+            {
+                name = jObject.Value<string>("Name");
+            }
+
+            if (jObject.ContainsKey("Version"))
+            {
+                version = jObject.Value<string>("Version");
+            }
+
+            if (jObject.ContainsKey("Description"))
+            {
+                description = jObject.Value<string>("Description");
+            }
+
+            if (jObject.ContainsKey("Group"))
+            {
+                group = jObject.Value<string>("Group");
+            }
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+
+            if(name != null)
+            {
+                jObject.Add("Name", name);
+            }
+
+            if (version != null)
+            {
+                jObject.Add("Version", version);
+            }
+
+            if (description != null)
+            {
+                jObject.Add("Description", description);
+            }
+
+            if (group != null)
+            {
+                jObject.Add("Group", group);
+            }
+
+            return jObject;
+        }
+
+        public override string ToString()
+        {
+            return name?.ToString();
+        }
+
+
+        public static implicit operator string(NCMName nCMName)
+        {
+            return nCMName?.Name;
+        }
+
+
+        public static implicit operator NCMName(string name)
+        {
+            if(name == null)
+            {
+                return null;
+            }
+
+            return new NCMName(name);
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Classes/NCM/NCMNameCollection.cs
@@ -0,0 +1,95 @@
+﻿using NetTopologySuite.Utilities;
+using Newtonsoft.Json.Linq;
+using SAM.Core;
+using SAM.Geometry.Planar;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public class NCMNameCollection : IEnumerable<NCMName>, IJSAMObject
+    {
+        private List<NCMName> nCMNames = new List<NCMName>();
+
+        public NCMNameCollection(IEnumerable<NCMName> nCMNames)
+        {
+            if(nCMNames != null)
+            {
+                this.nCMNames = new List<NCMName>(nCMNames);
+            }
+        }
+
+        public bool Add(NCMName nCMName)
+        {
+            if(nCMName == null)
+            {
+                return false;
+            }
+
+            nCMNames.Add(nCMName);
+            return true;
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if(jObject == null)
+            {
+                return false;
+            }
+
+            nCMNames = new List<NCMName>();
+
+            if(jObject.ContainsKey("NCMNames"))
+            {
+                JArray jArray = jObject.Value<JArray>("NCMNames");
+                foreach(JObject jObject_NCMName in jArray)
+                {
+                    if(jObject_NCMName == null)
+                    {
+                        continue;
+                    }
+
+                    nCMNames.Add(new NCMName(jObject_NCMName));
+                }
+
+            }
+
+            return true;
+        }
+
+        public IEnumerator<NCMName> GetEnumerator()
+        {
+            return nCMNames.GetEnumerator();
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Core.Query.FullTypeName(this));
+
+            if(nCMNames != null)
+            {
+                JArray jArray = new JArray();
+                foreach(NCMName nCMName in nCMNames)
+                {
+                    if(nCMName == null)
+                    {
+                        continue;
+                    }
+
+                    jArray.Add(nCMName.ToJObject());
+                }
+
+                jObject.Add("NCMNames", jArray);
+            }
+
+            return jObject;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
