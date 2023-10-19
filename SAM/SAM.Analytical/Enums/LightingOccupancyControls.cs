ab3cbc5ebb0b4ea5555aa100e6483cb2eec16ba6
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-10-19 10:47:20 +0200

    None Replaced for LightingOccupancyControls

--- SAM/SAM.Analytical/Enums/LightingOccupancyControls.cs
+++ SAM/SAM.Analytical/Enums/LightingOccupancyControls.cs
@@ -6,7 +6,8 @@ namespace SAM.Analytical
     public enum LightingOccupancyControls
     {
         [Description("Undefined")] Undefined,
-        [Description("None")] None,
+        [Description("ManualOn ManulaOff")] ManualOn_ManualOff,
+        [Description("ManualOn ManulaOff with add auto")] ManualOn_ManualOff_WithAdditionalAutomatic,
         [Description("AutoOn Dimmed")] AutoOn_Dimmed,
         [Description("AutoOn AutoOff")] AutoOn_AutoOff,
         [Description("ManualOn Dimmed")] ManualOn_Dimmed,
