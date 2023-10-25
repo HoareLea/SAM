f12e9b962b7a82ddb87a106f4d5fd151c53c025f
michaldengusiak <tassimulation@gmail.com>
2023-10-25 10:39:15 +0100

    Update ConstructionManager.cs

--- SAM/SAM.Analytical/Classes/ConstructionManager.cs
+++ SAM/SAM.Analytical/Classes/ConstructionManager.cs
@@ -66,7 +66,7 @@ namespace SAM.Analytical
         {
             get
             {
-                return apertureConstructionLibrary?.GetApertureConstructions().ConvertAll(x => x?.Clone());
+                return apertureConstructionLibrary?.GetApertureConstructions()?.ConvertAll(x => x?.Clone());
             }
         }
 
