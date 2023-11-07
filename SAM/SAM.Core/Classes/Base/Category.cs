b6d8004e249032e93636ce68f1e7872279ce2f21
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-11-07 17:19:59 +0100

    Category Implemented

---
+++ SAM/SAM.Core/Classes/Base/Category.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SAM.Core
+{
+    public class Category : IJSAMObject
+    {
+        private string name;
+        private Category subCategory;
+
+        public Category(string name)
+        {
+            this.name = name;
+        }
+
+        public Category(string name, Category subCategory)
+        {
+            this.name = name;
+            this.subCategory = subCategory;
+        }
+
+        public Category(Category category)
+        {
+            if(category != null)
+            {
+                name = category.name;
+                subCategory = category.subCategory == null ? null : new Category(category.subCategory);
+            }
+        }
+
+        public Category(JObject jObject)
+        {
+            FromJObject(jObject);
+        }
+
+        public bool FromJObject(JObject jObject)
+        {
+            if(jObject == null)
+            {
+                return false;
+            }
+
+            if(jObject.ContainsKey("Name"))
+            {
+                name = jObject.Value<string>("Name");
+            }
+
+            if(jObject.ContainsKey("SubCategory"))
+            {
+                subCategory = new Category(jObject.Value<JObject>("SubCategory"));
+            }
+
+            return true;
+        }
+
+        public JObject ToJObject()
+        {
+            JObject jObject = new JObject();
+            jObject.Add("_type", Query.FullTypeName(this));
+            
+            if(name != null)
+            {
+                jObject.Add("Name", name);
+            }
+
+            if (subCategory != null)
+            {
+                jObject.Add("SubCategory", subCategory.ToJObject());
+            }
+
+            return jObject;
+        }
+    }
+}
---
+++ SAM/SAM.Core/Create/Category.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Core
+{
+    public static partial class Create
+    {
+        public static Category Category(this IEnumerable<string> values)
+        {
+            if(values == null || values.Count() == 0)
+            {
+                return null;
+            }
+
+            Category result = null;
+            for (int i = values.Count() - 1; i >= 0; i++)
+            {
+                string name = values.ElementAt(i);
+                if(result == null)
+                {
+                    result = new Category(name);
+                }
+                else
+                {
+                    result = new Category(name, result);
+                }
+            }
+
+            return result;
+        }
+
+        public static Category Category(this string values, char separator)
+        {
+            if(values == null)
+            {
+                return null;
+            }
+
+            return Category(values.Split(separator));
+        }
+    }
+}
