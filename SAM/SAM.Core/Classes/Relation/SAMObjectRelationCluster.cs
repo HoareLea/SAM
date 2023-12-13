d1aaeb63d9ac354d6149d09c2a911f052f9ef4e5
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2023-12-13 17:27:21 +0100

    Temporary Changes

---
+++ SAM/SAM.Core/Classes/Relation/SAMObjectRelationCluster.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAM.Core
+{
+    public class SAMObjectRelationCluster<T> : RelationCluster_NEW<IJSAMObject>, IJSAMObject where T: IJSAMObject
+    {
+        public SAMObjectRelationCluster()
+            :base()
+        {
+
+        }
+
+        public SAMObjectRelationCluster(JObject jObject)
+            :base(jObject)
+        {
+        }
+
+        public SAMObjectRelationCluster(SAMObjectRelationCluster<T> sAMObjectRelationCluster)
+            : this(sAMObjectRelationCluster, false)
+        {
+
+        }
+
+        public SAMObjectRelationCluster(SAMObjectRelationCluster<T> sAMObjectRelationCluster, bool deepClone)
+            : base(sAMObjectRelationCluster)
+        {
+            if(deepClone)
+            {
+                List<Type> types = GetTypes();
+                if (types != null)
+                {
+                    foreach(Type type in types)
+                    {
+                        List<IJSAMObject> jSAMObjects = GetObjects(type);
+                        if(jSAMObjects != null)
+                        {
+                            foreach(IJSAMObject jSAMObject in jSAMObjects)
+                            {
+                                AddObject(jSAMObject.Clone());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public SAMObjectRelationCluster<T> Clone(bool deepClone)
+        {
+            return new SAMObjectRelationCluster<T> (this, deepClone);
+        }
+    }
+}
