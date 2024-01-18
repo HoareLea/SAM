bb18ec13856709b23e23819c1a43d848e57f8d25
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-18 17:28:03 +0100

    Input Name Updated for SAMAnalyticalHourOfYearToDateTime Grasshopper Component

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalHourOfYearToDateTime.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalHourOfYearToDateTime.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace SAM.Analytical.Grasshopper
 {
-    public class SAMAnalyticalIndexToDateTime : GH_SAMVariableOutputParameterComponent
+    public class SAMAnalyticalHourOfYearToDateTime : GH_SAMVariableOutputParameterComponent
     {
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
@@ -17,7 +17,7 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.2";
+        public override string LatestComponentVersion => "1.0.3";
 
         /// <summary>
         /// Provides an Icon for the component.
@@ -29,9 +29,9 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// Initializes a new instance of the SAM_point3D class.
         /// </summary>
-        public SAMAnalyticalIndexToDateTime()
-          : base("SAMAnalytical.IndexToDateTime", "SAMAnalytical.IndexToDateTime",
-              "Convert hour Index to DateTime",
+        public SAMAnalyticalHourOfYearToDateTime()
+          : base("SAMAnalytical.HourOfYearToDateTime", "SAMAnalytical.HourOfYearToDateTime",
+              "Convert hour of year to DateTime",
               "SAM", "Analytical")
         {
         }
@@ -44,7 +44,7 @@ namespace SAM.Analytical.Grasshopper
             get
             {
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "_index", NickName = "_index", Description = "Hour Index in Year", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "_hourOfYear", NickName = "_hourOfYear", Description = "Hour of the year", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
 
                 global::Grasshopper.Kernel.Parameters.Param_Integer param_Integer = new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "_year_", NickName = "_year_", Description = "Year", Access = GH_ParamAccess.item, Optional = true };
                 param_Integer.PersistentData.Append(new GH_Integer(2018));
@@ -62,13 +62,13 @@ namespace SAM.Analytical.Grasshopper
             get
             {
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Time() { Name = "DateTime", NickName = "DateTime", Description = "DateTime", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "Year", NickName = "Year", Description = "Year", Access = GH_ParamAccess.item }, ParamVisibility.Voluntary));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "Month", NickName = "Month", Description = "Month", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "Day", NickName = "Day", Description = "Day", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "Hour", NickName = "Hour", Description = "Hour", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "DayOfYear", NickName = "DayOfYear", Description = "Day of the year", Access = GH_ParamAccess.item }, ParamVisibility.Voluntary));
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "Text", NickName = "Text", Description = "DateTime as Text", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Time() { Name = "dateTime", NickName = "dateTime", Description = "DateTime", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "year", NickName = "year", Description = "Year", Access = GH_ParamAccess.item }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "month", NickName = "month", Description = "Month", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "day", NickName = "day", Description = "Day", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "hour", NickName = "hour", Description = "Hour", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "dayOfYear", NickName = "dayOfYear", Description = "Day of the year", Access = GH_ParamAccess.item }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "text", NickName = "text", Description = "DateTime as Text", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
 
                 return result.ToArray();
             }
@@ -85,7 +85,7 @@ namespace SAM.Analytical.Grasshopper
             int index = -1;
 
             int hourIndex = -1;
-            index = Params.IndexOfInputParam("_index");
+            index = Params.IndexOfInputParam("_hourOfYear");
             if(index == -1 || !dataAccess.GetData(index, ref hourIndex) || hourIndex < 0 || hourIndex > 8760)
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
@@ -101,31 +101,31 @@ namespace SAM.Analytical.Grasshopper
  
             DateTime dateTime = Analytical.Convert.ToDateTime(hourIndex, year);
 
-            index = Params.IndexOfOutputParam("DateTime");
+            index = Params.IndexOfOutputParam("dateTime");
             if (index != -1)
                 dataAccess.SetData(index, dateTime);
 
-            index = Params.IndexOfOutputParam("Year");
+            index = Params.IndexOfOutputParam("year");
             if (index != -1)
                 dataAccess.SetData(index, dateTime.Year);
 
-            index = Params.IndexOfOutputParam("Month");
+            index = Params.IndexOfOutputParam("month");
             if (index != -1)
                 dataAccess.SetData(index, dateTime.Month);
 
-            index = Params.IndexOfOutputParam("Day");
+            index = Params.IndexOfOutputParam("day");
             if (index != -1)
                 dataAccess.SetData(index, dateTime.Day);
 
-            index = Params.IndexOfOutputParam("Hour");
+            index = Params.IndexOfOutputParam("hour");
             if (index != -1)
                 dataAccess.SetData(index, dateTime.Hour);
 
-            index = Params.IndexOfOutputParam("DayOfYear");
+            index = Params.IndexOfOutputParam("dayOfYear");
             if (index != -1)
                 dataAccess.SetData(index, dateTime.DayOfYear);
 
-            index = Params.IndexOfOutputParam("Text");
+            index = Params.IndexOfOutputParam("text");
             if (index != -1)
                 dataAccess.SetData(index, Analytical.Convert.ToString(dateTime));
         }
