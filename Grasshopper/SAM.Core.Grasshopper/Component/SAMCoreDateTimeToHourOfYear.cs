1cd1300b3dc93e07d0072ccd954a19adeaa2e2a0
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-01-18 13:33:06 +0100

    SAMCoreDateTimeToHourOfYear Implemented

---
+++ Grasshopper/SAM.Core.Grasshopper/Component/SAMCoreDateTimeToHourOfYear.cs
@@ -0,0 +1,97 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Commands;
+using SAM.Core.Grasshopper.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper
+{
+    public class SAMCoreDateTimeToHourOfYear : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("7196e29d-75b4-461c-b484-e1845701cf24");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Get3;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        //private GH_OutputParamManager outputParamManager;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMCoreDateTimeToHourOfYear()
+          : base("SAMCore.DateTimeToHourOfYear", "SAMCore.DateTimeToHourOfYear",
+              "Convert DateTime to hour of year",
+              "SAM", "Core")
+        {
+        }
+
+        
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Time() { Name = "_dateTime", NickName = "_dateTime", Description = "Time", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Time() { Name = "dateTime", NickName = "dateTime", Description = "DateTime", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Integer() { Name = "hourOfYear", NickName = "hourOfYear", Description = "Hour of year", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_dateTime");
+            
+            DateTime dateTime = DateTime.MinValue;
+            if (index == -1 || !dataAccess.GetData(index, ref dateTime) || dateTime == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfOutputParam("dateTime");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, dateTime);
+            }
+
+            index = Params.IndexOfOutputParam("hourOfYear");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, dateTime.HourOfYear());
+            }
+        }
+    }
+}
