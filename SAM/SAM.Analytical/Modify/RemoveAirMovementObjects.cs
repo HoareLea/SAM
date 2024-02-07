210ec7fdfc10e36ce170e90f41c9d3599c7fa1a2
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-07 15:59:42 +0100

    RemoveAirMovementObjects Function Implemented

---
+++ SAM/SAM.Analytical/Modify/RemoveAirMovementObjects.cs
@@ -0,0 +1,37 @@
+﻿using SAM.Core;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Guid> RemoveAirMovementObjects<T>(this AdjacencyCluster adjacencyCluster) where T : IAirMovementObject 
+        {
+            if(adjacencyCluster == null)
+            {
+                return null;
+            }
+
+            List<T> airMovemenetObjects = adjacencyCluster.GetObjects<T>();
+            if(airMovemenetObjects == null)
+            {
+                return null;
+            }
+
+            List<SAMObject> sAMObjects = new List<SAMObject>(); 
+            foreach(T airMovemenetObject in airMovemenetObjects)
+            {
+                SAMObject sAMObject = airMovemenetObject as SAMObject;
+                if(airMovemenetObject == null)
+                {
+                    continue;
+                }
+
+                sAMObjects.Add(sAMObject);
+            }
+
+            return adjacencyCluster.Remove(sAMObjects);
+        }
+    }
+}
