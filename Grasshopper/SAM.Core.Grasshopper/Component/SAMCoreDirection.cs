00ec47cfe18f36a2f0b8432e9b693c5c45134c74
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-27 11:33:42 +0100

    Create SAMGeometry2DObjectCollection Implemented and SAMCoreDirection Grasshopper Component Added

---
+++ Grasshopper/SAM.Core.Grasshopper/Component/SAMCoreDirection.cs
@@ -0,0 +1,35 @@
+﻿using SAM.Core;
+using SAM.Core.Grasshopper;
+using SAM.Core.Grasshopper.Properties;
+using System;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMCoreDirection : GH_SAMEnumComponent<Direction>
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("4ac990e2-e42c-4b4c-b336-b45763f0a9cd");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Panel Type
+        /// </summary>
+        public SAMCoreDirection()
+          : base("SAMCore.Direction", "SAMCore.Direction",
+              "Select Direction",
+              "SAM", "Core")
+        {
+        }
+    }
+}
