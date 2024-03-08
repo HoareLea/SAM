70d5ae997b3947f0c914721f37b84f511959c808
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-08 16:07:28 +0100

    Code Formatting Updated

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalUpdatePollutantGains.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalUpdatePollutantGains.cs
@@ -16,7 +16,7 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// The latest version of this component
         /// </summary>
-        public override string LatestComponentVersion => "1.0.1";
+        public override string LatestComponentVersion => "1.0.2";
 
         /// <summary>
         /// Provides an Icon for the component.
@@ -31,14 +31,14 @@ namespace SAM.Analytical.Grasshopper
             {
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
                 result.Add(new GH_SAMParam(new GooAnalyticalModelParam() { Name = "_analyticalModel", NickName = "_analyticalModel", Description = "SAM Analytical AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
-                result.Add(new GH_SAMParam(new GooSpaceParam() { Name = "_spaces_", NickName = "_spaces_", Description = "SAM Analytical Spaces, if nothing connected all spaces from AnalyticalModel will be used", Access = GH_ParamAccess.list, Optional = true}, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooSpaceParam() { Name = "_spaces_", NickName = "_spaces_", Description = "SAM Analytical Spaces, if nothing connected all spaces from AnalyticalModel will be used", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
                 result.Add(new GH_SAMParam(new GooProfileParam() { Name = "profile_", NickName = "profile_", Description = "SAM Analytical Profile", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Binding));
 
                 global::Grasshopper.Kernel.Parameters.Param_Number number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_pollutantGPerPerson_", NickName = "_pollutantGPerPerson_", Description = "Pollutant Per Person [g/h/p]", Access = GH_ParamAccess.item };
                 number.SetPersistentData(28.7);
-                result.Add( new GH_SAMParam(number, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(number, ParamVisibility.Binding));
 
-                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "pollutantGPerHrPerPerson_", NickName = "pollutantGPerHrPerPerson_", Description = "Pollutant Per Hour Per Person [g/h/m2]", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "pollutantGPerHrPerArea_", NickName = "pollutantGPerHrPerArea_", Description = "Pollutant Per Hour Per Area [g/h/m2]", Access = GH_ParamAccess.item, Optional = true }, ParamVisibility.Voluntary));
                 return result.ToArray();
             }
         }
@@ -48,7 +48,7 @@ namespace SAM.Analytical.Grasshopper
             get
             {
                 List<GH_SAMParam> result = new List<GH_SAMParam>();
-                result.Add(new GH_SAMParam(new GooAnalyticalModelParam() {Name = "AnalyticalModel", NickName = "AnalyticalModel", Description = "SAM Analytical AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooAnalyticalModelParam() { Name = "AnalyticalModel", NickName = "AnalyticalModel", Description = "SAM Analytical AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
                 result.Add(new GH_SAMParam(new GooInternalConditionParam() { Name = "InternalConditions", NickName = "InternalConditions", Description = "SAM Analytical InternalConditions", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
                 return result.ToArray();
             }
@@ -59,8 +59,8 @@ namespace SAM.Analytical.Grasshopper
         /// </summary>
         public SAMAnalyticalUpdatePollutantGains()
           : base("SAMAnalytical.UpdatePollutantGains", "SAMAnalytical.UpdatePollutantGains",
-              "Updates Pollutant Gains Properties for Spaces"+
-              "\n*As the whole-body emission rates above were derived from Caucasian volunteers in Denmark, "+
+              "Updates Pollutant Gains Properties for Spaces" +
+              "\n*As the whole-body emission rates above were derived from Caucasian volunteers in Denmark, " +
               "\nwe estimate the total metabolic CO2 and CH4 emissions for the Danish Caucasian population using a simple calculation based on the obtained emission rates." +
               "\nWe assumed a population of 5.1 million (Caucasians in Denmark; The World Factbook at cia.gov) " +
               "\nemitting CO2 at a whole-body CO2 emission rate of 28.7 g/h/person (range 25.16–32.11)." +
@@ -80,67 +80,77 @@ namespace SAM.Analytical.Grasshopper
               "SAM", "Analytical")
         {
         }
-    protected override void SolveInstance(IGH_DataAccess dataAccess)
-    {
-        int index;
-        index = Params.IndexOfInputParam("_analyticalModel");
-        if(index == -1)
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
         {
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
-            return;
-        }
-        AnalyticalModel analyticalModel = null;
-        if (!dataAccess.GetData(index, ref analyticalModel) || analyticalModel == null)
-        {
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
-            return;
-        }
-        AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
-        List<Space> spaces = null;
-        index = Params.IndexOfInputParam("_spaces_");
-        if(index != -1)
-        {
-            spaces = new List<Space>();
-            dataAccess.GetDataList(index, spaces);
-            if (spaces != null && spaces.Count == 0)
-                spaces = null;
-        }
-        if (spaces == null)
-            spaces = analyticalModel.GetSpaces();
-        Profile profile = null;
-        index = Params.IndexOfInputParam("profile_");
-        if (index != -1)
-            dataAccess.GetData(index, ref profile);
-        double pollutantGenerationPerPerson = double.NaN;
-        index = Params.IndexOfInputParam("_pollutantGPerPerson_");
-        if (index != -1)
-            dataAccess.GetData(index, ref pollutantGenerationPerPerson);
-        double pollutantGenerationPerArea = double.NaN;
-        index = Params.IndexOfInputParam("pollutantGPerHrPerPerson_");
-        if (index != -1)
-            dataAccess.GetData(index, ref pollutantGenerationPerArea);
-        ProfileLibrary profileLibrary = analyticalModel.ProfileLibrary;
-        if (profile != null)
-            profileLibrary.Add(profile);
-        List<InternalCondition> internalConditions = new List<InternalCondition>();
-        foreach(Space space in spaces)
-        {
-            if (space == null)
-                continue;
-            Space space_Temp = adjacencyCluster.SAMObject<Space>(space.Guid);
-            if (space_Temp == null)
-                continue;
+            int index;
+            index = Params.IndexOfInputParam("_analyticalModel");
+            if (index == -1)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AnalyticalModel analyticalModel = null;
+            if (!dataAccess.GetData(index, ref analyticalModel) || analyticalModel == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+            List<Space> spaces = null;
+
+            index = Params.IndexOfInputParam("_spaces_");
+            if (index != -1)
+            {
+                spaces = new List<Space>();
+                dataAccess.GetDataList(index, spaces);
+                if (spaces != null && spaces.Count == 0)
+                    spaces = null;
+            }
+
+            if (spaces == null)
+                spaces = analyticalModel.GetSpaces();
+
+            Profile profile = null;
+            index = Params.IndexOfInputParam("profile_");
+            if (index != -1)
+                dataAccess.GetData(index, ref profile);
+
+            double pollutantGenerationPerPerson = double.NaN;
+            index = Params.IndexOfInputParam("_pollutantGPerPerson_");
+            if (index != -1)
+                dataAccess.GetData(index, ref pollutantGenerationPerPerson);
+
+            double pollutantGenerationPerArea = double.NaN;
+
+            index = Params.IndexOfInputParam("pollutantGPerHrPerArea_");
+            if (index != -1)
+                dataAccess.GetData(index, ref pollutantGenerationPerArea);
+
+            ProfileLibrary profileLibrary = analyticalModel.ProfileLibrary;
+            if (profile != null)
+                profileLibrary.Add(profile);
+
+            List<InternalCondition> internalConditions = new List<InternalCondition>();
+            foreach (Space space in spaces)
+            {
+                if (space == null)
+                    continue;
+                Space space_Temp = adjacencyCluster.SAMObject<Space>(space.Guid);
+                if (space_Temp == null)
+                    continue;
 
                 space_Temp = new Space(space_Temp);
 
                 InternalCondition internalCondition = space_Temp.InternalCondition;
-                if(internalCondition == null)
+                if (internalCondition == null)
                     internalCondition = new InternalCondition(space_Temp.Name);
 
                 if (profile != null)
                     internalCondition.SetValue(InternalConditionParameter.PollutantProfileName, profile.Name);
 
-                if(!double.IsNaN(pollutantGenerationPerPerson))
+                if (!double.IsNaN(pollutantGenerationPerPerson))
                     internalCondition.SetValue(InternalConditionParameter.PollutantGenerationPerPerson, pollutantGenerationPerPerson);
 
                 if (!double.IsNaN(pollutantGenerationPerArea))
@@ -152,7 +162,7 @@ namespace SAM.Analytical.Grasshopper
             }
 
             index = Params.IndexOfOutputParam("AnalyticalModel");
-            if(index != -1)
+            if (index != -1)
             {
                 analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster, analyticalModel.MaterialLibrary, profileLibrary);
                 dataAccess.SetData(index, new GooAnalyticalModel(analyticalModel));
