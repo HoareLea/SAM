16bfa9ac9ca701c945c899b60dea14e0589f9526
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-06-19 16:28:46 +0200

    SAMAnalyticalReplaceObject Grasshopper Component Added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalReplaceObject.cs
@@ -0,0 +1,224 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalReplaceObject : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("492b0921-b271-42f8-93d7-e47c2e577c9d");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalReplaceObject()
+          : base("SAMAnalytical.ReplaceObject", "SAMAnalytical.ReplaceObject",
+              "Replace Object in AnalyticalModel",
+              "SAM", "Analytical")
+        {
+        }
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalModelParam() { Name = "_analyticalModel", NickName = "_analyticalModel", Description = "SAM AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+                result.Add(new GH_SAMParam(new GooSAMObjectParam() { Name = "_existingObjects", NickName = "_existingObjects", Description = "SAM Objects", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooSAMObjectParam() { Name = "_newObjects", NickName = "_newObjects", Description = "SAM Objects", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+
+                result.Add(new GH_SAMParam(new GooConstructionManagerParam() { Name = "constructionManager_", NickName = "constructionManager_", Description = "SAM Analytical Construction Manager", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+
+
+                global::Grasshopper.Kernel.Parameters.Param_Boolean @boolean = null;
+                @boolean = new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "_run", NickName = "_run", Description = "Connect a boolean toggle to run.", Access = GH_ParamAccess.item };
+                @boolean.SetPersistentData(false);
+                result.Add(new GH_SAMParam(@boolean, ParamVisibility.Binding));
+                
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalModelParam() { Name = "_analyticalModel", NickName = "_analyticalModel", Description = "SAM AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            bool run = false;
+            index = Params.IndexOfInputParam("_run");
+            if(index != -1 )
+            {
+                dataAccess.GetData(index, ref run);
+            }
+
+            if (!run)
+            {
+                return;
+            }
+
+            AnalyticalModel analyticalModel = null;
+            index = Params.IndexOfInputParam("_analyticalModel");
+            if (index == -1 || !dataAccess.GetData(index, ref analyticalModel) || analyticalModel == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            analyticalModel = new AnalyticalModel(analyticalModel);
+
+            List<IJSAMObject> sAMObject_Existing = new List<IJSAMObject>();
+            index = Params.IndexOfInputParam("_existingObjects");
+            if (index == -1 || !dataAccess.GetDataList(index, sAMObject_Existing) || sAMObject_Existing == null || sAMObject_Existing.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<IJSAMObject> sAMObject_New = new List<IJSAMObject>();
+            index = Params.IndexOfInputParam("_newObjects");
+            if (index == -1 || !dataAccess.GetDataList(index, sAMObject_New) || sAMObject_New == null || sAMObject_New.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if(sAMObject_Existing.Count != sAMObject_New.Count)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            ConstructionManager constructionManager = null;
+            index = Params.IndexOfInputParam("constructionManager_");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref constructionManager);
+            }
+
+            if(constructionManager == null)
+            {
+                constructionManager = Analytical.Query.DefaultConstructionManager();
+            }
+
+            AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+            if(adjacencyCluster != null)
+            {
+                adjacencyCluster = new AdjacencyCluster(adjacencyCluster, true);
+            }
+
+            MaterialLibrary materialLibrary = analyticalModel.MaterialLibrary;
+
+            ProfileLibrary profileLibrary = analyticalModel.ProfileLibrary;
+
+            for(int i =0; i < sAMObject_Existing.Count; i++)
+            {
+                IJSAMObject jSAMObject_Existing = sAMObject_Existing[i];
+                IJSAMObject jSAMObject_New = sAMObject_New[i];
+                if(sAMObject_Existing == null || sAMObject_New == null)
+                {
+                    continue;
+                }
+
+                if(jSAMObject_Existing is ApertureConstruction)
+                {
+                    bool replaced = Analytical.Modify.Replace(adjacencyCluster, (ApertureConstruction)jSAMObject_Existing, jSAMObject_New as ApertureConstruction);
+                    if(replaced)
+                    {
+                        ApertureConstruction apertureConstruction = (ApertureConstruction)jSAMObject_New;
+                        List<IMaterial> materials = constructionManager.GetMaterials<IMaterial>(apertureConstruction);
+                        if(materials != null)
+                        {
+                            foreach(IMaterial material in materials)
+                            {
+                                if(!materialLibrary.Contains(material))
+                                {
+                                    materialLibrary.Add(material);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (jSAMObject_Existing is Construction)
+                {
+                    bool replaced = Analytical.Modify.Replace(adjacencyCluster, (Construction)jSAMObject_Existing, jSAMObject_New as Construction);
+                    if (replaced)
+                    {
+                        Construction construction = (Construction)jSAMObject_New;
+                        List<IMaterial> materials = constructionManager.GetMaterials<IMaterial>(construction);
+                        if (materials != null)
+                        {
+                            foreach (IMaterial material in materials)
+                            {
+                                if (!materialLibrary.Contains(material))
+                                {
+                                    materialLibrary.Add(material);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (jSAMObject_Existing is Space)
+                {
+                    bool replaced = Analytical.Modify.Replace(adjacencyCluster, (Space)jSAMObject_Existing, jSAMObject_New as Space);
+                    if (replaced)
+                    {
+
+                    }
+                }
+                else if (jSAMObject_Existing is IMaterial)
+                {
+                    bool replaced = Analytical.Modify.Replace(adjacencyCluster, (IMaterial)jSAMObject_Existing, jSAMObject_New as IMaterial);
+                    if (replaced)
+                    {
+                        Analytical.Modify.Replace(materialLibrary, (IMaterial)jSAMObject_Existing, jSAMObject_New as IMaterial);
+                    }
+                }
+                else if (jSAMObject_Existing is Profile)
+                {
+                    bool replaced = Analytical.Modify.Replace(adjacencyCluster, (Profile)jSAMObject_Existing, jSAMObject_New as Profile);
+                    if (replaced)
+                    {
+                        Analytical.Modify.Replace(profileLibrary, (Profile)jSAMObject_Existing, jSAMObject_New as Profile);
+                    }
+                }
+            }
+
+            analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster, materialLibrary, profileLibrary);
+            index = Params.IndexOfOutputParam("_analyticalModel");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, new GooAnalyticalModel(analyticalModel));
+            }
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/Replace.cs
@@ -0,0 +1,332 @@
+﻿using SAM.Architectural;
+using SAM.Core;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static bool Replace(this AdjacencyCluster adjacencyCluster, ApertureConstruction apertureConstruction_Existing, ApertureConstruction apertureConstruction_New)
+        {
+            if (adjacencyCluster == null || apertureConstruction_Existing == null || apertureConstruction_New == null)
+            {
+                return false;
+            }
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if(panels == null || panels.Count == 0)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach (Panel panel in panels) 
+            {
+                List<Aperture> apertures = panel?.Apertures;
+                if(apertures == null || apertures.Count == 0)
+                {
+                    continue;
+                }
+
+                Panel panel_New = null;
+                foreach (Aperture aperture in apertures)
+                {
+                    ApertureConstruction apertureConstruction = aperture?.ApertureConstruction;
+                    if(apertureConstruction.Guid != apertureConstruction_Existing.Guid)
+                    {
+                        continue;
+                    }
+
+                    Aperture aperture_New = new Aperture(aperture, apertureConstruction_New);
+
+                    if(panel_New == null)
+                    {
+                        panel_New = new Panel(panel);
+                    }
+
+                    panel_New.RemoveAperture(aperture.Guid);
+                    panel_New.AddAperture(aperture_New);
+                }
+
+                if(panel_New != null)
+                {
+                    result = true;
+                    adjacencyCluster.AddObject(panel_New);
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Replace(this AdjacencyCluster adjacencyCluster, Construction construction_Existing, Construction construction_New)
+        {
+            if (adjacencyCluster == null || construction_Existing == null || construction_New == null)
+            {
+                return false;
+            }
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if (panels == null || panels.Count == 0)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach (Panel panel in panels)
+            {
+                Construction construction = panel?.Construction;
+                if(construction == null)
+                {
+                    continue;
+                }
+
+                if (construction_Existing.Guid != construction.Guid)
+                {
+                    continue;
+                }
+
+                Panel panel_New = new Panel(panel, construction_New);
+                result = true;
+                adjacencyCluster.AddObject(panel_New);
+            }
+
+            return result;
+        }
+
+        public static bool Replace(this AdjacencyCluster adjacencyCluster, Space space_Existing, Space space_New)
+        {
+            if (adjacencyCluster == null || space_Existing == null || space_New == null)
+            {
+                return false;
+            }
+
+            List<Space> spaces = adjacencyCluster.GetSpaces();
+            if (spaces == null || spaces.Count == 0)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach (Space space in spaces)
+            {
+                if(space == null)
+                {
+                    continue;
+                }
+
+                if(space.Guid != space_Existing.Guid)
+                {
+                    continue;
+                }
+
+                List<IJSAMObject> jSAMObjects = adjacencyCluster.GetRelatedObjects<Core.IJSAMObject>(space);
+                adjacencyCluster.Remove(new List<Space>() { space });
+                adjacencyCluster.AddObject(space_New);
+                jSAMObjects.ForEach(x => adjacencyCluster.AddRelation(space_New, x));
+                return true;
+            }
+
+            return result;
+        }
+
+        public static bool Replace(this MaterialLibrary materialLibrary, IMaterial material_Existing, IMaterial material_New)
+        {
+            if (materialLibrary == null || material_Existing == null || material_New == null)
+            {
+                return false;
+            }
+
+            if(!materialLibrary.Contains(material_Existing))
+            {
+                return false;
+            }
+
+            materialLibrary.Remove(material_Existing);
+            materialLibrary.Add(material_New);
+            return true;
+        }
+
+        public static bool Replace(this AdjacencyCluster adjacencyCluster, IMaterial material_Existing, IMaterial material_New)
+        {
+            if (adjacencyCluster == null || material_Existing == null || material_New == null)
+            {
+                return false;
+            }
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if (panels == null || panels.Count == 0)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach (Panel panel in panels)
+            {
+                Panel panel_New = null;
+
+                Construction construction = panel?.Construction;
+                if(construction != null)
+                {
+                    List<ConstructionLayer> constructionLayers = construction.ConstructionLayers;
+                    if(constructionLayers != null)
+                    {
+                        for(int i = 0; i < constructionLayers.Count; i++)
+                        {
+                            if (constructionLayers[i].Name == material_Existing.Name)
+                            {
+                                if(panel_New == null)
+                                {
+                                    panel_New = new Panel(panel);
+                                }
+
+                                constructionLayers[i] = new ConstructionLayer(material_New.Name, constructionLayers[i].Thickness);
+                            }
+                        }
+                    }
+
+                    if(panel_New != null)
+                    {
+                        adjacencyCluster.AddObject(panel_New);
+                    }
+                }
+
+                panel_New = null;
+
+                List<Aperture> apertures = panel?.Apertures;
+                if (apertures == null || apertures.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (Aperture aperture in apertures)
+                {
+                    Aperture aperture_New = null;
+                    
+                    ApertureConstruction apertureConstruction = aperture?.ApertureConstruction;
+                    if (apertureConstruction != null)
+                    {
+                        List<ConstructionLayer> frameConstructionLayers = apertureConstruction.FrameConstructionLayers;
+                        if (frameConstructionLayers != null)
+                        {
+                            for (int i = 0; i < frameConstructionLayers.Count; i++)
+                            {
+                                if (frameConstructionLayers[i].Name == material_Existing.Name)
+                                {
+                                    if (aperture_New == null)
+                                    {
+                                        aperture_New = new Aperture(aperture);
+                                    }
+
+                                    frameConstructionLayers[i] = new ConstructionLayer(material_New.Name, frameConstructionLayers[i].Thickness);
+                                }
+                            }
+                        }
+
+                        List<ConstructionLayer> paneConstructionLayers = apertureConstruction.PaneConstructionLayers;
+                        if (paneConstructionLayers != null)
+                        {
+                            for (int i = 0; i < paneConstructionLayers.Count; i++)
+                            {
+                                if (paneConstructionLayers[i].Name == material_Existing.Name)
+                                {
+                                    if (aperture_New == null)
+                                    {
+                                        aperture_New = new Aperture(aperture);
+                                    }
+
+                                    paneConstructionLayers[i] = new ConstructionLayer(material_New.Name, paneConstructionLayers[i].Thickness);
+                                }
+                            }
+                        }
+
+                        if (aperture_New != null)
+                        {
+                            apertureConstruction = new ApertureConstruction(apertureConstruction, paneConstructionLayers, frameConstructionLayers);
+                            aperture_New = new Aperture(aperture, apertureConstruction);
+                        }
+                    }
+
+
+                    if (aperture_New == null)
+                    {
+                        continue;
+                    }
+
+                    if (panel_New == null)
+                    {
+                        panel_New = new Panel(panel);
+                    }
+
+                    panel_New.RemoveAperture(aperture.Guid);
+                    panel_New.AddAperture(aperture_New);
+                }
+
+                if (panel_New != null)
+                {
+                    result = true;
+                    adjacencyCluster.AddObject(panel_New);
+                }
+            }
+
+            return result;
+        }
+
+        public static bool Replace(this ProfileLibrary profileLibrary, Profile profile_Existing, Profile profile_New)
+        {
+            if (profileLibrary == null || profile_Existing == null || profile_New == null)
+            {
+                return false;
+            }
+
+            if (!profileLibrary.Contains(profile_Existing))
+            {
+                return false;
+            }
+
+            profileLibrary.Remove(profile_Existing);
+            profileLibrary.Add(profile_New);
+            return true;
+        }
+
+        public static bool Replace(this AdjacencyCluster adjacencyCluster, Profile profile_Existing, Profile profile_New)
+        {
+            if (adjacencyCluster == null || profile_Existing == null || profile_New == null)
+            {
+                return false;
+            }
+
+            List<Space> spaces = adjacencyCluster.GetSpaces();
+            if(spaces == null)
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach(Space space in spaces)
+            {
+                InternalCondition internalCondition = space?.InternalCondition;
+                if(internalCondition == null)
+                {
+                    continue;
+                }
+
+                string profileName = internalCondition.GetProfileName(profile_Existing.ProfileType);
+                if(profileName == profile_Existing.Name)
+                {
+                    continue;
+                }
+
+                internalCondition.SetProfileName(profile_Existing.ProfileType, profile_New.Name);
+
+                Space space_New = new Space(space);
+                space_New.InternalCondition = internalCondition;
+
+                adjacencyCluster.AddObject(space_New);
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
