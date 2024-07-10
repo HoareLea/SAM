47dfe3442ce07e97f124e84c5705dede8a0edbf3
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-07-10 10:37:00 +0200

    HeatingDesignDays and CoolingDesignDays Added to AnalyticalModel Parameters

--- SAM/SAM.Analytical/Enums/Parameter/AnalyticalModelParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/AnalyticalModelParameter.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using SAM.Core;
 using SAM.Core.Attributes;
 
 namespace SAM.Analytical
@@ -10,5 +11,7 @@ namespace SAM.Analytical
         [ParameterProperties("Cooling Sizing Factor", "Cooling Sizing Factor"), DoubleParameterValue(0)] CoolingSizingFactor,
         [ParameterProperties("Heating Sizing Factor", "Heating Sizing Factor"), DoubleParameterValue(0)] HeatingSizingFactor,
         [ParameterProperties("Weather Data", "Weather Data"), SAMObjectParameterValue(typeof(Weather.WeatherData))] WeatherData,
+        [ParameterProperties("Heating Design Days", "Heating Design Days"), SAMObjectParameterValue(typeof(SAMCollection<DesignDay>))] HeatingDesignDays,
+        [ParameterProperties("Cooling Design Days", "Cooling Design Days"), SAMObjectParameterValue(typeof(SAMCollection<DesignDay>))] CoolingDesignDays,
     }
 }
