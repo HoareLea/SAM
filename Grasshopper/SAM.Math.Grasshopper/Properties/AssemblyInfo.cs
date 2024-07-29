5b226e964d785dafc56c53166a499fe303754156
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-07-29 12:14:05 +0200

    Upgrade Grasshopper to .NET Core

--- Grasshopper/SAM.Math.Grasshopper/Properties/AssemblyInfo.cs
+++ Grasshopper/SAM.Math.Grasshopper/Properties/AssemblyInfo.cs
@@ -22,16 +22,6 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SAM.Geometry.Grasshopper")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SAM_Grasshopper")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -42,16 +32,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fe6bae1b-5e7f-4012-ae71-02a676ed8d21")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
