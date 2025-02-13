f2c51b952c177fe3e7dc9f62854fe9fa34015f76
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-02-13 11:42:08 +0100

    HashCode query implemented

---
+++ SAM/SAM.Core/Query/HashCode.cs
@@ -0,0 +1,21 @@
+﻿namespace SAM.Core
+{
+    public static partial class Query
+    {
+        public static int HashCode(params int[] values)
+        {
+            if (values == null)
+            {
+                return 0;
+            }
+
+            int result = 17;
+            foreach (int value in values)
+            {
+                result = result * 31 + value;
+            }
+
+            return result;
+        }
+    }
+}
