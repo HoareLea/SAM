2ccdfb104797d9a52b6b67f4c81e6846732ff26b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-05-16 15:33:34 +0200

    Hydra link variable added

---
+++ SAM/SAM.Core/Variables/Link.cs
@@ -0,0 +1,10 @@
+﻿namespace SAM.Core
+{
+    public static class Link
+    {
+        /// <summary>
+        /// Hydra web link
+        /// </summary>
+        public const string Hydra = "https://hydra.originaec.app/";
+    }
+}
