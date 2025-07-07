7ed1c78ade78829956dc142f0726e73c9880fe11
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-07-07 10:34:13 +0200

    ExportHydra initial implementation

---
+++ Grasshopper/SAM.Core.Grasshopper/Component/SAMHydraExportFile.cs
@@ -0,0 +1,168 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Core.Grasshopper.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper
+{
+    public class SAMHydraExportFile : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("e728a90f-a7e0-47e1-b45b-6e2a96222d1d");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small3;
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        //private GH_OutputParamManager outputParamManager;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMHydraExportFile()
+          : base("SAMHydra.ExportFile", "SAMHydra.ExportFile",
+              "Export File to Hydra.",
+              "SAM", "Hydra")
+        {
+        }
+
+        
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "_githubUserName", NickName = "_githubUserName", Description = "Github user name", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "_fileName", NickName = "_fileName", Description = "File name", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "_fileDescription", NickName = "_fileDescription", Description = "File description", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "changeLog_", NickName = "changeLog_", Description = "Change log", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "fileTags_", NickName = "fileTags_", Description = "File tags", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "targetFolder_", NickName = "targetFolder_", Description = "Target folder", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "includeRhino_", NickName = "includeRhino_", Description = "Include Rhino", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "gHForThumb_", NickName = "gHForThumb_", Description = "Grasshopper for thumb", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "additionalFiles_", NickName = "additionalFiles_", Description = "Additional files", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "_export", NickName = "_export", Description = "Export", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_String() { Name = "messages", NickName = "messages", Description = "Messages", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            string githubUserName = null;
+            index = Params.IndexOfInputParam("_githubUserName");
+            if (index == -1 || !dataAccess.GetData(index, ref githubUserName) || string.IsNullOrWhiteSpace(githubUserName))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            string fileName = null;
+            index = Params.IndexOfInputParam("_fileName");
+            if (index == -1 || !dataAccess.GetData(index, ref fileName) || string.IsNullOrWhiteSpace(fileName))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<string> description = new List<string>();
+            index = Params.IndexOfInputParam("_fileDescription");
+            if (index == -1 || !dataAccess.GetDataList(index, description) || description == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<string> changeLog = new List<string>();
+            index = Params.IndexOfInputParam("changeLog_");
+            if (index != -1)
+            {
+                dataAccess.GetDataList(index, changeLog);
+            }
+
+            List<string> fileTags = new List<string>();
+            index = Params.IndexOfInputParam("fileTags_");
+            if (index != -1)
+            {
+                dataAccess.GetDataList(index, fileTags);
+            }
+
+            string targetFolder = null;
+            index = Params.IndexOfInputParam("targetFolder_");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref targetFolder);
+            }
+
+            bool includeRhino = true;
+            index = Params.IndexOfInputParam("includeRhino_");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref includeRhino);
+            }
+
+            bool gHForThumb = true;
+            index = Params.IndexOfInputParam("gHForThumb_");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref gHForThumb);
+            }
+
+            List<string> additionalFiles = new List<string>();
+            index = Params.IndexOfInputParam("additionalFiles_");
+            if (index != -1)
+            {
+                dataAccess.GetDataList(index, additionalFiles);
+            }
+
+            bool export = false;
+            index = Params.IndexOfInputParam("_export");
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref export);
+            }
+
+            List<string> messages = new List<string>() { "Export not activated." };
+            if (export)
+            {
+                messages = Modify.ExportHydra(OnPingDocument(), githubUserName, fileName, description, changeLog, fileTags, targetFolder, includeRhino, gHForThumb, additionalFiles);
+            }
+
+            index = Params.IndexOfOutputParam("messages");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, messages?.ConvertAll(x => new GH_String(x)));
+            }
+
+        }
+    }
+}
---
+++ Grasshopper/SAM.Core.Grasshopper/Modify/ExportHydra.cs
@@ -0,0 +1,320 @@
+﻿using Grasshopper;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Rhino;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.Json;
+
+namespace SAM.Core.Grasshopper
+{
+    public static partial class Modify
+    {
+        public static List<string> ExportHydra(this GH_Document gH_Document, string githubUserName, string fileName, List<string> fileDescription, IEnumerable<string> changeLog, IEnumerable<string> fileTags, string targetFolder, bool includeRhino, bool gHForThumb, List<string> additionalFiles)
+        {
+            return null;
+
+
+            List<string> result = new List<string>();
+
+            if(gH_Document == null)
+            {
+                result.Add("Invalid document");
+                return result;
+            }
+
+            if (string.IsNullOrWhiteSpace(targetFolder) || !Directory.Exists(targetFolder))
+            {
+                targetFolder = "C:\\Temp\\ScriptsHydra\\";
+            }
+
+            RhinoDoc rhinoDoc = gH_Document.RhinoDocument;
+
+            string safeFileName = fileName.Replace(" ", "_");
+
+            result.Add($"Target folder used: {targetFolder}");
+
+            targetFolder = Path.Combine(targetFolder, "3-Examples_GH", safeFileName);
+
+            if(!Directory.Exists(targetFolder))
+            {
+                Directory.CreateDirectory(targetFolder);
+            }
+
+            string ghPath = rhinoDoc.Path;
+            if (string.IsNullOrEmpty(ghPath) || !File.Exists(ghPath))
+            {
+                result.Add($"Error: GH file not saved.");
+                return result;
+            }
+
+            string ghTarget = Path.Combine(targetFolder, safeFileName + ".gh");
+            File.Copy(ghPath, ghTarget, true);
+
+            // Write full-size Grasshopper image
+            string ghImgPath = WriteGHImage(safeFileName, targetFolder);
+            Bitmap ghImg = File.Exists(ghImgPath) ? new Bitmap(ghImgPath) : null;
+
+            // Copy Rhino file if needed & save Rhino image
+            string rhinoImgPath = null;
+            Bitmap rhinoImg = null;
+            if (includeRhino)
+            {
+                string rhinoPath = rhinoDoc.Path;
+                if (string.IsNullOrEmpty(rhinoPath) || !File.Exists(rhinoPath))
+                {
+                    ghImg?.Dispose();
+                    result.Add("Error: Rhino file not saved.");
+                    return result;
+                }
+                string rhinoTarget = Path.Combine(targetFolder, safeFileName + ".3dm");
+                File.Copy(rhinoPath, rhinoTarget, true);
+
+                rhinoImgPath = WriteRhinoImage(safeFileName, targetFolder, rhinoDoc);
+                if (File.Exists(rhinoImgPath))
+                    rhinoImg = new Bitmap(rhinoImgPath);
+            }
+
+            // Copy additional files
+            if (additionalFiles != null)
+            {
+                foreach (string additionalFile in additionalFiles)
+                {
+                    if (File.Exists(additionalFile))
+                        File.Copy(additionalFile, Path.Combine(targetFolder, Path.GetFileName(additionalFile)), true);
+                }
+            }
+
+            // Make thumbnail (aspect ratio, width 200px, from GH or Rhino image)
+            string thumbnailPath = MakeThumbnailImg(ghImg, rhinoImg, gHForThumb, targetFolder);
+
+            // Dispose images
+            ghImg?.Dispose();
+            rhinoImg?.Dispose();
+
+            // Write README.md
+            result.Add("### Description");
+            foreach (string line in fileDescription)
+            {
+                result.Add(line);
+            }
+
+            if (changeLog != null && changeLog.Count() > 0)
+            {
+                result.Add("\n### Changelog");
+                foreach (string line in changeLog)
+                {
+                    result.Add(line);
+                }
+            }
+
+            if (fileTags != null && fileTags.Count() > 0)
+            {
+                result.Add("\n### Tags");
+                result.Add(string.Join(", ", fileTags));
+            }
+
+            result.Add("\n### Thumbnail");
+            result.Add("![Screenshot](thumbnail.png)");
+            File.WriteAllText(Path.Combine(targetFolder, "README.md"), string.Join("\n", result).ToString());
+
+            // Write input.json (metadata)
+            List<IGH_DocumentObject> ghComps = GetAllTheComponents(gH_Document);
+            Dictionary<string, object> metaDataDict = GetMetaData(ghComps, fileTags?.ToList(), safeFileName, rhinoImgPath, additionalFiles, gHForThumb);
+            WriteMetadataFile(safeFileName, targetFolder, metaDataDict);
+
+            // Zip everything for upload
+            string zipPath = Path.Combine(Path.GetDirectoryName(targetFolder), safeFileName + ".zip");
+            if (File.Exists(zipPath)) File.Delete(zipPath);
+            ZipFile.CreateFromDirectory(targetFolder, zipPath);
+
+            // ========== GIT OPERATIONS ==========
+            string repoRoot = Directory.GetParent(Directory.GetParent(targetFolder).FullName).FullName;
+
+            string branchName = githubUserName + "_" + fileName;
+
+            // 1. Pull latest
+            ExecuteGitCommand(repoRoot, "pull");
+
+            // 2. Create new branch
+            ExecuteGitCommand(repoRoot, $"checkout -b {branchName}");
+
+            // 3. Add all
+            ExecuteGitCommand(repoRoot, "add .");
+
+            // 4. Commit
+            string commitMsg = $"Added {safeFileName}";
+            ExecuteGitCommand(repoRoot, $"commit -m \"{commitMsg}\"");
+
+            // 5. Push new branch
+            ExecuteGitCommand(repoRoot, $"push -u origin {branchName}");
+
+            // 6. Generate pull request link
+            string repoUrl = $"https://github.com/HoareLea/ScriptsHydra/compare/{branchName}?expand=1";
+
+            result.Add($"Export and Git push successful: {zipPath}\n\nTo create a pull request, open this link in your browser:\n{repoUrl}");
+            return result;
+
+        }
+
+        // Save hi-res image of Grasshopper canvas
+        private static string WriteGHImage(string fileName, string repoTargetFolder)
+        {
+            GH_Canvas canvas = Instances.ActiveCanvas;
+            RectangleF rectF = canvas.Document.BoundingBox(false);
+            Rectangle rect = Rectangle.Truncate(rectF);
+            Size size;
+            string tmpPath = canvas.GenerateHiResImage(rect, new GH_Canvas.GH_ImageSettings(), out size)[0];
+            string ghImgPath = Path.Combine(repoTargetFolder, fileName + "_GH.png");
+            File.Copy(tmpPath, ghImgPath, true);
+            File.Delete(tmpPath);
+            return ghImgPath;
+        }
+
+        // Save screenshot of active Rhino view
+        private static string WriteRhinoImage(string fileName, string repoTargetFolder, RhinoDoc rhinoDoc)
+        {
+            var view = rhinoDoc.Views.ActiveView;
+            int image_w = view.ActiveViewport.Size.Width;
+            int image_h = view.ActiveViewport.Size.Height;
+            Size viewSize = new Size(image_w, image_h);
+            Bitmap pic = view.CaptureToBitmap(viewSize);
+
+            string fullPath = Path.Combine(repoTargetFolder, fileName + "_Rhino.png");
+            pic.Save(fullPath);
+            pic.Dispose();
+            return fullPath;
+        }
+
+        // Make a 200px wide thumbnail from GH or Rhino image (aspect ratio)
+        private static string MakeThumbnailImg(Bitmap ghImg, Bitmap rhinoImg, bool GHForThumb_, string repoTargetFolder)
+        {
+            int thumbnailW = 200;
+            int thumbnailH = 200;
+
+            Bitmap sourceImg = rhinoImg;
+            if (GHForThumb_)
+            {
+                sourceImg = ghImg;
+            }
+
+            if (sourceImg == null)
+            {
+                return null;
+            }
+
+            double imgRatio = (double)sourceImg.Height / (double)sourceImg.Width;
+            thumbnailH = (int)(thumbnailW * imgRatio);
+            Size imgSize = new Size(thumbnailW, thumbnailH);
+
+            Bitmap thumbnail = new Bitmap(sourceImg, imgSize);
+            string thumbnailPath = Path.Combine(repoTargetFolder, "thumbnail.png");
+            thumbnail.Save(thumbnailPath);
+            thumbnail.Dispose();
+            return thumbnailPath;
+        }
+
+        // Gather all components (for metadata)
+        private static List<IGH_DocumentObject> GetAllTheComponents(GH_Document document)
+        {
+            return document.Objects.ToList();
+        }
+
+        // Compose metadata as dictionary (JSON serializable)
+        private static Dictionary<string, object> GetMetaData(
+            List<IGH_DocumentObject> ghComps,
+            List<string> fileTags,
+            string fileName,
+            string rhinoDocPath,
+            List<string> additionalFiles,
+            bool GHForThumb_
+            )
+        {
+            Dictionary<string, object> metaDataDict = new Dictionary<string, object>();
+            Dictionary<string, int> components = new Dictionary<string, int>();
+            List<string> dependencies = new List<string>();
+            List<Dictionary<string, string>> images = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> addFiles = new List<Dictionary<string, string>>();
+
+            // These lists can be adapted to match what you want to filter
+            string[] notImportantComps = new string[] {
+            "Hydra", "Scribble", "SAMHydra_ExportFile", "SAMHydra_ImportFile", "Group", "Panel", "Slider", "Boolean Toggle", "Custom Preview", "Colour Swatch",
+            "Button", "Control Knob", "Digit Scroller", "MD Slider", "Value List", "Point", "Vector", "Circle", "Circular Arc", "Curve", "Line", "Plane", "Rectangle", "Box", "Mesh", "Mesh Face", "Surface", "Twisted Box", "Field", "Geometry", "Geometry Cache", "Geometry Pipeline", "Transform"
+        };
+
+            foreach (IGH_DocumentObject comp in ghComps)
+            {
+                string componentName = comp.Name;
+                if (notImportantComps.Contains(componentName)) continue;
+                if (components.ContainsKey(componentName))
+                    components[componentName]++;
+                else
+                    components[componentName] = 1;
+
+                Type objType = comp.GetType();
+                string category = (objType.GetProperty("Category")?.GetValue(comp, null) ?? "User").ToString();
+                string subcategory = (objType.GetProperty("SubCategory")?.GetValue(comp, null) ?? "").ToString();
+                if (!dependencies.Contains(category) && !string.IsNullOrEmpty(category))
+                    dependencies.Add(category);
+            }
+
+            metaDataDict["components"] = components;
+            metaDataDict["dependencies"] = dependencies;
+            metaDataDict["tags"] = fileTags ?? new List<string>();
+
+            // Images
+            images.Add(new Dictionary<string, string> { { fileName + "_GH.png", "Grasshopper Definition" } });
+            if (!GHForThumb_)
+                images.Add(new Dictionary<string, string> { { fileName + "_Rhino.png", "Rhino Viewport Screenshot" } });
+            metaDataDict["images"] = images;
+
+            // Addfiles
+            if (additionalFiles != null)
+            {
+                foreach (string file in additionalFiles)
+                {
+                    if (File.Exists(file))
+                        addFiles.Add(new Dictionary<string, string> { { Path.GetFileName(file), "Additional file" } });
+                }
+            }
+            metaDataDict["Addfiles"] = addFiles;
+
+            metaDataDict["thumbnail"] = "thumbnail.png";
+            metaDataDict["file"] = fileName + ".zip";
+            return metaDataDict;
+        }
+
+        // Write metadata as JSON
+        private static void WriteMetadataFile(string fileName, string repoTargetFolder, Dictionary<string, object> metaDataDict)
+        {
+            string jsonFilePath = Path.Combine(repoTargetFolder, "input.json");
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(metaDataDict, options);
+            File.WriteAllText(jsonFilePath, json);
+        }
+
+        // Run git commands (helper)
+        private static void ExecuteGitCommand(string workingDir, string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "git",
+                Arguments = command,
+                WorkingDirectory = workingDir,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            using (Process process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+            }
+        }
+    }
+}
