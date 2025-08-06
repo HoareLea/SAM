4d0debd426f0b1eb5f0c4059419e00f0d5ee801b
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-08-06 17:16:52 +0200

    SAMCoreParameterType Grasshopper component added

---
+++ Grasshopper/SAM.Core.Grasshopper/Component/SAMCoreParameterType.cs
@@ -0,0 +1,33 @@
+﻿using SAM.Core.Grasshopper.Properties;
+using System;
+
+namespace SAM.Core.Grasshopper
+{
+    public class SAMCoreParameterType : GH_SAMEnumComponent<ParameterType>
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("96271426-fb5c-4bf5-9877-9ca5adda133a");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small3;
+
+        /// <summary>
+        /// Clear Option Enum Component
+        /// </summary>
+        public SAMCoreParameterType()
+          : base("SAMCore.ParameterType", "SAMCore.ParameterType",
+              "Select ParameterType",
+              "SAM", "Core")
+        {
+        }
+    }
+}
