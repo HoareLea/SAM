a1213fbc3a5cd9518b4a74ed1d2f16f74583ce40
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-08-07 17:52:49 +0200

    SAMAnalyticalSetDefaultConstructionLayerByPanelType  Grasshopper component added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalSetDefaultConstructionLayerByPanelType.cs
@@ -0,0 +1,121 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalSetDefaultConstructionLayerByPanelType : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("b9d64015-afb2-4ce5-a402-c3ad58a4e44d");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+                protected override System.Drawing.Bitmap Icon => Core.Convert.ToBitmap(Resources.SAM_Small);
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analytical", NickName = "_analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "_panels_", NickName = "_panels_", Description = "SAM Analytical Panels to be modified", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new global::Grasshopper.Kernel.Parameters.Param_GenericObject() { Name = "Analytical", NickName = "Analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "Panels", NickName = "Panels", Description = "Modified SAM Analytical Panels", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                result.Add(new GH_SAMParam(new GooPanelParam() { Name = "PanelsWithIssues", NickName = "PanelsWithIssues", Description = "Modified SAM Analytical Panels with issues", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMAnalyticalSetDefaultConstructionLayerByPanelType()
+          : base("SAMAnalytical.SetDefaultConstructionLayerByPanelType", "SAMAnalytical.SetDefaultConstructionLayerByPanelType",
+              "Sets Default Construction Layers By PanelType for Adjacency Cluster only if currect construction does not follow naming convenction ",
+              "SAM", "Analytical03")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            IAnalyticalObject analyticalObject = null;
+            index = Params.IndexOfInputParam("_analytical");
+            if (index == -1 || !dataAccess.GetData(0, ref analyticalObject) || analyticalObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_panels_");
+            List<Panel> panels = new List<Panel>();
+            if (index != -1)
+                dataAccess.GetDataList(index, panels);
+
+            List<Panel> panelsWithIssues = null;
+
+            if(analyticalObject is AdjacencyCluster)
+            {
+                AdjacencyCluster adjacencyCluster = new AdjacencyCluster((AdjacencyCluster)analyticalObject);
+                panels = adjacencyCluster.SetDefaultConstructionLayerByPanelType(out panelsWithIssues, panels?.ConvertAll(x => x.Guid))?.ToList();
+                analyticalObject = adjacencyCluster;
+            }
+            else if(analyticalObject is AnalyticalModel)
+            {
+                AnalyticalModel analyticalModel = new AnalyticalModel((AnalyticalModel)analyticalObject);
+                AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+                if(adjacencyCluster != null)
+                {
+                    adjacencyCluster = new AdjacencyCluster(adjacencyCluster);
+                    panels = adjacencyCluster.SetDefaultConstructionLayerByPanelType(out panelsWithIssues, panels?.ConvertAll(x => x.Guid))?.ToList();
+                    analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster);
+                }
+
+                analyticalObject = analyticalModel;
+            }
+
+            index = Params.IndexOfOutputParam("Analytical");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, analyticalObject);
+            }
+
+            index = Params.IndexOfOutputParam("Panels");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, panels?.ConvertAll(x => new GooPanel(x)));
+            }
+
+            index = Params.IndexOfOutputParam("PanelsWithIssues");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, panelsWithIssues?.ConvertAll(x => new GooPanel(x)));
+            }
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/SetDefaultConstructionLayerByPanelType.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static IEnumerable<Panel> SetDefaultConstructionLayerByPanelType(this AdjacencyCluster adjacencyCluster, out List<Panel> panelsWithIssues, IEnumerable<Guid> guids = null)
+        {
+            panelsWithIssues = null;
+
+            if (adjacencyCluster == null)
+                return null;
+
+            string wallInternalConstructionPrefix = "SIM_INT_";
+            string wallExternalConstructionPrefix = "SIM_EXT_";
+            string floorInternalConstructionPrefix = "SIM_INT_";
+            string roofExternalConstructionPrefix = "SIM_EXT_";
+            string floorExposedConstructionPrefix = "SIM_EXT_";
+            string slabOnGradeConstructionPrefix = "SIM_EXT_";
+            string floorConstructionName = "SIM_EXT_GRD_FLR";
+
+            List<Panel> panels = adjacencyCluster.GetObjects<Panel>();
+            if (panels == null || panels.Count == 0)
+                return null;
+
+            if (guids != null && guids.Count() > 0)
+                panels = panels.FindAll(x => guids.Contains(x.Guid));
+
+            List<Tuple<Panel, PanelType>> tuples = new List<Tuple<Panel, PanelType>>();
+
+            foreach (Panel panel in panels)
+            {
+                PanelType panelType = panel.PanelType;
+                if(panelType == PanelType.Undefined)
+                {
+                    continue;
+                }
+
+                bool update = false;
+
+                Construction construction = panel.Construction;
+                if (string.IsNullOrWhiteSpace(construction?.Name))
+                {
+                    if(panelType != PanelType.Air)
+                    {
+                        update = true;
+                    }
+                }
+                else
+                {
+                    double elevation = double.NaN;
+
+                    switch (panelType)
+                    {
+                        case PanelType.Air:
+                            update = construction != null;
+                            break;
+                        case PanelType.Ceiling:
+                            panelType = Query.PanelType(panel.Normal);
+                            List<Space> spaces = adjacencyCluster?.GetRelatedObjects<Space>(panel);
+                            switch (panelType.PanelGroup())
+                            {
+                                case PanelGroup.Wall:
+                                    update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                    panelType = spaces != null && spaces.Count > 1 ? PanelType.WallInternal : PanelType.WallExternal;
+                                    break;
+                                case PanelGroup.Floor:
+                                    update = !construction.Name.StartsWith(floorExposedConstructionPrefix);
+                                    panelType = spaces != null && spaces.Count > 1 ? PanelType.FloorInternal : PanelType.FloorExposed;
+                                    break;
+                                case PanelGroup.Roof:
+                                    update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
+                                    panelType = spaces != null && spaces.Count > 1 ? PanelType.FloorInternal : PanelType.Roof;
+                                    break;
+                            }
+                            break;
+                        case PanelType.CurtainWall:
+                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                            panelType = PanelType.Roof;
+                            break;
+                        case PanelType.Wall:
+                            elevation = Geometry.Object.Spatial.Query.MaxElevation(panel);
+
+                            if (elevation <= 0)
+                            {
+                                update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                panelType = PanelType.UndergroundWall;
+                            }
+                            else
+                            {
+                                update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                                panelType = PanelType.WallExternal;
+                            }
+                            break;
+                        case PanelType.WallExternal:
+                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix);
+                            break;
+                        case PanelType.WallInternal:
+                            update = !panel.Construction.Name.StartsWith(wallInternalConstructionPrefix);
+                            break;
+                        case PanelType.Roof:
+                            update = !construction.Name.StartsWith(roofExternalConstructionPrefix);
+                            break;
+                        case PanelType.Floor:
+                            PanelType panelType_Normal = Query.PanelType(panel.Normal);
+                            if (panelType_Normal == PanelType.Floor)
+                            {
+                                elevation = Geometry.Object.Spatial.Query.MaxElevation(panel);
+
+                                if (elevation == 0)
+                                {
+                                    update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
+                                    panelType = PanelType.SlabOnGrade;
+                                }
+                                else if (elevation < 0)
+                                {
+                                    update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix);
+                                    panelType = PanelType.SlabOnGrade;
+                                }
+                                else
+                                {
+                                    panelType = PanelType.FloorExposed;
+                                }
+                            }
+                            else if (panelType_Normal == PanelType.Roof)
+                            {
+                                panelType = PanelType.Roof;
+                            }
+                            break;
+                        case PanelType.SlabOnGrade:
+                            update = construction.Name.Equals(floorConstructionName) || !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.UndergroundSlab:
+                            update = !construction.Name.StartsWith(slabOnGradeConstructionPrefix) || !construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.FloorInternal:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
+                            break;
+                        case PanelType.UndergroundCeiling:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
+                            break;
+                        case PanelType.FloorExposed:
+                            update = !construction.Name.StartsWith(floorExposedConstructionPrefix) || construction.Name.Contains("_GRD_") || construction.Name.Contains("Roof");
+                            break;
+                        case PanelType.FloorRaised:
+                            update = !construction.Name.StartsWith(floorInternalConstructionPrefix);
+                            break;
+                        case PanelType.Shade:
+                            update = !construction.Name.Contains("_SHD_");
+                            break;
+                        case PanelType.UndergroundWall:
+                            update = !construction.Name.StartsWith(wallExternalConstructionPrefix) || !construction.Name.Contains("_GRD_");
+                            break;
+                    }
+                }
+
+                if (!update)
+                {
+                    continue;
+                }
+
+                if(tuples.Find(x => x.Item1.Guid == panel.Guid) != null)
+                {
+                    continue;
+                }
+
+                tuples.Add(new Tuple<Panel, PanelType>(panel, panelType));
+
+                //result[panel.Guid] = new Panel(panel, panel.Construction == null ? new Construction(construction) : new Construction(panel.Construction, construction.ConstructionLayers));
+            }
+
+            panelsWithIssues = new List<Panel>();
+
+            Dictionary<Guid, Panel> dictionary = new Dictionary<Guid, Panel>();
+            while (tuples.Count > 0)
+            {
+                Tuple<Panel, PanelType> tuple = tuples[0];
+
+                Panel panel = tuple.Item1;
+                Construction construction = panel.Construction;
+
+                List<Tuple<Panel, PanelType>> tuples_Construction = tuples.FindAll(x => x.Item1.Construction?.Guid == construction?.Guid);
+                tuples_Construction.ForEach(x => tuples.Remove(x));
+
+                IEnumerable<PanelType> panelTypes = tuples_Construction.ConvertAll(x => x.Item2).Distinct();
+                foreach(PanelType panelType in panelTypes)
+                {
+                    List<Tuple<Panel, PanelType>> tuples_PanelType = tuples_Construction.FindAll(x => x.Item2 == panelType);
+                    foreach(Tuple<Panel, PanelType> tuple_PanelType in tuples_PanelType)
+                    {
+                        Construction construction_Default = Query.DefaultConstruction(panelType);
+                        if (construction_Default == null)
+                        {
+                            continue;
+                        }
+
+                        Construction construction_PanelType = construction == null ? null : new Construction(Guid.NewGuid(), construction, construction.Name);
+
+                        Panel panel_New = new Panel(panel, construction_PanelType == null ? new Construction(construction_PanelType) : new Construction(construction_PanelType, construction_Default.ConstructionLayers));
+                        adjacencyCluster.AddObject(panel_New);
+                        dictionary[panel_New.Guid] = panel_New;
+
+                        if(panelTypes.Count() > 1)
+                        {
+                            panelsWithIssues.Add(panel_New);
+                        }
+                    }
+                }
+            }
+
+            return dictionary.Values;
+        }
+    }
+}
