b45776235357a6150ff05bc777a096a5b136fcc9
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-08-08 11:28:59 +0200

    SAMAnalyticalSetDefaultApertureConstructionLayers Grasshopper component added

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalSetDefaultApertureConstructionLayers.cs
@@ -0,0 +1,108 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalSetDefaultApertureConstructionLayers : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("30a2eeb9-fdbd-4531-b517-af30fb012a07");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+                protected override System.Drawing.Bitmap Icon => Core.Convert.ToBitmap(Resources.SAM_Small);
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analytical", NickName = "_analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooApertureParam() { Name = "_apertures_", NickName = "_apertures_", Description = "SAM Analytical Apertures to be modified", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "analytical", NickName = "analytical", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooApertureParam() { Name = "apertures", NickName = "apertures", Description = "Modified SAM Analytical Apertures", Access = GH_ParamAccess.list }, ParamVisibility.Voluntary));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMAnalyticalSetDefaultApertureConstructionLayers()
+          : base("SAMAdjacencyCluster.SetDefaultApertureConstructionLayers", "SAMAdjacencyCluster.SetDefaultApertureConstructionLayers",
+              "Sets Default ApertureConstruction Layers By PanelType and ApertureType for Adjacency Cluster only if currect ApertureConstruction does not follow naming convenction ",
+              "SAM", "Analytical03")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            SAMObject sAMObject = null;
+            index = Params.IndexOfInputParam("_analytical");
+            if (index == -1 || !dataAccess.GetData(0, ref sAMObject) || sAMObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("apertures_");
+            List<Aperture> apertures = new List<Aperture>();
+            if (index != -1)
+                dataAccess.GetDataList(index, apertures);
+
+            if(sAMObject is AdjacencyCluster)
+            {
+                AdjacencyCluster adjacencyCluster = new AdjacencyCluster((AdjacencyCluster)sAMObject);
+                apertures = adjacencyCluster.SetDefaultApertureConstructionLayers(apertures?.ConvertAll(x => x.Guid))?.ToList();
+                sAMObject = adjacencyCluster;
+            }
+            else if(sAMObject is AnalyticalModel)
+            {
+                AnalyticalModel analyticalModel = new AnalyticalModel((AnalyticalModel)sAMObject);
+                AdjacencyCluster adjacencyCluster = analyticalModel.AdjacencyCluster;
+                if(adjacencyCluster != null)
+                {
+                    adjacencyCluster = new AdjacencyCluster(adjacencyCluster);
+                    apertures = adjacencyCluster.SetDefaultApertureConstructionLayers(apertures?.ConvertAll(x => x.Guid))?.ToList();
+                    analyticalModel = new AnalyticalModel(analyticalModel, adjacencyCluster);
+                }
+
+                sAMObject = analyticalModel;
+            }
+
+            index = Params.IndexOfOutputParam("analytical");
+            if (index != -1)
+                dataAccess.SetData(index, sAMObject);
+
+            index = Params.IndexOfOutputParam("apertures");
+            if (index != -1)
+                dataAccess.SetDataList(index, apertures?.ConvertAll(x => new GooAperture(x)));
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/SetDefaultApertureConstructionLayers.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static IEnumerable<Aperture> SetDefaultApertureConstructionLayers(this AdjacencyCluster adjacencyCluster, IEnumerable<Guid> guids = null)
+        {
+            List<Panel> panels = adjacencyCluster?.GetPanels();
+            if(panels == null || panels.Count == 0)
+            {
+                return null;
+            }
+
+            List<Tuple<ApertureConstruction, List<Tuple<Aperture, Guid, PanelType>>>> tuples = new List<Tuple<ApertureConstruction, List<Tuple<Aperture, Guid, PanelType>>>>();
+
+            foreach(Panel panel in panels)
+            {
+                List<Aperture> apertures_Panel = panel?.Apertures;
+                if(apertures_Panel == null || apertures_Panel.Count == 0)
+                {
+                    continue;
+                }
+
+                PanelType panelType = panel.PanelType;
+
+                List<Aperture> apertures = new List<Aperture>();
+                foreach(Aperture aperture_Panel in apertures_Panel)
+                {
+                    if(aperture_Panel == null)
+                    {
+                        continue;
+                    }
+
+                    if (guids != null && guids.Count() != 0 && !guids.Contains(aperture_Panel.Guid))
+                    {
+                        continue;
+                    }
+
+                    ApertureConstruction apertureConstruction_Default = Query.DefaultApertureConstruction(panel, aperture_Panel.ApertureType);
+                    if(apertureConstruction_Default == null)
+                    {
+                        continue;
+                    }
+
+                    if(aperture_Panel.Name != null && apertureConstruction_Default.Name == aperture_Panel.Name)
+                    {
+                        continue;
+                    }
+
+                    apertures.Add(aperture_Panel);
+                }
+
+                if(apertures == null || apertures.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach(Aperture aperture in apertures)
+                {
+                    ApertureConstruction apertureConstruction = aperture.ApertureConstruction;
+                    int index = tuples.FindIndex(x => apertureConstruction == null ? x == null : apertureConstruction.Guid == x.Item1?.Guid);
+
+                    Tuple<ApertureConstruction, List<Tuple<Aperture, Guid, PanelType>>> tuple = null;
+
+                    if (index != -1) 
+                    { 
+                        tuple = tuples[index];
+                    }
+                    else
+                    {
+                        tuples.Add(new Tuple<ApertureConstruction, List<Tuple<Aperture, Guid, PanelType>>>(apertureConstruction, new List<Tuple<Aperture, Guid, PanelType>>()));
+                        tuple = tuples.Last();
+                    }
+
+                    tuple.Item2.Add(new Tuple<Aperture, Guid, PanelType>(aperture, panel.Guid, panel.PanelType));
+                }
+            }
+
+            List<Aperture> result = new List<Aperture>();
+            foreach(Tuple<ApertureConstruction, List<Tuple<Aperture, Guid, PanelType>>> tuple in tuples)
+            {
+                ApertureConstruction apertureConstruction = tuple.Item1;
+
+                foreach(ApertureType apertureType in Enum.GetValues(typeof(ApertureType)))
+                {
+                    List<Tuple<Aperture, Guid, PanelType>> tuples_ApertureType = tuple.Item2.FindAll(x => x.Item1.ApertureType == apertureType);
+                    if(tuples_ApertureType == null || tuples_ApertureType.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    foreach(PanelType panelType in Enum.GetValues(typeof(PanelType)))
+                    {
+                        List<Tuple<Aperture, Guid, PanelType>> tuples_PanelType = tuple.Item2.FindAll(x => x.Item3 == panelType);
+                        if (tuples_PanelType == null || tuples_PanelType.Count == 0)
+                        {
+                            continue;
+                        }
+
+                        ApertureConstruction apertureConstruction_Default = Query.DefaultApertureConstruction(panelType, apertureType);
+                        if (apertureConstruction_Default == null)
+                        {
+                            continue;
+                        }
+
+                        ApertureConstruction apertureConstruction_New = new ApertureConstruction(Guid.NewGuid(), apertureConstruction, apertureConstruction?.Name);
+                        apertureConstruction_New = new ApertureConstruction(apertureConstruction_New, apertureConstruction_Default.PaneConstructionLayers, apertureConstruction_Default.FrameConstructionLayers);
+
+                        foreach (Tuple<Aperture, Guid, PanelType> tuple_PanelType in tuples_PanelType)
+                        {
+                            Aperture aperture = tuple_PanelType.Item1;
+                            Panel panel = adjacencyCluster.GetObject<Panel>(tuple_PanelType.Item2);
+
+                            panel.RemoveAperture(aperture.Guid);
+
+                            aperture = new Aperture(aperture, apertureConstruction_New);
+
+                            panel.AddAperture(aperture);
+
+                            adjacencyCluster.AddObject(panel);
+
+                            result.Add(aperture);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
