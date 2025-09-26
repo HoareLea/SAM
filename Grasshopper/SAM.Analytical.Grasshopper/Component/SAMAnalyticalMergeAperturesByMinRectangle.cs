633d5f3fed0027b697fd4d653781ced132ea0bee
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-09-26 17:37:08 +0200

    SAMAnalyticalMergeAperturesByMinRectangle Grasshopper component implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalMergeAperturesByMinRectangle.cs
@@ -0,0 +1,200 @@
+﻿using Grasshopper.Kernel;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalMergeAperturesByMinRectangle : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("f73dd5e2-56ad-4532-8046-f62a2d57f2ce");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+                protected override System.Drawing.Bitmap Icon => Core.Convert.ToBitmap(Resources.SAM_Small);
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalMergeAperturesByMinRectangle()
+          : base(
+              "SAMAnalytical.MergeAperturesByMinRectangle",
+              "SAMAnalytical.MergeAperturesByMinRectangle",
+              "For each panel..",
+              "SAM",
+              "Analytical02")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analyticalObject", NickName = "_analyticalObject", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooApertureParam() { Name = "_apertures_", NickName = "_apertures_", Description = "SAM Analytical Apertures", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number paramNumber;
+
+                paramNumber = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "_distance", NickName = "_distance", Description = "Distance", Access = GH_ParamAccess.item};
+                result.Add(new GH_SAMParam(paramNumber, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Boolean paramBoolean;
+
+                paramBoolean = new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "_removeOverlap_", NickName = "_removeOverlap_", Description = "Remove Overlap apertures", Access = GH_ParamAccess.item, Optional = true };
+                paramBoolean.SetPersistentData(true);
+                result.Add(new GH_SAMParam(paramBoolean, ParamVisibility.Voluntary));
+
+                paramBoolean = new global::Grasshopper.Kernel.Parameters.Param_Boolean() { Name = "run_", NickName = "run_", Description = "Run", Access = GH_ParamAccess.item, Optional = true };
+                paramBoolean.SetPersistentData(false);
+                result.Add(new GH_SAMParam(paramBoolean, ParamVisibility.Binding));
+
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam { Name = "analyticalObject", NickName = "analyticalObject", Description = "SAM Analytical", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooApertureParam() { Name = "apertures", NickName = "apertures", Description = "SAM Analytical Apertures", Access = GH_ParamAccess.list }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index = -1;
+
+            index = Params.IndexOfInputParam("run_");
+            bool run = false;
+            if (!dataAccess.GetData(index, ref run))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if (!run)
+            {
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_distance");
+            double distance = double.NaN;
+            if (!dataAccess.GetData(index, ref distance) || double.IsNaN(distance))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("_removeOverlap_");
+            bool removeOverlap = true;
+            if (index != -1 && !dataAccess.GetData(index, ref removeOverlap))
+            {
+                removeOverlap = true;
+            }
+
+            index = Params.IndexOfInputParam("_analyticalObject");
+            SAMObject sAMObject = null;
+            if (index == -1 || !dataAccess.GetData(index, ref sAMObject) || sAMObject == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster = null;
+            if(sAMObject is AdjacencyCluster)
+            {
+                adjacencyCluster = (AdjacencyCluster)sAMObject;
+            }
+            else if(sAMObject is AnalyticalModel)
+            {
+                adjacencyCluster = ((AnalyticalModel)sAMObject).AdjacencyCluster;
+            }
+
+            adjacencyCluster = new AdjacencyCluster(adjacencyCluster, true);
+
+            index = Params.IndexOfInputParam("_apertures_");
+            List<Aperture> apertures = [];
+            if (index != -1)
+            {
+                dataAccess.GetDataList(index, apertures);
+            }
+
+            if (apertures == null || apertures.Count == 0)
+            {
+                apertures = null;
+            }
+
+            apertures?.RemoveAll(x => x == null);
+
+            List<Aperture> apertures_Result = [];
+
+            List<Panel> panels = adjacencyCluster.GetPanels();
+            if(panels != null && panels.Count != 0)
+            {
+                foreach (Panel panel in panels)
+                {
+                    Panel panel_Temp = Create.Panel(panel); 
+
+                    List<Aperture> apertures_Panel = panel_Temp.MergeApertures(distance, out List<Aperture> removedApertures_Temp, apertures?.ConvertAll(x => x.Guid), removeOverlap);
+                    if(apertures_Panel == null || apertures_Panel.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    adjacencyCluster.AddObject(panel);
+                    apertures_Result.AddRange(apertures_Panel);
+                }
+            }
+
+            if (sAMObject is AdjacencyCluster)
+            {
+                sAMObject = adjacencyCluster;
+            }
+            else if (sAMObject is AnalyticalModel)
+            {
+                sAMObject = new AnalyticalModel((AnalyticalModel)sAMObject, adjacencyCluster);
+            }
+
+            index = Params.IndexOfOutputParam("analyticalObject");
+            if (index != -1)
+            {
+                dataAccess.SetData(index, sAMObject);
+            }
+
+            index = Params.IndexOfOutputParam("apertures");
+            if (index != -1)
+            {
+                dataAccess.SetDataList(index, apertures_Result.ConvertAll(x => new GooAperture(x)));
+            }
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Modify/MergeApertures.cs
@@ -0,0 +1,246 @@
+﻿using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAM.Analytical
+{
+    public static partial class Modify
+    {
+        public static List<Aperture> MergeApertures(this Panel panel, double distance, out List<Aperture> removedApertures, IEnumerable<Guid> apertureGuids = null, bool removeOverlap = true)
+        {
+            removedApertures = null;
+
+            if (panel is null || double.IsNaN(distance))
+            {
+                return null;
+            }
+
+            List<Aperture> apertures = panel.Apertures;
+            if(apertureGuids != null && apertureGuids.Any())
+            {
+                for (int i = apertures.Count - 1; i >= 0; i--)
+                {
+                    if (!apertureGuids.Contains(apertures[i].Guid))
+                    {
+                        apertures.RemoveAt(i);
+                    }
+                }
+            }
+
+            if(apertures is null || apertures.Count == 0)
+            {
+                return null;
+            }
+
+            if(panel.Plane is not Plane plane)
+            {
+                return null;
+            }
+
+            Vector2D direction = plane.Convert(Vector3D.WorldZ);
+            if (direction is null || direction.Length < Core.Tolerance.MacroDistance)
+            {
+                direction = plane.Convert(Vector3D.WorldY);
+            }
+
+            if (direction is null || direction.Length < Core.Tolerance.MacroDistance)
+            {
+                return null;
+            }
+
+            List<List<Tuple<Aperture, Polygon2D>>> tuples_Group = [];
+            foreach(Aperture aperture in apertures)
+            {
+                if ((plane.Convert(aperture?.GetExternalEdge3D()) as ISegmentable2D)?.GetPoints() is not List<Point2D> point2Ds || point2Ds.Count < 3)
+                {
+                    continue;
+                }
+
+                tuples_Group.Add([new Tuple<Aperture, Polygon2D>(aperture, new Polygon2D(point2Ds))]);
+            }
+
+            List<Tuple<Guid, Guid, double>> tuples_Distance = [];
+
+            bool updated = true;
+
+            while(updated)
+            {
+                updated = false;
+
+                for (int i = 0; i < tuples_Group.Count; i++)
+                {
+                    List<Tuple<Aperture, Polygon2D>> tuples_Group_1 = tuples_Group[i];
+
+                    for (int j = tuples_Group.Count - 1; j > i; j--)
+                    {
+                        List<Tuple<Aperture, Polygon2D>> tuples_Group_2 = tuples_Group[j];
+
+                        bool match = false;
+                        for (int x = 0; x < tuples_Group_1.Count; x++)
+                        {
+                            Aperture aperture_1 = tuples_Group_1[x].Item1;
+                            Polygon2D polygon2D_1 = tuples_Group_1[x].Item2;
+
+                            Guid guid_1 = aperture_1.Guid;
+
+                            for (int y = 0; y < tuples_Group_2.Count; y++)
+                            {
+                                Aperture aperture_2 = tuples_Group_2[y].Item1;
+                                Polygon2D polygon2D_2 = tuples_Group_2[y].Item2;
+
+                                Guid guid_2 = aperture_2.Guid;
+
+                                int index = tuples_Distance.FindIndex(x => (x.Item1 == guid_1 && x.Item2 == guid_2) || (x.Item1 == guid_2 && x.Item2 == guid_1));
+                                if(index == -1)
+                                {
+                                    index = tuples_Distance.Count;
+                                    tuples_Distance.Add(new Tuple<Guid, Guid, double>(guid_1, guid_2, polygon2D_1.Distance(polygon2D_2)));
+                                    
+                                }
+
+                                if (tuples_Distance[index].Item3 > distance)
+                                {
+                                    continue;
+                                }
+
+                                match = true;
+                                break;
+
+                            }
+
+                            if(match)
+                            {
+                                break;
+                            }
+                        }
+
+                        if (!match)
+                        {
+                            continue;
+                        }
+
+                        updated = match;
+
+                        tuples_Group_1.AddRange(tuples_Group_2);
+                        tuples_Group.Remove(tuples_Group_2);
+                    }
+
+                    if(updated)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            tuples_Group.RemoveAll(x => x.Count <= 1);
+
+            if (tuples_Group.Count == 0)
+            {
+                return null;
+            }
+
+            List<Aperture> result = [];
+
+            removedApertures = [];
+
+            foreach (List<Tuple<Aperture, Polygon2D>> tuples in tuples_Group)
+            {
+                tuples.Sort((x, y) => y.Item2.GetArea().CompareTo(x.Item2.GetArea()));
+
+                List<Face3D> face3Ds = tuples.ConvertAll(x => x.Item1.GetFace3D());
+
+                double area_External = 0;
+                double area_Internal = 0;
+
+                foreach(Face3D face3D in face3Ds)
+                {
+                    if(face3D.GetExternalEdge3D() is not IClosedPlanar3D externalEdge)
+                    {
+                        continue;
+                    }
+
+                    double area = double.NaN;
+
+                    area = externalEdge.GetArea();
+                    if(!double.IsNaN(area))
+                    {
+                        area_External += area;
+                    }
+
+                    if(face3D.GetInternalEdge3Ds() is not List<IClosedPlanar3D> internalEdges || internalEdges.Count == 0)
+                    {
+                        if (!double.IsNaN(area))
+                        {
+                            area_Internal += area;
+                        }
+
+                        continue;
+                    }
+
+                    foreach(IClosedPlanar3D internalEdge in internalEdges)
+                    {
+                        area = internalEdge.GetArea();
+                        if (!double.IsNaN(area))
+                        {
+                            area_Internal += area;
+                        }
+                    }
+                }
+
+                if(double.IsNaN(area_External) || Core.Query.AlmostEqual(area_External, Core.Tolerance.MacroDistance))
+                {
+                    continue;
+                }
+
+                Aperture aperture = tuples[0].Item1;
+
+                Rectangle2D rectangle2D = Geometry.Planar.Create.Rectangle2D(tuples.ConvertAll(x => x.Item2));
+
+                Rectangle2D rectangle2D_External = rectangle2D;
+
+                if (direction.SmallestAngle(rectangle2D.HeightDirection) < direction.SmallestAngle(rectangle2D.WidthDirection))
+                {
+                    double width = area_External / rectangle2D.Height;
+                    double difference = rectangle2D.Width - width;
+
+                    rectangle2D_External = new Rectangle2D(rectangle2D.Origin, width, rectangle2D.Height, rectangle2D.HeightDirection);
+                    rectangle2D_External.Move(rectangle2D.WidthDirection * difference / 2);
+                }
+                else
+                {
+                    double height = area_External / rectangle2D.Width;
+                    double difference = rectangle2D.Height - height;
+
+                    rectangle2D_External = new Rectangle2D(rectangle2D.Origin, rectangle2D.Width, area_External / rectangle2D.Height, rectangle2D.HeightDirection);
+                    rectangle2D_External.Move(rectangle2D.HeightDirection * difference / 2);
+                }
+
+                if(rectangle2D_External.GetArea() < Core.Tolerance.MacroDistance)
+                {
+                    continue;
+                }
+
+                foreach(Aperture aperture_Temp in tuples.ConvertAll(x => x.Item1))
+                {
+                    if(!panel.RemoveAperture(aperture_Temp.Guid))
+                    {
+                        continue;
+                    }
+
+                    removedApertures.Add(aperture_Temp);
+                }
+
+                Polygon3D polygon3D = plane.Convert(new Polygon2D(rectangle2D_External.GetPoints()));
+
+                Aperture aperture_New = new Aperture(Guid.NewGuid(), aperture, new Face3D(polygon3D));
+                panel.AddAperture(aperture_New);
+                result.Add(aperture_New);
+
+            }
+
+            return result;
+        }
+    }
+}
