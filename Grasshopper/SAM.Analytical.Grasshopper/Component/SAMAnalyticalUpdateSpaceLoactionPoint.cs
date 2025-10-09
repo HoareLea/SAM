4faabab31d8d7a6762c560e6d0d0541afe5f2854
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-10-09 14:51:31 +0200

    SAMAnalyticalUpdateSpaceLoactionPoint initial implementation

---
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalUpdateSpaceLoactionPoint.cs
@@ -0,0 +1,228 @@
+﻿using Grasshopper.Kernel;
+using NetTopologySuite.Geometries;
+using SAM.Analytical.Classes;
+using SAM.Analytical.Grasshopper.Properties;
+using SAM.Core.Grasshopper;
+using SAM.Geometry;
+using SAM.Geometry.Planar;
+using SAM.Geometry.Spatial;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper
+{
+    public class SAMAnalyticalUpdateSpaceLoactionPoint : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("14da369b-312a-448c-b69a-3ea69ae7563c");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+                protected override System.Drawing.Bitmap Icon => Core.Convert.ToBitmap(Resources.SAM_Small);
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = [];
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() { Name = "_analyticalObject", NickName = "_analyticalObject", Description = "SAM Analytical Object such as AdjacencyCluster or AnalyticalModel", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooSpaceParam() { Name = "spaces_", NickName = "spaces_", Description = "SAM Analytical Spaces, if nothing connected all spaces from AnalyticalModel will be used", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
+
+                global::Grasshopper.Kernel.Parameters.Param_Number param_Number;
+
+                param_Number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "offset_", NickName = "offset_", Description = "Cutting plane offset from the bottom of space", Access = GH_ParamAccess.item, Optional = true };
+                param_Number.SetPersistentData(0.1);
+                result.Add(new GH_SAMParam(param_Number, ParamVisibility.Voluntary));
+
+                param_Number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "aspect_", NickName = "aspect_", Description = "Aspect", Access = GH_ParamAccess.item, Optional = true };
+                param_Number.SetPersistentData(2.0);
+                result.Add(new GH_SAMParam(param_Number, ParamVisibility.Voluntary));
+
+                param_Number = new global::Grasshopper.Kernel.Parameters.Param_Number() { Name = "clerance_", NickName = "clerance_", Description = "Clerance", Access = GH_ParamAccess.item, Optional = true };
+                param_Number.SetPersistentData(0.15);
+                result.Add(new GH_SAMParam(param_Number, ParamVisibility.Voluntary));
+
+
+                return [.. result];
+            }
+        }
+
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = [];
+                result.Add(new GH_SAMParam(new GooAnalyticalObjectParam() {Name = "AnalyticalObject", NickName = "AnalyticalObject", Description = "SAM Analytical Object", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new GooSpaceParam() { Name = "spaces_", NickName = "spaces_", Description = "SAM Analytical Spaces, if nothing connected all spaces from AnalyticalModel will be used", Access = GH_ParamAccess.list, Optional = true }, ParamVisibility.Binding));
+
+                return [.. result];
+            }
+        }
+
+        /// <summary>
+        /// Updates PanelTypes for AdjacencyCluster
+        /// </summary>
+        public SAMAnalyticalUpdateSpaceLoactionPoint()
+          : base("SAMAnalytical.UpdateSpaceLoactionPoint", "SAMAnalytical.UpdateSpaceLoactionPoint",
+              "Updates Space Loaction Point",
+              "SAM", "SAM_IC")
+        {
+        }
+
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            int index;
+
+            index = Params.IndexOfInputParam("_analyticalObject");
+            IAnalyticalObject? analyticalObject = null;
+            if(index == -1 || !dataAccess.GetData(index, ref analyticalObject))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            AdjacencyCluster adjacencyCluster = null;
+            if(analyticalObject is AdjacencyCluster adjacencyCluster_Temp)
+            {
+                adjacencyCluster = new AdjacencyCluster(adjacencyCluster_Temp, true);
+            }
+            else if(analyticalObject is AnalyticalModel analyticalModel)
+            {
+                adjacencyCluster = new AdjacencyCluster(analyticalModel.AdjacencyCluster, true);
+            }
+
+            if(adjacencyCluster is null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            index = Params.IndexOfInputParam("spaces_");
+            List<Space> spaces = [];
+            if (index != -1)
+            {
+                dataAccess.GetDataList(index, spaces);
+            }
+
+            index = Params.IndexOfInputParam("offset_");
+            double offset = 0;
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref offset);
+            }
+
+            index = Params.IndexOfInputParam("aspect_");
+            double aspect = 0;
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref aspect);
+            }
+
+            index = Params.IndexOfInputParam("clerance_");
+            double clerance = 0;
+            if (index != -1)
+            {
+                dataAccess.GetData(index, ref clerance);
+            }
+
+            if (spaces == null || spaces.Count == 0)
+            {
+                spaces = adjacencyCluster.GetSpaces();
+            }
+
+            if(spaces != null && spaces.Count != 0)
+            {
+                foreach (Space space in spaces)
+                {
+                    if(adjacencyCluster.GetObject<Space>(space.Guid) is not Space space_Temp)
+                    {
+                        continue;
+                    }
+
+                    Shell shell = adjacencyCluster.Shell(space);
+                    if (shell is null)
+                    {
+                        continue;
+                    }
+
+                    Plane plane = Geometry.Spatial.Create.Plane(shell.GetBoundingBox().Min.Z + offset);
+
+                    Dictionary<Face3D, List<ISegmentable2D>> dictionary = Geometry.Spatial.Query.SectionDictionary<ISegmentable2D>(shell.Face3Ds, plane);
+
+                    List<Segment2D> segment2Ds = [];
+                    foreach (KeyValuePair<Face3D, List<ISegmentable2D>> keyValuePair in dictionary)
+                    {
+                        foreach (ISegmentable2D segmentable2D in keyValuePair.Value)
+                        {
+                            segment2Ds.AddRange(segmentable2D.GetSegments());
+                        }
+                    }
+
+                    if(segment2Ds is null || segment2Ds.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    segment2Ds = Geometry.Planar.Query.Split(segment2Ds);
+                    segment2Ds = Geometry.Planar.Query.Snap(segment2Ds, true);
+
+                    List<Polygon2D> polygon2Ds = Geometry.Planar.Create.Polygon2Ds(segment2Ds);
+                    if (polygon2Ds == null || polygon2Ds.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    List<Face2D> face2Ds = Geometry.Planar.Create.Face2Ds(polygon2Ds, EdgeOrientationMethod.Opposite);
+                    if (face2Ds == null || face2Ds.Count == 0)
+                    {
+                        continue;
+                    }
+
+                    foreach(Face2D face2D in face2Ds)
+                    {
+                        (Coordinate center, double width, double height) coordinate = PolygonLabelCoordinate.LargestAxisAlignedRectangle(face2D.ToNTS(), aspect, clerance);
+
+
+                        Point2D point2D = coordinate.center.ToSAM();
+
+                        if(plane.Convert(point2D) is not Point3D point3D)
+                        {
+                            continue;
+                        }
+
+                        Point3D location = new Point3D(point3D.X, point3D.Y, space.Location.Z);
+                        space_Temp = new Space(space_Temp, space_Temp.Name, location);
+                        adjacencyCluster.AddObject(space_Temp);
+                        break;
+                    }
+
+                }
+            }
+
+            if (analyticalObject is AdjacencyCluster)
+            {
+                analyticalObject = adjacencyCluster;
+            }
+            else if (analyticalObject is AnalyticalModel)
+            {
+                analyticalObject = new AnalyticalModel((AnalyticalModel)analyticalObject, adjacencyCluster);
+            }
+
+            index = Params.IndexOfOutputParam("AnalyticalObject");
+            if(index != -1)
+            {
+                dataAccess.SetData(index, new GooAnalyticalObject(analyticalObject));
+            }
+        }
+    }
+}
---
+++ SAM/SAM.Analytical/Classes/RoomLabelCoordinate.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.Geometries.Prepared;
+
+namespace SAM.Analytical.Classes
+{
+    public static class PolygonLabelCoordinate
+    {
+        /// <summary>
+        /// Finds the center of the largest axis-aligned rectangle of fixed aspect ratio inside a room polygon.
+        /// Returns the rectangle center and its width/height. Units follow the polygon (e.g., meters).
+        /// </summary>
+        /// <param name="roomIn">Room polygon (may have holes). Must be valid and simple.</param>
+        /// <param name="aspect">Width / Height ratio. For H:W = 1:2 use aspect = 2.0.</param>
+        /// <param name="clearance">Optional clearance to walls (euclidean). E.g., 0.15 = 150 mm.</param>
+        /// <param name="gridFrac">Coarse grid step as fraction of min(room width,height). 0.05 is usually fine.</param>
+        /// <param name="tolerance">Target geometric tolerance for sizes/steps.</param>
+        /// <param name="maxIters">Max hill-climb iterations.</param>
+        public static (Coordinate center, double width, double height) LargestAxisAlignedRectangle(
+            Polygon roomIn,
+            double aspect = 2.0,
+            double clearance = 0.0,
+            double gridFrac = 0.05,
+            double tolerance = 0.005,
+            int maxIters = 120)
+        {
+            if (roomIn == null) throw new ArgumentNullException(nameof(roomIn));
+            if (aspect <= 0) throw new ArgumentOutOfRangeException(nameof(aspect));
+            if (gridFrac <= 0 || gridFrac > 0.5) throw new ArgumentOutOfRangeException(nameof(gridFrac));
+            if (tolerance <= 0) throw new ArgumentOutOfRangeException(nameof(tolerance));
+
+            // Apply clearance (shrink). If the shrink empties the room, fall back to original.
+            NetTopologySuite.Geometries.Geometry effective = clearance > 0 ? roomIn.Buffer(-clearance) : roomIn;
+            if (effective.IsEmpty) effective = roomIn;
+
+            // Work with the largest polygon piece if buffering split the space.
+            var room = LargestPolygon(effective) ?? roomIn;
+            var prep = PreparedGeometryFactory.Prepare(room);
+
+            var env = room.EnvelopeInternal;
+            double minSpan = System.Math.Min(env.Width, env.Height);
+            double step = System.Math.Max(tolerance * 10.0, gridFrac * minSpan);
+
+            // Seed from an interior point (fast and safe).
+            Coordinate bestC = room.InteriorPoint.Coordinate;
+            double bestS = MaxHalfWidthAt(prep, bestC, aspect, tolerance, step);
+
+            // Coarse grid search to avoid local traps (handles L/S shapes).
+            for (double x = env.MinX + step * 0.5; x <= env.MaxX - step * 0.5; x += step)
+            {
+                for (double y = env.MinY + step * 0.5; y <= env.MaxY - step * 0.5; y += step)
+                {
+                    var c = new Coordinate(x, y);
+                    if (!prep.Covers(new Point(c))) continue;
+                    double s = MaxHalfWidthAt(prep, c, aspect, tolerance, step);
+                    if (s > bestS)
+                    {
+                        bestS = s;
+                        bestC = c;
+                    }
+                }
+            }
+
+            // Hill climb / local refinement
+            double delta = step;
+            int iter = 0;
+            while (delta > tolerance && iter++ < maxIters)
+            {
+                bool improved = false;
+
+                foreach (var dir in Directions8)
+                {
+                    var c2 = new Coordinate(bestC.X + delta * dir.dx, bestC.Y + delta * dir.dy);
+                    if (!prep.Covers(new Point(c2))) continue;
+
+                    double s2 = MaxHalfWidthAt(prep, c2, aspect, tolerance, step);
+                    if (s2 > bestS + 1e-9)
+                    {
+                        bestS = s2;
+                        bestC = c2;
+                        improved = true;
+                        break; // greedy accept; restart neighborhood
+                    }
+                }
+
+                if (!improved) delta *= 0.5;
+            }
+
+            // Convert half-width to rectangle dims (axis-aligned)
+            double halfW = bestS;
+            double halfH = bestS / aspect;
+            double width = 2.0 * halfW;
+            double height = 2.0 * halfH;
+
+            return (bestC, width, height);
+        }
+
+        // --- Helpers ---
+
+        private static (double dx, double dy)[] Directions8 = new (double, double)[]
+        {
+        (1,0), (-1,0), (0,1), (0,-1),
+        ( 0.70710678,  0.70710678), ( 0.70710678, -0.70710678),
+        (-0.70710678,  0.70710678), (-0.70710678, -0.70710678)
+        };
+
+        /// <summary>
+        /// For a given center, finds the maximum half-width (along X) such that a rectangle
+        /// of aspect = width/height fits entirely inside the prepared polygon. Binary search.
+        /// </summary>
+        private static double MaxHalfWidthAt(IPreparedGeometry prep, Coordinate center, double aspect, double tol, double seed)
+        {
+            // Grow-to-fail to get an upper bound
+            double low = 0.0;
+            double high = System.Math.Max(seed, tol);
+            for (int i = 0; i < 32; i++)
+            {
+                if (Fits(prep, center, high, aspect)) { low = high; high *= 2.0; }
+                else break;
+            }
+
+            // If even tiny rectangle doesn't fit, return 0
+            if (low == 0.0 && !Fits(prep, center, tol, aspect)) return 0.0;
+
+            // Binary search to tolerance
+            for (int i = 0; i < 40; i++)
+            {
+                double mid = 0.5 * (low + high);
+                if (high - low <= tol) break;
+                if (Fits(prep, center, mid, aspect)) low = mid; else high = mid;
+            }
+            return low;
+        }
+
+        /// <summary>
+        /// Checks if an axis-aligned rectangle centered at 'c' with half-width 'halfW'
+        /// and aspect = width/height fits inside the prepared polygon.
+        /// </summary>
+        private static bool Fits(IPreparedGeometry prep, Coordinate c, double halfW, double aspect)
+        {
+            if (halfW <= 0) return false;
+            double halfH = halfW / aspect;
+
+            var ring = new LinearRing(new[]
+            {
+            new Coordinate(c.X - halfW, c.Y - halfH),
+            new Coordinate(c.X + halfW, c.Y - halfH),
+            new Coordinate(c.X + halfW, c.Y + halfH),
+            new Coordinate(c.X - halfW, c.Y + halfH),
+            new Coordinate(c.X - halfW, c.Y - halfH)
+        });
+
+            var rect = new Polygon(ring);
+
+            // Covers allows touching boundary; if you want strict inside, use Contains.
+            return prep.Covers(rect);
+        }
+
+        private static Polygon LargestPolygon(NetTopologySuite.Geometries.Geometry g)
+        {
+            if (g is Polygon p) return p;
+
+            if (g is MultiPolygon mp)
+            {
+                double bestA = double.NegativeInfinity;
+                Polygon best = null;
+                for (int i = 0; i < mp.NumGeometries; i++)
+                {
+                    var pi = (Polygon)mp.GetGeometryN(i);
+                    double a = pi.Area;
+                    if (a > bestA) { bestA = a; best = pi; }
+                }
+                return best;
+            }
+            // If it's something else (e.g., empty), give up.
+            return null;
+        }
+    }
+}
