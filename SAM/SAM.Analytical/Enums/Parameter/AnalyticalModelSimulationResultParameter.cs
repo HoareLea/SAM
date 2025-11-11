522109376de709d873cff7310ee9b4d0d9de1d8e
Michal Dengusiak <tassimulation@gmail.com>
2025-11-11 15:58:08 +0100

    Update AnalyticalModelSimulationResultParameter.cs

--- SAM/SAM.Analytical/Enums/Parameter/AnalyticalModelSimulationResultParameter.cs
+++ SAM/SAM.Analytical/Enums/Parameter/AnalyticalModelSimulationResultParameter.cs
@@ -4,19 +4,15 @@ using SAM.Core.Attributes;
 
 namespace SAM.Analytical
 {
-    [AssociatedTypes(typeof(AnalyticalModel)), Description("AnalyticalModel Simulation Result Parameter")]
+    [AssociatedTypes(typeof(AnalyticalModelSimulationResult)), Description("AnalyticalModel Simulation Result Parameter")]
     public enum AnalyticalModelSimulationResultParameter
     {
         [ParameterProperties("Consumption Heating", "Consumption Heating [kWh]"), ParameterValue(ParameterType.Double)] ConsumptionHeating,
         [ParameterProperties("Peak Heating Load", "Peak Heating Load [kW]"), ParameterValue(ParameterType.Double)] PeakHeatingLoad,
         [ParameterProperties("Peak Heating Hour", "Peak Heating Hour [hr]"), ParameterValue(ParameterType.Integer)] PeakHeatingHour,
-        [ParameterProperties("Unmet Hours Heating", "Unmet Hours Heating"), ParameterValue(ParameterType.Integer)] UnmetHeatingHours,
 
         [ParameterProperties("Consumption Cooling", "Consumption Cooling [kWh]"), ParameterValue(ParameterType.Double)] ConsumptionCooling,
         [ParameterProperties("Peak Cooling Load", "Peak Cooling Load [kW]"), ParameterValue(ParameterType.Double)] PeakCoolingLoad,
         [ParameterProperties("Peak Cooling Hour", "Peak Cooling Hour [hr]"), ParameterValue(ParameterType.Integer)] PeakCoolingHour,
-        [ParameterProperties("Unmet Hours Cooling", "Unmet Hours Cooling"), ParameterValue(ParameterType.Integer)] UnmetCoolingHours,
-
-        [ParameterProperties("Unmet Hours", "Unmet Hours"), ParameterValue(ParameterType.Integer)] UnmetHours,
     }
 }
