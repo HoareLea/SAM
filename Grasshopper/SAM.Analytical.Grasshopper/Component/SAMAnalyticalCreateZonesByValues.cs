513438cc1ff2527548e8c465ff504badbb08aca1
Michal Dengusiak <tassimulation@gmail.com>
2026-04-08 09:44:35 +0200

    Update SAMAnalyticalCreateZonesByValues.cs

--- Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalCreateZonesByValues.cs
+++ Grasshopper/SAM.Analytical.Grasshopper/Component/SAMAnalyticalCreateZonesByValues.cs
@@ -22,7 +22,7 @@ namespace SAM.Analytical.Grasshopper
         /// <summary>
         /// The latest version of this component.
         /// </summary>
-        public override string LatestComponentVersion => "1.0.1";
+        public override string LatestComponentVersion => "1.0.2";
 
         /// <summary>
         /// Provides an icon for the component.
